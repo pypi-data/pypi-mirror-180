58ff0a372609371baba7bf7632272ffd4b801101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:10 +0000

    Add package deepdriver/0.0.46/deepdriver-0.0.46.tar.gz

---
+++ packages/deepdriver/deepdriver-0.0.46.tar.gz/deepdriver-0.0.46/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deepdriver",
+    version="0.0.46",
+    author="bokchi",
+    author_email="molamola.bokchi@gmail.com",
+    description="deepdriver experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bokchi.com",
+    project_urls={
+        "bokchi git hub": "https://github.com/molabokchi",
+    },
+    install_requires=[
+        "wheel",
+        "assertpy",
+        "grpcio",
+        "grpcio-tools",
+        "numpy",
+        "pandas",
+        "Pillow",
+        "plotly",
+        "psutil",
+        "pynvml",
+        "requests",
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"":"."},
+    packages=["deepdriver","deepdriver.sdk","deepdriver.sdk.chart","deepdriver.sdk.data_types","deepdriver.sdk.interface",""],
+    python_requires=">=3.6"
+)
