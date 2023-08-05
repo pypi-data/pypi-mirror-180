c96c92e881e8cdf3245718b73a7018d03a76a62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:31 +0000

    Add package random-int/0.0.3/random_int-0.0.3.tar.gz

---
+++ packages/random-int/random_int-0.0.3.tar.gz/random_int-0.0.3/README.md
@@ -0,0 +1,8 @@
+# fakername
+Get a random number
+
+# 安装
+pip install random_int
+
+# 准备工作
+随机获取一个数字
---
+++ packages/random-int/random_int-0.0.3.tar.gz/random_int-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="random_int",
+    version="0.0.3",
+    author="Rtt.01_-v",
+    author_email="2835836810@qq.com",
+    description="Get a random number",
+
+    # 项目主页
+    # url="https://github.com/sunjian-github/ezface",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages(),
+)
+
