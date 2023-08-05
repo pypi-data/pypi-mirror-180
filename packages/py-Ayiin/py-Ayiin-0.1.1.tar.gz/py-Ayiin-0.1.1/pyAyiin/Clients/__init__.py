43e356e1c27f73af71902eebcd1b49657e60cda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:42 +0000

    Add package py-Ayiin/0.1.1/py-Ayiin-0.1.1.tar.gz

---
+++ packages/py-Ayiin/py-Ayiin-0.1.1.tar.gz/py-Ayiin-0.1.1/pyAyiin/Clients/__init__.py
@@ -0,0 +1,17 @@
+# py - Ayiin
+# Copyright (C) 2022-2023 @AyiinXd
+#
+# This file is a part of < https://github.com/AyiinXd/pyAyiin >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/AyiinXd/pyAyiin/blob/main/LICENSE/>.
+#
+# FROM py-Ayiin <https://github.com/AyiinXd/pyAyiin>
+# t.me/AyiinChat & t.me/AyiinSupport
+
+
+# ========================×========================
+#            Jangan Hapus Credit Ngentod
+# ========================×========================
+
+from .client import *
+from .startup import StartPyrogram
