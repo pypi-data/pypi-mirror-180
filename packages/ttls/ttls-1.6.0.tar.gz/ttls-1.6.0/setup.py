dd62210793e06bf6c3dfccfb5af29b9a5984ad66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:25 +0000

    Add package ttls/1.6.0/ttls-1.6.0.tar.gz

---
+++ packages/ttls/ttls-1.6.0.tar.gz/ttls-1.6.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ttls']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0', 'colour>=0.1.5,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['ttls = ttls.cli:main']}
+
+setup_kwargs = {
+    'name': 'ttls',
+    'version': '1.6.0',
+    'description': 'Twinkly Twinkly Little Star',
+    'long_description': '# Twinkly Twinkly Little Star\n\n`ttls` is a small package to help you make async requests to Twinkly LEDs. A command line tool (also called `ttls`) is also included, as well as some examples how to create both loadable movies and realtime sequences.\n\nWritten based on the [excellent XLED documentation](https://xled-docs.readthedocs.io/en/latest/) by [@scrool](https://github.com/scrool).\n',
+    'author': 'Jakob Schlyter',
+    'author_email': 'jakob@schlyter.se',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/jschlyter/ttls',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
