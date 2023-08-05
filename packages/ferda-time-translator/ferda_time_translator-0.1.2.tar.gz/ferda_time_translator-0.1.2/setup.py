16d563f4ddbacb0370547b83d4553a4d61dd0537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:55 +0000

    Add package ferda-time-translator/0.1.2/ferda_time_translator-0.1.2.tar.gz

---
+++ packages/ferda-time-translator/ferda_time_translator-0.1.2.tar.gz/ferda_time_translator-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ferda_time_translator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ferda-time-translator',
+    'version': '0.1.2',
+    'description': 'Simple program that will translate the current time into ferda standard time (FST)',
+    'long_description': None,
+    'author': 'Will Lavalliere',
+    'author_email': 'willlavalliere@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9',
+}
+
+
+setup(**setup_kwargs)
