98951271a06b211a2db6a4416291c5c5a2e0b095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package pltrdy/0.3.11/pltrdy-0.3.11-py3-none-any.whl

---
+++ packages/pltrdy/pltrdy-0.3.11-py3-none-any.whl/pltrdy/__init__.py
@@ -0,0 +1,20 @@
+import pltrdy.percentiles
+import pltrdy.assertion
+import pltrdy.explore_results
+import pltrdy.gen_script
+import pltrdy.pickle
+import pltrdy.rouge
+import pltrdy.stats
+import pltrdy.time
+import pltrdy.tee
+import pltrdy.wc  # noqa F401
+import pltrdy.quiet
+import pltrdy.tmpdir
+import pltrdy.types
+import pltrdy.cmd
+import pltrdy.git
+
+from pltrdy.describe import describe_str, describe_lines, describe, tab_print
+from pltrdy.reload import reload_class
+
+__version__ = "0.3.11"
---
+++ packages/pltrdy/pltrdy-0.3.11-py3-none-any.whl/pltrdy/git.py
@@ -0,0 +1,29 @@
+import subprocess
+from types import ModuleType
+
+from pathlib import Path
+
+
+def get_git_revision_hash(root, short=False):
+    short_str = "--short" if short else ""
+    cmd = f"git rev-parse {short_str} HEAD"
+    args = cmd.split()
+
+    if isinstance(root, str):
+        root = Path(root)
+
+    elif isinstance(root, Path):
+        pass
+
+    elif isinstance(root, ModuleType):
+        root = Path(root.__file__).resolve().parent
+
+    else:
+        raise TypeError(
+            f"Invalid root type ({type(root)}), should be either str, Path or module"
+        )
+
+    if root.is_file():
+        root = root.parent
+
+    return subprocess.check_output(args, cwd=root).decode("ascii").strip()
