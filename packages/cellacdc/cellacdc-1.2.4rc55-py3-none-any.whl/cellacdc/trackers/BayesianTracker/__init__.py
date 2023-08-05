d61c9f5417060355015168618aae57927bf37527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:21 +0000

    Add package cellacdc/1.2.4rc55/cellacdc-1.2.4rc55-py3-none-any.whl

---
+++ packages/cellacdc/cellacdc-1.2.4rc55-py3-none-any.whl/cellacdc/trackers/BayesianTracker/__init__.py
@@ -0,0 +1,17 @@
+try:
+    import btrack
+except ModuleNotFoundError:
+    pkg_name = 'BayesianTracker'
+    import os
+    import sys
+    import subprocess
+    from PyQt5.QtWidgets import QMessageBox
+    from cellacdc import myutils
+    cancel = myutils.install_package_msg(pkg_name)
+    if cancel:
+        raise ModuleNotFoundError(
+            f'User aborted {pkg_name} installation'
+        )
+    subprocess.check_call(
+        [sys.executable, '-m', 'pip', 'install', 'btrack']
+    )
