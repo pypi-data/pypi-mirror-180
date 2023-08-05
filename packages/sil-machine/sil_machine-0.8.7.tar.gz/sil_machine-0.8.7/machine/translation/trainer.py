dde01a8a63ccd57e7fbb8691658be64cc0967a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:40 +0000

    Add package sil-machine/0.8.7/sil_machine-0.8.7.tar.gz

---
+++ packages/sil-machine/sil_machine-0.8.7.tar.gz/sil_machine-0.8.7/machine/translation/trainer.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Dict, Optional
+
+from ..utils.progress_status import ProgressStatus
+
+
+class TrainStats:
+    def __init__(self) -> None:
+        self.train_corpus_size: int = 0
+        self._metrics: Dict[str, float] = {}
+
+    @property
+    def metrics(self) -> Dict[str, float]:
+        return self._metrics
+
+
+class Trainer(ABC):
+    @abstractmethod
+    def train(
+        self,
+        progress: Optional[Callable[[ProgressStatus], None]] = None,
+        check_canceled: Optional[Callable[[], None]] = None,
+    ) -> None:
+        ...
+
+    @abstractmethod
+    def save(self) -> None:
+        ...
+
+    @property
+    @abstractmethod
+    def stats(self) -> TrainStats:
+        ...
