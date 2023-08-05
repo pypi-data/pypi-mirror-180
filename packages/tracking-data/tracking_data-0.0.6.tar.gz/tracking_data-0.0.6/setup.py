87c33693f7be02fac9b4a79cd661a3e7ecd99499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:01 +0000

    Add package tracking-data/0.0.6/tracking_data-0.0.6.tar.gz

---
+++ packages/tracking-data/tracking_data-0.0.6.tar.gz/tracking_data-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Tracking annotated data package'
+LONG_DESCRIPTION = 'Package which formats and structures video data for single object tracking'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="tracking_data",
+        version=VERSION,
+        author="Rodion Shyshkin",
+        author_email="<rodion.shyshkin@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['opencv-contrib-python'], # add any additional packages that
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python', 'tracking', 'datasets'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
