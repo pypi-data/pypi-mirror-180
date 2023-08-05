d3a2d565dde6068590e81444790a7ca408b0def2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:50 +0000

    Add package potyk-doc/0.1.0/potyk-doc-0.1.0.tar.gz

---
+++ packages/potyk-doc/potyk-doc-0.1.0.tar.gz/potyk-doc-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['potyk_doc']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'potyk-doc',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'potykion',
+    'author_email': 'potykion@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
