d9f520a876df86efd35bfeca3f735085c9b92230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:42:07 +0000

    Add package drf-friendly-errors-egt/0.16.1/drf_friendly_errors_egt-0.16.1-py2.py3-none-any.whl

---
+++ packages/drf-friendly-errors-egt/drf_friendly_errors_egt-0.16.1-py2.py3-none-any.whl/rest_framework_friendly_errors/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "drf-friendly-errors"
+__version__ = "0.16.1"
+__author__ = "Tomasz Łaszczuk(Original), Mrityunjay Saxena(This fork)"
+__contact__ = "mrityunjay.saxena@egyanamtech.com"
+__license__ = "MIT"
+
+
+# Version synonym
+VERSION = __version__
