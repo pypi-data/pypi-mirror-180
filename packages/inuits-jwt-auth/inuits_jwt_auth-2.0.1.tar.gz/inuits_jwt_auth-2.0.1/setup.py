3cc870008b9868bef1e9fac0b96e21d9761c7df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:55 +0000

    Add package inuits-jwt-auth/2.0.1/inuits_jwt_auth-2.0.1.tar.gz

---
+++ packages/inuits-jwt-auth/inuits_jwt_auth-2.0.1.tar.gz/inuits_jwt_auth-2.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    author="Inuits",
+    author_email="developers@inuits.eu",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    description="A small library to handle JWT auth including roles and permissions",
+    install_requires=[
+        "Authlib>=1.2.0",
+        "requests>=2.25.0",
+        "Flask>=1.1.2",
+        "Werkzeug>=1.0.1",
+    ],
+    license="GPLv2",
+    name="inuits_jwt_auth",
+    packages=["inuits_jwt_auth"],
+    provides=["inuits_jwt_auth"],
+    version="2.0.1",
+)
