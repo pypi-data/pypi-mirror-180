3eb42817405eb1dd2412af260e18123dd0638135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:42:24 +0000

    Add package python-nomad/1.5.0/python-nomad-1.5.0.tar.gz

---
+++ packages/python-nomad/python-nomad-1.5.0.tar.gz/python-nomad-1.5.0/nomad/api/allocation.py
@@ -0,0 +1,69 @@
+import nomad.api.exceptions
+
+from nomad.api.base import Requester
+
+
+class Allocation(Requester):
+    """
+    The allocation endpoint is used to query the a specific allocation.
+    By default, the agent's local region is used; another region can be
+    specified using the ?region= query parameter.
+
+    https://www.nomadproject.io/docs/http/alloc.html
+    """
+
+    ENDPOINT = "allocation"
+
+    def __init__(self, **kwargs):
+        super(Allocation, self).__init__(**kwargs)
+
+    def __str__(self):
+        return "{0}".format(self.__dict__)
+
+    def __repr__(self):
+        return "{0}".format(self.__dict__)
+
+    def __getattr__(self, item):
+        raise AttributeError
+
+    def __contains__(self, item):
+        try:
+            response = self.get_allocation(item)
+
+            if response["ID"] == item:
+                return True
+        except nomad.api.exceptions.URLNotFoundNomadException:
+            return False
+
+    def __getitem__(self, item):
+        try:
+            response = self.get_allocation(item)
+
+            if response["ID"] == item:
+                return response
+        except nomad.api.exceptions.URLNotFoundNomadException:
+            raise KeyError
+
+    def get_allocation(self, id):
+        """ Query a specific allocation.
+
+           https://www.nomadproject.io/docs/http/alloc.html
+
+            returns: dict
+            raises:
+              - nomad.api.exceptions.BaseNomadException
+              - nomad.api.exceptions.URLNotFoundNomadException
+        """
+        return self.request(id, method="get").json()
+
+    def stop_allocation(self, id):
+        """ Stop a specific allocation.
+
+           https://www.nomadproject.io/api-docs/allocations/#stop-allocation
+
+            returns: dict
+            raises:
+              - nomad.api.exceptions.BaseNomadException
+              - nomad.api.exceptions.URLNotFoundNomadException
+        """
+        return self.request(id, "stop", method="post").json()
---
+++ packages/python-nomad/python-nomad-1.5.0.tar.gz/python-nomad-1.5.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python-nomad',
+    version='1.5.0',
+    install_requires=['requests'],
+    packages=['nomad', 'nomad.api'],
+    url='http://github.com/jrxfive/python-nomad',
+    license='MIT',
+    author='jrxfive',
+    author_email='jrxfive@gmail.com',
+    description='Client library for Hashicorp Nomad',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords='nomad hashicorp client',
+)
