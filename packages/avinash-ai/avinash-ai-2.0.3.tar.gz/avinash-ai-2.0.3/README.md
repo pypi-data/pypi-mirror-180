f3242a3031c56f78a430f5341999dc87065caf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:22 +0000

    Add package avinash-ai/2.0.3/avinash-ai-2.0.3.tar.gz

---
+++ packages/avinash-ai/avinash-ai-2.0.3.tar.gz/avinash-ai-2.0.3/README.md
@@ -0,0 +1,8 @@
+# A general purpose AI model 
+
+```python
+from avinash_ai.analyzer import analyze_your_query
+
+response = analyze_your_query("What is 2+2 ?")
+print(response)
+```
---
+++ packages/avinash-ai/avinash-ai-2.0.3.tar.gz/avinash-ai-2.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="avinash-ai",
+    version="2.0.3",
+    description="Build your own ML",
+    packages=["avinash_ai"],
+    install_requires=["openai"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+
