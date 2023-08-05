8606f48d67758de59d43184e9e31c4887e0c0f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:52 +0000

    Add package perun/0.1.0b17/perun-0.1.0b17.tar.gz

---
+++ packages/perun/perun-0.1.0b17.tar.gz/perun-0.1.0b17/perun/backend/intel_rapl.py
@@ -0,0 +1,127 @@
+"""Defines Intel RAPL related classes."""
+import os
+import re
+from pathlib import Path
+from typing import Callable, List, Set
+
+import cpuinfo
+import numpy as np
+
+from perun import log
+from perun.units import Joule
+
+from .backend import Backend, backend
+from .device import Device
+
+RAPL_PATH = "/sys/class/powercap/"
+
+DIR_RGX = r"intel-rapl:(\d)$"
+SUBDIR_RGX = r"intel-rapl:\d:\d$"
+
+
+@backend
+class IntelRAPLBackend(Backend):
+    """Intel RAPL as a source of cpu and memory devices.
+
+    Uses pyRAPL to gather device information and creates metrics for each available device
+    """
+
+    name = "Intel RAPL"
+    description = "Reads energy usage from CPUs and DRAM using Intel RAPL"
+
+    def __init__(self) -> None:
+        """Init IntelRAPLBackend."""
+        super().__init__()
+        log.info("Initialized Intel RAPL")
+
+    def setup(self):
+        """Check Intel RAPL access."""
+        cpuInfo = cpuinfo.get_cpu_info()
+        self.cpu_name = cpuInfo["brand_raw"]
+        self.thread_count = cpuInfo["count"]
+
+        raplPath = Path(RAPL_PATH)
+        self._devices = {}
+
+        if not raplPath.exists():
+            raise ImportWarning("No powercap interface")
+
+        for child in raplPath.iterdir():
+            match = re.match(DIR_RGX, child.name)
+            if match:
+                if os.access(child / "energy_uj", os.R_OK):
+                    socket = match.groups()[0]
+                    device_name = open(child / "name", "r").readline().strip()
+                    self._devices[f"{socket}_{device_name}"] = {
+                        "max_energy_uj": np.uint64(
+                            open(child / "max_energy_range_uj", "r").readline().strip()
+                        ),
+                        "energy_path": str(child / "energy_uj"),
+                    }
+                    for grandchild in child.iterdir():
+                        match = re.match(SUBDIR_RGX, grandchild.name)
+                        if match:
+                            device_name = (
+                                open(grandchild / "name", "r").readline().strip()
+                            )
+                            self._devices[f"{socket}_{device_name}"] = {
+                                "max_energy_uj": np.uint64(
+                                    open(grandchild / "max_energy_range_uj", "r")
+                                    .readline()
+                                    .strip()
+                                ),
+                                "energy_path": str(grandchild / "energy_uj"),
+                            }
+
+    def close(self) -> None:
+        """Backend shutdown code (does nothing for intel rapl)."""
+        return
+
+    def visibleDevices(self) -> Set[str]:
+        """
+        Return string ids of visible devices.
+
+        Returns:
+            Set[str]: Set with device string ids
+        """
+        return set(self._devices.keys())
+
+    def getDevices(self, deviceList: Set[str]) -> List[Device]:
+        """
+        Gather devive objects based on a set of device ids.
+
+        Args:
+            deviceList (Set[str]): Set containing devices ids
+
+        Returns:
+            List[Device]: Device objects
+        """
+
+        def getCallback(filePath) -> Callable[[], np.number]:
+            def func() -> np.number:
+                return np.uint64(open(filePath, "r").readline().strip())
+
+            return func
+
+        self.devices: List[Device] = []
+        for device in deviceList:
+            if device in self._devices:
+
+                deviceInfo = self._devices[device]
+                self.devices.append(
+                    Device(
+                        device,
+                        f"{self.cpu_name}:{device}",
+                        Joule(),
+                        "micro",
+                        np.uint64(0),
+                        np.uint64(deviceInfo["max_energy_uj"]),
+                        "uint64",
+                        getCallback(deviceInfo["energy_path"]),
+                    )
+                )
+
+        return self.devices
+
+
+IntelRAPLBackend()
---
+++ packages/perun/perun-0.1.0b17.tar.gz/perun-0.1.0b17/perun/backend/nvml.py
@@ -0,0 +1,97 @@
+"""Nvidia Mangement Library Source definition."""
+from typing import Callable, Set
+
+import numpy as np
+import pynvml
+from pynvml import NVMLError
+
+from perun import log
+from perun.units import Watt
+
+from .backend import Backend, backend
+from .device import Device
+
+
+@backend
+class NVMLSource(Backend):
+    """NVMLSource class.
+
+    Setups connection to NVML and creates relevant devices
+    """
+
+    name = "NVIDIA ML"
+    description: str = "Access GPU information from NVML python bindings"
+
+    def __init__(self) -> None:
+        """Init NVIDIA ML Backend."""
+        super().__init__()
+        log.info("Initialized NVIDIA SMI Backend")
+
+    def close(self):
+        """Backend shutdown code."""
+        pynvml.nvmlShutdown()
+
+    def visibleDevices(self) -> Set[str]:
+        """
+        Return string ids of visible devices.
+
+        Returns:
+            Set[str]: Set with device string ids
+        """
+        devices = set()
+        for i in range(pynvml.nvmlDeviceGetCount()):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+            devices.add(pynvml.nvmlDeviceGetUUID(handle).decode("utf-8"))
+        return devices
+
+    def getDevices(self, deviceList: Set[str]):
+        """
+        Gather device objects based on a set of device ids.
+
+        Args:
+            deviceList (Set[str]): Set containing devices ids
+
+        Returns:
+            List[Device]: Device objects
+        """
+
+        def getCallback(handle) -> Callable[[], np.number]:
+            def func() -> np.number:
+                return np.uint32(pynvml.nvmlDeviceGetPowerUsage(handle))
+
+            return func
+
+        for device in deviceList:
+            try:
+                handle = pynvml.nvmlDeviceGetHandleByUUID(device.encode())
+
+                name = f"CUDA:{device}"
+                long_name = f"{pynvml.nvmlDeviceGetName(handle).decode('utf-8')}"
+                max_power = np.uint32(pynvml.nvmlDeviceGetPowerManagementLimit(handle))
+
+                self.devices.append(
+                    Device(
+                        name,
+                        long_name,
+                        Watt(),
+                        "mili",
+                        np.uint32(0),
+                        np.uint32(max_power),
+                        "uint32",
+                        getCallback(handle),
+                    )
+                )
+            except NVMLError as e:
+                log.debug(f"Could not find device {device}")
+                log.debug(e)
+
+        return self.devices
+
+    def setup(self):
+        """Init pynvml and gather number of devices."""
+        pynvml.nvmlInit()
+        deviceCount = pynvml.nvmlDeviceGetCount()
+        log.debug(f"NVML Device count: {deviceCount}")
+
+
+NVMLSource()
