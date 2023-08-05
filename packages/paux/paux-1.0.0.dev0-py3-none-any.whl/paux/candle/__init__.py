858f5aab8a18fec0ca2b4addb8b3346663b0e18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:21:18 +0000

    Add package paux/1.0.0.dev0/paux-1.0.0.dev0-py3-none-any.whl

---
+++ packages/paux/paux-1.0.0.dev0-py3-none-any.whl/paux/candle/__init__.py
@@ -0,0 +1,6 @@
+from pix.candle.history import candle_okx as OKX
+from pix.candle.history import candle_ib as IB
+from pix.candle.history import candle_tda as TDA
+from pix.candle.history import candle_huobi as HUOBI
+from pix.candle.history import candle_binance as BINANCE
+from pix.candle.history import candle_binance as BINANCE
