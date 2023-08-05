f9caea896fdb9225a9867f447398a0dbfb354625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:23 +0000

    Add package thermopack/2.1.0/thermopack-2.1.0-py3-none-win_amd64.whl

---
+++ packages/thermopack/thermopack-2.1.0-py3-none-win_amd64.whl/thermopack/platform_specifics.py
@@ -0,0 +1,13 @@
+# Module for platform specific stuff. Automatically generated.
+# Timestamp : 2022-12-06T21:06:46.125811
+
+
+def get_platform_specifics():
+    pf_specifics = {}
+    pf_specifics["os_id"] = "win"
+    pf_specifics["prefix"] = ""
+    pf_specifics["module"] = "_mp_"
+    pf_specifics["postfix"] = "_"
+    pf_specifics["postfix_no_module"] = "_"
+    pf_specifics["dyn_lib"] = "thermopack.dll"
+    return pf_specifics
