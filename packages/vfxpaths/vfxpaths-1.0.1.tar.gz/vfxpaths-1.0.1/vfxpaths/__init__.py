93d4396e79435c61d65c9ba4395e0b81bdb36b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:33 +0000

    Add package vfxpaths/1.0.1/vfxpaths-1.0.1.tar.gz

---
+++ packages/vfxpaths/vfxpaths-1.0.1.tar.gz/vfxpaths-1.0.1/vfxpaths/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from vfxpaths.resolve import Resolve
+from vfxpaths.utils import RW
+from vfxpaths.ability.regex_match import MatchModel
+from vfxpaths.initialization import (init_pattern_config,
+                                     add_config_template,
+                                     remove_config_template)
+
+
+def init_config():
+    # initialization config
+    init_pattern_config()
+
+
+__all__ = ["Resolve",
+           "add_config_template",
+           "remove_config_template",
+           "MatchModel"]
+
