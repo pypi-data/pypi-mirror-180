7b8a7da22ba01684808ee59bf2bcebc71e24265b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:26 +0000

    Add package cellacdc/1.2.4rc55/cellacdc-1.2.4rc55.tar.gz

---
+++ packages/cellacdc/cellacdc-1.2.4rc55.tar.gz/cellacdc-1.2.4rc55/cellacdc/models/YeaZ/__init__.py
@@ -0,0 +1,16 @@
+try:
+    import tensorflow
+except ModuleNotFoundError:
+    pkg_name = 'tensorflow'
+    import os
+    import sys
+    import subprocess
+    from cellacdc import myutils
+    cancel = myutils.install_package_msg(pkg_name)
+    if cancel:
+        raise ModuleNotFoundError(
+            f'User aborted {pkg_name} installation'
+        )
+    subprocess.check_call(
+        [sys.executable, '-m', 'pip', 'install', 'tensorflow']
+    )
