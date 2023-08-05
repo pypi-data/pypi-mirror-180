1902aa5a36e612c135e46dc3615b6632db547c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:18 +0000

    Add package stockplt/0.0.1/stockplt-0.0.1.tar.gz

---
+++ packages/stockplt/stockplt-0.0.1.tar.gz/stockplt-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+# from distutils.core import setup
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'stockplt'
+
+# Setting up
+setup(
+    name="stockplt",
+    version=VERSION,
+    author="Hong Tang",
+    author_email="<stanghong@gmail.com>",
+    description=DESCRIPTION,
+    # url = 'https://github.com/stanghong/pypi_publication', 
+    # packages=find_packages('addup'),
+    packages=[], #'quicklookts'
+    install_requires=['numpy','pandas',],
+    # download_url = 'https://github.com/stanghong/pypi_publication/archive/refs/tags/v0.0.9.tar.gz',
+    
+    keywords=['python', 'visualization','plot'],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
