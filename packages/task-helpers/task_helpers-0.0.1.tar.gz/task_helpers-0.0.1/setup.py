c4afbd06a6fcb0cab7b7432499503e7616b8cc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:09 +0000

    Add package task-helpers/0.0.1/task_helpers-0.0.1.tar.gz

---
+++ packages/task-helpers/task_helpers-0.0.1.tar.gz/task_helpers-0.0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="task_helpers",
+      version="0.0.1",
+      description="A package for creating task helpers.",
+      author="Viacheslav Loievskyi",
+      author_email="loievskyi.slava@gmail.com",
+      packages=["distutils", "distutils.command"],
+      )
