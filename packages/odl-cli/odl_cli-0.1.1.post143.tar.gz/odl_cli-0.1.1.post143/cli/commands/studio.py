aaf15abc0cf32ccf22510a1b37c51cbc9b627a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package odl-cli/0.1.1.post143/odl_cli-0.1.1.post143.tar.gz

---
+++ packages/odl-cli/odl_cli-0.1.1.post143.tar.gz/odl_cli-0.1.1.post143/cli/commands/studio.py
@@ -0,0 +1,123 @@
+"""
+
+cd function
+
+Examples:
+    $python cli.py studio --datasetname coco
+    >> now you can open the dataset viewer on localhost:9192:
+"""
+
+
+from utils.views.view import View
+from commands.cmdbase import CmdBase
+from commons.argument_parser import EnvDefaultVar
+from utils.admin import DBClient
+from commands.const import (
+    DEFAULT_CLI_CONFIG_FILE,
+    DEFAULT_CLI_LOG_FILE_PATH,
+    DEFAULT_CONFIG_DIR,
+    DEFAULT_LOCAL_STORAGE_PATH,
+    PROG_NAME,
+    SQLITE_DB_PATH,
+)
+
+
+class Studio(CmdBase):
+    """
+    cd command
+    """
+
+    def init_parser(self, subparsers):
+        """
+        Initialize the parser for the command
+        document : https://docs.python.org/zh-cn/3/library/argparse.html#
+
+        Args:
+            subparsers:
+
+        Returns:
+
+        """
+        studio_parser = subparsers.add_parser(
+            "studio",
+            help="view the specified dataset on webpage.",
+            example="studio.example",
+        )
+        studio_parser.add_argument(
+            "-l",
+            "--local",
+            nargs="?",
+            default="True",
+            const="True",
+            type=bool,
+            help="view local[default] dataset.",
+            metavar="SET to True to VIEW LOCAL DATASET",
+        )
+        studio_parser.add_argument(
+            "-r",
+            "--remote",
+            nargs="?",
+            default="False",
+            const="False",
+            type=bool,
+            help="view remote[when explicitly specified to true] dataset.",
+            metavar="SET to True(default false) to VIEW REMOTE DATASET",
+        )
+        studio_parser.add_argument(
+            "-d",
+            "--dataset-name",
+            action=EnvDefaultVar,
+            envvar="DSDL_CLI_DATASET_NAME",
+            nargs=1,
+            type=str,
+            help="dataset name",
+            metavar="[DATASET NAME]",
+            required=True,
+        )
+        return studio_parser
+
+    def cmd_entry(self, cmdargs, config, *args, **kwargs):
+        """
+        Entry point for the command
+
+        Args:
+            self:
+            cmdargs:
+            config:
+
+        Returns:
+
+        """
+
+        dataset_name = cmdargs.dataset_name[0]
+        local = cmdargs.local
+        remote = cmdargs.remote
+
+        dbcli = DBClient()
+        local_exists = dbcli.is_dataset_local_exist(dataset_name)
+
+        # TODO support remote dataset exists or not in the future:
+        # remote_exists = dbcli.is_dataset_remote_exist(dataset_name)
+
+        view = View(dataset_name)
+
+        print(f"local:{local}")
+        print(f"remote:{str(remote)}")
+        print(f"exists on localhost: {local_exists}")
+
+        if remote is True:
+            if local_exists is True:
+                use_local = input(
+                    f"Dataset {dataset_name} already exists on local.\nDo you want view using local dataset:(y/n)?"
+                )
+                if use_local in ["y", "Y", "yes", "Yes"]:
+                    view.view_local_dataset()
+                elif use_local in ["n", "N", "no", "No"]:
+                    view.view_remote_dataset()
+            else:
+                view.view_remote_dataset()
+        elif local is True:
+            if local_exists is True:
+                view.view_local_dataset()
+            else:
+                print(f"Dataset {dataset_name} is not exists on local.")
