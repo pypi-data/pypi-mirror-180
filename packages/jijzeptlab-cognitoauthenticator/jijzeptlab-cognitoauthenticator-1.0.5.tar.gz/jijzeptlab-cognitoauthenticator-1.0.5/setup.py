b65468bdf564ee602733b228b6a44fef63c06ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:15 +0000

    Add package jijzeptlab-cognitoauthenticator/1.0.5/jijzeptlab-cognitoauthenticator-1.0.5.tar.gz

---
+++ packages/jijzeptlab-cognitoauthenticator/jijzeptlab-cognitoauthenticator-1.0.5.tar.gz/jijzeptlab-cognitoauthenticator-1.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jijzeptlab-cognitoauthenticator",
+    version="1.0.5",
+    description="JijZeptLab Native Authenticator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jij-Inc/cognitoauthenticator",
+    author="Leticia Portella",
+    author_email="leportella@protonmail.com",
+    license="3 Clause BSD",
+    packages=find_packages(),
+    install_requires=["jupyterhub>=1.3", "bcrypt", "onetimepass", "boto3"],
+    include_package_data=True,
+)
