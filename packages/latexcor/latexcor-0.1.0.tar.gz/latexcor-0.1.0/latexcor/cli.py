20c6cb51c219bd746073d3830af3efab3bee6824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:24 +0000

    Add package latexcor/0.1.0/latexcor-0.1.0.tar.gz

---
+++ packages/latexcor/latexcor-0.1.0.tar.gz/latexcor-0.1.0/latexcor/cli.py
@@ -0,0 +1,13 @@
+import argparse
+import codecs
+import sys
+
+parser = argparse.ArgumentParser(description="""Basic usage: latexcornouaille""")
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
