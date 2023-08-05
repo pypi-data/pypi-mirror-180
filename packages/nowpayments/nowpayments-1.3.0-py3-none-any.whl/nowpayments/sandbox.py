de0dee49d96b1040bcafb25e6da4dcae54f01e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:25 +0000

    Add package nowpayments/1.3.0/nowpayments-1.3.0-py3-none-any.whl

---
+++ packages/nowpayments/nowpayments-1.3.0-py3-none-any.whl/nowpayments/sandbox.py
@@ -0,0 +1,14 @@
+"""
+A Python wrapper for the NOWPayments API Sandbox.
+"""
+from nowpayments import NOWPayments
+
+
+class NOWPaymentsSandbox(NOWPayments):
+    """
+    Class to used for the NOWPayments API Sandbox.
+    """
+
+    # This has recently been changed from https://api.sandbox.nowpayments.io/v1/ to this.
+    API_URL = "https://api-sandbox.nowpayments.io/v1/{}"
+    IS_SANDBOX = True
