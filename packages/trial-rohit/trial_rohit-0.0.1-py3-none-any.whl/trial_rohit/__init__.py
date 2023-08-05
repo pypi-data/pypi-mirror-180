50d982eb00e2a66e06bafc8a7faa82166b35bf5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:38 +0000

    Add package trial-rohit/0.0.1/trial_rohit-0.0.1-py3-none-any.whl

---
+++ packages/trial-rohit/trial_rohit-0.0.1-py3-none-any.whl/trial_rohit/__init__.py
@@ -0,0 +1,6 @@
+from check import get
+__version__ = "0.0.1"
+def tr():
+    k=get()
+    print(k)
+    return k
---
+++ packages/trial-rohit/trial_rohit-0.0.1-py3-none-any.whl/trial_rohit/trial_rohit.py
@@ -0,0 +1,7 @@
+import  numpy as np
+
+
+def get():
+    return np.multiply(5,9)
+
+print(get())
