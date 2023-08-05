7c27fbfca525fc744dca8f6c88187a72b2aaf8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:30 +0000

    Add package udata-hydra/0.3.0.dev450/udata_hydra-0.3.0.dev450-py3-none-any.whl

---
+++ packages/udata-hydra/udata_hydra-0.3.0.dev450-py3-none-any.whl/udata_hydra/cli.py
@@ -0,0 +1,234 @@
+import csv
+import os
+
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import aiohttp
+import asyncpg
+from humanfriendly import parse_size
+from minicli import cli, run, wrap
+from progressist import ProgressBar
+
+from udata_hydra import config
+from udata_hydra.crawl import check_url as crawl_check_url
+from udata_hydra.logger import setup_logging
+
+
+CATALOG_URL = "https://www.data.gouv.fr/fr/datasets/r/4babf5f2-6a9c-45b5-9144-ca5eae6a7a6d"
+
+context = {}
+log = setup_logging()
+
+
+@cli
+async def init_db(drop=False, table=None, index=False, reindex=False):
+    """Create the DB structure"""
+    log.info("Initializing database...")
+    if drop:
+        if table == "catalog" or not table:
+            await context["conn"].execute("DROP TABLE IF EXISTS catalog")
+        if table == "checks" or not table:
+            await context["conn"].execute("DROP TABLE IF EXISTS checks")
+    await context["conn"].execute(
+        """
+        CREATE TABLE IF NOT EXISTS catalog(
+            id serial PRIMARY KEY,
+            dataset_id VARCHAR(24),
+            resource_id UUID,
+            url VARCHAR,
+            deleted BOOLEAN NOT NULL,
+            last_check INT,
+            priority BOOLEAN NOT NULL,
+            initialization BOOLEAN NOT NULL DEFAULT FALSE,
+            UNIQUE(dataset_id, resource_id, url)
+        )
+    """
+    )
+    await context["conn"].execute(
+        """
+        CREATE TABLE IF NOT EXISTS checks(
+            id serial PRIMARY KEY,
+            resource_id UUID,
+            url VARCHAR,
+            domain VARCHAR,
+            created_at TIMESTAMP DEFAULT NOW(),
+            status INT,
+            headers JSONB,
+            timeout BOOLEAN NOT NULL,
+            response_time FLOAT,
+            error VARCHAR,
+            checksum VARCHAR,
+            filesize BIGINT,
+            mime_type VARCHAR
+        )
+    """
+    )
+    if reindex:
+        await context["conn"].execute(
+            """
+            DROP INDEX IF EXISTS url_idx;
+            DROP INDEX IF EXISTS domain_idx;
+        """
+        )
+    if index or reindex:
+        await context["conn"].execute(
+            """
+            CREATE INDEX IF NOT EXISTS url_idx ON checks (url);
+            CREATE INDEX IF NOT EXISTS domain_idx ON checks (domain);
+        """
+        )
+
+
+async def download_file(url, fd):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            while True:
+                chunk = await resp.content.read(1024)
+                if not chunk:
+                    break
+                fd.write(chunk)
+
+
+@cli
+async def load_catalog(url=CATALOG_URL):
+    """Load the catalog into DB from CSV file"""
+    try:
+        log.info(f"Downloading catalog from {url}...")
+        with NamedTemporaryFile(delete=False) as fd:
+            await download_file(url, fd)
+        log.info("Upserting catalog in database...")
+        # consider everything deleted, deleted will be updated when loading new catalog
+        await context["conn"].execute("UPDATE catalog SET deleted = TRUE")
+        with open(fd.name) as fd:
+            reader = csv.DictReader(fd, delimiter=";")
+            rows = list(reader)
+            bar = ProgressBar(total=len(rows))
+            for row in bar.iter(rows):
+                await context["conn"].execute(
+                    """
+                    INSERT INTO catalog (dataset_id, resource_id, url, deleted, priority, initialization)
+                    VALUES ($1, $2, $3, FALSE, FALSE, TRUE)
+                    ON CONFLICT (dataset_id, resource_id, url) DO UPDATE SET deleted = FALSE
+                """,
+                    row["dataset.id"],
+                    row["id"],
+                    row["url"],
+                )
+        log.info("Catalog successfully upserted into DB.")
+    except Exception as e:
+        raise e
+    finally:
+        fd.close()
+        os.unlink(fd.name)
+
+
+@cli
+async def check_url(url, method="get"):
+    """Quickly check an URL"""
+    log.info(f"Checking url {url}")
+    async with aiohttp.ClientSession(timeout=None) as session:
+        timeout = aiohttp.ClientTimeout(total=5)
+        _method = getattr(session, method)
+        try:
+            async with _method(
+                url, timeout=timeout, allow_redirects=True
+            ) as resp:
+                print("Status :", resp.status)
+                print("Headers:", resp.headers)
+        except Exception as e:
+            log.error(e)
+
+
+@cli
+async def check_resource(resource_id, method="get"):
+    """Trigger a complete check for a given resource_id"""
+    q = "SELECT * FROM catalog WHERE resource_id = $1"
+    res = await context["conn"].fetch(q, resource_id)
+    if not res:
+        log.error("Resource not found in catalog")
+        return
+    async with aiohttp.ClientSession(timeout=None) as session:
+        await crawl_check_url(res[0], session, method=method)
+
+
+@cli
+async def csv_sample(size=1000, download=False, max_size="100M"):
+    """Get a csv sample from latest checks
+
+    :size: Size of the sample (how many files to query)
+    :download: Download files or just list them
+    :max_size: Maximum size for one file (from headers)
+    """
+    max_size = parse_size(max_size)
+    start_q = f"""
+        SELECT catalog.resource_id, catalog.dataset_id, checks.url,
+            checks.headers->>'content-type' as content_type,
+            checks.headers->>'content-length' as content_length
+        FROM checks, catalog
+        WHERE catalog.last_check = checks.id
+        AND checks.headers->>'content-type' LIKE '%csv%'
+        AND checks.status >= 200 and checks.status < 400
+        AND CAST(checks.headers->>'content-length' AS INTEGER) <= {max_size}
+    """
+    end_q = f"""
+        ORDER BY RANDOM()
+        LIMIT {size / 2}
+    """
+    # get remote stuff for half the sample
+    q = f"""{start_q}
+        -- ignore ODS, they're correctly formated from a datastore
+        AND checks.url NOT LIKE '%/explore/dataset/%'
+        AND checks.url NOT LIKE '%/api/datasets/1.0/%'
+        -- ignore ours
+        AND checks.domain <> 'static.data.gouv.fr'
+        {end_q}
+    """
+    res = await context["conn"].fetch(q)
+    # and from us for the rest
+    q = f"""{start_q}
+        AND checks.domain = 'static.data.gouv.fr'
+        {end_q}
+    """
+    res += await context["conn"].fetch(q)
+
+    data_path = Path("./data")
+    dl_path = data_path / "downloaded"
+    dl_path.mkdir(exist_ok=True, parents=True)
+    if download:
+        log.debug("Cleaning up...")
+        (data_path / "_index.csv").unlink(missing_ok=True)
+        [p.unlink() for p in Path(dl_path).glob("*.csv")]
+
+    lines = []
+    bar = ProgressBar(total=len(res))
+    for r in bar.iter(res):
+        line = dict(r)
+        line["resource_id"] = str(line["resource_id"])
+        filename = dl_path / f"{r['dataset_id']}_{r['resource_id']}.csv"
+        line["filename"] = filename.__str__()
+        lines.append(line)
+        if not download:
+            continue
+        await download_file(r["url"], filename.open("wb"))
+        with os.popen(f"file {filename} -b --mime-type") as proc:
+            line["magic_mime"] = proc.read().lower().strip()
+        line["real_size"] = filename.stat().st_size
+
+    with (data_path / "_index.csv").open("w") as ofile:
+        writer = csv.DictWriter(ofile, fieldnames=lines[0].keys())
+        writer.writeheader()
+        writer.writerows(lines)
+
+
+@wrap
+async def cli_wrapper():
+    dsn = config.DATABASE_URL
+    context["conn"] = await asyncpg.connect(dsn=dsn)
+    context["dsn"] = dsn
+    yield
+    await context["conn"].close()
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/udata-hydra/udata_hydra-0.3.0.dev450-py3-none-any.whl/udata_hydra/datalake_service.py
@@ -0,0 +1,195 @@
+from datetime import datetime
+import hashlib
+import json
+import logging
+import os
+import tempfile
+from typing import BinaryIO
+
+import aiohttp
+import magic
+import pandas as pd
+
+from udata_hydra import config, context
+from udata_hydra.utils.csv import detect_encoding, find_delimiter
+from udata_hydra.utils.db import update_check, get_check
+from udata_hydra.utils.http import send
+from udata_hydra.utils.json import is_json_file
+from udata_hydra.utils.minio import save_resource_to_minio
+
+
+log = logging.getLogger("udata-hydra")
+
+
+async def download_resource(url: str, headers: dict) -> BinaryIO:
+    """
+    Attempts downloading a resource from a given url.
+    Returns the downloaded file object.
+    Raises IOError if the resource is too large.
+    """
+    tmp_file = tempfile.NamedTemporaryFile(delete=False)
+
+    if float(headers.get("content-length", -1)) > float(config.MAX_FILESIZE_ALLOWED):
+        raise IOError("File too large to download")
+
+    chunk_size = 1024
+    i = 0
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, allow_redirects=True) as response:
+            async for chunk in response.content.iter_chunked(chunk_size):
+                if i * chunk_size < float(config.MAX_FILESIZE_ALLOWED):
+                    tmp_file.write(chunk)
+                else:
+                    tmp_file.close()
+                    log.error(f"File {url} is too big, skipping")
+                    raise IOError("File too large to download")
+                i += 1
+    tmp_file.close()
+    return tmp_file
+
+
+# TODO: we're sending analysis info to udata every time a resource is crawled
+# although we only send "meta" check info only when they change
+# maybe introduce conditionnal webhook trigger here too
+async def process_resource(check_id) -> None:
+    check = await get_check(check_id)
+    if not check:
+        log.error(f"Check not found by id {check_id}")
+        return
+
+    resource_id = check["resource_id"]
+    dataset_id = check["dataset_id"]
+    url = check["url"]
+    headers = json.loads(check["headers"] or "{}")
+
+    log.debug(f"Analysis for resource {resource_id} in dataset {dataset_id}")
+    tmp_file = None
+    mime_type = None
+    sha1 = None
+    filesize = None
+    error = None
+
+    try:
+        tmp_file = await download_resource(url, headers)
+        # Get file size
+        filesize = os.path.getsize(tmp_file.name)
+
+        # Get checksum
+        sha1 = await compute_checksum_from_file(tmp_file.name)
+
+        # Check resource MIME type
+        # FIXME: this never seems to output text/csv, maybe override it later
+        mime_type = magic.from_file(tmp_file.name, mime=True)
+        if mime_type in ["text/plain", "text/csv", "application/csv"] and not is_json_file(tmp_file.name):
+            # Save resource only if CSV
+            try:
+                # Try to detect encoding from suspected csv file. If fail, set up to utf8 (most common)
+                encoding = detect_encoding(tmp_file.name) or "utf-8"
+                # Try to detect delimiter from suspected csv file. If fail, set up to None
+                # (pandas will use python engine and try to guess separator itself)
+                delimiter = find_delimiter(tmp_file.name)
+
+                # Try to read first 1000 rows with pandas
+                pd.read_csv(tmp_file.name, sep=delimiter, encoding=encoding, nrows=1000)
+
+                if config.SAVE_TO_MINIO:
+                    save_resource_to_minio(tmp_file, dataset_id, resource_id)
+                    storage_location = "/".join([
+                        config.MINIO_URL,
+                        config.MINIO_BUCKET,
+                        config.MINIO_FOLDER,
+                        dataset_id,
+                        resource_id,
+                    ])
+                    log.debug(
+                        f"Sending message to udata for resource stored {resource_id} in dataset {dataset_id}"
+                    )
+                    document = {"store:data_location": storage_location}
+                    await send(dataset_id=dataset_id,
+                               resource_id=resource_id,
+                               document=document)
+            except ValueError:
+                log.warning(
+                    f"Resource {resource_id} in dataset {dataset_id} is not a parsable CSV"
+                )
+
+        # Send udata a message for both CSV and non CSV resources
+        log.debug(
+            f"Sending a message to udata for resource analysed {resource_id} in dataset {dataset_id}"
+        )
+        document = {
+            "analysis:error": None,
+            "analysis:filesize": filesize,
+            "analysis:mime": mime_type,
+        }
+        # Check if checksum has been modified
+        # TODO: improve file modification logic
+        checksum_modified = await has_checksum_been_modified(resource_id, sha1)
+        if checksum_modified:
+            document["analysis:checksum_last_modified"] = datetime.utcnow().isoformat()
+
+        await send(
+            dataset_id=dataset_id,
+            resource_id=resource_id,
+            document=document
+        )
+        await update_check(check_id, {
+            "checksum": sha1,
+            "error": error,
+            "filesize": filesize,
+            "mime_type": mime_type
+        })
+    except IOError:
+        error = "File too large to download"
+        document = {
+            "analysis:error": error,
+            "analysis:filesize": None,
+            "analysis:mime": None,
+        }
+        await send(
+            dataset_id=dataset_id,
+            resource_id=resource_id,
+            document=document
+        )
+        await update_check(check_id, {
+            "checksum": sha1,
+            "error": error,
+            "filesize": filesize,
+            "mime_type": mime_type
+        })
+    finally:
+        if tmp_file:
+            os.remove(tmp_file.name)
+
+
+async def has_checksum_been_modified(resource_id, new_checksum):
+    q = """
+    SELECT
+        checksum
+    FROM checks
+    WHERE resource_id = $1 and checksum IS NOT NULL
+    ORDER BY created_at DESC
+    LIMIT 1
+    """
+    pool = await context.pool()
+    async with pool.acquire() as connection:
+        data = await connection.fetch(q, resource_id)
+        if data:
+            if data[0]["checksum"] != new_checksum:
+                return True
+            else:
+                return False
+        else:
+            # First check, thus we don't consider the checksum has been modified
+            return False
+
+
+async def compute_checksum_from_file(filename):
+    # Compute sha1 in blocks
+    sha1sum = hashlib.sha1()
+    with open(filename, "rb") as f:
+        block = f.read(2**16)
+        while len(block) != 0:
+            sha1sum.update(block)
+            block = f.read(2**16)
+    return sha1sum.hexdigest()
