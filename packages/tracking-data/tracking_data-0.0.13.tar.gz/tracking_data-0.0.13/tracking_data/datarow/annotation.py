fd2f044b317118eaf6b8f04ead3d2274df24ef36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:24 +0000

    Add package tracking-data/0.0.13/tracking_data-0.0.13.tar.gz

---
+++ packages/tracking-data/tracking_data-0.0.13.tar.gz/tracking_data-0.0.13/tracking_data/datarow/annotation.py
@@ -0,0 +1,40 @@
+import json
+from datarow_error import DatarowError
+
+class Annotation:
+    @staticmethod
+    def return_value(error_code=DatarowError.NONE):
+        status = None
+        if error_code == DatarowError.NONE:
+            status = True
+        else:
+            status = False
+        return (status, error_code)
+
+    @staticmethod
+    def create(path):
+        format = path.split('.')[-1]
+        if format != 'json':
+            return None
+        gt = json.load(open(path, 'r'))
+        try:
+            images = gt['images']
+            annotations = gt['annotations']
+        except:
+            return None
+        return Annotation(gt)
+
+    def __init__(self, gt):
+        self.__gt = gt
+        self.__images = self.__gt['images']
+        self.__annotations = self.__gt['annotations']
+
+    def get(self, image_name):
+        id_list = [i['id'] for i in self.__images if i['file_name'] == image_name]
+        if id_list == []:
+            return None
+        image_id = id_list[0]
+        #print(image_id)
+
+        info = self.__annotations[image_id]
+        return info['bbox']
