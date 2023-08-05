f5a86d133abd948379067aebe63f5500c47c731d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:00 +0000

    Add package spatialgeometry/1.0.3/spatialgeometry-1.0.3-cp37-cp37m-win32.whl

---
+++ packages/spatialgeometry/spatialgeometry-1.0.3-cp37-cp37m-win32.whl/spatialgeometry/__init__.py
@@ -0,0 +1,20 @@
+from spatialgeometry.geom import *
+from spatialgeometry import tools
+
+
+__all__ = [
+    # aliased
+    "tools",
+    # geom
+    "Shape",
+    "CollisionShape",
+    "Mesh",
+    "Cylinder",
+    "Cuboid",
+    "Box",
+    "Sphere",
+    "Axes",
+    "Arrow",
+    "SceneNode",
+    "SceneGroup",
+]
