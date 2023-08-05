734c006412c2cfb564299f39ce9f736bf531ed9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:03 +0000

    Add package streamlit-pandas/0.0.7/streamlit_pandas-0.0.7.tar.gz

---
+++ packages/streamlit-pandas/streamlit_pandas-0.0.7.tar.gz/streamlit_pandas-0.0.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '0.0.7'
+DESCRIPTION = 'Create a Streamlit Pandas App'
+
+setup(
+    name="streamlit_pandas",
+    author="WJB Mattingly",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["pandas>=1.0.0,<2.0.0"],
+)
