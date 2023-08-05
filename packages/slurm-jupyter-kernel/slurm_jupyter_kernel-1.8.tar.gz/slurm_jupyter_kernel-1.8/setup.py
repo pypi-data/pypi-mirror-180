097e26775aa2bef223cef48d4c3201bdefe79b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:01 +0000

    Add package slurm-jupyter-kernel/1.8/slurm_jupyter_kernel-1.8.tar.gz

---
+++ packages/slurm-jupyter-kernel/slurm_jupyter_kernel-1.8.tar.gz/slurm_jupyter_kernel-1.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup;
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read();
+
+setup(
+    name='slurm_jupyter_kernel',
+    version='1.8',
+    description='Manage and start jupyter slurm kernels',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pc2/slurm_jupyter_kernel",
+    project_urls={
+        "Bug Tracker": "https://github.com/pc2/slurm_jupyter_kernel/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    author='Marcel-Brian Wilkowsky',
+    author_email='marcel.wilkowsky@uni-paderborn.de',
+	include_package_data=True,
+	packages=['slurm_jupyter_kernel'],
+    scripts=['bin/slurmkernel'],
+    install_requires=['pexpect>=4.8.0', 'ipython>=8.5.0', 'jupyter_client>=7.3.5']
+);
