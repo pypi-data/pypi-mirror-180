593abe13f11e3e0620242e14cd549b951a1d48cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:53 +0000

    Add package qualysetl/0.8.1/qualysetl-0.8.1-py3-none-any.whl

---
+++ packages/qualysetl/qualysetl-0.8.1-py3-none-any.whl/qualys_etl/etld_test_system/test_system_03_controller.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from qualys_etl.etld_lib import etld_lib_config
+from qualys_etl.etld_lib import etld_lib_credentials
+from qualys_etl.etld_lib import etld_lib_functions
+from qualys_etl.etld_lib import etld_lib_test_system
+
+
+def main():
+    etld_lib_test_system.main()
+
+
+if __name__ == "__main__":
+    etld_lib_functions.main(my_logger_prog_name='test_system_03_controller')
+    etld_lib_config.main()
+    etld_lib_credentials.main()
+    main()
