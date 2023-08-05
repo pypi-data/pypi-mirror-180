841c1a8b10b4296b057783105aa9987e2c72c5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:35 +0000

    Add package tracking-data/0.0.3/tracking_data-0.0.3-py3-none-any.whl

---
+++ packages/tracking-data/tracking_data-0.0.3-py3-none-any.whl/data/dataset.py
@@ -0,0 +1,51 @@
+import json
+from enum import Enum
+#from datarow.datarow import Datarow
+#import datarow.datarow
+from datarow import Datarow
+
+
+#from datarow.datarow import Datarow
+
+class DatasetError(Enum):
+    NONE = 0,
+    FILE_NOT_FOUND = 1
+
+class Dataset:
+    def __init__(self, storage):
+        self.storage = storage
+
+    def __str__(self):
+        return f'Video dataset with {self.size()} datarows.'
+
+    @staticmethod
+    def load(path):
+        try:
+            json_file = json.load(open(path, 'r'))
+        except:
+            return DatasetError.FILE_NOT_FOUND
+
+        storage = {}
+        for video_name in json_file:
+            storage[video_name] = Datarow.create(video_name, json_file[video_name]['dir'], json_file[video_name]['GT'])
+        return Dataset(storage)
+
+    def get(self, name):
+        if name in self.storage:
+            return self.storage[name]
+        else:
+            return None
+
+    def video_names(self):
+        return self.storage.keys()
+
+    def size(self):
+        return len(self.storage.keys())
+
+dataset = Dataset.load('../videos.json')
+#print(dataset.video_names())
+#print(dataset.get('igor1_1'))
+print(dataset)
+#print(type(dataset.get['igor1_1']))
+#datarow.Datarow()
+#a = Datarow.create("igor1", "/Users/rodion/Documents/trackers/python/sova_data/dataset/igor1", "/Users/rodion/Documents/trackers/python/sova_data/dataset/igor1.json")
