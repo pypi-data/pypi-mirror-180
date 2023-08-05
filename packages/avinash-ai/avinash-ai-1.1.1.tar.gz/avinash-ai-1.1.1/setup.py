3c1cb27ca4c1569f3a3f30b22b527f92e873b19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:31 +0000

    Add package avinash-ai/1.1.1/avinash-ai-1.1.1.tar.gz

---
+++ packages/avinash-ai/avinash-ai-1.1.1.tar.gz/avinash-ai-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="avinash-ai",
+    version="1.1.1",
+    description="Build your own ML",
+    packages=["avinash-ai"],
+    install_requires=["openai"],
+    zip_safe=False,
+    include_package_data=True
+)
+
