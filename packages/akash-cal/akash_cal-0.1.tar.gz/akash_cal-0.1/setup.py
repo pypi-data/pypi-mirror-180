4bca98bede93c1ef247c331ecc7ec5d7899221ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:33 +0000

    Add package akash-cal/0.1/akash_cal-0.1.tar.gz

---
+++ packages/akash-cal/akash_cal-0.1.tar.gz/akash_cal-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='akash_cal',
+      version='0.1',
+      description='contains many data science modules',
+      author='Akash Kumar',
+      packages=['akash_cal'],
+      zip_safe=False)
