68ef282b14da1483c7dac4ac10c665a71671a8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:02 +0000

    Add package thermopack/2.1.0/thermopack-2.1.0-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/thermopack/thermopack-2.1.0-py3-none-manylinux_2_28_x86_64.whl/thermopack/platform_specifics.py
@@ -0,0 +1,13 @@
+# Module for platform specific stuff. Automatically generated.
+# Timestamp : 2022-12-04T19:42:20.869450
+
+
+def get_platform_specifics():
+    pf_specifics = {}
+    pf_specifics["os_id"] = "linux"
+    pf_specifics["prefix"] = "__"
+    pf_specifics["module"] = "_MOD_"
+    pf_specifics["postfix"] = ""
+    pf_specifics["postfix_no_module"] = "_"
+    pf_specifics["dyn_lib"] = "libthermopack.so"
+    return pf_specifics
