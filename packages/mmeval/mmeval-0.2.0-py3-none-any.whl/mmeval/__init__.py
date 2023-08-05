25a280380ab9d196aebf4ff45b2f4703b82a22e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:54 +0000

    Add package mmeval/0.2.0/mmeval-0.2.0-py3-none-any.whl

---
+++ packages/mmeval/mmeval-0.2.0-py3-none-any.whl/mmeval/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
+# flake8: noqa
+
+from .core import *
+from .fileio import *
+from .metrics import *
+from .utils import *
+from .version import __version__
