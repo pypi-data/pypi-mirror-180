b17c42d71898c8e3dbc2093c70c0039855d206bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:05 +0000

    Add package csp-cli/1.2.0a3/csp_cli-1.2.0a3-py3-none-any.whl

---
+++ packages/csp-cli/csp_cli-1.2.0a3-py3-none-any.whl/csp/login/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+# @Time    : 2022/11/16 11:25
+# @Author  : xgy
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @python version: 3.7.13
+"""
+
+from csp.login.login_server import user_login as login
+from csp.login.login_server import check_user
+from csp.login.login_server import user_logout as logout
+from csp.login.login_server import re_login as relogin
+
+if __name__ == '__main__':
+    print("start")
