75670206182e412f38a285e3bbdcea097525fe17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:22 +0000

    Add package Lshengpackage/0.3.5/Lshengpackage-0.3.5-py3-none-any.whl

---
+++ packages/Lshengpackage/Lshengpackage-0.3.5-py3-none-any.whl/Lshengpackage/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+
+from Lshengpackage.Load import load,load_click
+from Lshengpackage.Delay import Delay
+from Lshengpackage.Chaojiying import Verification
+from Lshengpackage.simulate.adb.Command_adb import command
+from Lshengpackage.simulate.adb.Pac_adb import Mouse, Pic
+from Lshengpackage.simulate.dm.Pac_dm import mouse, color, pic, word
+from Lshengpackage.simulate.dm.Reg import RegDm
+from Lshengpackage.simulate.dm.Win_dm import windows
+from Lshengpackage.simulate.pc.find_pic import screen_shot, find_image
+from Lshengpackage.simulate.pc.find_word import fr
