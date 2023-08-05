2ebbd7765e1b4827691146b33db6d726990c4667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:27 +0000

    Add package dnastack-client-library/3.0.251/dnastack_client_library-3.0.251-py3-none-any.whl

---
+++ packages/dnastack-client-library/dnastack_client_library-3.0.251-py3-none-any.whl/dnastack/client/constants.py
@@ -0,0 +1,21 @@
+from typing import TypeVar
+
+from dnastack.client.base_client import BaseServiceClient
+from dnastack.client.collections.client import CollectionServiceClient
+from dnastack.client.data_connect import DataConnectClient
+from dnastack.client.drs import DrsClient
+from dnastack.client.service_registry.client import ServiceRegistry
+
+# All known client classes
+ALL_SERVICE_CLIENT_CLASSES = (CollectionServiceClient, DataConnectClient, DrsClient, ServiceRegistry)
+
+# All client classes for data access
+DATA_SERVICE_CLIENT_CLASSES = (CollectionServiceClient, DataConnectClient, DrsClient)
+
+# Type variable for the service client
+SERVICE_CLIENT_CLASS = TypeVar('SERVICE_CLIENT_CLASS',
+                               BaseServiceClient,
+                               CollectionServiceClient,
+                               DataConnectClient,
+                               DrsClient,
+                               ServiceRegistry)
---
+++ packages/dnastack-client-library/dnastack_client_library-3.0.251-py3-none-any.whl/dnastack/constants.py
@@ -0,0 +1,5 @@
+import os
+
+__version__ = "3.0.251"
+
+LOCAL_STORAGE_DIRECTORY = os.path.expanduser(os.path.join('~', '.dnastack'))
