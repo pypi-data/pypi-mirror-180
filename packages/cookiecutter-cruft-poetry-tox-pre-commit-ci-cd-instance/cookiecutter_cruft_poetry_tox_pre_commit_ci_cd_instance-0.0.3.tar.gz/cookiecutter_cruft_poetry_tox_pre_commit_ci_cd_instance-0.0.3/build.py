c06a153152e6b42f03e2655d62a5dc0b6f95b8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:17 +0000

    Add package cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/0.0.3/cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance-0.0.3.tar.gz

---
+++ packages/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance-0.0.3.tar.gz/cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance-0.0.3/build.py
@@ -0,0 +1,18 @@
+import pathlib
+
+import mypyc.build
+
+
+def build(setup_kwargs: dict) -> None:
+    """
+    This function is mandatory in order to build the extensions.
+    """
+    project_dir = pathlib.Path(__file__).resolve().parent
+    ext_modules = [
+        str(file)
+        for file in (
+            project_dir / "cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance"
+        ).rglob("*.py")
+    ]
+
+    setup_kwargs.update({"ext_modules": mypyc.build.mypycify(ext_modules)})
