0a03a54874be6dead740d532fd59c690eec116b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:36 +0000

    Add package modelscope/1.0.4/modelscope-1.0.4.tar.gz

---
+++ packages/modelscope/modelscope-1.0.4.tar.gz/modelscope-1.0.4/modelscope/version.py
@@ -0,0 +1,5 @@
+# Make sure to modify __release_datetime__ to release time when making official release.
+__version__ = '1.0.4'
+# default release datetime for branches under active development is set
+# to be a time far-far-away-into-the-future
+__release_datetime__ = '2022-11-27 21:58:00'
