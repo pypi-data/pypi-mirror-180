bf7c55c303dd5ee91303e1412f4751577f7f70f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:50 +0000

    Add package multibot-core/0.1.1/multibot_core-0.1.1.tar.gz

---
+++ packages/multibot-core/multibot_core-0.1.1.tar.gz/multibot_core-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['multibot_core']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'multibot-core',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'qvirus2',
+    'author_email': 'qvirus2@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
