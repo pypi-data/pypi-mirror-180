b12e8b86e8270940aa2a88369c2f8eb716b0f86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package ebilab/2.0.0b1/ebilab-2.0.0b1-py3-none-any.whl

---
+++ packages/ebilab/ebilab-2.0.0b1-py3-none-any.whl/ebilab/experiment/__init__.py
@@ -0,0 +1,9 @@
+from typing import List, Type
+
+from ._experiment_controller import IExperimentProtocol, ExperimentContext, ExperimentController, IExperimentPlotter, PlotterContext
+
+def launch_experiment(experiments: List[Type[IExperimentProtocol]]):
+    from ._ui_tkinter import ExperimentUITkinter
+    ui = ExperimentUITkinter()
+    app = ExperimentController(experiments=experiments, ui=ui)
+    app.launch()
