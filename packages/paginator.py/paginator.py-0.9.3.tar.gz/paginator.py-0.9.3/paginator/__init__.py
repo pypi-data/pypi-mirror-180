a87bbce68a588cb251f19ca7871b084abde3273e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:56 +0000

    Add package paginator.py/0.9.3/paginator.py-0.9.3.tar.gz

---
+++ packages/paginator.py/paginator.py-0.9.3.tar.gz/paginator.py-0.9.3/paginator/__init__.py
@@ -0,0 +1,10 @@
+from .paginator import *
+from .objects import *
+
+__title__ = 'paginator.py'
+__summary__ = 'Discord paginator for messages and embeds.'
+__uri__ = 'https://github.com/FlamptX/paginator.py'
+__author__ = 'Flampt'
+__version__ = '0.9.3'
+__license__ = 'MIT'
+__copyright__ = '2021-present Flampt.'
