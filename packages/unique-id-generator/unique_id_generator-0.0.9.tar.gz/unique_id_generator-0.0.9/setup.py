1656a87a07fc0e395701097e18079250aefa040b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:27 +0000

    Add package unique-id-generator/0.0.9/unique_id_generator-0.0.9.tar.gz

---
+++ packages/unique-id-generator/unique_id_generator-0.0.9.tar.gz/unique_id_generator-0.0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.9'
+DESCRIPTION = 'Unique ID Generator'
+LONG_DESCRIPTION = 'An implementation for System Design Interview books'
+
+setup(
+    name="unique_id_generator",
+    version=VERSION,
+    author="Hoang N. Truong",
+    author_email="hoang@hoang.tech",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    url="https://github.com/hoangperry/system-design-implementation/tree/master/unique-id-generator",
+    install_requires=[],
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+    ]
+)
