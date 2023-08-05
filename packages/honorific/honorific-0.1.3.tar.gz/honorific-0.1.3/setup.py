4846939c8e64cf0727df9f9db0b2f91476b201b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:47 +0000

    Add package honorific/0.1.3/honorific-0.1.3.tar.gz

---
+++ packages/honorific/honorific-0.1.3.tar.gz/honorific-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['honorific']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pymorphy2>=0.9.1,<0.10.0', 'spacy>=3.4.3,<4.0.0', 'twine>=4.0.2,<5.0.0']
+
+setup_kwargs = {
+    'name': 'honorific',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': '# honorific\n\nГоноратив',
+    'author': 'Viktor Merkurev',
+    'author_email': 'v.merkurev@promo-bot.ru',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
