cd3cf88f37dc14bac4d9501d84f2a3a8d0777154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:03 +0000

    Add package nsg-python-sdk/0.0.1/nsg-python-sdk-0.0.1.tar.gz

---
+++ packages/nsg-python-sdk/nsg-python-sdk-0.0.1.tar.gz/nsg-python-sdk-0.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='nsg-python-sdk',
+                 version='0.0.1',
+                 description='NetSpyGlass SDK',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 classifiers=[
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
+                     'Programming Language :: Python :: 3.9',
+                 ],
+                 url='https://github.com/happygears/nsg-python-sdk',
+                 author='Happy Gears, Inc',
+                 license='Apache',
+                 install_requires=[
+                     'ipaddr', 'protobuf==3.19.4', 'click', 'sortedcontainers'],
+                 py_modules=['nsg', 'ipaddr', 'serialization', 'tag_selector', 'view'],
+                 entry_points={
+                     'console_scripts': [
+                         'nsgsdk=bin.nsgsdk:process_request',  # command=package.module:function
+                         'nsgsdktest=bin.nsgsdktest:run_test'
+                     ],
+                 },
+                 packages=setuptools.find_packages(),
+                 python_requires='>=3.6')
