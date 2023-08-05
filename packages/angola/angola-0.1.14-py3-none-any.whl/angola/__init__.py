428d0eaf85307cb0eff8d096da189f636f085ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:26 +0000

    Add package angola/0.1.14/angola-0.1.14-py3-none-any.whl

---
+++ packages/angola/angola-0.1.14-py3-none-any.whl/angola/__init__.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+# -- Angola --
+# ------------------------------------------------------------------------------
+
+import database
+from .database import Database as db
+from arango.exceptions import ArangoError
+
+
