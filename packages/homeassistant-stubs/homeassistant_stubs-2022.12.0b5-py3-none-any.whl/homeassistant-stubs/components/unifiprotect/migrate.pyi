2a95a569c0074fb850fa5e12d6a2a7961306849b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:36 +0000

    Add package homeassistant-stubs/2022.12.0b5/homeassistant_stubs-2022.12.0b5-py3-none-any.whl

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2022.12.0b5-py3-none-any.whl/homeassistant-stubs/components/unifiprotect/migrate.pyi
@@ -0,0 +1,16 @@
+from .const import DOMAIN as DOMAIN
+from _typeshed import Incomplete
+from homeassistant.config_entries import ConfigEntry as ConfigEntry
+from homeassistant.const import Platform as Platform
+from homeassistant.core import HomeAssistant as HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady as ConfigEntryNotReady
+from homeassistant.helpers.issue_registry import IssueSeverity as IssueSeverity
+from pyunifiprotect import ProtectApiClient as ProtectApiClient
+from pyunifiprotect.data import Bootstrap as Bootstrap, NVR as NVR, ProtectAdoptableDeviceModel as ProtectAdoptableDeviceModel
+
+_LOGGER: Incomplete
+
+async def async_migrate_data(hass: HomeAssistant, entry: ConfigEntry, protect: ProtectApiClient) -> None: ...
+async def async_get_bootstrap(protect: ProtectApiClient) -> Bootstrap: ...
+async def async_migrate_buttons(hass: HomeAssistant, entry: ConfigEntry, protect: ProtectApiClient) -> None: ...
+async def async_migrate_device_ids(hass: HomeAssistant, entry: ConfigEntry, protect: ProtectApiClient) -> None: ...
