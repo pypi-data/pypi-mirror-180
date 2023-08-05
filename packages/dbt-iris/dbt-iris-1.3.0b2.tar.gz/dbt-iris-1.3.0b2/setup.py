2f7f0ce8d9bcc8c7c244b42b479d46a7ab9b4eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:13 +0000

    Add package dbt-iris/1.3.0b2/dbt-iris-1.3.0b2.tar.gz

---
+++ packages/dbt-iris/dbt-iris-1.3.0b2.tar.gz/dbt-iris-1.3.0b2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import find_namespace_packages, setup
+
+package_name = "dbt-iris"
+# make sure this always matches dbt/adapters/{adapter}/__version__.py
+package_version = "1.3.0b2"
+description = """The InterSystems IRIS adapter plugin for dbt"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    license='MIT',
+    author="CaretDev",
+    author_email="info@caretdev.com",
+    url="https://github.com/caretdev/dbt-iris",
+    packages=find_namespace_packages(include=["dbt", "dbt.*"]),
+    include_package_data=True,
+    install_requires=[
+        "dbt-core~=1.3.0",
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: MIT License',
+        
+        'Operating System :: OS Independent',
+
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires=">3.7,<3.11",
+)
