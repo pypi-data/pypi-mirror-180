db2f4e45edc44d31bf519be4d708b815047edaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:17 +0000

    Add package oct2py/5.6.0/oct2py-5.6.0-py3-none-any.whl

---
+++ packages/oct2py/oct2py-5.6.0-py3-none-any.whl/oct2py/_version.py
@@ -0,0 +1,21 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import re
+from collections import namedtuple
+from typing import List
+
+VersionInfo = namedtuple("VersionInfo", ["major", "minor", "micro", "releaselevel", "serial"])
+
+# Version string must appear intact for hatch versioning
+__version__ = "5.6.0"
+
+# Build up version_info tuple for backwards compatibility
+pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<micro>\d+)(?P<releaselevel>.*?)(?P<serial>\d*)"
+match = re.match(pattern, __version__)
+assert match is not None
+parts: List[object] = [int(match[part]) for part in ["major", "minor", "micro"]]
+parts.append(match["releaselevel"] or "")
+parts.append(match["serial"] or "")
+
+version_info = VersionInfo(*parts)
---
+++ packages/oct2py/oct2py-5.6.0-py3-none-any.whl/oct2py/utils.py
@@ -0,0 +1,48 @@
+# Copyright (c) oct2py developers.
+# Distributed under the terms of the MIT License.
+
+import logging
+import sys
+
+
+class Oct2PyError(Exception):
+    """Called when we can't open Octave or Octave throws an error"""
+
+    pass
+
+
+def get_log(name=None):
+    """Return a console logger.
+
+    Output may be sent to the logger using the `debug`, `info`, `warning`,
+    `error` and `critical` methods.
+
+    Parameters
+    ----------
+    name : str
+        Name of the log.
+    """
+    if name is None:
+        name = "oct2py"
+    else:
+        name = "oct2py." + name
+
+    log = logging.getLogger(name)
+    log.setLevel(logging.INFO)
+    return log
+
+
+def _setup_log():
+    """Configure root logger."""
+    try:
+        handler = logging.StreamHandler(stream=sys.stdout)
+    except TypeError:  # pragma: no cover
+        handler = logging.StreamHandler(strm=sys.stdout)
+
+    log = get_log()
+    log.addHandler(handler)
+    log.setLevel(logging.INFO)
+    log.propagate = False
+
+
+_setup_log()
