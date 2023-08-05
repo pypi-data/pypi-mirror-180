44cb7b86a0cf3ce7a119e7a7847ac8619e7ba100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:54 +0000

    Add package accim/0.4.0/accim-0.4.0.tar.gz

---
+++ packages/accim/accim-0.4.0.tar.gz/accim-0.4.0/accim/sample_files/script_usage/accis_01.py
@@ -0,0 +1,13 @@
+from accim.sim import accis
+accis.addAccis(
+    ScriptType='ex_mm',
+    Outputs='standard',
+    EnergyPlus_version='9.5',
+    TempCtrl='temp',
+    ComfStand=[1],
+    CAT=[1, 2, 3],
+    ComfMod=[3],
+    HVACmode=[2],
+    VentCtrl=[0],
+    confirmGen=True
+)
---
+++ packages/accim/accim-0.4.0.tar.gz/accim-0.4.0/accim/sample_files/script_usage/accis_04.py
@@ -0,0 +1,29 @@
+"""Example of script usage with arguments."""
+
+from accim.sim import accis
+
+scriptTypeList = ['vrf_mm']
+outputsList = ['simplified', 'standard', 'timestep']
+EPlist = ['22.2']
+
+for i in scriptTypeList:
+    for j in outputsList:
+        for k in EPlist:
+            accis.addAccis(
+                ScriptType=i,
+                Outputs=j,
+                EnergyPlus_version=k,
+                TempCtrl='temp',
+                ComfStand=[1],
+                CAT=[1],
+                ComfMod=[1],
+                HVACmode=[2],
+                VentCtrl=[0],
+                VSToffset=[0],
+                MinOToffset=[50],
+                MaxWindSpeed=[50],
+                ASTtol_start=0.1,
+                ASTtol_end_input=0.1,
+                ASTtol_steps=0.1,
+                NameSuffix=j
+            )
