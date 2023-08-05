d33f94e61b6a0512d89a7d73bf49d0d317bea21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:58 +0000

    Add package accim/0.4.1/accim-0.4.1.tar.gz

---
+++ packages/accim/accim-0.4.1.tar.gz/accim-0.4.1/accim/sample_files/script_usage/accis_02.py
@@ -0,0 +1,9 @@
+from accim.sim import accis
+accis.addAccis(ScriptType='ex_ac',
+               Outputs='standard',
+               EnergyPlus_version='9.5',
+               TempCtrl='temp',
+               ComfStand=[1],
+               CAT=[2],
+               ComfMod=[3],
+               confirmGen=True)
