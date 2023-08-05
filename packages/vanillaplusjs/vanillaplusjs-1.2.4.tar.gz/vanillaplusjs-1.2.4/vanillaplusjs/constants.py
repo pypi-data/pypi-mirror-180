760e3a69077eaf2c4cc129ab9cf6ef353a769c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:40 +0000

    Add package vanillaplusjs/1.2.4/vanillaplusjs-1.2.4.tar.gz

---
+++ packages/vanillaplusjs/vanillaplusjs-1.2.4.tar.gz/vanillaplusjs-1.2.4/vanillaplusjs/constants.py
@@ -0,0 +1,7 @@
+CONFIGURATION_VERSION = "1.0.0"
+"""The configuration version for vanillaplusjs.json, so that we can
+automatically migrate old configuration files.
+"""
+
+PROCESSOR_VERSION = "1.2.4"
+"""The version of vanillaplusjs we're using"""
