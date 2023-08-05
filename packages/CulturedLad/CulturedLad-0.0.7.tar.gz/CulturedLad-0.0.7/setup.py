22cf76b2ccf4493666c49d9574d0a288220d3210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:32 +0000

    Add package CulturedLad/0.0.7/CulturedLad-0.0.7.tar.gz

---
+++ packages/CulturedLad/CulturedLad-0.0.7.tar.gz/CulturedLad-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CulturedLad",
+    version="0.0.7",
+    author="CulturedLad",
+    author_email="623178111@qq.com",
+
+    description="This is my module. Refer to README.md for its functions",
+
+    # 项目主页
+    url="https://gitee.com/zhangxiao999",
+
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+)
