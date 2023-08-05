d6ab23f863b21496380e47c9f3299aa4b9d373aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:12 +0000

    Add package yellowdog-python-examples/4.0.7/yellowdog_python_examples-4.0.7-py3-none-any.whl

---
+++ packages/yellowdog-python-examples/yellowdog_python_examples-4.0.7-py3-none-any.whl/yd_commands/version.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Report version numbers
+"""
+
+from sys import executable
+from sys import version as python_version
+
+from yellowdog_client._version import __version__ as yd_sdk_version
+
+from yd_commands.__init__ import __version__
+
+
+def main():
+    print(f" YellowDog Python Examples Version:   {__version__}")
+    print(f" YellowDog SDK Version:               {yd_sdk_version}")
+    print(
+        f" Python Version:                      {python_version.split()[0]} "
+        f"({executable})"
+    )
+
+
+if __name__ == "__main__":
+    main()
