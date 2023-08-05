48cafbce0b2f726134a375766e634f2e30d9f4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:57 +0000

    Add package thermopack/2.1.0/thermopack-2.1.0-py3-none-macosx_11_0_arm64.whl

---
+++ packages/thermopack/thermopack-2.1.0-py3-none-macosx_11_0_arm64.whl/pyctp/platform_specifics.py
@@ -0,0 +1,13 @@
+# Module for platform specific stuff. Automatically generated.
+# Timestamp : 2022-11-09T13:49:51.680520
+
+
+def get_platform_specifics():
+    pf_specifics = {}
+    pf_specifics["os_id"] = "darwin"
+    pf_specifics["prefix"] = "__"
+    pf_specifics["module"] = "_MOD_"
+    pf_specifics["postfix"] = ""
+    pf_specifics["postfix_no_module"] = "_"
+    pf_specifics["dyn_lib"] = "libthermopack.dynlib"
+    return pf_specifics
---
+++ packages/thermopack/thermopack-2.1.0-py3-none-macosx_11_0_arm64.whl/thermopack/platform_specifics.py
@@ -0,0 +1,13 @@
+# Module for platform specific stuff. Automatically generated.
+# Timestamp : 2022-12-06T08:44:58.881194
+
+
+def get_platform_specifics():
+    pf_specifics = {}
+    pf_specifics["os_id"] = "darwin"
+    pf_specifics["prefix"] = "__"
+    pf_specifics["module"] = "_MOD_"
+    pf_specifics["postfix"] = ""
+    pf_specifics["postfix_no_module"] = "_"
+    pf_specifics["dyn_lib"] = "libthermopack.dynlib"
+    return pf_specifics
