24031637e573234375a1a629733d5fafa571925d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:06 +0000

    Add package moabdb/0.1.1/moabdb-0.1.1.tar.gz

---
+++ packages/moabdb/moabdb-0.1.1.tar.gz/moabdb-0.1.1/moabdb/globals.py
@@ -0,0 +1,31 @@
+# MoabDB
+
+_api_key = ""
+_api_username = ""
+_dx_url = "https://api.moabdb.com/"
+
+
+def login(username: str, key: str):
+    """
+    Logs in to the API to provide data that is not publically available
+    :param key: The API key to use
+    :return: None
+    """
+
+    global _api_key
+    global _api_username
+
+    _api_key = key
+    _api_username = username
+
+
+def set_dx_url(url: str):
+    """
+    Sets the URL for the MoabDB API
+    Only useful for testing
+    :param url: The URL to use
+    :return: None
+    """
+
+    global _dx_url
+    _dx_url = url
