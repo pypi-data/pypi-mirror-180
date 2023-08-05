e554e7de1414f42a67f115001974d52937d0f6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:51 +0000

    Add package HalRing/0.0.1/HalRing-0.0.1.tar.gz

---
+++ packages/HalRing/HalRing-0.0.1.tar.gz/HalRing-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+    name="HalRing",
+    version="0.0.1",
+    #url='https://www.python.org',
+    author="peixiaodong",
+    author_email="pxd7th@vip.qq.com",
+    packages=find_packages(exclude=["unittest"]),
+
+)
