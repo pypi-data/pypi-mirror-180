a780b91963bb1f77639c018aea8d5159e061325e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:17 +0000

    Add package nonebot-plugin-gpt/0.1.5/nonebot_plugin_gpt-0.1.5.tar.gz

---
+++ packages/nonebot-plugin-gpt/nonebot_plugin_gpt-0.1.5.tar.gz/nonebot_plugin_gpt-0.1.5/nonebot_plugin_gpt/config.py
@@ -0,0 +1,13 @@
+from nonebot import get_driver
+from pydantic import BaseModel, Extra
+from typing import Optional
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    gpt_session_token: Optional[str]
+    gpt_api_key: Optional[str]
+    gpt_sudoers: list[int]
+    gpt_probability: float = 0
+
+
+gpt_config: Config = Config.parse_obj(get_driver().config)
