87896089a5f33227a61449c059a1652b65bc8c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:08 +0000

    Add package sh2d/0.5/sh2d-0.5.tar.gz

---
+++ packages/sh2d/sh2d-0.5.tar.gz/sh2d-0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r",encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sh2d",
+    version="0.5",
+    author="sh2d",
+    author_email="xx@qq.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    install_requires=['requests','dnspython','paramiko','pymysql','xlrd==1.2.0','xlsxwriter','zmail','xlwt','openpyxl','python-docx','docxtpl','pycryptodome','ddddocr'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
