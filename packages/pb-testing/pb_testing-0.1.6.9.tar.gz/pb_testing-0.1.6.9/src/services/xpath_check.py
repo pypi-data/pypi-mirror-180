0fdd712b02f0c381c75166dbffc4c21b96da01bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:43 +0000

    Add package pb-testing/0.1.6.9/pb_testing-0.1.6.9.tar.gz

---
+++ packages/pb-testing/pb_testing-0.1.6.9.tar.gz/pb_testing-0.1.6.9/src/services/xpath_check.py
@@ -0,0 +1,19 @@
+import lxml.html as PARSER
+
+
+def get_number_of_match(html, xpath):
+    print(html)
+    print(xpath)
+    if html is not None or html != '':
+        root = PARSER.fromstring(html)
+
+        try:
+            exists = root.xpath(xpath)
+        except Exception as e:
+            print(type(e).__name__)
+            return None, None
+
+        if exists:
+            return exists, len(exists)
+        else:
+            return 0, 0
