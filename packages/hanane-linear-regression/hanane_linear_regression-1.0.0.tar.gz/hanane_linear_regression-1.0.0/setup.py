d49b723219e13f56ac65ddc4e2f95b8792960267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:49 +0000

    Add package hanane-linear-regression/1.0.0/hanane_linear_regression-1.0.0.tar.gz

---
+++ packages/hanane-linear-regression/hanane_linear_regression-1.0.0.tar.gz/hanane_linear_regression-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hanane_linear_regression',
+    version='1.0.0',
+    author='Hanane SABOUNI',
+    author_email='h.sabouni@esi-sba.dz',
+    packages=['hanane_linear_regression'],
+    url='http://pypi.python.org/pypi/hanane_linear_regression/',
+    license='LICENSE.txt',
+    description='An awesome package that implements a Simple Linear Regression Model.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[]
+)
