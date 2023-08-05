33b899c1fe10d953b3ea6ef7b7a1153c1e717bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:47 +0000

    Add package image-depot/0.9.3/image_depot-0.9.3-py3-none-any.whl

---
+++ packages/image-depot/image_depot-0.9.3-py3-none-any.whl/image_depot/depot/riyugo.py
@@ -0,0 +1,27 @@
+import random
+from typing import Optional
+import requests
+from image_depot import DepotType
+from .base import Depot
+
+
+class Riyugo(Depot):
+    @classmethod
+    def depot_type(cls) -> DepotType:
+        return DepotType.Riyugo
+
+    def _upload(self, content) -> Optional[str]:
+        data = {
+            'uuid': 'o_' + self._random_str(27),
+            'nameMode': 'isRenameMode',
+        }
+        files = {
+            'file': content,
+        }
+        response = requests.post('https://4ae.cn/localup.php', data=data, files=files)
+        if response.status_code != 200:
+            return self._set_error(f'upload fail. code: {response.status_code}. content: {response.text}')
+        data = response.json()
+        if data.get('result') != 'success' or not data.get('url'):
+            return self._set_error(response.text)
+        return data.get('url')
