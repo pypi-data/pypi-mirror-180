d376cf1c1a59b868c3d037daca2e749d055d5b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:51 +0000

    Add package mathgroup11/0.0.2/mathgroup11-0.0.2.tar.gz

---
+++ packages/mathgroup11/mathgroup11-0.0.2.tar.gz/mathgroup11-0.0.2/mathgroup11/__init__.py
@@ -0,0 +1,13 @@
+def connectArray(list1, list2): 
+    return list1 + list2
+
+def calculate_mean(n):
+    s = sum(n)
+    N = len(n)
+    
+    mean = s / N
+    
+    return mean
+
+def add(a, b): 
+    return a + b
