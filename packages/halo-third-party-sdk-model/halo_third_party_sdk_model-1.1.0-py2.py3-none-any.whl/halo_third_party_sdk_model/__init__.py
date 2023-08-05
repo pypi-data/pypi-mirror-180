ec6377863aac148f6eed9ca705c1b7971ab69aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:37 +0000

    Add package halo-third-party-sdk-model/1.1.0/halo_third_party_sdk_model-1.1.0-py2.py3-none-any.whl

---
+++ packages/halo-third-party-sdk-model/halo_third_party_sdk_model-1.1.0-py2.py3-none-any.whl/halo_third_party_sdk_model/__init__.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+#
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License'). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+#
+from __future__ import absolute_import
+
+from .calorie_info import CalorieInfo
+from .sleep_duration import SleepDuration
+from .deauthorization import Deauthorization
+from .activity_session import ActivitySession
+from .activity_summary_duration import ActivitySummaryDuration
+from .sleep_score import SleepScore
+from .body import Body
+from .measurement import Measurement
+from .activity_session_duration import ActivitySessionDuration
+from .fat_percentage import FatPercentage
+from .challenge import Challenge
+from .data import Data
+from .sleep import Sleep
+from .request_envelope import RequestEnvelope
+from .heart_rate import HeartRate
+from .step_info import StepInfo
+from .record import Record
+from .activity_summary import ActivitySummary
+from .activity import Activity
+from .count import Count
