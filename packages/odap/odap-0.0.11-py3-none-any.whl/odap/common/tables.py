4515a8878ac2ad2439462bd0b827055e8caaef54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:29 +0000

    Add package odap/0.0.11/odap-0.0.11-py3-none-any.whl

---
+++ packages/odap/odap-0.0.11-py3-none-any.whl/odap/common/tables.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from pyspark.sql import SparkSession, DataFrame
+
+
+def hive_table_exists(spark: SparkSession, full_table_name: str) -> bool:
+    db_name = full_table_name.split(".")[0]
+    table_name = full_table_name.split(".")[1]
+    databases = [db.databaseName for db in spark.sql("SHOW DATABASES").collect()]
+
+    if db_name not in databases:
+        return False
+
+    return spark.sql(f'SHOW TABLES IN {db_name} LIKE "{table_name}"').collect() != []
+
+
+def get_existing_table(table_name: str) -> Optional[DataFrame]:
+    spark = SparkSession.getActiveSession()  # pylint: disable=W0641
+
+    if hive_table_exists(spark, table_name):
+        return spark.read.table(table_name)
+
+    return None
