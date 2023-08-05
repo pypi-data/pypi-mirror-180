6b8ff20ada175856df96fcf8182b96e21121c108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:57 +0000

    Add package devantech-relays/0.1.3/devantech_relays-0.1.3.tar.gz

---
+++ packages/devantech-relays/devantech_relays-0.1.3.tar.gz/devantech_relays-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['devantech_relays']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'devantech-relays',
+    'version': '0.1.3',
+    'description': "Tool for controlling Devantech's ETH-series of relays",
+    'long_description': None,
+    'author': 'Karl Fredrik Haugland',
+    'author_email': 'kfh@tla.wtf',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
