22aff6bf556d0ee0aedbdbe57c80d8c4747f22c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:33 +0000

    Add package dezmq/0.1.0a1/dezmq-0.1.0a1-py3-none-any.whl

---
+++ packages/dezmq/dezmq-0.1.0a1-py3-none-any.whl/dezmq/__init__.py
@@ -0,0 +1,5 @@
+"""Init."""
+__version__ = "0.1.0a1"
+from .dezmq import dezmq
+
+__all__ = ("dezmq",)
---
+++ packages/dezmq/dezmq-0.1.0a1-py3-none-any.whl/dezmq/dezmq.py
@@ -0,0 +1,7 @@
+"""Define dezmq."""
+from logzero import logger
+
+
+def dezmq():
+    """Serve aligned results for ezbee."""
+    logger.debug(" entry ")
