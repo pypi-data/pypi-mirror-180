96d774bad684f18df50ddcd22cba55c0fc5b8ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:44 +0000

    Add package paradigm/2.0.0/paradigm-2.0.0.tar.gz

---
+++ packages/paradigm/paradigm-2.0.0.tar.gz/paradigm-2.0.0/paradigm/_core/arboreal/execution.py
@@ -0,0 +1,22 @@
+import ast
+from pathlib import Path
+
+from paradigm._core import namespacing
+from . import construction
+
+
+def execute_statement(node: ast.stmt,
+                      *,
+                      source_path: Path,
+                      namespace: namespacing.Namespace) -> None:
+    _execute_tree(construction.from_node(node),
+                  source_path=source_path,
+                  namespace=namespace)
+
+
+def _execute_tree(node: ast.Module,
+                  *,
+                  source_path: Path,
+                  namespace: namespacing.Namespace) -> None:
+    code = compile(node, str(source_path), 'exec')
+    exec(code, namespace)
