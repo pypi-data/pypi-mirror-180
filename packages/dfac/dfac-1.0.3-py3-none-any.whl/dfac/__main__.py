4c6f1a840d1d23e3543b7e2b54f65225ed6b6efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:57 +0000

    Add package dfac/1.0.3/dfac-1.0.3-py3-none-any.whl

---
+++ packages/dfac/dfac-1.0.3-py3-none-any.whl/dfac/__main__.py
@@ -0,0 +1,20 @@
+'''
+Data Factory Entry Point
+
+Input data from shell
+
+
+'''
+import sys
+from dfac.app.Help import Help
+from dfac.app.Shell import Shell
+
+if __name__ == '__main__':
+    sysinput =sys.argv[1:]
+
+    if len(sysinput) == 0:
+        Help().noValue()
+
+    if len(sysinput) > 0:
+        shellcall=Shell()
+        shellcall.parse(sysinput)
