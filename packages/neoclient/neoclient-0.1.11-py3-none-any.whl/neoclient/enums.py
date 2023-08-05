6a3d927921cfc922a46de28f28933dc1d2020b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package neoclient/0.1.11/neoclient-0.1.11-py3-none-any.whl

---
+++ packages/neoclient/neoclient-0.1.11-py3-none-any.whl/neoclient/enums.py
@@ -0,0 +1,39 @@
+from enum import Enum, auto
+from typing import Sequence
+
+__all__: Sequence[str] = (
+    "HttpMethod",
+    "MethodKind",
+    "HeaderName",
+)
+
+
+class HiddenValueEnum(Enum):
+    def __repr__(self) -> str:
+        return f"<{type(self).__name__}.{self.name}>"
+
+
+class StrEnum(str, Enum):
+    def __str__(self) -> str:
+        return self.value
+
+
+class HttpMethod(HiddenValueEnum, StrEnum):
+    PUT = "PUT"
+    GET = "GET"
+    POST = "POST"
+    HEAD = "HEAD"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+
+
+class MethodKind(HiddenValueEnum):
+    METHOD = auto()
+    CLASS_METHOD = auto()
+    STATIC_METHOD = auto()
+
+
+class HeaderName(HiddenValueEnum, StrEnum):
+    USER_AGENT = "User-Agent"
+    CONTENT_TYPE = "Content-Type"
