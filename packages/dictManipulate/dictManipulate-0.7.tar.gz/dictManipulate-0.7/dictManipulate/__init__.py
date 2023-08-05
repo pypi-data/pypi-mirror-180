8e5db94da9f5e1a8b0422c6df4a9353ea0c68d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:30 +0000

    Add package dictManipulate/0.7/dictManipulate-0.7.tar.gz

---
+++ packages/dictManipulate/dictManipulate-0.7.tar.gz/dictManipulate-0.7/dictManipulate/__init__.py
@@ -0,0 +1,1 @@
+from .dictManipulate import DictManipulate
---
+++ packages/dictManipulate/dictManipulate-0.7.tar.gz/dictManipulate-0.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+name = "dictManipulate",
+version = 0.7,
+description = "data_manipulate",
+packages=['dictManipulate'],
+zip_safe =False
+)
