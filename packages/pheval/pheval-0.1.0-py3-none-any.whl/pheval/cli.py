b4cbfd7f096c6f56dfbf6c884dec9c147c5970dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:46:29 +0000

    Add package pheval/0.1.0/pheval-0.1.0-py3-none-any.whl

---
+++ packages/pheval/pheval-0.1.0-py3-none-any.whl/pheval/cli.py
@@ -0,0 +1,48 @@
+"""PhEval CLI Module """
+import logging
+
+import click
+
+from .cli_pheval import run
+from .cli_pheval_utils import scramble_phenopacket, scramble_semsim
+
+info_log = logging.getLogger("info")
+
+
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.option("-q", "--quiet")
+def main(verbose=1, quiet=False) -> None:
+    """main CLI method for pheval
+
+    Args:
+        verbose (int): _description_
+        quiet (bool): _description_
+    """
+    if verbose >= 2:
+        info_log.setLevel(level=logging.DEBUG)
+    elif verbose == 1:
+        info_log.setLevel(level=logging.INFO)
+    else:
+        info_log.setLevel(level=logging.WARNING)
+    if quiet:
+        info_log.setLevel(level=logging.ERROR)
+
+
+@click.group()
+def pheval():
+    """pheval"""
+
+
+@click.group()
+def pheval_utils():
+    """pheval_utils"""
+
+
+pheval.add_command(run)
+
+pheval_utils.add_command(scramble_semsim)
+pheval_utils.add_command(scramble_phenopacket)
+
+if __name__ == "__main__":
+    main()
