c8c06b0e746ff34f8062e4edce777335fc7f1467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:53 +0000

    Add package grai-cli/0.1.7/grai_cli-0.1.7-py3-none-any.whl

---
+++ packages/grai-cli/grai_cli-0.1.7-py3-none-any.whl/grai_cli/api/config/config.py
@@ -0,0 +1,57 @@
+import typer
+from grai_cli.api.config.setup import config_app
+from grai_cli.settings.config import config
+from grai_cli.utilities import utilities
+from grai_cli.utilities.styling import GraiColors, default_styler, strip_style
+from grai_cli.utilities.validators import (
+    host_callback,
+    password_callback,
+    port_callback,
+    username_callback,
+)
+
+
+@config_app.command("init")
+@utilities.writes_config
+def cli_init_config(
+    username: str = typer.Option(
+        ..., prompt=True, callback=username_callback, prompt_required=True
+    ),
+    password: str = typer.Option(
+        ...,
+        prompt=True,
+        prompt_required=True,
+        hide_input=True,
+        confirmation_prompt=True,
+        callback=strip_style(password_callback),
+    ),
+    host: str = typer.Option(
+        default=default_styler(config["server"]["host"].get(str)),
+        prompt="Server host",
+        prompt_required=True,
+        callback=strip_style(host_callback),
+    ),
+    port: str = typer.Option(
+        default=default_styler(config["server"]["port"].get(str)),
+        prompt="Server port",
+        prompt_required=True,
+        callback=strip_style(port_callback),
+    ),
+    # config_location: str = typer.Option(
+    #     default=default_styler(config.config_filename),
+    #     prompt="Config path",
+    #     prompt_required=True,
+    #     callback=strip_style(lambda x: x),
+    # ),
+):
+    """Initialize a new config file"""
+    config["auth"]["username"].set(username)
+    config["auth"]["password"].set(password)
+    config["server"]["host"].set(host)
+    config["server"]["port"].set(port)
+
+
+@config_app.command(help="Print config to console")
+def view():
+    """Initialize a new config file"""
+    utilities.print(config.view())
---
+++ packages/grai-cli/grai_cli-0.1.7-py3-none-any.whl/grai_cli/api/config/set.py
@@ -0,0 +1,95 @@
+from functools import wraps
+from typing import Callable
+
+import typer
+from grai_cli.api.config.setup import set_app
+from grai_cli.utilities.utilities import get_config_view, writes_config
+from grai_cli.utilities.validators import (
+    host_callback,
+    password_callback,
+    port_callback,
+    username_callback,
+)
+
+
+def setter_helper(path: str) -> Callable:
+    name = path.split(".")[-1]
+
+    def set_function_maker(fn: Callable) -> Callable:
+        @set_app.command(name)
+        @writes_config
+        @wraps(fn)
+        def set_function(*args, **kwargs):
+            view = get_config_view(path)
+            view.set(kwargs.pop(name))
+
+        return set_function
+
+    return set_function_maker
+
+
+@setter_helper("auth.username")
+def _(username: str = typer.Argument(..., callback=username_callback)):
+    """Sets config value for auth.username"""
+    pass
+
+
+@setter_helper("auth.password")
+def _(
+    password: str = typer.Option(
+        ...,
+        hide_input=True,
+        callback=password_callback,
+        prompt=True,
+        prompt_required=True,
+        confirmation_prompt=True,
+    )
+):
+    """Sets config value for auth.password"""
+    pass
+
+
+@setter_helper("auth.api_key")
+def _(
+    api_key: str = typer.Option(
+        ...,
+        hide_input=True,
+        prompt=True,
+        prompt_required=True,
+        confirmation_prompt=True,
+    )
+):
+    """Sets config value for auth.api_key"""
+    pass
+
+
+@setter_helper("auth.token")
+def _(
+    token: str = typer.Option(
+        ...,
+        hide_input=True,
+        prompt=True,
+        prompt_required=True,
+        confirmation_prompt=True,
+    )
+):
+    """Sets config value for auth.token"""
+    pass
+
+
+@setter_helper("server.host")
+def _(host: str = typer.Argument(..., callback=host_callback)):
+    """Sets config value for server.host"""
+    pass
+
+
+@setter_helper("server.port")
+def _(port: str = typer.Argument(..., callback=port_callback)):
+    """Sets config value for server.port"""
+    pass
+
+
+@setter_helper("context.namespace")
+def _(namespace: str = typer.Argument(...)):
+    """Sets config value for context.namespace"""
+    pass
