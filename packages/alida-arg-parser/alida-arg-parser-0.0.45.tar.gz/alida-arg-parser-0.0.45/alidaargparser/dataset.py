7e8db94ba8ef9691040004f2fe394fd055d7a7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:20 +0000

    Add package alida-arg-parser/0.0.45/alida-arg-parser-0.0.45.tar.gz

---
+++ packages/alida-arg-parser/alida-arg-parser-0.0.45.tar.gz/alida-arg-parser-0.0.45/alidaargparser/dataset.py
@@ -0,0 +1,8 @@
+
+
+
+class Dataset:
+    def __init__(self, name=None, description=None, columns_type=None):
+        self.name = name
+        self.description = description
+        self.columns_type = columns_type
