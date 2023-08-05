cb75b43d3a4eb8ebdcc007543c63ee70d0b58b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:51 +0000

    Add package feris-first-pypi-package/0.1/feris-first-pypi-package-0.1.tar.gz

---
+++ packages/feris-first-pypi-package/feris-first-pypi-package-0.1.tar.gz/feris-first-pypi-package-0.1/README.md
@@ -0,0 +1,3 @@
+# Feris First PyPI Package
+
+this package was created based on a Udacity Course.
---
+++ packages/feris-first-pypi-package/feris-first-pypi-package-0.1.tar.gz/feris-first-pypi-package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='feris-first-pypi-package',
+      version='0.1',
+      description='Udacity Gaussian distributions',
+      packages=['feris-first-pypi-package'],
+      zip_safe=False)
