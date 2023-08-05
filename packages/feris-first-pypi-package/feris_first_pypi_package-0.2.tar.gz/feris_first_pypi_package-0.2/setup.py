660d9a483f1b6cc1bed322054fa450936b5f4b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:29 +0000

    Add package feris-first-pypi-package/0.2/feris_first_pypi_package-0.2.tar.gz

---
+++ packages/feris-first-pypi-package/feris_first_pypi_package-0.2.tar.gz/feris_first_pypi_package-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='feris_first_pypi_package',
+      version='0.2',
+      description='Udacity Gaussian distributions',
+      packages=['feris_first_pypi_package'],
+      zip_safe=False)
