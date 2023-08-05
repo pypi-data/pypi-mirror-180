ab7b824654a1dd199b51ab466fb3b354ecda28a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:53 +0000

    Add package folio-migration-tools/1.6.4/folio_migration_tools-1.6.4.tar.gz

---
+++ packages/folio-migration-tools/folio_migration_tools-1.6.4.tar.gz/folio_migration_tools-1.6.4/src/folio_migration_tools/marc_rules_transformation/rules_mapper_holdings.py
@@ -0,0 +1,266 @@
+import logging
+
+from folio_uuid.folio_namespaces import FOLIONamespaces
+from folio_uuid.folio_uuid import FolioUUID
+from pymarc.field import Field
+from pymarc.record import Record
+
+from folio_migration_tools.custom_exceptions import TransformationFieldMappingError
+from folio_migration_tools.custom_exceptions import TransformationProcessError
+from folio_migration_tools.custom_exceptions import TransformationRecordFailedError
+from folio_migration_tools.helper import Helper
+from folio_migration_tools.library_configuration import LibraryConfiguration
+from folio_migration_tools.marc_rules_transformation.conditions import Conditions
+from folio_migration_tools.marc_rules_transformation.holdings_statementsparser import (
+    HoldingsStatementsParser,
+)
+from folio_migration_tools.marc_rules_transformation.rules_mapper_base import (
+    RulesMapperBase,
+)
+from folio_migration_tools.report_blurbs import Blurbs
+
+
+class RulesMapperHoldings(RulesMapperBase):
+    def __init__(
+        self,
+        folio,
+        instance_id_map,
+        location_map,
+        task_configuration,
+        library_configuration: LibraryConfiguration,
+    ):
+        self.instance_id_map = instance_id_map
+        self.task_configuration = task_configuration
+        self.conditions = Conditions(
+            folio,
+            self,
+            "holdings",
+            self.task_configuration.default_call_number_type_name,
+        )
+        self.folio = folio
+        super().__init__(folio, library_configuration, task_configuration, self.conditions)
+        self.location_map = location_map
+        self.schema = self.holdings_json_schema
+        self.holdings_id_map = {}
+        self.ref_data_dicts = {}
+        self.fallback_holdings_type_id = self.task_configuration.fallback_holdings_type_id
+
+    def parse_hold(self, marc_record, legacy_id):
+        """Parses a mfhd recod into a FOLIO Inventory instance object
+        Community mapping suggestion: https://tinyurl.com/3rh52e2x
+         This is the main function
+
+        Args:
+            marc_record (_type_): _description_
+            legacy_id (_type_): _description_
+
+        Raises:
+            TransformationRecordFailedError: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        self.print_progress()
+        folio_holding = {
+            "metadata": self.folio_client.get_metadata_construct(),
+        }
+        self.migration_report.add(Blurbs.RecordStatus, marc_record.leader[5])
+        ignored_subsequent_fields = set()
+        num_852s = 0
+        for marc_field in marc_record:
+            try:
+                if marc_field.tag == "852":
+                    num_852s += 1
+                self.process_marc_field(
+                    marc_field,
+                    ignored_subsequent_fields,
+                    folio_holding,
+                    legacy_id,
+                )
+            except TransformationFieldMappingError as tfme:
+                tfme.log_it()
+        if num_852s > 1:
+            Helper.log_data_issue(legacy_id, "More than 1 852 found", "")
+
+        folio_holding["id"] = str(
+            FolioUUID(
+                self.folio_client.okapi_url,
+                FOLIONamespaces.holdings,
+                legacy_id,
+            )
+        )
+
+        if not folio_holding.get("instanceId", ""):
+            raise TransformationRecordFailedError(
+                legacy_id,
+                "No Instance id mapped. ",
+                folio_holding["formerIds"],
+            )
+        self.perform_additional_mapping(marc_record, folio_holding, legacy_id)
+        cleaned_folio_holding = self.validate_required_properties(
+            "-".join(folio_holding.get("formerIds")),
+            folio_holding,
+            self.holdings_json_schema,
+            FOLIONamespaces.holdings,
+        )
+        props_to_not_dedupe = (
+            []
+            if self.task_configuration.deduplicate_holdings_statements
+            else [
+                "holdingsStatements",
+                "holdingsStatementsForIndexes",
+                "holdingsStatementsForSupplements",
+            ]
+        )
+        self.dedupe_rec(cleaned_folio_holding, props_to_not_dedupe)
+        self.holdings_id_map[legacy_id] = self.get_id_map_dict(legacy_id, cleaned_folio_holding)
+
+        self.report_folio_mapping(cleaned_folio_holding, self.schema)
+        return cleaned_folio_holding
+
+    def process_marc_field(
+        self,
+        marc_field: Field,
+        ignored_subsequent_fields,
+        folio_holding,
+        index_or_legacy_ids,
+    ):
+        self.migration_report.add(Blurbs.Trivia, "Total number of Tags processed")
+        if marc_field.tag not in self.mappings:
+            self.report_legacy_mapping(marc_field.tag, True, False)
+        elif marc_field.tag not in ignored_subsequent_fields:
+            mappings = self.mappings[marc_field.tag]
+            self.map_field_according_to_mapping(
+                marc_field, mappings, folio_holding, index_or_legacy_ids
+            )
+            self.report_legacy_mapping(marc_field.tag, True, True)
+            if any(m.get("ignoreSubsequentFields", False) for m in mappings):
+                ignored_subsequent_fields.add(marc_field.tag)
+
+    def perform_additional_mapping(self, marc_record: Record, folio_holding, legacy_id: str):
+        """Perform additional tasks not easily handled in the mapping rules
+
+        Args:
+            marc_record (Record): _description_
+            folio_holding (_type_): _description_
+            legacy_id (str): _description_
+        """
+        self.set_holdings_type(marc_record, folio_holding, legacy_id)
+        self.set_default_call_number_type_if_empty(folio_holding)
+        self.pick_first_location_if_many(folio_holding, legacy_id)
+        self.parse_coded_holdings_statements(marc_record, folio_holding, legacy_id)
+        self.hrid_handler.handle_hrid(
+            FOLIONamespaces.holdings, folio_holding, marc_record, [legacy_id]
+        )
+
+    def pick_first_location_if_many(self, folio_holding, legacy_id: str):
+        if " " in folio_holding.get("permanentLocationId", ""):
+            Helper.log_data_issue(
+                legacy_id,
+                "Space in permanentLocationId. Was this MFHD attached to multiple holdings?",
+                folio_holding["permanentLocationId"],
+            )
+            folio_holding["permanentLocationId"] = folio_holding["permanentLocationId"].split(" ")[
+                0
+            ]
+
+    def parse_coded_holdings_statements(self, marc_record: Record, folio_holding, legacy_id):
+        # TODO: Should one be able to switch these things off?
+        a = {
+            "holdingsStatements": ("853", "863", "866"),
+            "holdingsStatementsForIndexes": ("855", "865", "868"),
+            "holdingsStatementsForSupplements": ("854", "864", "867"),
+        }
+        for key, v in a.items():
+            try:
+                res = HoldingsStatementsParser.get_holdings_statements(
+                    marc_record,
+                    v[0],
+                    v[1],
+                    v[2],
+                    legacy_id,
+                    self.task_configuration.deduplicate_holdings_statements,
+                )
+                if res["statements"]:
+                    folio_holding[key] = res["statements"]
+                for mr in res["migration_report"]:
+                    self.migration_report.add(
+                        Blurbs.HoldingsStatementsParsing, f"{mr[0]} -- {mr[1]}"
+                    )
+            except TransformationFieldMappingError as tfme:
+                Helper.log_data_issue(tfme.index_or_id, tfme.message, tfme.data_value)
+                self.migration_report.add(Blurbs.FieldMappingErrors, tfme.message)
+
+    def wrap_up(self):
+        logging.info("Mapper wrapping up")
+        if not self.task_configuration.never_update_hrid_settings:
+            if self.task_configuration.create_source_records:
+                self.store_hrid_settings()
+            else:
+                logging.info("NOT storing HRID settings since that is managed by FOLIO")
+
+    def set_holdings_type(self, marc_record: Record, folio_holding, legacy_id: str):
+        # Holdings type mapping
+        ldr06 = marc_record.leader[6]
+        # TODO: map this better
+        # type = type_map.get(ldr06, "Unknown")
+        if folio_holding.get("holdingsTypeId", ""):
+            self.migration_report.add(
+                Blurbs.HoldingsTypeMapping,
+                f"Already set to {folio_holding.get('holdingsTypeId')}. LDR[06] was {ldr06}",
+            )
+        else:
+            holdings_type_map = {
+                "u": "Unknown",
+                "v": "Multi-part monograph",
+                "x": "Monograph",
+                "y": "Serial",
+            }
+            holdings_type = holdings_type_map.get(ldr06, "")
+            if t := self.conditions.get_ref_data_tuple_by_name(
+                self.conditions.holdings_types, "hold_types", holdings_type
+            ):
+                folio_holding["holdingsTypeId"] = t[0]
+                self.migration_report.add(
+                    Blurbs.HoldingsTypeMapping,
+                    f"{ldr06} -> {holdings_type} -> {t[1]} ({t[0]}",
+                )
+                if holdings_type == "Unknown":
+                    Helper.log_data_issue(
+                        legacy_id,
+                        (
+                            f"{Blurbs.HoldingsTypeMapping[0]} is 'unknown'. "
+                            "(leader 06 is set to 'u') Check if this is correct"
+                        ),
+                        ldr06,
+                    )
+            else:
+                if not self.fallback_holdings_type_id:
+                    raise TransformationProcessError(
+                        "",
+                        "No fallbackHoldingsTypeId set up. Add to task configuration",
+                    )
+                folio_holding["holdingsTypeId"] = self.fallback_holdings_type_id
+                self.migration_report.add(
+                    Blurbs.HoldingsTypeMapping,
+                    f"A Unmapped {ldr06} -> {holdings_type} -> Unmapped",
+                )
+                Helper.log_data_issue(
+                    legacy_id,
+                    (f"{Blurbs.HoldingsTypeMapping[0]}. leader 06 was unmapped."),
+                    ldr06,
+                )
+
+    def set_default_call_number_type_if_empty(self, folio_holding):
+        if not folio_holding.get("callNumberTypeId", ""):
+            folio_holding["callNumberTypeId"] = self.conditions.default_call_number_type["id"]
+
+    def remove_from_id_map(self, former_ids):
+        """removes the ID from the map in case parsing failed
+
+        Args:
+            former_ids (_type_): _description_
+        """
+        for former_id in [id for id in former_ids if id]:
+            if former_id in self.holdings_id_map:
+                del self.holdings_id_map[former_id]
---
+++ packages/folio-migration-tools/folio_migration_tools-1.6.4.tar.gz/folio_migration_tools-1.6.4/src/folio_migration_tools/migration_tasks/bibs_transformer.py
@@ -0,0 +1,190 @@
+import logging
+import sys
+from io import IOBase
+from typing import Annotated
+from typing import List
+from typing import Optional
+
+from folio_uuid.folio_namespaces import FOLIONamespaces
+from pydantic import Field
+from pymarc import MARCReader
+from pymarc.record import Record
+
+from folio_migration_tools.custom_exceptions import TransformationProcessError
+from folio_migration_tools.custom_exceptions import TransformationRecordFailedError
+from folio_migration_tools.helper import Helper
+from folio_migration_tools.library_configuration import FileDefinition
+from folio_migration_tools.library_configuration import HridHandling
+from folio_migration_tools.library_configuration import IlsFlavour
+from folio_migration_tools.library_configuration import LibraryConfiguration
+from folio_migration_tools.marc_rules_transformation.bibs_processor import BibsProcessor
+from folio_migration_tools.marc_rules_transformation.rules_mapper_bibs import (
+    BibsRulesMapper,
+)
+from folio_migration_tools.migration_report import MigrationReport
+from folio_migration_tools.migration_tasks.migration_task_base import MigrationTaskBase
+from folio_migration_tools.report_blurbs import Blurbs
+from folio_migration_tools.task_configuration import AbstractTaskConfiguration
+
+
+class BibsTransformer(MigrationTaskBase):
+    class TaskConfiguration(AbstractTaskConfiguration):
+        name: str
+        add_administrative_notes_with_legacy_ids: Annotated[
+            bool,
+            Field(
+                description=(
+                    "If set to true, an Administrative note will be added to the records "
+                    "containing the legacy ID. Use this in order to protect the values from "
+                    "getting overwritten by overlays,"
+                ),
+            ),
+        ] = True
+        migration_task_type: str
+        use_tenant_mapping_rules: Optional[bool] = True
+        hrid_handling: Optional[HridHandling] = HridHandling.default
+        deactivate035_from001: Optional[bool] = False
+        files: List[FileDefinition]
+        ils_flavour: IlsFlavour
+        tags_to_delete: Optional[List[str]] = []
+        reset_hrid_settings: Optional[bool] = False
+        never_update_hrid_settings: Optional[bool] = False
+
+    @staticmethod
+    def get_object_type() -> FOLIONamespaces:
+        return FOLIONamespaces.instances
+
+    def __init__(
+        self,
+        task_config: TaskConfiguration,
+        library_config: LibraryConfiguration,
+        use_logging: bool = True,
+    ):
+
+        super().__init__(library_config, task_config, use_logging)
+        self.processor: BibsProcessor = None
+        self.check_source_files(
+            self.folder_structure.legacy_records_folder, self.task_configuration.files
+        )
+        logging.info(self.task_configuration.json(indent=4))
+        self.mapper = BibsRulesMapper(self.folio_client, library_config, self.task_configuration)
+        self.bib_ids: set = set()
+        logging.info("Init done")
+        if (
+            self.task_configuration.reset_hrid_settings
+            and not self.task_configuration.never_update_hrid_settings
+        ):
+            self.mapper.reset_instance_hrid_counter()
+
+    def do_work(self):
+        logging.info("Starting....")
+
+        with open(self.folder_structure.created_objects_path, "w+") as created_records_file:
+            self.processor = BibsProcessor(
+                self.mapper,
+                self.folio_client,
+                created_records_file,
+                self.folder_structure,
+            )
+            with open(self.folder_structure.failed_bibs_file, "wb") as failed_bibs_file:
+                for file_obj in self.task_configuration.files:
+                    try:
+                        with open(
+                            self.folder_structure.legacy_records_folder / file_obj.file_name,
+                            "rb",
+                        ) as marc_file:
+                            reader = MARCReader(marc_file, to_unicode=True, permissive=True)
+                            reader.hide_utf8_warnings = True
+                            reader.force_utf8 = False
+                            logging.info("running %s", file_obj.file_name)
+                            self.read_records(reader, file_obj, failed_bibs_file)
+                    except TransformationProcessError as tpe:
+                        logging.critical(tpe)
+                        sys.exit(1)
+                    except Exception:
+                        logging.exception(file_obj, stack_info=True)
+                        logging.critical(
+                            "File %s failed for unknown reason. Halting", file_obj.file_name
+                        )
+                        sys.exit(1)
+
+    def wrap_up(self):
+        logging.info("Done. Wrapping up...")
+        self.extradata_writer.flush()
+        self.processor.wrap_up()
+        with open(self.folder_structure.migration_reports_file, "w+") as report_file:
+            self.mapper.migration_report.write_migration_report(
+                "Bibliographic records transformation report",
+                report_file,
+                self.start_datetime,
+            )
+            Helper.print_mapping_report(
+                report_file,
+                self.mapper.parsed_records,
+                self.mapper.mapped_folio_fields,
+                self.mapper.mapped_legacy_fields,
+            )
+
+        logging.info(
+            "Done. Transformation report written to %s",
+            self.folder_structure.migration_reports_file.name,
+        )
+
+    def read_records(self, reader, source_file: FileDefinition, failed_bibs_file: IOBase):
+        for idx, record in enumerate(reader):
+            self.mapper.migration_report.add_general_statistics("Records in file before parsing")
+            try:
+                # None = Something bad happened
+                if record is None:
+                    self.report_failed_parsing(reader, source_file, failed_bibs_file, idx)
+                # The normal case
+                else:
+                    self.set_leader(record, self.mapper.migration_report)
+                    self.mapper.migration_report.add_general_statistics(
+                        "Records successfully decoded from MARC21",
+                    )
+                    self.processor.process_record(idx, record, source_file)
+            except TransformationRecordFailedError as error:
+                error.log_it()
+        logging.info("Done reading %s records from file", idx + 1)
+
+    def report_failed_parsing(self, reader, source_file, failed_bibs_file, idx):
+        self.mapper.migration_report.add_general_statistics(
+            "Records with encoding errors - parsing failed",
+        )
+        failed_bibs_file.write(reader.current_chunk)
+        raise TransformationRecordFailedError(
+            f"Index in {source_file.file_name}:{idx}",
+            f"MARC parsing error: {reader.current_exception}",
+            "Failed records stored in results/failed_bib_records.mrc",
+        )
+
+    @staticmethod
+    def set_leader(marc_record: Record, migration_report: MigrationReport):
+        if marc_record.leader[9] != "a":
+            migration_report.add(
+                Blurbs.LeaderManipulation,
+                f"Set leader 09 (Character coding scheme) from {marc_record.leader[9]} to a",
+            )
+            marc_record.leader = f"{marc_record.leader[:9]}a{marc_record.leader[10:]}"
+
+        if not marc_record.leader.endswith("4500"):
+            migration_report.add(
+                Blurbs.LeaderManipulation,
+                f"Set leader 20-23 from {marc_record.leader[-4:]} to 4500",
+            )
+            marc_record.leader = f"{marc_record.leader[:-4]}4500"
+
+        if marc_record.leader[10] != "2":
+            migration_report.add(
+                Blurbs.LeaderManipulation,
+                f"Set leader 10 (Indicator count) from {marc_record.leader[10]} to 2",
+            )
+            marc_record.leader = f"{marc_record.leader[:10]}2{marc_record.leader[11:]}"
+
+        if marc_record.leader[11] != "2":
+            migration_report.add(
+                Blurbs.LeaderManipulation,
+                f"Set leader 10 (Subfield code count) from {marc_record.leader[11]} to 2",
+            )
+            marc_record.leader = f"{marc_record.leader[:11]}2{marc_record.leader[12:]}"
