b45aa09d2891c97c38939d402cfe611de22929b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:34 +0000

    Add package fiftyone/0.18.1rc2/fiftyone-0.18.1rc2.tar.gz

---
+++ packages/fiftyone/fiftyone-0.18.1rc2.tar.gz/fiftyone-0.18.1rc2/fiftyone/server/static/assets/index.fb4a69e7.js
@@ -0,0 +1,1 @@
+import{a5 as t,w as a,r as s,R as o,L as c}from"./index.973e0482.js";const n=({})=>{const e=t(a);return s.exports.useLayoutEffect(()=>{e(null)},[]),o.createElement(c,null,"No dataset selected")},l=n;export{l as default};
