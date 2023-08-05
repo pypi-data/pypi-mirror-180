707b12cfc7e482ada3098b65253f62e031be1838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:09 +0000

    Add package lexandra/1.0.0/lexandra-1.0.0-py3-none-any.whl

---
+++ packages/lexandra/lexandra-1.0.0-py3-none-any.whl/lexandra/__init__.py
@@ -0,0 +1,4 @@
+"""Lexandra Package"""
+from lexandra import factory, utils, core
+
+__version__ = "1.0.0"
---
+++ packages/lexandra/lexandra-1.0.0-py3-none-any.whl/lexandra/factory/constants.py
@@ -0,0 +1,16 @@
+NUMBERS = "0123456789"
+LETTERS = "qwertyuiopasdfghjklzxcvbnm"
+UPPERS = "QWERTYUIOPASDFGHJKLZXCVBNM"
+
+NUMB_TOKEN = "numbs"
+FLT_TOKEN = "float"
+WORD_TOKEN = "word"
+STR_TOKEN = "str"
+DLT_TOKEN = "dlt"
+
+
+__all__ = [
+    "NUMBERS", "LETTERS", "UPPERS",
+    "NUMB_TOKEN", "WORD_TOKEN",
+    "STR_TOKEN", "DLT_TOKEN"
+    ]
