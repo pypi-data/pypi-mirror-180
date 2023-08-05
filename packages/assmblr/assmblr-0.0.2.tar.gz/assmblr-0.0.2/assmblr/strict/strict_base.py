2ed325a34b296e49e1cdef81be4c5f0c5ca5b859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:25 +0000

    Add package assmblr/0.0.2/assmblr-0.0.2.tar.gz

---
+++ packages/assmblr/assmblr-0.0.2.tar.gz/assmblr-0.0.2/assmblr/strict/strict_base.py
@@ -0,0 +1,84 @@
+from collections import OrderedDict
+
+
+class _StrictlyMeta(type):
+    def __or__(cls, other):
+        return cls(other)
+
+    def __ror__(cls, other):
+        return cls(other)
+
+
+class StrictlyDescriptor(metaclass = _StrictlyMeta):
+    predicate_cache = OrderedDict()
+
+    def __init__(
+            self,
+            *predicates,
+            msg = "Value does not match any of the predicates",
+    ):
+        self.predicates = predicates
+        self.msg = msg
+
+    def __set_name__(self, owner, name):
+        self.__name__ = name
+
+    def __set__(self, instance, value):
+        if not self.map(value):
+            msg = f"attempted to set {self.__name__!r} to {value!r} but {self.msg}"
+            raise ValueError(msg)
+
+    def bind(self, *funcs):
+        self.predicates += funcs
+        return self
+
+    def map(self, value):
+        try:
+            key = hash(value)
+        except TypeError:
+            """If the value is unhashable, we can't cache it"""
+        else:
+            if key in self.predicate_cache:
+                return self.predicate_cache[key]
+        result = True
+        for predicate in self.predicates:
+            if not predicate(value):
+                result = False
+                break
+        try:
+            return self.predicate_cache.setdefault(key, result)  # NOQA
+        except NameError:
+            return result
+
+    def message(self, msg):
+        self.msg = msg
+        return self
+
+    def __call__(self, *args):
+        self.bind(*args)
+
+    def __or__(self, other):
+        if isinstance(other, str):
+            return self.message(other)
+        elif isinstance(other, StrictPredicate):
+            return self.bind(other)
+        return self.bind(other)
+
+    def __ror__(self, other):
+        return self.__or__(other)
+
+
+class StrictPredicate(metaclass = _StrictlyMeta):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, value):
+        return self.func(value)
+
+    def __or__(self, other):
+        if callable(other):
+            return StrictPredicate(lambda x: self(x) and other(x))
+        raise TypeError(f"Cannot combine {self} with {other}")
+
+
+__all__ = ["StrictlyDescriptor", "StrictPredicate"]
---
+++ packages/assmblr/assmblr-0.0.2.tar.gz/assmblr-0.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['assmblr', 'assmblr.strict']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'assmblr',
+    'version': '0.0.2',
+    'description': 'Tools for assembling classes in a concise, performant way',
+    'long_description': '### Assmblr\n___\n Simple to use class building tools to make your life easier. The goal is to dry up code by providing generics of common functions and utilities.\n___\n\nCurrently includes: \n- Strict type enforcement',
+    'author': 'Alyce',
+    'author_email': 'Alyceosbourne@outlook.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
