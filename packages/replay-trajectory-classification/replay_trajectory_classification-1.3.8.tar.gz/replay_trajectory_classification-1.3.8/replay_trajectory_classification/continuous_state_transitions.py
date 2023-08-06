4dce203bea3746f9851887dbfc258eb583b76ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:10 +0000

    Add package replay-trajectory-classification/1.3.8/replay_trajectory_classification-1.3.8.tar.gz

---
+++ packages/replay-trajectory-classification/replay_trajectory_classification-1.3.8.tar.gz/replay_trajectory_classification-1.3.8/replay_trajectory_classification/continuous_state_transitions.py
@@ -0,0 +1,298 @@
+"""Classes for constructing different types of movement models."""
+from dataclasses import dataclass
+
+import numpy as np
+from replay_trajectory_classification.core import atleast_2d
+from replay_trajectory_classification.environments import diffuse_each_bin
+from scipy.stats import multivariate_normal
+
+
+def _normalize_row_probability(x):
+    """Ensure the state transition matrix rows sum to 1"""
+    x /= x.sum(axis=1, keepdims=True)
+    x[np.isnan(x)] = 0
+    return x
+
+
+def estimate_movement_var(position, sampling_frequency):
+    """Estimates the movement variance based on position.
+
+    Parameters
+    ----------
+    position : np.ndarray, shape (n_time, n_position_dim)
+
+    Returns
+    -------
+    movement_std : np.ndarray, shape (n_position_dim,)
+
+    """
+    position = atleast_2d(position)
+    is_nan = np.any(np.isnan(position), axis=1)
+    position = position[~is_nan]
+    movement_var = np.cov(np.diff(position, axis=0), rowvar=False)
+    return movement_var * sampling_frequency
+
+
+def _random_walk_on_track_graph(
+    place_bin_centers,
+    movement_mean,
+    movement_var,
+    place_bin_center_ind_to_node,
+    distance_between_nodes,
+):
+    state_transition = np.zeros((place_bin_centers.size, place_bin_centers.size))
+    gaussian = multivariate_normal(mean=movement_mean, cov=movement_var)
+
+    for bin_ind1, node1 in enumerate(place_bin_center_ind_to_node):
+        for bin_ind2, node2 in enumerate(place_bin_center_ind_to_node):
+            try:
+                state_transition[bin_ind1, bin_ind2] = gaussian.pdf(
+                    distance_between_nodes[node1][node2]
+                )
+            except KeyError:
+                # bins not on track interior will be -1 and not in distance
+                # between nodes
+                continue
+
+    return state_transition
+
+
+@dataclass
+class RandomWalk:
+    """A transition where the movement stays locally close in space
+
+    Attributes
+    ----------
+    environment_name : str, optional
+        Name of environment to fit
+    movement_var : float, optional
+        How far the animal is can move in one time bin during normal
+        movement.
+    movement_mean : float, optional
+    use_diffusion : bool, optional
+        Use diffusion to respect the geometry of the environment
+    """
+
+    environment_name: str = ""
+    movement_var: float = 6.0
+    movement_mean: float = 0.0
+    use_diffusion: bool = False
+
+    def make_state_transition(self, environments: tuple):
+        self.environment = environments[environments.index(self.environment_name)]
+
+        if self.environment.track_graph is None:
+            n_position_dims = self.environment.place_bin_centers_.shape[1]
+
+            if (n_position_dims == 1) or not self.use_diffusion:
+                transition_matrix = np.stack(
+                    [
+                        multivariate_normal(
+                            mean=center + self.movement_mean, cov=self.movement_var
+                        ).pdf(self.environment.place_bin_centers_)
+                        for center in self.environment.place_bin_centers_
+                    ],
+                    axis=1,
+                )
+            else:
+                dx = self.environment.edges_[0][1] - self.environment.edges_[0][0]
+                dy = self.environment.edges_[1][1] - self.environment.edges_[1][0]
+                n_total_bins = np.prod(self.environment.is_track_interior_.shape)
+                transition_matrix = diffuse_each_bin(
+                    self.environment.is_track_interior_,
+                    self.environment.is_track_boundary_,
+                    dx,
+                    dy,
+                    std=np.sqrt(self.movement_var),
+                ).reshape((n_total_bins, -1), order="F")
+        else:
+            place_bin_center_ind_to_node = np.asarray(
+                self.environment.place_bin_centers_nodes_df_.node_id
+            )
+            transition_matrix = _random_walk_on_track_graph(
+                self.environment.place_bin_centers_,
+                self.movement_mean,
+                self.movement_var,
+                place_bin_center_ind_to_node,
+                self.environment.distance_between_nodes_,
+            )
+
+        is_track_interior = self.environment.is_track_interior_.ravel(order="F")
+        transition_matrix[~is_track_interior] = 0.0
+        transition_matrix[:, ~is_track_interior] = 0.0
+
+        return _normalize_row_probability(transition_matrix)
+
+
+@dataclass
+class Uniform:
+    """
+    Attributes
+    ----------
+    environment_name : str, optional
+        Name of first environment to fit
+    environment_name2 : str, optional
+        Name of second environment to fit if going from one environment to
+        another
+    """
+
+    environment_name: str = ""
+    environment2_name: str = None
+
+    def make_state_transition(self, environments: tuple):
+        self.environment1 = environments[environments.index(self.environment_name)]
+        n_bins1 = self.environment1.place_bin_centers_.shape[0]
+        is_track_interior1 = self.environment1.is_track_interior_.ravel(order="F")
+
+        if self.environment2_name is None:
+            n_bins2 = n_bins1
+            is_track_interior2 = is_track_interior1.copy()
+        else:
+            self.environment2 = environments[environments.index(self.environment2_name)]
+            n_bins2 = self.environment2.place_bin_centers_.shape[0]
+            is_track_interior2 = self.environment2.is_track_interior_.ravel(order="F")
+
+        transition_matrix = np.ones((n_bins1, n_bins2))
+
+        transition_matrix[~is_track_interior1] = 0.0
+        transition_matrix[:, ~is_track_interior2] = 0.0
+
+        return _normalize_row_probability(transition_matrix)
+
+
+@dataclass
+class Identity:
+    """A transition where the movement stays within a place bin
+
+    Attributes
+    ----------
+    environment_name : str, optional
+        Name of environment to fit
+    """
+
+    environment_name: str = ""
+
+    def make_state_transition(self, environments: tuple):
+        self.environment = environments[environments.index(self.environment_name)]
+        n_bins = self.environment.place_bin_centers_.shape[0]
+
+        transition_matrix = np.identity(n_bins)
+
+        is_track_interior = self.environment.is_track_interior_.ravel(order="F")
+        transition_matrix[~is_track_interior] = 0.0
+        transition_matrix[:, ~is_track_interior] = 0.0
+
+        return _normalize_row_probability(transition_matrix)
+
+
+@dataclass
+class EmpiricalMovement:
+    """A transition matrix trained on the animal's actual movement
+
+    Attributes
+    ----------
+    environment_name : str, optional
+        Name of environment to fit
+    encoding_group : str, optional
+        Name of encoding group to fit
+    speedup : int, optional
+        Used to
+        make the empirical transition matrix "faster", means allowing for
+        all the same transitions made by the animal but sped up by
+        `speedup` times. So `speedup​=20` means 20x faster than the
+        animal's movement.
+    """
+
+    environment_name: str = ""
+    encoding_group: str = None
+    speedup: int = 1
+
+    def make_state_transition(
+        self,
+        environments: tuple,
+        position: np.ndarray,
+        is_training: np.ndarray = None,
+        encoding_group_labels: np.ndarray = None,
+        environment_labels: np.ndarray = None,
+    ):
+
+        self.environment = environments[environments.index(self.environment_name)]
+
+        n_time = position.shape[0]
+        if is_training is None:
+            is_training = np.ones((n_time,), dtype=np.bool)
+
+        if encoding_group_labels is None:
+            is_encoding = np.ones((n_time,), dtype=np.bool)
+        else:
+            is_encoding = encoding_group_labels == self.encoding_group
+
+        if environment_labels is None:
+            is_environment = np.ones((n_time,), dtype=np.bool)
+        else:
+            is_environment = environment_labels == self.environment_name
+
+        position = atleast_2d(position)[is_training & is_encoding & is_environment]
+        state_transition, _ = np.histogramdd(
+            np.concatenate((position[1:], position[:-1]), axis=1),
+            bins=self.environment.edges_ * 2,
+            range=self.environment.position_range,
+        )
+        original_shape = state_transition.shape
+        n_position_dims = position.shape[1]
+        shape_2d = np.product(original_shape[:n_position_dims])
+        state_transition = _normalize_row_probability(
+            state_transition.reshape((shape_2d, shape_2d), order="F")
+        )
+
+        return np.linalg.matrix_power(state_transition, self.speedup)
+
+
+@dataclass
+class RandomWalkDirection1:
+    """A Gaussian random walk in that can only go one direction
+
+    Attributes
+    ----------
+    environment_name : str, optional
+        Name of environment to fit
+    movement_var : float, optional
+        How far the animal is can move in one time bin during normal
+        movement.
+    """
+
+    environment_name: str = ""
+    movement_var: float = 6.0
+
+    def make_state_transition(self, environments: tuple):
+        self.environment = environments[environments.index(self.environment_name)]
+        random = RandomWalk(
+            self.environment_name, self.movement_var
+        ).make_state_transition(environments)
+
+        return _normalize_row_probability(np.triu(random))
+
+
+@dataclass
+class RandomWalkDirection2:
+    """A Gaussian random walk in that can only go one direction
+
+    Attributes
+    ----------
+    environment_name : str, optional
+        Name of environment to fit
+    movement_var : float, optional
+        How far the animal is can move in one time bin during normal
+        movement.
+    """
+
+    environment_name: str = ""
+    movement_var: float = 6.0
+
+    def make_state_transition(self, environments: tuple):
+        self.environment = environments[environments.index(self.environment_name)]
+        random = RandomWalk(
+            self.environment_name, self.movement_var
+        ).make_state_transition(environments)
+
+        return _normalize_row_probability(np.tril(random))
---
+++ packages/replay-trajectory-classification/replay_trajectory_classification-1.3.8.tar.gz/replay_trajectory_classification-1.3.8/replay_trajectory_classification/decoder.py
@@ -0,0 +1,478 @@
+"""Main classeses for decoding trajectories from population spiking"""
+
+from copy import deepcopy
+from logging import getLogger
+
+import joblib
+import numpy as np
+import sklearn
+import xarray as xr
+from replay_trajectory_classification.continuous_state_transitions import (
+    EmpiricalMovement,
+    RandomWalk,
+)
+from replay_trajectory_classification.core import (
+    _acausal_decode,
+    _acausal_decode_gpu,
+    _causal_decode,
+    _causal_decode_gpu,
+    atleast_2d,
+    get_centers,
+    mask,
+    scaled_likelihood,
+)
+from replay_trajectory_classification.environments import Environment
+from replay_trajectory_classification.initial_conditions import UniformInitialConditions
+from replay_trajectory_classification.likelihoods import (
+    _SORTED_SPIKES_ALGORITHMS,
+    _ClUSTERLESS_ALGORITHMS,
+)
+from sklearn.base import BaseEstimator
+
+logger = getLogger(__name__)
+
+sklearn.set_config(print_changed_only=False)
+
+_DEFAULT_CLUSTERLESS_MODEL_KWARGS = {
+    "mark_std": 24.0,
+    "position_std": 6.0,
+}
+
+_DEFAULT_SORTED_SPIKES_MODEL_KWARGS = {
+    "position_std": 6.0,
+    "use_diffusion": False,
+    "block_size": None,
+}
+
+
+class _DecoderBase(BaseEstimator):
+    def __init__(
+        self,
+        environment=Environment(environment_name=""),
+        transition_type=RandomWalk(),
+        initial_conditions_type=UniformInitialConditions(),
+        infer_track_interior=True,
+    ):
+        self.environment = environment
+        self.transition_type = transition_type
+        self.initial_conditions_type = initial_conditions_type
+        self.infer_track_interior = infer_track_interior
+
+    def fit_environment(self, position):
+        self.environment.fit_place_grid(
+            position, infer_track_interior=self.infer_track_interior
+        )
+
+    def fit_initial_conditions(self):
+        logger.info("Fitting initial conditions...")
+        self.initial_conditions_ = self.initial_conditions_type.make_initial_conditions(
+            [self.environment], [self.environment.environment_name]
+        )[0]
+
+    def fit_state_transition(
+        self, position, is_training=None, transition_type=RandomWalk()
+    ):
+        logger.info("Fitting state transition...")
+        if isinstance(self.transition_type, EmpiricalMovement):
+            if is_training is None:
+                is_training = np.ones((position.shape[0],), dtype=np.bool)
+            is_training = np.asarray(is_training).squeeze()
+            n_time = position.shape[0]
+            encoding_group_labels = np.zeros((n_time,), dtype=np.int32)
+            self.state_transition_ = self.transition_type.make_state_transition(
+                (self.environment,),
+                position,
+                is_training,
+                encoding_group_labels,
+                environment_labels=None,
+            )
+        else:
+            self.state_transition_ = self.transition_type.make_state_transition(
+                (self.environment,)
+            )
+
+    def fit(self):
+        raise NotImplementedError
+
+    def predict(self):
+        raise NotImplementedError
+
+    def save_model(self, filename="model.pkl"):
+        joblib.dump(self, filename)
+
+    @staticmethod
+    def load_model(filename="model.pkl"):
+        return joblib.load(filename)
+
+    def copy(self):
+        return deepcopy(self)
+
+    def _get_results(
+        self,
+        results,
+        n_time,
+        time=None,
+        is_compute_acausal=True,
+        use_gpu=False,
+    ):
+
+        is_track_interior = self.environment.is_track_interior_.ravel(order="F")
+        n_position_bins = is_track_interior.shape[0]
+        st_interior_ind = np.ix_(is_track_interior, is_track_interior)
+
+        logger.info("Estimating causal posterior...")
+        if not use_gpu:
+            results["causal_posterior"] = np.full(
+                (n_time, n_position_bins), np.nan, dtype=np.float64
+            )
+            (
+                results["causal_posterior"][:, is_track_interior],
+                data_log_likelihood,
+            ) = _causal_decode(
+                self.initial_conditions_[is_track_interior].astype(np.float64),
+                self.state_transition_[st_interior_ind].astype(np.float64),
+                results["likelihood"][:, is_track_interior].astype(np.float64),
+            )
+        else:
+            results["causal_posterior"] = np.full(
+                (n_time, n_position_bins), np.nan, dtype=np.float32
+            )
+            (
+                results["causal_posterior"][:, is_track_interior],
+                data_log_likelihood,
+            ) = _causal_decode_gpu(
+                self.initial_conditions_[is_track_interior],
+                self.state_transition_[st_interior_ind],
+                results["likelihood"][:, is_track_interior],
+            )
+
+        if is_compute_acausal:
+            logger.info("Estimating acausal posterior...")
+            if not use_gpu:
+                results["acausal_posterior"] = np.full(
+                    (n_time, n_position_bins, 1), np.nan, dtype=np.float64
+                )
+                results["acausal_posterior"][:, is_track_interior] = _acausal_decode(
+                    results["causal_posterior"][
+                        :, is_track_interior, np.newaxis
+                    ].astype(np.float64),
+                    self.state_transition_[st_interior_ind].astype(np.float64),
+                )
+            else:
+                results["acausal_posterior"] = np.full(
+                    (n_time, n_position_bins, 1), np.nan, dtype=np.float32
+                )
+                results["acausal_posterior"][
+                    :, is_track_interior
+                ] = _acausal_decode_gpu(
+                    results["causal_posterior"][:, is_track_interior, np.newaxis],
+                    self.state_transition_[st_interior_ind],
+                )
+
+        if time is None:
+            time = np.arange(n_time)
+
+        return self.convert_results_to_xarray(results, time, data_log_likelihood)
+
+    def convert_results_to_xarray(self, results, time, data_log_likelihood):
+        n_position_dims = self.environment.place_bin_centers_.shape[1]
+        n_time = time.shape[0]
+
+        attrs = {"data_log_likelihood": data_log_likelihood}
+
+        if n_position_dims > 1:
+            dims = ["time", "x_position", "y_position"]
+            coords = dict(
+                time=time,
+                x_position=get_centers(self.environment.edges_[0]),
+                y_position=get_centers(self.environment.edges_[1]),
+            )
+        else:
+            dims = ["time", "position"]
+            coords = dict(
+                time=time,
+                position=get_centers(self.environment.edges_[0]),
+            )
+        new_shape = (n_time, *self.environment.centers_shape_)
+        is_track_interior = self.environment.is_track_interior_.ravel(order="F")
+        try:
+            results = xr.Dataset(
+                {
+                    key: (
+                        dims,
+                        mask(value, is_track_interior)
+                        .reshape(new_shape)
+                        .swapaxes(-1, -2),
+                    )
+                    for key, value in results.items()
+                },
+                coords=coords,
+                attrs=attrs,
+            )
+        except ValueError:
+            results = xr.Dataset(
+                {
+                    key: (dims, mask(value, is_track_interior).reshape(new_shape))
+                    for key, value in results.items()
+                },
+                coords=coords,
+                attrs=attrs,
+            )
+
+        return results
+
+
+class SortedSpikesDecoder(_DecoderBase):
+    def __init__(
+        self,
+        environment=Environment(environment_name=""),
+        transition_type=RandomWalk(),
+        initial_conditions_type=UniformInitialConditions(),
+        infer_track_interior=True,
+        sorted_spikes_algorithm="spiking_likelihood_kde",
+        sorted_spikes_algorithm_params=_DEFAULT_SORTED_SPIKES_MODEL_KWARGS,
+    ):
+        """
+
+        Attributes
+        ----------
+        environment : Environment
+            The spatial environment and topology to fit
+        transition_type : (RandomWalk, Uniform, ...)
+            The continuous state transition class instance to fit
+        initial_conditions_type : InitialConditions
+            The initial conditions class instance to fit
+        infer_track_interior : bool, optional
+            Whether to infer the valid position bins
+        knot_spacing : float, optional
+            How far apart the spline knots are in position
+        spike_model_penalty : float, optional
+            L2 penalty (ridge) for the size of the regression coefficients
+        """
+        super().__init__(
+            environment, transition_type, initial_conditions_type, infer_track_interior
+        )
+        self.sorted_spikes_algorithm = sorted_spikes_algorithm
+        self.sorted_spikes_algorithm_params = sorted_spikes_algorithm_params
+
+    def fit_place_fields(self, position, spikes, is_training=None):
+        logger.info("Fitting place fields...")
+        if is_training is None:
+            is_training = np.ones((position.shape[0],), dtype=np.bool)
+        is_training = np.asarray(is_training).squeeze()
+        kwargs = self.sorted_spikes_algorithm_params
+        if kwargs is None:
+            kwargs = {}
+        self.place_fields_ = _SORTED_SPIKES_ALGORITHMS[self.sorted_spikes_algorithm][0](
+            position[is_training],
+            spikes[is_training],
+            place_bin_centers=self.environment.place_bin_centers_,
+            place_bin_edges=self.environment.place_bin_edges_,
+            edges=self.environment.edges_,
+            is_track_interior=self.environment.is_track_interior_,
+            is_track_boundary=self.environment.is_track_boundary_,
+            **kwargs
+        )
+
+    def plot_place_fields(self, sampling_frequency=1, col_wrap=5):
+        """Plots the fitted 2D place fields for each neuron.
+
+        Parameters
+        ----------
+        sampling_frequency : float, optional
+        col_wrap : int, optional
+
+        Returns
+        -------
+        g : xr.plot.FacetGrid instance
+
+        """
+        try:
+            g = (
+                self.place_fields_.unstack("position").where(
+                    self.environment.is_track_interior_
+                )
+                * sampling_frequency
+            ).plot(x="x_position", y="y_position", col="neuron", col_wrap=col_wrap)
+        except ValueError:
+            g = (self.place_fields_ * sampling_frequency).plot(
+                x="position", col="neuron", col_wrap=col_wrap
+            )
+
+        return g
+
+    def fit(self, position, spikes, is_training=None):
+        """
+
+        Parameters
+        ----------
+        position : np.ndarray, shape (n_time, n_position_dims)
+        spikes : np.ndarray, shape (n_time, n_neurons)
+        is_training : None or bool np.ndarray, shape (n_time), optional
+            Time bins to be used for encoding.
+
+        Returns
+        -------
+        self
+
+        """
+        position = atleast_2d(np.asarray(position))
+        spikes = np.asarray(spikes)
+        self.fit_environment(position)
+        self.fit_initial_conditions()
+        self.fit_state_transition(
+            position, is_training, transition_type=self.transition_type
+        )
+        self.fit_place_fields(position, spikes, is_training)
+
+        return self
+
+    def predict(self, spikes, time=None, is_compute_acausal=True, use_gpu=False):
+        """
+
+        Parameters
+        ----------
+        spikes : np.ndarray, shape (n_time, n_neurons)
+        time : ndarray or None, shape (n_time,), optional
+        is_compute_acausal : bool, optional
+        use_gpu : bool, optional
+            Use GPU for the state space part of the model, not the likelihood.
+
+        Returns
+        -------
+        results : xarray.Dataset
+
+        """
+        spikes = np.asarray(spikes)
+        n_time = spikes.shape[0]
+
+        logger.info("Estimating likelihood...")
+        results = {}
+        results["likelihood"] = scaled_likelihood(
+            _SORTED_SPIKES_ALGORITHMS[self.sorted_spikes_algorithm][1](
+                spikes, np.asarray(self.place_fields_)
+            )
+        )
+        return self._get_results(results, n_time, time, is_compute_acausal, use_gpu)
+
+
+class ClusterlessDecoder(_DecoderBase):
+    """
+
+    Attributes
+    ----------
+    environment : Environment
+        The spatial environment and topology to fit
+    transition_type : (RandomWalk, Uniform, ...)
+        The continuous state transition class instance to fit
+    initial_conditions_type : InitialConditions
+        The initial conditions class instance to fit
+    infer_track_interior : bool, optional
+        Whether to infer the valid position bins
+    clusterless_algorithm : str
+        The type of clusterless algorithm. See _ClUSTERLESS_ALGORITHMS for keys
+    clusterless_algorithm_params : dict
+        Parameters for the clusterless algorithms.
+    """
+
+    def __init__(
+        self,
+        environment=Environment(environment_name=""),
+        transition_type=RandomWalk(),
+        initial_conditions_type=UniformInitialConditions(),
+        infer_track_interior=True,
+        clusterless_algorithm="multiunit_likelihood",
+        clusterless_algorithm_params=_DEFAULT_CLUSTERLESS_MODEL_KWARGS,
+    ):
+        super().__init__(
+            environment, transition_type, initial_conditions_type, infer_track_interior
+        )
+        self.clusterless_algorithm = clusterless_algorithm
+        self.clusterless_algorithm_params = clusterless_algorithm_params
+
+    def fit_multiunits(self, position, multiunits, is_training=None):
+        """
+
+        Parameters
+        ----------
+        position : array_like, shape (n_time, n_position_dims)
+        multiunits : array_like, shape (n_time, n_marks, n_electrodes)
+        is_training : None or array_like, shape (n_time,)
+
+        """
+        logger.info("Fitting multiunits...")
+        if is_training is None:
+            is_training = np.ones((position.shape[0],), dtype=np.bool)
+        is_training = np.asarray(is_training).squeeze()
+
+        kwargs = self.clusterless_algorithm_params
+        if kwargs is None:
+            kwargs = {}
+
+        self.encoding_model_ = _ClUSTERLESS_ALGORITHMS[self.clusterless_algorithm][0](
+            position=position[is_training],
+            multiunits=multiunits[is_training],
+            place_bin_centers=self.environment.place_bin_centers_,
+            is_track_interior=self.environment.is_track_interior_.ravel(order="F"),
+            **kwargs
+        )
+
+    def fit(self, position, multiunits, is_training=None):
+        """
+
+        Parameters
+        ----------
+        position : array_like, shape (n_time, n_position_dims)
+        multiunits : array_like, shape (n_time, n_marks, n_electrodes)
+        is_training : None or array_like, shape (n_time,)
+
+        Returns
+        -------
+        self
+
+        """
+        position = atleast_2d(np.asarray(position))
+        multiunits = np.asarray(multiunits)
+
+        self.fit_environment(position)
+        self.fit_initial_conditions()
+        self.fit_state_transition(
+            position, is_training, transition_type=self.transition_type
+        )
+        self.fit_multiunits(position, multiunits, is_training)
+
+        return self
+
+    def predict(self, multiunits, time=None, is_compute_acausal=True, use_gpu=False):
+        """
+
+        Parameters
+        ----------
+        multiunits : array_like, shape (n_time, n_marks, n_electrodes)
+        time : None or np.ndarray, shape (n_time,)
+        is_compute_acausal : bool, optional
+            Use future information to compute the posterior.
+        use_gpu : bool, optional
+            Use GPU for the state space part of the model, not the likelihood.
+
+        Returns
+        -------
+        results : xarray.Dataset
+
+        """
+        multiunits = np.asarray(multiunits)
+        is_track_interior = self.environment.is_track_interior_.ravel(order="F")
+        n_time = multiunits.shape[0]
+
+        logger.info("Estimating likelihood...")
+        results = {}
+        results["likelihood"] = scaled_likelihood(
+            _ClUSTERLESS_ALGORITHMS[self.clusterless_algorithm][1](
+                multiunits=multiunits,
+                place_bin_centers=self.environment.place_bin_centers_,
+                is_track_interior=is_track_interior,
+                **self.encoding_model_
+            )
+        )
+
+        return self._get_results(results, n_time, time, is_compute_acausal, use_gpu)
