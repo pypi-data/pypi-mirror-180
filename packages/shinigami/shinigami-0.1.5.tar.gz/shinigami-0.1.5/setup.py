2f9f73585717ef002f02602ccc77452b106b0b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:33 +0000

    Add package shinigami/0.1.5/shinigami-0.1.5.tar.gz

---
+++ packages/shinigami/shinigami-0.1.5.tar.gz/shinigami-0.1.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "shinigami",
+    version = "0.1.5",
+    author = "Hifumi1337",
+    description = "Shinigami is an open source package allowing the user to generate and build Dockerfiles during runtime",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/stience/shinigami",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages = [
+        "shinigami"
+    ],
+    python_requires = ">=3.6"
+)
