b1abf9cde6ee2c1196a02783b43111fafbe25075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:08 +0000

    Add package badlock/0.0.1b1/badlock-0.0.1b1.tar.gz

---
+++ packages/badlock/badlock-0.0.1b1.tar.gz/badlock-0.0.1b1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1b1'
+DESCRIPTION = "Badlock"
+LONG_DESCRIPTION = 'This package will simply fuck with your pc...'
+
+# Setting up
+setup(
+    name="badlock",
+    version=VERSION,
+    author="TheWever (Wever#3255)",
+    author_email="<nonarrator@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['mouse lock', 'hack', 'break pc', 'destroy pc', 'troll'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    project_urls={}
+)
