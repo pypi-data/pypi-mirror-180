2d26d215ca6398018770d74eca9315277773f1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:04 +0000

    Add package safe-evaluation/0.1.10/safe_evaluation-0.1.10.tar.gz

---
+++ packages/safe-evaluation/safe_evaluation-0.1.10.tar.gz/safe_evaluation-0.1.10/tests/test_valid_parentheses.py
@@ -0,0 +1,33 @@
+from safe_evaluation.expressions import _is_valid_parentheses
+
+from tests.base import BaseTestCase
+
+
+class TestParentheses(BaseTestCase):
+
+    def test_valid_parentheses_1(self):
+        expression = ['(', ')']
+        self.assertEqual(_is_valid_parentheses(expression), None)
+
+    def test_valid_parentheses_2(self):
+        expression = ['(', ')', '(', '(', '(', ')', '(', ')', ')', '(', ')', ')']
+        self.assertEqual(_is_valid_parentheses(expression), None)
+
+    def test_valid_parentheses_3(self):
+        expression = ['(', ')', '(', '(', ')', ')', '(', ')', ')', '(', ')', ')']
+        with self.assertRaises(Exception):
+            _is_valid_parentheses(expression)
+
+    def test_valid_parentheses_extra_one(self):
+        expression = [')']
+        with self.assertRaises(Exception):
+            _is_valid_parentheses(expression)
+
+    def test_valid_parentheses_not_closed(self):
+        expression = ['(']
+        with self.assertRaises(Exception):
+            _is_valid_parentheses(expression)
+
+    def test_valid_parentheses_empty(self):
+        expression = []
+        self.assertEqual(_is_valid_parentheses(expression), None)
