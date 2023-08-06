c335309b8195ca3191b29a8105a79dd642ec6145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:14 +0000

    Add package encapsulamientopython/0.2/encapsulamientopython-0.2.tar.gz

---
+++ packages/encapsulamientopython/encapsulamientopython-0.2.tar.gz/encapsulamientopython-0.2/encapsulamientopython.py
@@ -0,0 +1,2 @@
+def hola_mundo(name="Mundo"):
+    return "Libreria creada e importada con exito. Hola, {name}".format(name = name)
---
+++ packages/encapsulamientopython/encapsulamientopython-0.2.tar.gz/encapsulamientopython-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name='encapsulamientopython',
+    version='0.2',
+    package=find_packages(),
+    py_modules=['encapsulamientopython']
+
+)
