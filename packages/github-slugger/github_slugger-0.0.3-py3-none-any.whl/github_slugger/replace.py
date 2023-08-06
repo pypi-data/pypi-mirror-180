430b715f6f260acadb49bb54428091c084ac58db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:41 +0000

    Add package github-slugger/0.0.3/github_slugger-0.0.3-py3-none-any.whl

---
+++ packages/github-slugger/github_slugger-0.0.3-py3-none-any.whl/github_slugger/replace.py
@@ -0,0 +1,88 @@
+from .regex import single_byte, multi_byte
+
+
+def range_matcher(lower, higher):
+    return lambda chr: chr >= lower and chr <= higher
+
+
+def matcher(match):
+    return lambda chr: chr == match
+
+
+def range(str):
+    """Turns a string into a character matcher
+
+    input \uD80A matches only that number
+    input \uD80A\uD80B matches either the first or the last
+    input \uD80A-\uD80B matches including the the first and last
+    input \uD80A\uD80C-\uD80D matches the first char and the range for the
+        second chars
+    """
+    str_len = len(str)
+    str_pos = 0
+    res = []
+    while str_pos < str_len:
+        if str_pos < str_len - 1 and str[str_pos + 1] == '-':
+            res.append(range_matcher(ord(str[str_pos]), ord(str[str_pos + 2])))
+            str_pos += 3
+        else:
+            res.append(matcher(ord(str[str_pos])))
+            str_pos += 1
+    if len(res) == 1:
+        return res[0]
+
+    def chk(chr):
+        return next(filter(lambda fn: fn(chr), res), None) is not None
+
+    return chk
+
+
+def mb_range(tuple):
+    lower, higher = tuple
+    return range(lower), range(higher)
+
+
+single = range(single_byte)
+multi = list(map(mb_range, multi_byte))
+
+
+def read(b, i):
+    return b[i] | b[i + 1] << 8
+
+
+def multi_match(num):
+    for lower, higher in multi:
+        match = lower(num)
+        if match:
+            return higher
+    return None
+
+
+def replace(str):
+    b = str.encode('utf-16-le')
+    b_pos = 0
+    b_len = len(b)
+    res = []
+    while b_pos < b_len:
+        byte_16 = read(b, b_pos)
+        b_pos += 2
+        if byte_16 == 32:  # ' ' to '-'
+            res.append(45)
+            res.append(0)
+            continue
+        if single(byte_16):
+            # remove single match characters
+            continue
+        matcher = multi_match(byte_16)
+        if matcher is not None:
+            if b_pos == b_len:
+                break
+            next = read(b, b_pos)
+            if matcher(next):
+                b_pos += 2
+                continue
+        # append characters that match neighter 16 nor 32
+        res.append(byte_16 & 0xff)
+        res.append(byte_16 >> 8)
+
+    return bytes(res).decode('utf-16-le')
---
+++ packages/github-slugger/github_slugger-0.0.3-py3-none-any.whl/github_slugger/slug.py
@@ -0,0 +1,59 @@
+from .replace import replace
+
+
+class GithubSlugger:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        """Forget all previous slugs"""
+        self.occurrences = dict()
+
+    def slug(self, value: str, maintain_case: bool = False):
+        """Generate a unique slug.
+
+          Tracks previously generated slugs: repeated calls with the same value
+          will result in different slugs.
+          Use the `slug` function to get same slugs.
+
+          Parameters:
+          value (str): String of text to slugify
+          maintain_case (bool): Keep the current case, otherwise make all
+          lowercase
+
+          Returns:
+          str: A unique slug string
+        """
+        result = slug(value, maintain_case)
+        original_slug = result
+        count = self.occurrences.get(original_slug, 0)
+        while self.occurrences.get(result, None) is not None:
+            count += 1
+            result = f'{original_slug}-{count}'
+
+        self.occurrences[original_slug] = count
+        self.occurrences[result] = 0
+        return result
+
+
+def slug(value: str, maintain_case: bool = False):
+    """Generate a slug.
+
+      Does not track previously generated slugs: repeated calls with the same
+      value will result in the exact same slug.
+      Use the `GithubSlugger` class to get unique slugs.
+
+      Parameters:
+      value (str): String of text to slugify
+      maintain_case (bool): Keep the current case, otherwise make all lowercase
+
+      Returns:
+      str: A unique slug string
+    """
+    if type(value) != str:
+        return ''
+
+    if not maintain_case:
+        value = value.lower()
+
+    return replace(value)
