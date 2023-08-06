3194275cf0e5b3b64cdc2801034e00aa7979454f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:00 +0000

    Add package ZefroinLibrary/0.0.1/ZefroinLibrary-0.0.1-py3-none-any.whl

---
+++ packages/ZefroinLibrary/ZefroinLibrary-0.0.1-py3-none-any.whl/ZefroinLibrary/__init__.py
@@ -0,0 +1,8 @@
+from file import method
+
+from zfunct import rand
+from zfunct import slist
+from zfunct import splist
+from zfunct import rmlist
+from zfunct import fwrite
+from zfunct import fread
