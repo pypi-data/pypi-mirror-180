23d8f4ed422acd07525299f0aea87c9e300456ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:57 +0000

    Add package bread-slice/0.0.1a0/bread-slice-0.0.1a0.tar.gz

---
+++ packages/bread-slice/bread-slice-0.0.1a0.tar.gz/bread-slice-0.0.1a0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="bread-slice", 
+    version="0.0.1a", 
+    description="A Discord bot", 
+    author="Mattimated", 
+    packages=setuptools.find_packages(), 
+    project_urls={
+        "Source Code": "https://github.com/Mattimated/Bread-Slice",
+        "Issue Tracker": "https://github.com/Mattimated/Bread-Slice/issues", 
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning", 
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent",
+    ],
+
+)
