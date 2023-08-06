4f316208b9e50c15a61c1fc233d0dca73fe22bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:12 +0000

    Add package nonebot-plugin-multi-chatgpt/0.0.4.1/nonebot_plugin_multi_chatgpt-0.0.4.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-multi-chatgpt/nonebot_plugin_multi_chatgpt-0.0.4.1-py3-none-any.whl/nonebot_plugin_multi_chatgpt/config.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    # Your Config Here
+    chatgpt_session_token_list : List[str] = []
+    chatgpt_email_list: List[str] = []
+    chatgpt_passwd_list: List[str] = []
+
+    class Config:
+        extra = "ignore"
