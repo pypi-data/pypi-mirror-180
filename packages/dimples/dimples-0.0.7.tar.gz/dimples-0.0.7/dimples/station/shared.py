cede4c30265e1d6b19cb2442e41aaa0951804464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:27 +0000

    Add package dimples/0.0.7/dimples-0.0.7.tar.gz

---
+++ packages/dimples/dimples-0.0.7.tar.gz/dimples-0.0.7/dimples/station/shared.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2022 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from typing import Optional
+
+from dimsdk import ID
+
+from ..utils import Singleton
+from ..common import CommonFacebook
+from ..common import AccountDBI, MessageDBI, SessionDBI
+from ..database import AccountDatabase, MessageDatabase, SessionDatabase
+from ..server import Pusher, DefaultPusher, PushCenter
+from ..server import Dispatcher
+from ..server import UserDeliver, BotDeliver, StationDeliver
+from ..server import GroupDeliver, BroadcastDeliver
+from ..server import DeliverWorker, DefaultRoamer
+
+from ..config import Config
+from .ans import AddressNameService, AddressNameServer, ANSFactory
+
+
+@Singleton
+class GlobalVariable:
+
+    def __init__(self):
+        super().__init__()
+        self.config: Optional[Config] = None
+        self.adb: Optional[AccountDBI] = None
+        self.mdb: Optional[MessageDBI] = None
+        self.sdb: Optional[SessionDBI] = None
+        self.facebook: Optional[CommonFacebook] = None
+        self.pusher: Optional[Pusher] = None
+
+
+def create_database(shared: GlobalVariable) -> (AccountDBI, MessageDBI, SessionDBI):
+    config = shared.config
+    root = config.database_root
+    public = config.database_public
+    private = config.database_private
+    # create database
+    adb = AccountDatabase(root=root, public=public, private=private)
+    mdb = MessageDatabase(root=root, public=public, private=private)
+    sdb = SessionDatabase(root=root, public=public, private=private)
+    adb.show_info()
+    mdb.show_info()
+    sdb.show_info()
+    shared.adb = adb
+    shared.mdb = mdb
+    shared.sdb = sdb
+    # add neighbors
+    neighbors = config.neighbors
+    for node in neighbors:
+        print('adding neighbor node: (%s:%d), ID="%s"' % (node.host, node.port, node.identifier))
+        sdb.add_neighbor(host=node.host, port=node.port, identifier=node.identifier)
+    return adb, mdb, sdb
+
+
+def create_facebook(shared: GlobalVariable) -> CommonFacebook:
+    # set account database
+    facebook = CommonFacebook(database=shared.adb)
+    shared.facebook = facebook
+    # set current station
+    sid = shared.config.station_id
+    if sid is not None:
+        # make sure private key exists
+        assert facebook.private_key_for_visa_signature(identifier=sid) is not None, \
+            'failed to get sign key for current station: %s' % sid
+        print('set current user: %s' % sid)
+        facebook.current_user = facebook.user(identifier=sid)
+    return facebook
+
+
+def create_ans(shared: GlobalVariable) -> AddressNameService:
+    ans = AddressNameServer()
+    factory = ID.factory()
+    ID.register(factory=ANSFactory(factory=factory, ans=ans))
+    # load ANS records from 'config.ini'
+    config = shared.config
+    ans.fix(fixed=config.ans_records)
+    return ans
+
+
+def create_pusher(shared: GlobalVariable) -> Pusher:
+    pusher = DefaultPusher(facebook=shared.facebook)
+    shared.pusher = pusher
+    # start PushCenter
+    center = PushCenter()
+    # TODO: add push services
+    center.start()
+    return pusher
+
+
+def create_dispatcher(shared: GlobalVariable) -> Dispatcher:
+    # create dispatcher
+    dispatcher = Dispatcher()
+    dispatcher.database = shared.mdb
+    dispatcher.facebook = shared.facebook
+    # set base deliver delegates
+    user_deliver = UserDeliver(database=shared.mdb, pusher=shared.pusher)
+    bot_deliver = BotDeliver(database=shared.mdb)
+    station_deliver = StationDeliver()
+    dispatcher.set_user_deliver(deliver=user_deliver)
+    dispatcher.set_bot_deliver(deliver=bot_deliver)
+    dispatcher.set_station_deliver(deliver=station_deliver)
+    # set special deliver delegates
+    group_deliver = GroupDeliver(facebook=shared.facebook)
+    broadcast_deliver = BroadcastDeliver(database=shared.sdb)
+    dispatcher.set_group_deliver(deliver=group_deliver)
+    dispatcher.set_broadcast_deliver(deliver=broadcast_deliver)
+    # set roamer & worker
+    roamer = DefaultRoamer(database=shared.mdb)
+    worker = DeliverWorker(database=shared.sdb, facebook=shared.facebook)
+    dispatcher.set_roamer(roamer=roamer)
+    dispatcher.set_deliver_worker(worker=worker)
+    # start all delegates
+    user_deliver.start()
+    bot_deliver.start()
+    station_deliver.start()
+    roamer.start()
+    return dispatcher
+
+
+# noinspection PyUnusedLocal
+def stop_dispatcher(shared: GlobalVariable) -> bool:
+    # TODO: stop Dispatcher
+    # dispatcher = Dispatcher()
+    # dispatcher.stop()
+    return True
+
+
+# noinspection PyUnusedLocal
+def stop_pusher(shared: GlobalVariable) -> bool:
+    # stop PushCenter
+    center = PushCenter()
+    center.stop()
+    return True
---
+++ packages/dimples/dimples-0.0.7.tar.gz/dimples-0.0.7/dimples/station/start.py
@@ -0,0 +1,138 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#   DIMS : DIM Station
+#
+#                                Written in 2022 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2022 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+
+import os
+import sys
+import getopt
+from socketserver import ThreadingTCPServer
+
+path = os.path.abspath(__file__)
+path = os.path.dirname(path)
+path = os.path.dirname(path)
+path = os.path.dirname(path)
+sys.path.insert(0, path)
+
+from dimples.utils import Log
+from dimples.database import Storage
+
+from dimples.config import Config
+from dimples.station.shared import GlobalVariable
+from dimples.station.shared import create_database, create_facebook
+from dimples.station.shared import create_ans, create_pusher, stop_pusher
+from dimples.station.shared import create_dispatcher, stop_dispatcher
+from dimples.station.handler import RequestHandler
+
+
+#
+# show logs
+#
+Log.LEVEL = Log.DEVELOP
+
+
+DEFAULT_CONFIG = '/etc/dim/station.ini'
+
+
+def show_help():
+    cmd = sys.argv[0]
+    print('')
+    print('    DIM Network Station')
+    print('')
+    print('usages:')
+    print('    %s [--config=<FILE>]' % cmd)
+    print('    %s [-h|--help]' % cmd)
+    print('')
+    print('optional arguments:')
+    print('    --config        config file path (default: "%s")' % DEFAULT_CONFIG)
+    print('    --help, -h      show this help message and exit')
+    print('')
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(args=sys.argv[1:],
+                                   shortopts='hf:',
+                                   longopts=['help', 'config='])
+    except getopt.GetoptError:
+        show_help()
+        sys.exit(1)
+    # check options
+    ini_file = None
+    for opt, arg in opts:
+        if opt == '--config':
+            ini_file = arg
+        else:
+            show_help()
+            sys.exit(0)
+    # check config filepath
+    if ini_file is None:
+        ini_file = DEFAULT_CONFIG
+    if not Storage.exists(path=ini_file):
+        show_help()
+        print('')
+        print('!!! config file not exists: %s' % ini_file)
+        print('')
+        sys.exit(0)
+    # load config
+    config = Config.load(file=ini_file)
+    # initializing
+    print('[DB] init with config: %s => %s' % (ini_file, config))
+    shared = GlobalVariable()
+    shared.config = config
+    create_database(shared=shared)
+    create_facebook(shared=shared)
+    create_ans(shared=shared)
+    create_pusher(shared=shared)
+    create_dispatcher(shared=shared)
+
+    # start TCP server
+    try:
+        # ThreadingTCPServer.allow_reuse_address = True
+        server_address = (config.station_host, config.station_port)
+        server = ThreadingTCPServer(server_address=server_address,
+                                    RequestHandlerClass=RequestHandler,
+                                    bind_and_activate=False)
+        Log.info(msg='>>> TCP server %s starting...' % str(server_address))
+        server.allow_reuse_address = True
+        server.server_bind()
+        server.server_activate()
+        Log.info(msg='>>> TCP server %s is listening...' % str(server_address))
+        server.serve_forever()
+    except KeyboardInterrupt as ex:
+        Log.info(msg='~~~~~~~~ %s' % ex)
+    finally:
+        stop_dispatcher(shared=shared)
+        stop_pusher(shared=shared)
+        Log.info(msg='======== station shutdown!')
+
+
+if __name__ == '__main__':
+    main()
