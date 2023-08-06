b7240c470ab882faf3beabf0e3d8c9f97b4e904d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:01:25 +0000

    Add package discord-ipc/0.0.6/discord-ipc-0.0.6.tar.gz

---
+++ packages/discord-ipc/discord-ipc-0.0.6.tar.gz/discord-ipc-0.0.6/discord/ext/ipc/__init__.py
@@ -0,0 +1,13 @@
+import collections
+
+from discord.ext.ipc.client import Client
+from discord.ext.ipc.server import Server
+from discord.ext.ipc.errors import *
+
+if discord.version_info.major < 2:
+    raise RuntimeError("You must have discord.py (v2.0 or greater) to use this library.")
+
+_VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
+
+version = "0.0.6"
+version_info = _VersionInfo(0, 0, 6, "final", 0)
