40f697f840e79c8155fe0d6b2b242dd880328f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:59 +0000

    Add package mariadb-dyncol/3.6.1/mariadb_dyncol-3.6.1-py3-none-any.whl

---
+++ packages/mariadb-dyncol/mariadb_dyncol-3.6.1-py3-none-any.whl/mariadb_dyncol/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from .base import DynColLimitError
+from .base import DynColNotSupported
+from .base import DynColTypeError
+from .base import DynColValueError
+from .base import pack
+from .base import unpack
+
+__all__ = (
+    "DynColLimitError",
+    "DynColNotSupported",
+    "DynColTypeError",
+    "DynColValueError",
+    "pack",
+    "unpack",
+)
