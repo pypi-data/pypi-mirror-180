741f2189d29766c0dc86ce5cc8bae3899ea7a492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:51:11 +0000

    Add package latest-news/0.1/latest-news-0.1.tar.gz

---
+++ packages/latest-news/latest-news-0.1.tar.gz/latest-news-0.1/BeritaTerkini/__init__.py
@@ -0,0 +1,60 @@
+import requests as requests
+from bs4 import BeautifulSoup
+
+
+def isi():
+
+    try:
+        link = requests.get('https://www.kompas.com/')
+
+
+
+    except Exception:
+        return Exception
+
+    if link.status_code == 200:
+        package = BeautifulSoup(link.text,'html.parser')
+        DataPackage = package.find('div',{'class':'ga--latest'})
+
+        DataPackage = DataPackage.findChildren('h3')
+        i=0;
+        data1 = None
+        data2 = None
+        data3 = None
+        data4 = None
+
+        for dataS in DataPackage:
+
+            if i == 1:
+                data1 = dataS.text
+            elif i == 2:
+                data2 = dataS.text
+            elif i == 3:
+                data3 = dataS.text
+            elif i == 4:
+                data4 = dataS.text
+            i = i +1
+
+
+
+
+
+        data = dict()
+        data['data1'] = data1
+        data['data2'] = data2
+        data['data3'] = data3
+        data['data4'] = data4
+        return data
+
+
+
+def wadah(data):
+    print(f'Berita bahwa {data["data1"]}')
+    print(f'Dan mengenai peristiwa bahwa {data["data2"]}')
+    print(f'Adapun juga  berita tentang {data["data3"]}')
+    print(f'Dan dilanjutkan dengan  {data["data4"]}')
+
+
+if __name__ == '__main__':
+    data = isi()
+    wadah(data)
---
+++ packages/latest-news/latest-news-0.1.tar.gz/latest-news-0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latest-news",
+    version="0.1",
+    author="Rafi Ramdhani",
+    author_email="rafiramdhani1122@gmail.com",
+    description="This package will get the latest news from the source website https://www.kompas.com/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/017RAFIRAMDHANI/PYTHON.git",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
