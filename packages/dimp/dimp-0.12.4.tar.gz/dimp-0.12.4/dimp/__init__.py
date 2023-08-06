9f53b36db842fee6e21843a9deef9adfee10bb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:36 +0000

    Add package dimp/0.12.4/dimp-0.12.4.tar.gz

---
+++ packages/dimp/dimp-0.12.4.tar.gz/dimp-0.12.4/dimp/__init__.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+#
+#   DIMP : Decentralized Instant Messaging Protocol
+#
+#                                Written in 2019 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from mkm import *
+from dkd import *
+
+from .protocol import *
+from .mkm import *
+from .dkd import *
+
+from .barrack import Barrack
+from .transceiver import Transceiver
+from .packer import Packer
+from .processor import Processor
+
+name = "DIMP"
+
+__author__ = 'Albert Moky'
+
+__all__ = [
+
+    #
+    #   MingKeMing
+    #
+    'EntityType', 'MetaType',
+    'Address', 'AddressFactory',
+    'ID', 'IDFactory',
+    'Meta', 'MetaFactory',
+    'Document', 'DocumentFactory',
+    'Visa', 'Bulletin',
+
+    'BaseAddressFactory',  # 'BroadcastAddress',
+    # 'IdentifierFactory', 'Identifier',
+    'ANYWHERE', 'EVERYWHERE', 'ANYONE', 'EVERYONE', 'FOUNDER',
+    'BaseMeta',
+    'BaseDocument', 'BaseVisa', 'BaseBulletin',
+
+    #
+    #   DaoKeDao
+    #
+    'ContentType', 'Content', 'ContentFactory',
+    'Envelope',  # 'EnvelopeFactory',
+    'Message', 'InstantMessage', 'SecureMessage', 'ReliableMessage',
+    # 'InstantMessageFactory', 'SecureMessageFactory', 'ReliableMessageFactory',
+    'InstantMessageDelegate', 'SecureMessageDelegate', 'ReliableMessageDelegate',
+
+    'BaseContent',
+    # 'MessageEnvelope', 'MessageEnvelopeFactory',
+    # 'BaseMessage',
+    # 'PlainMessage', 'PlainMessageFactory',
+    # 'EncryptedMessage', 'EncryptedMessageFactory',
+    # 'NetworkMessage', 'NetworkMessageFactory',
+
+    #
+    #   Protocol
+    #
+    'TextContent', 'ForwardContent', 'ArrayContent',
+    'MoneyContent', 'TransferContent',
+    'FileContent', 'ImageContent', 'AudioContent', 'VideoContent',
+    'PageContent', 'CustomizedContent',
+
+    'Command', 'CommandFactory',  # 'command_name',
+    'MetaCommand', 'DocumentCommand',
+
+    'HistoryCommand', 'GroupCommand',
+    'InviteCommand', 'ExpelCommand', 'JoinCommand',
+    'QuitCommand', 'QueryCommand', 'ResetCommand',
+
+    #
+    #   Core
+    #
+    'EntityDelegate',
+    'EntityDataSource', 'UserDataSource', 'GroupDataSource',
+    'Entity', 'User', 'Group',
+    'BaseEntity', 'BaseUser', 'BaseGroup',
+
+    'BaseTextContent', 'SecretContent', 'ListContent',
+    'BaseMoneyContent', 'TransferMoneyContent',
+    'BaseFileContent', 'ImageFileContent', 'AudioFileContent', 'VideoFileContent',
+    'WebPageContent', 'AppCustomizedContent',
+    'BaseCommand',
+    'BaseMetaCommand', 'BaseDocumentCommand',
+    'BaseHistoryCommand', 'BaseGroupCommand',
+    'InviteGroupCommand', 'ExpelGroupCommand', 'JoinGroupCommand',
+    'QuitGroupCommand', 'QueryGroupCommand', 'ResetGroupCommand',
+
+    # 'ContentFactoryBuilder', 'CommandFactoryBuilder',
+    # 'GeneralCommandFactory', 'HistoryCommandFactory', 'GroupCommandFactory',
+    # 'register_message_factories', 'register_content_factories', 'register_command_factories',
+
+    'Barrack', 'Transceiver', 'Packer', 'Processor',
+]
---
+++ packages/dimp/dimp-0.12.4.tar.gz/dimp-0.12.4/dimp/protocol/command.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+#
+#   DIMP : Decentralized Instant Messaging Protocol
+#
+#                                Written in 2019 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from abc import ABC, abstractmethod
+from typing import Optional, Any, Dict
+
+from mkm.types import Wrapper
+from dkd.protocol import content_type
+from dkd import Content
+
+
+class Command(Content, ABC):
+    """
+        Command Message Content
+        ~~~~~~~~~~~~~~~~~~~~~~~
+
+        data format: {
+            type : 0x88,
+            sn   : 123,
+
+            cmd     : "...", // command name
+            extra   : info   // command parameters
+        }
+    """
+
+    # -------- command names begin --------
+    META = 'meta'
+    DOCUMENT = 'document'
+    # -------- command names end --------
+
+    @property
+    @abstractmethod
+    def cmd(self) -> str:
+        """ get command name """
+        # return command_name(content=self.dictionary)
+        raise NotImplemented
+
+    #
+    #   Factory method
+    #
+
+    @classmethod
+    def parse(cls, content: Any):  # -> Command:
+        if content is None:
+            return None
+        elif isinstance(content, Command):
+            return content
+        info = Wrapper.get_dictionary(content)
+        # assert info is not None, 'command content error: %s' % content
+        cmd = command_name(content=info)
+        factory = cls.factory(cmd=cmd)
+        if factory is None:
+            # unknown command name, get base command factory
+            msg_type = content_type(content=info)
+            factory = Content.factory(msg_type=msg_type)
+            assert isinstance(factory, CommandFactory), 'command factory error: %d, %s' % (msg_type, factory)
+        # assert isinstance(factory, CommandFactory), 'command factory error: %s' % factory
+        return factory.parse_command(content=info)
+
+    @classmethod
+    def factory(cls, cmd: str):  # -> Optional[CommandFactory]:
+        return g_command_factories.get(cmd)
+
+    @classmethod
+    def register(cls, cmd: str, factory):
+        g_command_factories[cmd] = factory
+
+
+def command_name(content: Dict[str, Any]) -> str:
+    # TODO: modify after all server/clients support 'cmd'
+    cmd = content.get('cmd')
+    if cmd is None:
+        cmd = content.get('command')
+    return cmd
+
+
+class CommandFactory:
+
+    @abstractmethod
+    def parse_command(self, content: Dict[str, Any]) -> Optional[Command]:
+        """
+        Parse map object to command
+
+        :param content: command content
+        :return: Command
+        """
+        raise NotImplemented
+
+
+g_command_factories: Dict[str, CommandFactory] = {}
