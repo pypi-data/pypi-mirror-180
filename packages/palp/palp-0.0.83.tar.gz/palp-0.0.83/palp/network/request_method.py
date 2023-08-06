b2586db447c1b6fad64cd2648133f0bcec70dc06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:10 +0000

    Add package palp/0.0.83/palp-0.0.83.tar.gz

---
+++ packages/palp/palp-0.0.83.tar.gz/palp-0.0.83/palp/network/request_method.py
@@ -0,0 +1,44 @@
+from palp.network.request import Request
+
+
+class RequestGet(Request):
+    def __init__(self, url, **kwargs):
+        kwargs.update({'url': url, 'method': 'GET'})
+        super().__init__(**kwargs)
+
+
+class RequestPost(Request):
+    def __init__(self, url, **kwargs):
+        kwargs.update({'url': url, 'method': 'POST'})
+        super().__init__(**kwargs)
+
+
+class RequestOptions(Request):
+    def __init__(self, url, **kwargs):
+        kwargs.update({'url': url, 'method': 'OPTIONS'})
+        super().__init__(**kwargs)
+
+
+class RequestHead(Request):
+    def __init__(self, url, **kwargs):
+        kwargs.update({'url': url, 'method': 'HEAD'})
+        super().__init__(**kwargs)
+
+
+class RequestPatch(Request):
+    def __init__(self, url, **kwargs):
+        kwargs.update({'url': url, 'method': 'PATCH'})
+        super().__init__(**kwargs)
+
+
+class RequestDelete(Request):
+    def __init__(self, url, **kwargs):
+        kwargs.update({'url': url, 'method': 'DELETE'})
+        super().__init__(**kwargs)
+
+
+if __name__ == '__main__':
+    response = RequestGet(url='https://www.baidu.com', titan_meta={'name': '张三'}).send()[1]
+    print(response.text)
+    print(response.titan)
+    print(response.titan_meta)
