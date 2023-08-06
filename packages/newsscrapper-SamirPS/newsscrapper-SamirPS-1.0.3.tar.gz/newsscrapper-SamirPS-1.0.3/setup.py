fc36f8d0a3df12bd87cb8a60bfb26647a76beba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:30:31 +0000

    Add package newsscrapper-SamirPS/1.0.3/newsscrapper-SamirPS-1.0.3.tar.gz

---
+++ packages/newsscrapper-SamirPS/newsscrapper-SamirPS-1.0.3.tar.gz/newsscrapper-SamirPS-1.0.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup,find_packages
+from setuptools.command.install import install as _install
+
+
+class DownloadNLTK(_install):
+    def run(self):
+        self.do_egg_install()
+        import nltk
+        required_nltk = [
+            'brown',  # Required for FastNPExtractor
+            'punkt',  # Required for WordTokenizer
+            'maxent_treebank_pos_tagger',  # Required for NLTKTagger
+            'movie_reviews',  # Required for NaiveBayesAnalyzer
+            'wordnet',  # Required for lemmatization and Wordnet
+            'stopwords'
+        ]
+        for i in required_nltk:
+            nltk.download(i)
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="newsscrapper-SamirPS",  # Replace with your own username
+    version="1.0.3",
+    author="SamirPS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SamirPS/News-Scrapper",
+    install_requires=["requests", "bs4", "lxml","newspaper3k"],
+    packages=["newsscrapper"],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    cmdclass={'download_nltk': DownloadNLTK},
+)
+
+
