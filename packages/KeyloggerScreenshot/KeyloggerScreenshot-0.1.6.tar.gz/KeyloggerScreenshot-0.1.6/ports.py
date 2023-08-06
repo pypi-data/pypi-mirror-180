ff25f78c9e22b53db35ffa29937da73b01ce1900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:08 +0000

    Add package KeyloggerScreenshot/0.1.6/KeyloggerScreenshot-0.1.6.tar.gz

---
+++ packages/KeyloggerScreenshot/KeyloggerScreenshot-0.1.6.tar.gz/KeyloggerScreenshot-0.1.6/ports.py
@@ -0,0 +1,13 @@
+port_photos = 6357
+port_keylogger = 5187
+port_listener = 7128
+port_time = 3926port_photos = 5198
+port_keylogger = 8546
+port_listener = 9174
+port_time = 2431port_photos = 6457
+port_keylogger = 1548
+port_listener = 6498
+port_time = 2951port_photos = 4783
+port_keylogger = 2817
+port_listener = 9754
+port_time = 5431
