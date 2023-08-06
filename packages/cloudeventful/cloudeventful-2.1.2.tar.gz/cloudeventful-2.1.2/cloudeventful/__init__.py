562059118c8772f0f2cd6cb07429359ceb7b9e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:37 +0000

    Add package cloudeventful/2.1.2/cloudeventful-2.1.2.tar.gz

---
+++ packages/cloudeventful/cloudeventful-2.1.2.tar.gz/cloudeventful-2.1.2/cloudeventful/__init__.py
@@ -0,0 +1,5 @@
+"""Cloud Eventful classes and models."""
+from cloudeventful._cloud_event import CloudEvent
+from cloudeventful._cloud_eventful import CloudEventful
+
+__all__ = ("CloudEvent", "CloudEventful")
