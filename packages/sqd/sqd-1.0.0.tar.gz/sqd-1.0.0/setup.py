d930127bb302f29131034e4e920cf7df67ba85be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:04 +0000

    Add package sqd/1.0.0/sqd-1.0.0.tar.gz

---
+++ packages/sqd/sqd-1.0.0.tar.gz/sqd-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sqd',
+    version='1.0.0',
+    license='MIT',
+    author='xllwhoami',
+    description='Go to homepage',
+    author_email='xllwhoami@ukr.net',
+    packages=find_packages('sqd'),
+    package_dir={'': 'sqd'},
+    url='https://github.com/xllwhoami/sqd',
+    keywords='sqlite db config database lite',
+    install_requires=[],
+
+)
