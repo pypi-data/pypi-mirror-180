315effe3c2bae451f41b9a1a95dd88c85e607ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:04 +0000

    Add package SalamCrypt/0.0.5/SalamCrypt-0.0.5.tar.gz

---
+++ packages/SalamCrypt/SalamCrypt-0.0.5.tar.gz/SalamCrypt-0.0.5/src/SalamCrypt/__init__.py
@@ -0,0 +1,51 @@
+try:
+	import requests  ,re, os , sys , random , uuid , user_agent , json,secrets,secrets
+	from uuid import uuid4
+	from secrets import *
+	from user_agent import generate_user_agent
+	import requests
+	import names
+	import uuid,string
+	import instaloader
+	import hashlib
+	import urllib
+	import mechanize
+	import json
+	import secrets
+	import smtplib
+	import time
+    
+	
+except ImportError:
+	os.system('pip install requests')
+
+	
+from base64 import b64encode, b64decode
+from binascii import unhexlify
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+import random
+import sys
+import datetime,threading,time
+import secrets,socket,pyfiglet
+import os,marshal
+from SalamHunter import *
+salamhunter3=AES.MODE_CBC
+salamhunter4=AES.block_size
+class SalamCrypt:
+    def enc(salamhunter5):
+        namaste22=pad(salamhunter5.encode(),salamhunter4)
+        salamhunter=os.urandom(16)
+        salamhunter1=AES.new(salamhunter,salamhunter3,salamhunter)
+        salamhunter2=salamhunter1.encrypt(namaste22)
+        out = b64encode(salamhunter2).decode('utf-8')
+        out1 = b64encode(salamhunter).decode('utf-8')
+        return f'{out1}??{out}'
+    def ex(namaste99):
+        namaste6= namaste99.split('??')[0]
+        namaste7= namaste99.split('??')[1]
+        namaste8= pad(namaste7.encode(),salamhunter4)
+        namaste10 = AES.new(b64decode(namaste6),salamhunter3,b64decode(namaste6))
+        exec(unpad(namaste10.decrypt(b64decode(namaste8)),salamhunter4).decode('utf-8'))
+
+
