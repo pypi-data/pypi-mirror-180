faded508a67e40cdb1f9da0755589a3029724ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:19 +0000

    Add package meu-pacote-aa/1.4.0/meu_pacote_aa-1.4.0.tar.gz

---
+++ packages/meu-pacote-aa/meu_pacote_aa-1.4.0.tar.gz/meu_pacote_aa-1.4.0/meu_pacote_aa/aa.py
@@ -0,0 +1,11 @@
+import hello
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog='meu_pacote_aa', description='Meu pacote python aa')
+    parser.add_argument('name', type=str, help='O seu nome')
+
+    args = parser.parse_args()
+
+    hello.hello_word(args.name)
+
---
+++ packages/meu-pacote-aa/meu_pacote_aa-1.4.0.tar.gz/meu_pacote_aa-1.4.0/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup, find_packages
+  
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+  
+long_description = 'Meu pacotezinho python aaaa \
+      hmmmm que pacote interessante'
+  
+setup(
+        name ='meu_pacote_aa',
+        version ='1.4.0',
+        author ='Rafael Nobre',
+        author_email ='rafaelmedeirosnobre2001@gmail.com',
+        description ='Um simples pacote python hmmmm',
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        packages = find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'meu_pacote_aa = meu_pacote_aa.aa:main'
+            ]
+        },
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords ='meu_pacote_aa aa',
+        install_requires = requirements,
+        zip_safe = False
+)
