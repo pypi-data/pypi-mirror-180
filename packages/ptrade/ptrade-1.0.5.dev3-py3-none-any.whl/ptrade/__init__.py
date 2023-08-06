e9d82c7c89ec0b9fc9dc33c92126106cd53a082c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:18 +0000

    Add package ptrade/1.0.5.dev3/ptrade-1.0.5.dev3-py3-none-any.whl

---
+++ packages/ptrade/ptrade-1.0.5.dev3-py3-none-any.whl/ptrade/__init__.py
@@ -0,0 +1,9 @@
+from ptrade import component
+from ptrade import exception
+from ptrade.simulate import Simulate, SimulateMultiply
+from ptrade.version import __version__
+
+
+def test():
+    print('aaa')
+
