c5e700c52b2aa0373af85785785049048e2d5393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:18 +0000

    Add package gradio/3.12.0b7/gradio-3.12.0b7.tar.gz

---
+++ packages/gradio/gradio-3.12.0b7.tar.gz/gradio-3.12.0b7/gradio/templates/cdn/assets/color.4bbfe36f.js
@@ -0,0 +1,2 @@
+import{aj as o}from"./index.3133cfbd.js";const t=r=>o[r%o.length];export{t as g};
+//# sourceMappingURL=color.4bbfe36f.js.map
---
+++ packages/gradio/gradio-3.12.0b7.tar.gz/gradio-3.12.0b7/gradio/templates/cdn/assets/index.f30166d0.js
@@ -0,0 +1,2 @@
+import{ar as r}from"./index.3133cfbd.js";const e=["static","dynamic"],o=t=>({type:"string",description:"text string",example_data:t.value||"hello world"});export{r as Component,o as document,e as modes};
+//# sourceMappingURL=index.f30166d0.js.map
