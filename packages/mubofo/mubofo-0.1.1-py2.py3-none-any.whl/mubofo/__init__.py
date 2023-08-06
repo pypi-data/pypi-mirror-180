7f63ddb974d69f7e9c31accf841a4b5dfd850fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:56 +0000

    Add package mubofo/0.1.1/mubofo-0.1.1-py2.py3-none-any.whl

---
+++ packages/mubofo/mubofo-0.1.1-py2.py3-none-any.whl/mubofo/__init__.py
@@ -0,0 +1,7 @@
+"""Boosted forest regression with native support for multioutput problems."""
+
+__version__ = '0.1.1'
+
+from ._models import BoostedForestRegressor
+
+__all__ = ['BoostedForestRegressor']
