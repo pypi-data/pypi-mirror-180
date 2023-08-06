440f2c8abc7dfd3beebc849d015a53ddd005744c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:59 +0000

    Add package chinilla-blockchain/1.3.1/chinilla-blockchain-1.3.1.tar.gz

---
+++ packages/chinilla-blockchain/chinilla-blockchain-1.3.1.tar.gz/chinilla-blockchain-1.3.1/chinilla/types/spend_bundle_conditions.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from chinilla_rs import Spend, SpendBundleConditions
+
+__all__ = ["Spend", "SpendBundleConditions"]
---
+++ packages/chinilla-blockchain/chinilla-blockchain-1.3.1.tar.gz/chinilla-blockchain-1.3.1/chinilla/wallet/cat_wallet/cat_constants.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+STABLY_USDS = {
+    "asset_id": "6d95dae356e32a71db5ddcb42224754a02524c615c5fc35f568c2af04774e589",
+    "name": "Stably USD",
+    "symbol": "USDS",
+}
+
+DEFAULT_CATS = {
+    STABLY_USDS["asset_id"]: STABLY_USDS,
+}
