2313c5ad622dafdcf274c6083a9f843e4e8d627c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:40 +0000

    Add package mpl-chord-diagram/0.4.1/mpl_chord_diagram-0.4.1.zip

---
+++ packages/mpl-chord-diagram/mpl_chord_diagram-0.4.1.zip/mpl_chord_diagram-0.4.1/__init__.py
@@ -0,0 +1,9 @@
+"""
+Chord diagram
+=============
+
+Tools to draw a chord diagram in Python, using matplotlib.
+"""
+
+from ._info import __author__, __version__
+from .chord_diagram import chord_diagram
