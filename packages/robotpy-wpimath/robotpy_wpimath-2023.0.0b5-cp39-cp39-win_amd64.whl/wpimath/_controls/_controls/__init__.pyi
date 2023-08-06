062a14766c72d7a6fd50854389fbfc0672d5b8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:15 +0000

    Add package robotpy-wpimath/2023.0.0b5/robotpy_wpimath-2023.0.0b5-cp39-cp39-win_amd64.whl

---
+++ packages/robotpy-wpimath/robotpy_wpimath-2023.0.0b5-cp39-cp39-win_amd64.whl/wpimath/_controls/_controls/__init__.pyi
@@ -0,0 +1,14 @@
+from __future__ import annotations
+import wpimath._controls._controls
+import typing
+
+__all__ = [
+    "constraint",
+    "controller",
+    "estimator",
+    "plant",
+    "system",
+    "trajectory"
+]
+
+
