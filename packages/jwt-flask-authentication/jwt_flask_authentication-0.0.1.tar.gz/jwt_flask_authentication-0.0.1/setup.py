8daf9db92e8694b3893a63f292c4a70be9044812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:57:41 +0000

    Add package jwt-flask-authentication/0.0.1/jwt_flask_authentication-0.0.1.tar.gz

---
+++ packages/jwt-flask-authentication/jwt_flask_authentication-0.0.1.tar.gz/jwt_flask_authentication-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='jwt_flask_authentication',
+    version='0.0.1',
+    author='Eteil Junior Djoumatchoua',
+    description='Provide an  easy way to handle authentication(login with jwt generated, token required decorator) in flask applications',
+    packages=['jwt_flask_authentication']
+)
