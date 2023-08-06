d820fb690847cab8413ba0fae46dd630d618607d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:30 +0000

    Add package rqrcode/0.0.1/rqrcode-0.0.1-cp37-abi3-win32.whl

---
+++ packages/rqrcode/rqrcode-0.0.1-cp37-abi3-win32.whl/rqrcode/__init__.py
@@ -0,0 +1,19 @@
+# import platform
+
+# if platform.system() == 'Windows':
+#     from rqrcode import rqrcode
+# elif platform.system() == 'Linux' or platform.system() == 'Darwin':
+#     from .rqrcode import rqrcode
+
+
+# from rqrcode import rqrcode
+
+# __all__=['qrcode_unicode','qrcode_svg','qrcode_img']
+
+# import os
+
+# os.path.abspath(os.path.dirname(__file__))
+# from . import rqrcode
+
+# __name__ ==   rqrcode
+    
