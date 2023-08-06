da539c6e3b749df0a695eb2f95f70262e0742d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:11 +0000

    Add package collective.contentsections/1.0.0a14/collective.contentsections-1.0.0a14.tar.gz

---
+++ packages/collective.contentsections/collective.contentsections-1.0.0a14.tar.gz/collective.contentsections-1.0.0a14/src/collective/contentsections/sections/links/views.py
@@ -0,0 +1,24 @@
+from collective.contentsections.sections.base import BaseLinksSectionView
+from collective.contentsections.sections.base import ISection
+from plone import api
+
+
+class LinksSectionView(BaseLinksSectionView):
+    """LinksSection view"""
+
+    @property
+    def items(self):
+        lead_image_scale = self.item_lead_image_scale
+        brains = api.content.find(context=self.context, depth=1, portal_type="Link")
+        results = [
+            {
+                "title": brain.Title,
+                "description": brain.Description,
+                "url": brain.getRemoteUrl,
+                "lead_image_url": f"{brain.getURL()}/@@images/image/{lead_image_scale}",
+                "effective_date": None,
+                "tags": brain.Subject,
+            }
+            for brain in brains
+        ]
+        return results
