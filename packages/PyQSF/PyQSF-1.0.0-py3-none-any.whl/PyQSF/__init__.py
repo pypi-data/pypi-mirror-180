c1f3adc301973c35aa422545cb16677829a6e811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:03 +0000

    Add package PyQSF/1.0.0/PyQSF-1.0.0-py3-none-any.whl

---
+++ packages/PyQSF/PyQSF-1.0.0-py3-none-any.whl/PyQSF/__init__.py
@@ -0,0 +1,15 @@
+# Name -> Python Quelert System Files [PyQSF]
+# Version -> 1.0.0
+
+# Publisher -> Zadvornow Georgy
+# Owner -> Zadvornow Georgy
+# Pypi -> https://pypi.org/
+# Site -> None
+# Company -> None
+# The person in the team -> 0 
+
+def init():
+    import PyQSF
+
+if __name__ == "__main__":
+    init()
