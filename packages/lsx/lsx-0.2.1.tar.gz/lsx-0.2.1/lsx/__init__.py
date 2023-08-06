6adf7cfac9a103017c3493f5c8dd352010bfe6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:36 +0000

    Add package lsx/0.2.1/lsx-0.2.1.tar.gz

---
+++ packages/lsx/lsx-0.2.1.tar.gz/lsx-0.2.1/lsx/__init__.py
@@ -0,0 +1,51 @@
+# top module functions
+all = help = None
+
+# base on built-in library
+from .cls import *
+from .dec import *
+from .new import *
+from .line import *
+from .useful import *
+from .windll import *
+
+# 3rd-library needed
+from .auto import *
+from .doc import *
+from .gui import *
+from .np import *
+
+
+def all():
+    import sys
+    import inspect
+    table = {}
+    for module, obj in globals().items():
+        if inspect.ismodule(obj) and obj.__name__.count('.') == 1:
+            for name in obj.__all__:
+                if name not in table:
+                    table[name] = module
+                elif name != '__all__':
+                    print("Warnning: '%s.%s' exist in '%s.%s'" % (module, name, table[name], name), file=sys.stderr)
+    return ['all', 'help'] + list(table)
+
+
+def help():
+    import inspect
+    import importlib
+    print("\n# Lishixian Library")
+    print("Contain %d functions." % len(__all__))
+    print("\n## Top module")
+    for k, v in globals().items():
+        if not k.startswith('_'):
+            if inspect.ismodule(v):
+                print("\n## Module '%s'" % k)
+            else:
+                try:
+                    argspec = str(inspect.signature(v))
+                    print("- %s%s" % (k, argspec))
+                except (TypeError, ValueError) as e:
+                    print("- %s = %r" % (k, v))
+
+
+__all__ = all()
