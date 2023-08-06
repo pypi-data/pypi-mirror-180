4d629e1753a702dd593886cc61e5d5f445166cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:25 +0000

    Add package openskill/4.0.0/openskill-4.0.0.tar.gz

---
+++ packages/openskill/openskill-4.0.0.tar.gz/openskill-4.0.0/openskill/__init__.py
@@ -0,0 +1,12 @@
+from openskill.rate import (
+    Rating,
+    create_rating,
+    ordinal,
+    predict_draw,
+    predict_rank,
+    predict_win,
+    rate,
+    team_rating,
+)
+
+__version__ = "4.0.0"
