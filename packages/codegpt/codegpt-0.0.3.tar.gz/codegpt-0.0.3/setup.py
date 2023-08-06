29c6c5cab0a012d39e3188dadc64431a2f20cbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:53 +0000

    Add package codegpt/0.0.3/codegpt-0.0.3.tar.gz

---
+++ packages/codegpt/codegpt-0.0.3.tar.gz/codegpt-0.0.3/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['codegpt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nltk>=3.7,<4.0',
+ 'openai>=0.2,<0.3',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'typer>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['codegpt = codegpt.codegpt:app']}
+
+setup_kwargs = {
+    'name': 'codegpt',
+    'version': '0.0.3',
+    'description': "A CLI tool for refactoring Python code using OpenAI's text-davinci-003 model",
+    'long_description': '# Codegpt\n\nA tool for using GPT just a little quicker. A nearly truly automated footgun. Learn how to revert with git before trying please.\n\n# Getting Started\n\n`pip install codegpt`\n\n`Then find a file you hate (Back it up! Don\'t do it live!) and give it a shot.\ncodegpt refactor .\\helper.py "Break this up into smaller functions where you can. Add google style docstrings. Feel free to rewrite any code doesn\'t make sense."`\n\nYou\'ll see something like:\n```\nThis prompt is 254 tokens, are you sure you want to continue?\nThe most GPT-3 can return in response is 3843. [y/N]: y\n\n(and after a short wait...)\n\nExplanation: The code has been refactored into smaller functions to improve readability, and Google style docstrings have been added.\n```\n\nPropose endpoints as issues, I\'ve got a few ideas:\n\n- Explain file\n- Write tests for file\n- Generate new file\n- Generate documentation from file\n\nJust remember this is paid - 2 cents per 1k tokens is a lot when you\'re working on files with a few hundred lines.\n',
+    'author': 'John Partee',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/morganpartee/codegpt',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
