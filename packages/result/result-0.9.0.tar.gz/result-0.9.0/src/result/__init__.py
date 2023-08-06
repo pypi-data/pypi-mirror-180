87ca330002d14fd23aec462709bd545924b2b1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:31 +0000

    Add package result/0.9.0/result-0.9.0.tar.gz

---
+++ packages/result/result-0.9.0.tar.gz/result-0.9.0/src/result/__init__.py
@@ -0,0 +1,11 @@
+from .result import Err, Ok, OkErr, Result, UnwrapError, as_result
+
+__all__ = [
+    "Err",
+    "Ok",
+    "OkErr",
+    "Result",
+    "UnwrapError",
+    "as_result",
+]
+__version__ = "0.9.0"
