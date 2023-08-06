04f0a6ac236e3d47ab91a190841546e3406b3564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:54 +0000

    Add package bf-func/0.0.2/bf_func-0.0.2-py3-none-any.whl

---
+++ packages/bf-func/bf_func-0.0.2-py3-none-any.whl/bf_func/retrying/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 2022/12/12 16:52
+@File: __init__.py.py
+---------
+@summary:
+---------
+@Author: clark
+@Email: clark1203@foxmail.com
+"""
+from retrying import *
