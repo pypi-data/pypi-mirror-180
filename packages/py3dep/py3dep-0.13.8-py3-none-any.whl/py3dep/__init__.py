57d2640e921b7884ef9e7f559c5e7df2fde7662f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:36 +0000

    Add package py3dep/0.13.8/py3dep-0.13.8-py3-none-any.whl

---
+++ packages/py3dep/py3dep-0.13.8-py3-none-any.whl/py3dep/__init__.py
@@ -0,0 +1,48 @@
+"""Top-level package for Py3DEP."""
+from importlib.metadata import PackageNotFoundError, version
+
+from .exceptions import (
+    DependencyError,
+    InputTypeError,
+    InputValueError,
+    MissingColumnError,
+    MissingCRSError,
+)
+from .print_versions import show_versions
+from .py3dep import (
+    check_3dep_availability,
+    elevation_bycoords,
+    elevation_bygrid,
+    elevation_profile,
+    get_map,
+    query_3dep_sources,
+    static_3dep_dem,
+)
+from .utils import deg2mpm, fill_depressions
+
+try:
+    __version__ = version("py3dep")
+except PackageNotFoundError:
+    __version__ = "999"
+
+__all__ = [
+    # Functions
+    "fill_depressions",
+    "get_map",
+    "check_3dep_availability",
+    "query_3dep_sources",
+    "deg2mpm",
+    "elevation_bycoords",
+    "elevation_bygrid",
+    "elevation_profile",
+    "static_3dep_dem",
+    "show_versions",
+    # Exceptions
+    "MissingColumnError",
+    "MissingCRSError",
+    "DependencyError",
+    "InputTypeError",
+    "InputValueError",
+    # Constants
+    "__version__",
+]
