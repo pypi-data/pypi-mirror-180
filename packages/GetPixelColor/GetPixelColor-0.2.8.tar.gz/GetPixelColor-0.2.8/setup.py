cc47c0a2537440f120cf0db653dbc1f1bba114cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:17 +0000

    Add package GetPixelColor/0.2.8/GetPixelColor-0.2.8.tar.gz

---
+++ packages/GetPixelColor/GetPixelColor-0.2.8.tar.gz/GetPixelColor-0.2.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import sys
+readme = open('README.md').read()
+
+setup(
+    name='GetPixelColor',
+    version='0.1.3',    
+    description='A cross-platform python library for getting the color of a given pixel on screen.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    data_files=[('static', ['static/get-pixel-color'])],
+    url='https://github.com/Bobrobot1/GetPixelColor',
+    author='Bobrobot1',
+    author_email='bobrobot@zoho.com',
+    license='MIT',
+    packages=['getpixelcolor'],
+    install_requires=[
+                    'Pillow',
+                    'numpy',
+                    'pyautogui',
+                    'pyobjc; platform_system == "darwin"',
+                    'pyobjc-core; platform_system == "darwin"',
+                    'pasteboard; platform_system == "darwin"',
+                    'win32api; platform_system == "Windows"',
+                    'get-pixel-color; platform_system == "darwin"'
+                    ],
+    platforms=['Windows', 'Linux', 'Mac OS X'],
+    
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',        
+        'Programming Language :: Python :: 3'
+    ],
+)
