e943368ccc63a22f906a6a0cb15d0980f45d22fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package fuzzy-couscous/2.4.6/fuzzy_couscous-2.4.6.tar.gz

---
+++ packages/fuzzy-couscous/fuzzy_couscous-2.4.6.tar.gz/fuzzy_couscous-2.4.6/fuzzy_couscous/main.py
@@ -0,0 +1,30 @@
+import typer
+
+from .commands import make_project
+from .commands import remove_poetry
+from .commands import work
+from .commands import write_env_file
+from .commands import setup_postgres
+
+
+def get_app():
+    app = typer.Typer(
+        name="fuzzy-couscous",
+        short_help="Initialize a new django project using the fuzzy-couscous project template.",
+        help="""This is a wrapper around the django-admin startproject  command using my custom project template at
+        https://github.com/Tobi-De/fuzzy-couscous. This cli also includes some additional commands to make setting up
+        a new project faster.""",
+    )
+
+    app.command(name="make")(make_project)
+    app.command(name="work")(work)
+    app.command(name="remove-poetry")(remove_poetry)
+    app.command(name="write-env")(write_env_file)
+    app.command(name="setup-pg")(setup_postgres)
+    return app
+
+
+cli = get_app()
+
+if __name__ == "__main__":
+    cli()
