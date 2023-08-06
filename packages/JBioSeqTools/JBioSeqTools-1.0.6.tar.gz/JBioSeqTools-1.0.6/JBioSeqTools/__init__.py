e8147796431c9cee1754ec64c89ee141dce913aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:58:54 +0000

    Add package JBioSeqTools/1.0.6/JBioSeqTools-1.0.6.tar.gz

---
+++ packages/JBioSeqTools/JBioSeqTools-1.0.6.tar.gz/JBioSeqTools-1.0.6/JBioSeqTools/__init__.py
@@ -0,0 +1,11 @@
+print()
+print('Welcome in JBioSeqTools v.1.0.6 library')
+print('')
+print('Loading required  packages...')
+
+from JBioSeqTools.seq_tools import seq_tools
+from JBioSeqTools.vector_build import vector_build
+from JBioSeqTools.vector_graph import graph_plot
+
+
+print('JBioSeqTools is ready to use')
