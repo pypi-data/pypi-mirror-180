fffd04b8d8def40c9f529f52c3e4fc97b4fab1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:06 +0000

    Add package PyQuelert/1.0.0/PyQuelert-1.0.0-py3-none-any.whl

---
+++ packages/PyQuelert/PyQuelert-1.0.0-py3-none-any.whl/Quelert/__init__.py
@@ -0,0 +1,11 @@
+# Name: PyQuelert -> Python Quelert.
+# PyQSF version -> 1.0.0
+# Pypi -> https://pypi.org/
+# Site -> None
+# Company -> None
+# Project creation date -> 10.12.2022 -- 18:25
+# Upload date to Pypi -> None
+# Publisher -> Georgy
+# Owner -> Georgy
+
+import pyquelert
