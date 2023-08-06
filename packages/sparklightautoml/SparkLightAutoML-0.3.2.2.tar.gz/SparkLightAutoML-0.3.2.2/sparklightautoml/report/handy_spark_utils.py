60897a32c5f1fe1d98426149fc225bbfebd05bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:03 +0000

    Add package sparklightautoml/0.3.2.2/SparkLightAutoML-0.3.2.2.tar.gz

---
+++ packages/sparklightautoml/SparkLightAutoML-0.3.2.2.tar.gz/SparkLightAutoML-0.3.2.2/sparklightautoml/report/handy_spark_utils.py
@@ -0,0 +1,15 @@
+"""
+The following methods have been copied from the HandySpark project and modified.
+https://github.com/dvgodoy/handyspark
+"""
+from pyspark.mllib.common import _java2py, _py2java
+
+
+# noinspection PyProtectedMember
+def call2(obj, name, *a):
+    serde = obj._sc._jvm.org.apache.spark.mllib.api.python.SerDe
+    args = [_py2java(obj._sc, a) for a in a]
+    java_res = getattr(obj._java_model, name)(*args)
+    java_res = serde.fromTuple2RDD(java_res)
+    res = _java2py(obj._sc, java_res)
+    return res
