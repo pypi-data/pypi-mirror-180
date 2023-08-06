854186c5922fc010ab928fceefb450d7df08d81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package bmb-klondike-client/0.2.0/bmb_klondike_client-0.2.0-py3-none-any.whl

---
+++ packages/bmb-klondike-client/bmb_klondike_client-0.2.0-py3-none-any.whl/bmb_klondike_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from bmb_klondike_client.api.claims_api import ClaimsApi
+from bmb_klondike_client.api.unaddressed_messages_api import UnaddressedMessagesApi
