969d474f714c53f7313db710e51e65a9bf74ac82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:55:33 +0000

    Add package authress-sdk/1.1.17/authress_sdk-1.1.17-py3-none-any.whl

---
+++ packages/authress-sdk/authress_sdk-1.1.17-py3-none-any.whl/authress_sdk/models/access_record_users.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+class AccessRecordUsers(object):
+
+    """
+    Attributes:
+      openapi_spec_types (dict): The key is attribute name and the value is attribute type.
+      attribute_serialization_map (dict): The key is attribute name and the value is json key in definition.
+    """
+    openapi_spec_types = {
+        'user_id': 'str'
+    }
+
+    attribute_serialization_map = {
+        'user_id': 'userId'
+    }
+
+    def __init__(self, user_id=None):
+        """AccessRecordUsers"""
+        self._user_id = None
+        self.user_id = user_id
+
+    @property
+    def user_id(self):
+        """Gets the user_id of this AccessRecordUsers.
+
+
+        :return: The user_id of this AccessRecordUsers.
+        :rtype: str
+        """
+        return self._user_id
+
+    @user_id.setter
+    def user_id(self, user_id):
+        """Sets the user_id of this AccessRecordUsers.
+
+
+        :param user_id: The user_id of this AccessRecordUsers.
+        :type: str
+        """
+        if user_id is None:
+            raise ValueError("Invalid value for `user_id`, must not be `None`")
+
+        self._user_id = user_id
