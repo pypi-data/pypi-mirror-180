6aab7f93214fc38c5f0e648192f0d6dbcfbddff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:59 +0000

    Add package mongo-taskqueue/0.2.1/mongo_taskqueue-0.2.1-py3-none-any.whl

---
+++ packages/mongo-taskqueue/mongo_taskqueue-0.2.1-py3-none-any.whl/mongotq/__init__.py
@@ -0,0 +1,7 @@
+from mongotq.anomalies import NonPendingAssignedAnomaly
+from mongotq.interface import get_task_queue
+from mongotq.task import Task, \
+    STATUS_NEW, STATUS_PENDING, STATUS_FAILED, STATUS_SUCCESSFUL
+from mongotq.task_queue import TaskQueue
+
+__version__ = '0.2.1'
