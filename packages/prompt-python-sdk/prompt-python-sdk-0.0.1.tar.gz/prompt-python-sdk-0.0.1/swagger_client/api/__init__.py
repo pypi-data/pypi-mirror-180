de3e8ef15e81df8f7744a0c25df706a77a83908f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:54 +0000

    Add package prompt-python-sdk/0.0.1/prompt-python-sdk-0.0.1.tar.gz

---
+++ packages/prompt-python-sdk/prompt-python-sdk-0.0.1.tar.gz/prompt-python-sdk-0.0.1/swagger_client/api/__init__.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.api_keys_api import APIKeysApi
+from swagger_client.api.analytics_api import AnalyticsApi
+from swagger_client.api.automations_api import AutomationsApi
+from swagger_client.api.bots_api import BotsApi
+from swagger_client.api.bulk_actions_api import BulkActionsApi
+from swagger_client.api.channel_id_integration_api import ChannelIdIntegrationApi
+from swagger_client.api.contact_list_contacts_api import ContactListContactsApi
+from swagger_client.api.contact_lists_api import ContactListsApi
+from swagger_client.api.conversations_api import ConversationsApi
+from swagger_client.api.customers_api import CustomersApi
+from swagger_client.api.data_api import DataApi
+from swagger_client.api.event_endpoints_api import EventEndpointsApi
+from swagger_client.api.group_members_api import GroupMembersApi
+from swagger_client.api.groups_api import GroupsApi
+from swagger_client.api.instant_app_schemas_api import InstantAppSchemasApi
+from swagger_client.api.instant_apps_api import InstantAppsApi
+from swagger_client.api.integrations_api import IntegrationsApi
+from swagger_client.api.maps_api import MapsApi
+from swagger_client.api.messages_api import MessagesApi
+from swagger_client.api.org_channels_api import OrgChannelsApi
+from swagger_client.api.org_settings_api import OrgSettingsApi
+from swagger_client.api.orgs_api import OrgsApi
+from swagger_client.api.pool_channels_api import PoolChannelsApi
+from swagger_client.api.pools_api import PoolsApi
+from swagger_client.api.pounces_api import PouncesApi
+from swagger_client.api.presence_api import PresenceApi
+from swagger_client.api.scheduled_actions_api import ScheduledActionsApi
+from swagger_client.api.smart_links_api import SmartLinksApi
+from swagger_client.api.tag_groups_api import TagGroupsApi
+from swagger_client.api.tags_api import TagsApi
+from swagger_client.api.team_members_api import TeamMembersApi
+from swagger_client.api.uploads_api import UploadsApi
