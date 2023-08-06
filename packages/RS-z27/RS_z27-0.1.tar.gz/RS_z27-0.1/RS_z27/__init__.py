7cd116488b4bee4027889c0257757f623c1b6835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:33 +0000

    Add package RS-z27/0.1/RS_z27-0.1.tar.gz

---
+++ packages/RS-z27/RS_z27-0.1.tar.gz/RS_z27-0.1/RS_z27/__init__.py
@@ -0,0 +1,6 @@
+from .algoritms import *
+from mydata import *
+
+__author__ = 'SergKornev'
+__version__ = '0.0'
+__email__ = 'sergeyroot77@gmail.com'
---
+++ packages/RS-z27/RS_z27-0.1.tar.gz/RS_z27-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='RS_z27',
+      version='0.1',
+      description='Recommend System',
+      packages=['RS_z27'],
+      author_email='sergeyroot77@gmail.com',
+      zip_safe=False)
