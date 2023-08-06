bfac2dec40bdd9d0dffecf8e726192834cc8a9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:39 +0000

    Add package autodiff30/0.0.3/autodiff30-0.0.3.tar.gz

---
+++ packages/autodiff30/autodiff30-0.0.3.tar.gz/autodiff30-0.0.3/src/autodiff30/__init__.py
@@ -0,0 +1,36 @@
+from .ad import adstruc, adfunction
+from .dual import DualNumber
+from .functions import (
+    cos,
+    sin,
+    tan,
+    arccos,
+    arcsin,
+    arctan,
+    exp,
+    log,
+    sqrt,
+    logistic,
+    sinh,
+    cosh,
+    tanh,
+)
+
+__all__ = [
+    "adstruc",
+    "adfunction",
+    "DualNumber",
+    "cos",
+    "sin",
+    "tan",
+    "arccos",
+    "arcsin",
+    "arctan",
+    "exp",
+    "log",
+    "sqrt",
+    "logistic",
+    "sinh",
+    "cosh",
+    "tanh",
+]
