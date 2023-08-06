1ac0d6c129cb0a9e4b6ae3380938f6bb4184959f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:45 +0000

    Add package damv1-paramiko-ssh/0.0.4/damv1_paramiko_ssh-0.0.4-py3-none-any.whl

---
+++ packages/damv1-paramiko-ssh/damv1_paramiko_ssh-0.0.4-py3-none-any.whl/damv1-paramiko-ssh/paramiko.py
@@ -0,0 +1,23 @@
+import paramiko
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+def ssh_command(host,username,port,privatekey,command):
+    print("Bismillah, welcome to command with paramiko-ssh")
+
+    ssh_privatekey = f"""\
+    -----BEGIN OPENSSH PRIVATE KEY----- 
+    {privatekey}
+     -----END OPENSSH PRIVATE KEY-----"""
+
+    pkey = paramiko.RSAKey.from_private_key(StringIO(ssh_privatekey))
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, username, pkey=pkey)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    lines= stdout.readlines()
+    print(lines)
+    ssh.close()
+
