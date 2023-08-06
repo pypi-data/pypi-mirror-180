e74f11539fca17ea57f85a407d8000b08bcc0d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:12 +0000

    Add package avlwrapper/0.3.2/avlwrapper-0.3.2-py3-none-any.whl

---
+++ packages/avlwrapper/avlwrapper-0.3.2-py3-none-any.whl/avlwrapper/__init__.py
@@ -0,0 +1,11 @@
+""" AVLWrapper
+"""
+VERSION = "0.3.2"
+
+from .config import default_config, Configuration
+from .model import (Aircraft, Case, Control, DataAirfoil, DesignVar,
+                    Geometry, FileAirfoil, NacaAirfoil, Parameter, Point,
+                    ProfileDrag, Section, Symmetry, Spacing, Surface, Vector)
+from .output import OutputReader
+from .session import Session
+from .tools import create_sweep_cases, partitioned_cases, show_image
