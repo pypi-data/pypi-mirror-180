38841a4eb5db0d9ff0c5a620c2b2427651242a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:25 +0000

    Add package trie/2.1.0/trie-2.1.0.tar.gz

---
+++ packages/trie/trie-2.1.0.tar.gz/trie-2.1.0/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+extras_require = {
+    "test": [
+        "hypothesis>=6.56.4,<7",
+        "pycryptodome",
+        "pytest-xdist>=2.4.0,<3",
+        "tox==3.14.6",
+        "pytest>=6.2.5",
+    ],
+    "lint": [
+        "black>=22",
+        "flake8==5.0.4",
+        "isort>=5.10.1",
+    ],
+    "dev": [
+        "bumpversion>=0.5.3,<1",
+        "wheel",
+        "twine",
+        "eth-hash>=0.1.0,<1.0.0",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"] + extras_require["test"] + extras_require["lint"]
+)
+
+with open("README.md") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="trie",
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version="2.1.0",
+    description="""Python implementation of the Ethereum Trie structure""",
+    long_description_markdown_filename="README.md",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/py-trie",
+    include_package_data=True,
+    py_modules=["trie"],
+    python_requires=">=3.7,<4",
+    install_requires=[
+        "eth-hash>=0.1.0,<1.0.0",
+        "eth-utils>=2.0.0,<3.0.0",
+        "hexbytes>=0.2.0",
+        "rlp>=3,<4",
+        "sortedcontainers>=2.1.0,<3",
+        "typing-extensions>=4.0.0,<5; python_version < '3.8'",
+    ],
+    extras_require=extras_require,
+    license="MIT",
+    zip_safe=False,
+    keywords="ethereum blockchain evm trie merkle",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+)
