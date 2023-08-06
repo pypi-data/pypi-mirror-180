928bfe457642b7b0da722132b8b135ca26c2e304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:43 +0000

    Add package mosaicml-streaming/0.2.0/mosaicml-streaming-0.2.0.tar.gz

---
+++ packages/mosaicml-streaming/mosaicml-streaming-0.2.0.tar.gz/mosaicml-streaming-0.2.0/streaming/base/util.py
@@ -0,0 +1,20 @@
+# Copyright 2022 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Utility and helper functions for datasets."""
+
+from typing import List
+
+__all__ = ['get_list_arg']
+
+
+def get_list_arg(text: str) -> List[str]:
+    """Pass a list as a command-line flag.
+
+    Args:
+        text (str): Text to split.
+
+    Returns:
+        List[str]: Splits, if any.
+    """
+    return text.split(',') if text else []
