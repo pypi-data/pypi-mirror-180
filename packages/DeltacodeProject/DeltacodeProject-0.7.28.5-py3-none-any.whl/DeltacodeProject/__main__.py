8a91ea815e8379a72c02c835f3b53aa329f561a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:09 +0000

    Add package DeltacodeProject/0.7.28.5/DeltacodeProject-0.7.28.5-py3-none-any.whl

---
+++ packages/DeltacodeProject/DeltacodeProject-0.7.28.5-py3-none-any.whl/DeltacodeProject/__main__.py
@@ -0,0 +1,16 @@
+# __main__.py
+
+import sys
+from DeltacodeProject.DeltacodeProject import main as menu
+from DeltacodeProject.main import main as ui
+
+def code():
+	"""Encode & Decode"""
+	
+	if ".ui" in sys.argv:
+		ui().run()
+	else:
+		menu().run()
+		
+if __name__ == "__main__":
+	code()
