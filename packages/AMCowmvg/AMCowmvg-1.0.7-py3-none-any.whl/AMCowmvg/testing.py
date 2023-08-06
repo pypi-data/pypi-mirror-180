8857a35a345598244f3a13a3e760cc973c2332ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:49 +0000

    Add package AMCowmvg/1.0.7/AMCowmvg-1.0.7-py3-none-any.whl

---
+++ packages/AMCowmvg/AMCowmvg-1.0.7-py3-none-any.whl/AMCowmvg/testing.py
@@ -0,0 +1,19 @@
+#from AMCowmvg import app
+
+try:
+    from __init__ import app
+except:
+    print("")
+
+try:
+    from AMCowmvg.__init__ import app
+except:
+    print("")    
+#testing testing
+
+
+def runApp():
+    if __name__ == '__main__':
+        app.run(debug=True, host='127.0.0.1')
+
+runApp()
