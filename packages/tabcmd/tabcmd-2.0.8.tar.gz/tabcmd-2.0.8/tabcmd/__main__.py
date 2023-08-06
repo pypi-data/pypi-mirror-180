2ac50f462540b51fb831e0a833b8dbb76f738fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:10 +0000

    Add package tabcmd/2.0.8/tabcmd-2.0.8.tar.gz

---
+++ packages/tabcmd/tabcmd-2.0.8.tar.gz/tabcmd-2.0.8/tabcmd/__main__.py
@@ -0,0 +1,11 @@
+import sys
+
+try:
+    from tabcmd.tabcmd import main
+except ImportError:
+    print(sys.stderr, "Tabcmd needs to be run as a module, it cannot be run as a script")
+    print(sys.stderr, "Try running python -m tabcmd")
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
