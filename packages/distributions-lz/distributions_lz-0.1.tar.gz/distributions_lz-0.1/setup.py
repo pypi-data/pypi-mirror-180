1d661f33f5a1c5261c370d8b3851abdc7629837f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:10 +0000

    Add package distributions-lz/0.1/distributions_lz-0.1.tar.gz

---
+++ packages/distributions-lz/distributions_lz-0.1.tar.gz/distributions_lz-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_lz',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_lz'],
+      zip_safe=False)
