4aa01bba78a0145938c4e8bacfbb5bb000f7152a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:02 +0000

    Add package shreyascodes/0.1/shreyascodes-0.1.tar.gz

---
+++ packages/shreyascodes/shreyascodes-0.1.tar.gz/shreyascodes-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="shreyascodes",
+version="0.1",
+description="this is made only for making work easy",
+author="Shreyas Naik",
+packages=['shreyascodes'],
+license='MIT',
+install_requires =[])
