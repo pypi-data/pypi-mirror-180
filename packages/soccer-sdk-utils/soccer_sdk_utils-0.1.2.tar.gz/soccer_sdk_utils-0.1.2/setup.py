85d9cf55c0d7048ac0395d9b1915cb2ddbf9091d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:03 +0000

    Add package soccer-sdk-utils/0.1.2/soccer_sdk_utils-0.1.2.tar.gz

---
+++ packages/soccer-sdk-utils/soccer_sdk_utils-0.1.2.tar.gz/soccer_sdk_utils-0.1.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['soccer_sdk_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0',
+ 'requests-mock>=1.10.0,<2.0.0',
+ 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'soccer-sdk-utils',
+    'version': '0.1.2',
+    'description': 'A utility library for Soccer SDK packages.',
+    'long_description': 'None',
+    'author': 'Omar Crosby',
+    'author_email': 'omar.crosby@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
