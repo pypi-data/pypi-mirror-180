18a7a45a0c8e8f2fbe707bb2cb60c709c18fe2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:27 +0000

    Add package metagym/0.2.0.dev2/metagym-0.2.0.dev2.tar.gz

---
+++ packages/metagym/metagym-0.2.0.dev2.tar.gz/metagym-0.2.0.dev2/README.md
@@ -0,0 +1,19 @@
+# MetaGym
+
+MetaGym provides abundant environments for benchmarking Reinforcement Learning and Meta Reinforcement Learning
+
+# Environments Updating
+
+- [LiftSim](metagym/liftsim)：Simulator for Evelvator Dispatching (Sep, 2019)
+
+- [Quadrotor](metagym/quadrotor): 3D Quadrotor simulator for different tasks (Mar, 2020)
+
+- [Quadrupedal](metagym/quadrupedal): Quadrupedal robot adapting to different terrains (Seq, 2021)
+
+- [MetaMaze](metagym/metamaze): 2D/3D maze generators for task generalization (Oct, 2021)
+
+- [MetaLocomotion](metagym/metalocomotion): Locomotion simulator with diverse geometries (June, 2022)
+
+- [MetaLM](metagym/metalm): Meta language model dataset (Dec, 2022)
+
+- [Bandits](metagym/bandits): Bandits task generalization (Dec, 2022)
---
+++ packages/metagym/metagym-0.2.0.dev2.tar.gz/metagym-0.2.0.dev2/setup.py
@@ -0,0 +1,65 @@
+#   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import io
+from setuptools import setup, find_packages
+
+__version__ = '0.2.0.dev2'
+
+with io.open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='metagym',
+    version=__version__,
+    author='parl_dev',
+    author_email='',
+    description=('MetaGym: environments for benchmarking Reinforcement Learning and Meta Reinforcement Learning'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/PaddlePaddle/MetaGym',
+    license="GPLv3",
+    packages=[package for package in find_packages()
+              if package.startswith('metagym')],
+    package_data={'metagym': [
+        './liftsim/config.ini',
+        './liftsim/environment/animation/resources/*.png',
+        './liftsim/environment/mansion/person_generators/mansion_flow.npy',
+        './quadrotor/quadcopter.stl',
+        './quadrotor/texture.png',
+        './quadrotor/config.json',
+        './metamaze/envs/img/*',
+        './metalocomotion/envs/assets/ants/*.xml',
+        './metalocomotion/envs/assets/humanoids/*.xml',
+        './metalocomotion/envs/assets/scenes/*',
+        './metalocomotion/envs/assets/*.png',
+        ]
+    },
+    tests_require=['pytest', 'mock'],
+    include_package_data=True,
+    install_requires=[
+        'gym>=0.18.0',
+        'numpy>=1.16.4',
+        'Pillow>=6.2.2',
+        'six>=1.12.0',
+    ],
+    extras_require={
+        'metalocomotion': ['numpy>=1.20.3', 'pybullet>=3.0.7'],
+        'metamaze': ['pygame>=2.0.2dev2', 'numba>=0.54.0'],
+        'quadrupedal': ['scipy>=0.12.0', 'pybullet>=3.0.7', 'attrs>=20.3.0'],
+        'quadrotor': ['scipy>=0.12.0', 'networkx>=2.2', 'trimesh>=3.2.39', 'networkx>=2.2', 'colour>=0.1.5'],
+        'liftsim': ['configparser>=3.7.4', 'pyglet==1.5.0; python_version>="3"', 'pyglet==1.4.0; python_version<"3"'],
+    },
+    zip_safe=False,
+)
