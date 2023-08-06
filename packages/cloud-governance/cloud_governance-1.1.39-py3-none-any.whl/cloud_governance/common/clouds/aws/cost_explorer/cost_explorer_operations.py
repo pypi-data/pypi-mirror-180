243fee30f008e842b83e511be16a3def70ea268a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:01 +0000

    Add package cloud-governance/1.1.39/cloud_governance-1.1.39-py3-none-any.whl

---
+++ packages/cloud-governance/cloud_governance-1.1.39-py3-none-any.whl/cloud_governance/common/clouds/aws/cost_explorer/cost_explorer_operations.py
@@ -0,0 +1,33 @@
+import json
+from datetime import datetime, timedelta
+
+import boto3
+
+
+class CostExplorerOperations:
+    """
+    This class extracts the price data from AWS Cost Explorer
+    """
+
+    def __init__(self):
+        self.cost_explorer_client = boto3.client('ce')
+
+    def get_cost_by_tags(self, tag: str, granularity: str = 'DAILY', cost_metric: str = 'UnblendedCost', start_date: str = '', end_date: str = ''):
+        """
+        This method extracts the price by Tag provided
+        @return:
+        """
+        if not start_date and not end_date:
+            end_date = datetime.now() - timedelta(1)
+            start_date = end_date - timedelta(1)
+            start_date = str(start_date.strftime('%Y-%m-%d'))
+            end_date = str(end_date.strftime('%Y-%m-%d'))
+        return self.cost_explorer_client.get_cost_and_usage(
+            TimePeriod={
+                'Start': start_date,
+                'End': end_date
+            },
+            Granularity=granularity,
+            Metrics=[cost_metric],
+            GroupBy=[{'Type': 'TAG', 'Key': tag}]
+        )
