8dae5c8743cbe1be7cf5f324a5c0fb95c8ec7d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:05 +0000

    Add package pymydumper/0.0.1/pymydumper-0.0.1-py3-none-any.whl

---
+++ packages/pymydumper/pymydumper-0.0.1-py3-none-any.whl/pymydumper/cli.py
@@ -0,0 +1,12 @@
+def main():
+    print('hello from main')
+    another_function()
+
+
+def another_function():
+    print('hello from another_function')
+
+
+if __name__ == "__main__":
+    """This runs when you execute '$ python3 mypackage/mymodule.py'"""
+    main()
