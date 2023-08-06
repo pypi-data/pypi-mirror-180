9168110d86a5fe4217206bba7915868e5fe7996a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:13 +0000

    Add package modal-client/0.41.479/modal_client-0.41.479-py3-none-any.whl

---
+++ packages/modal-client/modal_client-0.41.479-py3-none-any.whl/client_test/object_test.py
@@ -0,0 +1,25 @@
+# Copyright Modal Labs 2022
+import pytest
+
+from modal import ref
+from modal.aio import AioQueue, AioStub
+from modal.exception import DeprecationError
+
+
+@pytest.mark.asyncio
+async def test_async_factory(servicer, client):
+    stub = AioStub()
+    stub["my_factory"] = AioQueue()
+    async with stub.run(client=client) as running_app:
+        # assert isinstance(running_app["my_factory"], AioQueue)  # TODO(erikbern(): is a handle now
+        assert running_app["my_factory"].object_id == "qu-1"
+
+
+@pytest.mark.asyncio
+async def test_use_object(servicer, client):
+    stub = AioStub()
+    with pytest.raises(DeprecationError):
+        stub["my_q_1"] = ref("foo-queue")
+    stub["my_q_2"] = AioQueue.from_name("foo-queue")
+    async with stub.run(client=client) as running_app:
+        assert running_app["my_q_2"].object_id == "qu-foo"
---
+++ packages/modal-client/modal_client-0.41.479-py3-none-any.whl/modal/__init__.py
@@ -0,0 +1,48 @@
+# Copyright Modal Labs 2022
+from modal_version import __version__
+
+from .app import App, container_app, is_local
+from .dict import Dict
+from .exception import Error
+from .functions import Function, current_input_id
+from .image import Conda, DebianSlim, DockerfileImage, DockerhubImage, Image
+from .mount import Mount, create_package_mount, create_package_mounts
+from .object import lookup, ref
+from .proxy import Proxy
+from .queue import Queue
+from .rate_limit import RateLimit
+from .retries import Retries
+from .schedule import Cron, Period
+from .secret import Secret
+from .shared_volume import SharedVolume
+from .stub import Stub
+
+__all__ = [
+    "__version__",
+    "App",
+    "Cron",
+    "Conda",
+    "DebianSlim",
+    "Dict",
+    "DockerfileImage",
+    "DockerhubImage",
+    "Error",
+    "Function",
+    "Image",
+    "Mount",
+    "Period",
+    "Proxy",
+    "Queue",
+    "RateLimit",
+    "Retries",
+    "Secret",
+    "SharedVolume",
+    "Stub",
+    "container_app",
+    "create_package_mount",
+    "create_package_mounts",
+    "is_local",
+    "lookup",
+    "ref",
+    "current_input_id",
+]
