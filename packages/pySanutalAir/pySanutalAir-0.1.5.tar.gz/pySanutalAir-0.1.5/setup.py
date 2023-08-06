2ecb3948566191b86670dc56b97d08e50e165f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:47 +0000

    Add package pySanutalAir/0.1.5/pySanutalAir-0.1.5.tar.gz

---
+++ packages/pySanutalAir/pySanutalAir-0.1.5.tar.gz/pySanutalAir-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+long_description = None
+
+with open("README.md", 'r') as fp:
+    long_description = fp.read()
+
+setup(
+    name = "pySanutalAir",
+    packages = ["sanutal_air"],
+    version="0.1.5",
+    description="Python3 library for home ventilation system from Sanutal",
+    long_description=long_description,
+    python_requires='>=3.6.7',
+    author="Guillaume-HIS",
+    license="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
