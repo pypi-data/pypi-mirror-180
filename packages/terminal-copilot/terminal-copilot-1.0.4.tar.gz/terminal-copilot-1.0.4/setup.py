9a550e6cbd2d8c5fa887ab300bbe7f3cf64ddfcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:22 +0000

    Add package terminal-copilot/1.0.4/terminal-copilot-1.0.4.tar.gz

---
+++ packages/terminal-copilot/terminal-copilot-1.0.4.tar.gz/terminal-copilot-1.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+  
+long_description = "terminal-copilot"
+
+setup(
+        name ='terminal-copilot',
+        version ='1.0.4',
+        author ='Methexis',
+        author_email ='joelkronander@gmail.com',
+        url ='https://github.com/Methexis-Inc/terminal-copilot',
+        description ='A smart terminal assistant that helps you find the right command',
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='Apache License 2.0',
+        packages = find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'copilot=copilot.copilot:main'
+            ]
+        },
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ),
+        keywords ='terminal copilot openai gpt3',
+        install_requires = requirements,
+        zip_safe = False
+)
