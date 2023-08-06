e3b5647197ba5550e1f1d6297c6198d4122e0765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:43 +0000

    Add package isolate-proto/0.0.10/isolate_proto-0.0.10-py3-none-any.whl

---
+++ packages/isolate-proto/isolate_proto-0.0.10-py3-none-any.whl/isolate_proto/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from google.protobuf.json_format import MessageToDict as struct_to_dict
+from google.protobuf.struct_pb2 import Struct
+
+# Imitates how includes in our protobuf files are handled.
+from isolate.connections.grpc.definitions import *
+from isolate.server.definitions import *
+from isolate_proto.controller_pb2 import *
+from isolate_proto.controller_pb2_grpc import (
+    IsolateControllerServicer,
+    IsolateControllerStub,
+)
+from isolate_proto.controller_pb2_grpc import (
+    add_IsolateControllerServicer_to_server as register_controller,
+)
