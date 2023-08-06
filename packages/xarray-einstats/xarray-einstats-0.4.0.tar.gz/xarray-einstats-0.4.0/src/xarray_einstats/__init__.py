68693f9b47581a0ff8f0f1845da837d2024e59e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:57 +0000

    Add package xarray-einstats/0.4.0/xarray-einstats-0.4.0.tar.gz

---
+++ packages/xarray-einstats/xarray-einstats-0.4.0.tar.gz/xarray-einstats-0.4.0/src/xarray_einstats/__init__.py
@@ -0,0 +1,9 @@
+"""Stats, linear algebra and einops for xarray."""
+
+from __future__ import annotations
+
+from .linalg import einsum, raw_einsum, einsum_path, matmul
+
+__all__ = ["einsum", "raw_einsum", "einsum_path", "matmul"]
+
+__version__ = "0.4.0"
