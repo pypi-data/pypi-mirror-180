032e79f2ae0704f76787f7f25b7c966e233aa699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:07 +0000

    Add package clayton/0.0.1/clayton-0.0.1-py3-none-any.whl

---
+++ packages/clayton/clayton-0.0.1-py3-none-any.whl/clayton/rng/__init__.py
@@ -0,0 +1,34 @@
+from .base import CopulaTypes, Multivariate, Extreme
+from .evd import (Logistic, AsymmetricLogistic, HuslerReiss,
+                  AsyNegLog, TEV, Bilog)
+from .archimedean import (Clayton, Frank, Amh,
+                          Joe, Nelsen9, Nelsen10,
+                          Nelsen11, Nelsen12, Nelsen13,
+                          Nelsen14, Nelsen15, Nelsen22)
+from .elliptical import Gaussian, Student
+
+__all__ = (
+    'CopulaTypes',
+    'Multivariate',
+    'Extreme',
+    'Logistic',
+    'AsymmetricLogistic',
+    'HuslerReiss',
+    'AsyNegLog',
+    'TEV',
+    'Bilog',
+    'Clayton',
+    'Frank',
+    'Amh',
+    'Joe',
+    'Nelsen9',
+    'Nelsen10',
+    'Nelsen11',
+    'Nelsen12',
+    'Nelsen13',
+    'Nelsen14',
+    'Nelsen15',
+    'Nelsen22',
+    'Gaussian',
+    'Student'
+)
