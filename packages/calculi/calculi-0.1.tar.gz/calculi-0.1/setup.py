6d8d13966b4e9bb7e3a3ebe4f81245e2262b4b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:07 +0000

    Add package calculi/0.1/calculi-0.1.tar.gz

---
+++ packages/calculi/calculi-0.1.tar.gz/calculi-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='calculi',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Saviour Eking',
+      author_Email = 'saviour.eking@outlook.com',
+      packages=['calculi'],
+      zip_safe=False)
