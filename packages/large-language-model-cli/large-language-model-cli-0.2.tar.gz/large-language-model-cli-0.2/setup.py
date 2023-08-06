2e4a1a487bb1d20d778268720cdcabae7656dba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:55 +0000

    Add package large-language-model-cli/0.2/large-language-model-cli-0.2.tar.gz

---
+++ packages/large-language-model-cli/large-language-model-cli-0.2.tar.gz/large-language-model-cli-0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+import distutils.core
+
+setuptools.setup(
+    name="large-language-model-cli",
+    version=0.2,
+    author="Talwrii",
+    author_email="Talwrii@googlemail.com",
+    description="Command-line for interacting with large language models. not TUI. chatgpt",
+    license="mit",
+    keywords="",
+    url="",
+    packages=["llmcli"],
+    long_description=open("readme.md").read(),
+    entry_points={"console_scripts": ["llmcli=llmcli:main"]},
+    classifiers=[],
+)
