6fd568d89971a2ec571dc15105c12dc472ede2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:44 +0000

    Add package vimwiki-to-org/1.0.5/vimwiki_to_org-1.0.5.tar.gz

---
+++ packages/vimwiki-to-org/vimwiki_to_org-1.0.5.tar.gz/vimwiki_to_org-1.0.5/vimwiki_to_org/__main__.py
@@ -0,0 +1,30 @@
+import argparse
+from pathlib import Path
+from vimwiki_to_org.src.app import App
+import os
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('-d',
+                        '--wiki-path',
+                        help="path to vimwiki directory (optional)",
+                        default=f"/home/{os.getlogin()}/vimwiki/",
+                        type=str)
+
+    parser.add_argument('-o',
+                        '--output-path',
+                        help="path to output directory (optional)",
+                        default=f"{Path('converted_files').absolute()}",
+                        type=str)
+
+    args = parser.parse_args()
+    import_path = Path(args.wiki_path).absolute()
+    output_path = Path(args.output_path).absolute()
+    App(import_path, output_path).run()
+
+
+if __name__ == "__main__":
+    main()
