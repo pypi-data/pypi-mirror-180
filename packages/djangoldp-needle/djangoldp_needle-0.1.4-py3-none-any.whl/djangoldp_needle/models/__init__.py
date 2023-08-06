f425ec8515eb78cea6a14e95beca5df28a2fd81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:36 +0000

    Add package djangoldp-needle/0.1.4/djangoldp_needle-0.1.4-py3-none-any.whl

---
+++ packages/djangoldp-needle/djangoldp_needle-0.1.4-py3-none-any.whl/djangoldp_needle/models/__init__.py
@@ -0,0 +1,5 @@
+from .tag import Tag
+from .annotation import Annotation
+from .annotation_target import AnnotationTarget
+from .needle_activity import NeedleActivity
+from .activity_signal_receiver import post_save_user
