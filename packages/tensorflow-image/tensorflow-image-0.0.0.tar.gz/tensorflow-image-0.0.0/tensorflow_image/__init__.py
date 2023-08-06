2e6629b0e1cdda4fb0669470cb72e4d0f532fcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:30 +0000

    Add package tensorflow-image/0.0.0/tensorflow-image-0.0.0.tar.gz

---
+++ packages/tensorflow-image/tensorflow-image-0.0.0.tar.gz/tensorflow-image-0.0.0/tensorflow_image/__init__.py
@@ -0,0 +1,18 @@
+from .define import *
+
+from .image_classification_ai import ImageClassificationAi
+from .image_multi_label_ai import ImageMultiLabelAi
+from .image_regression_ai import ImageRegressionAi
+
+from .pix2pix import PixToPix
+
+from .image_lib import *
+
+#__version__: Final[str] = "1.4.0"
+
+__copyright__ = "Copyright (C) 2022 Nicoyou"
+__version__ = "0.0.0"
+__license__ = "MIT"
+__author__ = "nicoyou"
+__author_email__ = "nicoyou.info@gmail.com"
+__url__ = "https://github.com/nicoyou/TensorflowImageAi"
