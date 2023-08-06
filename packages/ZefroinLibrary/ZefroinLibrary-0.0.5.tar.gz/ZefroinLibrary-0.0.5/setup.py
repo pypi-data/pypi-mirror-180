fea28631da4c3f9a535de2ef7a5211d94878e0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package ZefroinLibrary/0.0.5/ZefroinLibrary-0.0.5.tar.gz

---
+++ packages/ZefroinLibrary/ZefroinLibrary-0.0.5.tar.gz/ZefroinLibrary-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5' 
+DESCRIPTION = 'Tools for Zefroin'
+LONG_DESCRIPTION = 'CHANGELOG: Minor bug fixes.'
+
+# Setting up
+setup(
+        name="ZefroinLibrary", 
+        version=VERSION,
+        author="AzureianGH",
+        author_email="azureiangh@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        
+        keywords=['python', 'Zefroin', 'zss', 'zefroin', 'zf', 'zef', 'azureian'],
+        classifiers= [
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
