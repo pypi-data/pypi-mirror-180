7fafe4b24b16dad98c376ddef75b38d8d43b30b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:12 +0000

    Add package ZefroinLibrary/0.0.4/ZefroinLibrary-0.0.4.tar.gz

---
+++ packages/ZefroinLibrary/ZefroinLibrary-0.0.4.tar.gz/ZefroinLibrary-0.0.4/ZefroinLibrary/zfunct.py
@@ -0,0 +1,69 @@
+import random
+import os
+def rand(rnd1,rnd2):
+  random.randint(rnd1, rnd2)
+
+def curdir():
+  return os.getcwd()
+
+def get_file_path(file_name):
+  return os.path.join(curdir(), file_name)
+
+def get_file_name(file_path):
+  return os.path.basename(file_path)
+
+def get_file_extension(file_name):
+  return os.path.splitext(file_name)[1]
+
+def get_file_size(file_path):
+  return os.path.getsize(file_path)
+
+def get_file_date(file_path):
+  return os.path.getmtime
+
+def openf(filename):
+  os.system(filename)
+
+def slist(inp,revers):
+  if type(inp) is list:
+    if revers:
+      return inp.sort(reverse=True)
+    else:
+      return inp.sort()
+
+  else:
+    print("REQUIRED LIST, GOT: ", type(inp))
+
+def splist(inp):
+  
+  lista = []
+  inp = inp.split(' ', ',')
+  for i in inp:
+    lista.append(i)
+  lista = str(lista)
+  lista = lista.replace('[','')
+  lista = lista.replace(']','')
+  lista = lista.replace(',','')
+  lista = lista.replace("'", '')
+
+  return lista
+  
+def rmlist(liste):
+  if type(liste) is list:
+    liste = str(liste)
+    liste = liste.replace('[','')
+    liste = liste.replace(']','')
+    liste = liste.replace(',','')
+    liste = liste.replace("'", '')
+  else:
+    print("REQUIRED LIST, GOT: ", type(liste))
+    pass
+    
+def fwrite(file,text): 
+  tempf = open(file,'a')
+  tempf.close()
+  with open(file,'w') as f:
+    f.write(text)
+def fread(file):
+  with open(file,'r') as f:
+    return f.read()
---
+++ packages/ZefroinLibrary/ZefroinLibrary-0.0.4.tar.gz/ZefroinLibrary-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'Tools for Zefroin'
+LONG_DESCRIPTION = 'Tools for Zefroin that are used throughout the project. CHANGELOG: Added new defs for ZefroinLib, Fixed bugs.'
+
+# Setting up
+setup(
+        name="ZefroinLibrary", 
+        version=VERSION,
+        author="AzureianGH",
+        author_email="azureiangh@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        
+        keywords=['python', 'Zefroin', 'zss', 'zefroin', 'zf', 'zef', 'azureian'],
+        classifiers= [
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
