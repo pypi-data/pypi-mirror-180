d4e404a90f2e214a2ea058667bdd5371f7a23a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:38 +0000

    Add package ape-arbitrum/0.5.2/ape-arbitrum-0.5.2.tar.gz

---
+++ packages/ape-arbitrum/ape-arbitrum-0.5.2.tar.gz/ape-arbitrum-0.5.2/tests/test_provider.py
@@ -0,0 +1,7 @@
+def test_basic(accounts, networks):
+    with networks.arbitrum.local.use_provider("test"):
+        a = accounts.test_accounts[0]
+        receipt = a.transfer(a, 100)
+
+        assert not receipt.failed
+        assert receipt.value == 100
