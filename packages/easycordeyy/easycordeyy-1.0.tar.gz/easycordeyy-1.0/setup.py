202f667ded0c8772350bff31ab7c873626ba0cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package easycordeyy/1.0/easycordeyy-1.0.tar.gz

---
+++ packages/easycordeyy/easycordeyy-1.0.tar.gz/easycordeyy-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+
+    name='easycordeyy',
+    packages=['easycordeyy'],
+    version='1.0',
+    license='MIT',
+    description='makes life easy',
+    author='bluesatan',
+    keywords=['style'],
+    install_requires=[''],
+    classifiers=['Development Status :: 5 - Production/Stable']
+
+)
