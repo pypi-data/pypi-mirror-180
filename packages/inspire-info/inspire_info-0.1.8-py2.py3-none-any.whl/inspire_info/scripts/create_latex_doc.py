9f5203b0ddbd929d9da144636fb8fd61905813f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:07 +0000

    Add package inspire-info/0.1.8/inspire_info-0.1.8-py2.py3-none-any.whl

---
+++ packages/inspire-info/inspire_info-0.1.8-py2.py3-none-any.whl/inspire_info/scripts/create_latex_doc.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+__author__ = 'Tim Michael Heinz Wolf'
+__version__ = '0.1.8'
+__license__ = 'MIT'
+__email__ = 'tim.wolf@mpi-hd.mpg.de'
+
+import argparse
+from inspire_info import LatexCreator
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Command line tool to search for authors in inspire')
+    parser.add_argument('--source_dir',
+                        type=str,
+                        help="Directory where the bibtex-output is stored.",
+                        default="bibtex"
+                        )
+    parser.add_argument('--output_dir',
+                        type=str,
+                        help="Directory where the latex-output is stored.",
+                        required=True
+                        )
+
+    return dict(vars(parser.parse_args()))
+
+template = r"""\documentclass[11pt]{article}
+
+\title{Multiple Bibliographies with \texttt{multibib}}
+\author{Tim Wolf}
+\date{}
+
+\usepackage[resetlabels,labeled]{multibib}
+\newcites{Math}{Math Readings}
+\newcites{Phys}{Physics Readings}
+
+\begin{document}
+
+\maketitle
+
+__NOCITES__
+
+\bibliographystyle{unsrt}
+\bibliography{references}
+
+% \bibliographystyleMath{unsrt}
+% \bibliographyMath{refs-etc}
+
+% \bibliographystylePhys{unsrt}
+% \bibliographyPhys{refs-etc}
+
+\end{document}
+"""
+
+def main():
+    parsed_args = parse_args()
+    document_maker = LatexCreator(template=template,
+                                  outdir=parsed_args['output_dir'],
+                                  source_folder=parsed_args["source_dir"])
+    document_maker.make_bibliography()
+    document_maker.create_latex_doc(filename="publications.tex")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/inspire-info/inspire_info-0.1.8-py2.py3-none-any.whl/inspire_info/scripts/get_members.py
@@ -0,0 +1,54 @@
+__author__ = 'Tim Michael Heinz Wolf'
+__version__ = '0.1.8'
+__license__ = 'MIT'
+__email__ = 'tim.wolf@mpi-hd.mpg.de'
+
+from tkinter import W
+from inspire_info.InspireInfo import InspireInfo
+import sys
+import argparse
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description='Scraping of inspire for institute publications')
+    parser.add_argument('--retrieve',
+                        action='store_true',
+                        help="""If added API-call is created, otherwise 
+            cache-file is going to be used.""")
+    parser.add_argument(
+        '--get_links',
+        action='store_true',
+        help="Prints the inspire quieries to the found publications")
+    parser.add_argument('--get_name_proposal',
+                        action='store_true',
+                        help="Write out name_proposal.txt")
+    parser.add_argument('--create_cache',
+                        action='store_true',
+                        help="Create the cache-file")
+    parser.add_argument('--config',
+                        type=str,
+                        help="Config file to read.",
+                        required=True)
+
+    return dict(vars(parser.parse_args(args)))
+
+
+def main(arguments):
+    parsed_args = parse_args(args=arguments)
+
+    inspire_getter = InspireInfo(config_path=parsed_args["config"])
+    inspire_getter.get_data(retrieve=parsed_args["retrieve"])
+    _, matched_publications, _ = inspire_getter.match_publications_by_keywords(
+    )
+    all_authors_from_institution_named = inspire_getter.match_authors(
+        matched_publications)
+    inspire_getter.name_proposal_data = all_authors_from_institution_named
+    inspire_getter.write_name_proposal()
+
+    if parsed_args["get_links"]:
+        inspire_getter.print_links(match_type="matched")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
