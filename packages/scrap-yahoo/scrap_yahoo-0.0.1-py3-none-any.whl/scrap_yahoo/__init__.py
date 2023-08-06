4e2b8331be57403db8270dcb9013853664e53be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:40 +0000

    Add package scrap-yahoo/0.0.1/scrap_yahoo-0.0.1-py3-none-any.whl

---
+++ packages/scrap-yahoo/scrap_yahoo-0.0.1-py3-none-any.whl/scrap_yahoo/__init__.py
@@ -0,0 +1,5 @@
+from scrap_mod import get_forex_rate
+from scrap_mod import scrap_data
+from scrap_mod import _parse_table
+from scrap_mod import get_income_statement
+from scrap_mod import get_balance_sheet
---
+++ packages/scrap-yahoo/scrap_yahoo-0.0.1-py3-none-any.whl/scrap_yahoo/main.py
@@ -0,0 +1,4 @@
+import scrap_mod
+
+if __name__ == '__main__':
+    print(scrap_mod.get_forex_rate('HKD','USD'))
