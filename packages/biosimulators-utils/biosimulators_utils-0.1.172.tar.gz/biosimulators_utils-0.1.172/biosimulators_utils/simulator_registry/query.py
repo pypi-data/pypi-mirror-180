1e19c83148b334b076f58bef16f99d6408a5d6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:09 +0000

    Add package biosimulators-utils/0.1.172/biosimulators_utils-0.1.172.tar.gz

---
+++ packages/biosimulators-utils/biosimulators_utils-0.1.172.tar.gz/biosimulators_utils-0.1.172/biosimulators_utils/simulator_registry/query.py
@@ -0,0 +1,42 @@
+""" Methods for querying the BioSimulators registry
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2020-12-06
+:Copyright: 2020, Center for Reproducible Biomedical Modeling
+:License: MIT
+"""
+
+from ..biosimulations.utils import validate_biosimulations_api_response
+from ..config import get_config, Config  # noqa: F401
+import requests
+
+
+__all__ = ['get_simulator_version_specs']
+
+
+def get_simulator_version_specs(id, config=None):
+    """ Get the specifications of the versions of a simulator in the BioSimulators registry
+
+    Args:
+        id (:obj:`str`): simulator id
+        config (:obj:`Config`, optional): configuration
+
+    Returns:
+        :obj:`list` of :obj:`dict`: specifications of the registered versions of the simulator
+    """
+
+    if config is None:
+        config = get_config()
+
+    endpoint = config.BIOSIMULATORS_API_ENDPOINT
+    response = requests.get('{}simulators/{}'.format(endpoint, id))
+
+    try:
+        intro_failure_msg = "The specifications of the versions of `{}` could not be retrieved from the BioSimulators registry.".format(id)
+        validate_biosimulations_api_response(response, intro_failure_msg, ValueError)
+        version_specs = response.json()
+    except ValueError:
+        if response.status_code != 404:
+            raise
+        version_specs = []
+    return version_specs
