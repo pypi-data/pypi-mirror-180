bd32aafb43e556adc40079872b69ed929d417086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:29 +0000

    Add package pytoil/0.38.0/pytoil-0.38.0-py3-none-any.whl

---
+++ packages/pytoil/pytoil-0.38.0-py3-none-any.whl/pytoil/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+__version__ = "0.38.0"
+
+__doc__ = """
+
+Helpful CLI to automate the development workflow.
+
+- Create and manage your local and remote projects
+
+- Build projects from cookiecutter templates.
+
+- Easily create/manage virtual environments.
+
+- Minimal configuration required.
+"""
