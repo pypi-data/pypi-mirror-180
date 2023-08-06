0de5df7cd740feb316f124540eaf9a903dc1755d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:43 +0000

    Add package carbon-simulator/1.4/carbon-simulator-1.4.tar.gz

---
+++ packages/carbon-simulator/carbon-simulator-1.4.tar.gz/carbon-simulator-1.4/carbon/__init__.py
@@ -0,0 +1,19 @@
+"""
+The Carbon Simulator, a simulation engine for the Carbon protcol
+
+see https://carbondefi.xyz for details and more documentation
+see https://github.com/bancorprotocol/carbon-simulator for the repo
+
+(c) Copyright Bprotocol foundation 2022.
+Licensed under MIT
+"""
+from .routers import (
+    ExactRouterX0Y0N,
+)
+from .pair import CarbonPair
+from .simulators import CarbonSimulatorUI
+from .simulators import sim_analytics as analytics
+
+
+__version__ = "1.4"
+__date__ = "6/Dec/2022"
