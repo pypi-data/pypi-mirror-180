eee36a731014c9904ed2e3a331c3a9f72358d184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:00 +0000

    Add package chinilla-blockchain/1.4.0b4/chinilla-blockchain-1.4.0b4.tar.gz

---
+++ packages/chinilla-blockchain/chinilla-blockchain-1.4.0b4.tar.gz/chinilla-blockchain-1.4.0b4/chinilla/wallet/cat_wallet/cat_constants.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+FOUNDERS_TOKEN = {
+    "asset_id": "33bbcb7aa011958c7c3f2eb40b243779a70dedf6160c343670e7540f5403ab15",
+    "name": "Founders Token",
+    "symbol": "CFT",
+}
+
+EARLY_FARMER = {
+    "asset_id": "9c0916102ee58599dd639542cbd069776759eb1af10901873b518b79191cbbf6",
+    "name": "Early Farmer",
+    "symbol": "CEF",
+}
+
+DEFAULT_CATS = {
+    FOUNDERS_TOKEN["asset_id"]: FOUNDERS_TOKEN,
+    EARLY_FARMER["asset_id"]: EARLY_FARMER,
+}
