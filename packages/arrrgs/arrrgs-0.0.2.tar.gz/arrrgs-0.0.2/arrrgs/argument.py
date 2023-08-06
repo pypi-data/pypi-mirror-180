d9e6fba7064e77ae01b43168603eb9182c75b088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:00 +0000

    Add package arrrgs/0.0.2/arrrgs-0.0.2.tar.gz

---
+++ packages/arrrgs/arrrgs-0.0.2.tar.gz/arrrgs-0.0.2/arrrgs/argument.py
@@ -0,0 +1,7 @@
+"""Arrrgs argument"""
+from typing import List
+
+def arg(*name_or_flags: List[str], **kwargs):
+    """Convenience function to properly format arguments
+    to pass to the subcommand decorator."""
+    return (list(name_or_flags), kwargs)
