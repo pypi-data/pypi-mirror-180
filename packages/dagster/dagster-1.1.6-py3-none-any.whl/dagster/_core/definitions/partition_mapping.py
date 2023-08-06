de3c9b356787f1dbaa0f9fcb4f6040d5978b8c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:29 +0000

    Add package dagster/1.1.6/dagster-1.1.6-py3-none-any.whl

---
+++ packages/dagster/dagster-1.1.6-py3-none-any.whl/dagster/_core/definitions/partition_mapping.py
@@ -0,0 +1,144 @@
+from abc import ABC, abstractmethod
+from typing import NamedTuple, Optional
+
+import dagster._check as check
+from dagster._annotations import experimental, public
+from dagster._core.definitions.partition import PartitionsDefinition
+from dagster._core.definitions.partition_key_range import PartitionKeyRange
+from dagster._serdes import whitelist_for_serdes
+
+
+@experimental
+class PartitionMapping(ABC):
+    """Defines a correspondence between the partitions in an asset and the partitions in an asset
+    that it depends on.
+    """
+
+    @public
+    @abstractmethod
+    def get_upstream_partitions_for_partition_range(
+        self,
+        downstream_partition_key_range: Optional[PartitionKeyRange],
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        """Returns the range of partition keys in the upstream asset that include data necessary
+        to compute the contents of the given partition key range in the downstream asset.
+
+        Args:
+            downstream_partition_key_range (PartitionKeyRange): The range of partition keys in the
+                downstream asset.
+            downstream_partitions_def (PartitionsDefinition): The partitions definition for the
+                downstream asset.
+            upstream_partitions_def (PartitionsDefinition): The partitions definition for the
+                upstream asset.
+        """
+
+    @public
+    @abstractmethod
+    def get_downstream_partitions_for_partition_range(
+        self,
+        upstream_partition_key_range: PartitionKeyRange,
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        """Returns the range of partition keys in the downstream asset that use the data in the given
+        partition key range of the downstream asset.
+
+        Args:
+            upstream_partition_key_range (PartitionKeyRange): The range of partition keys in the
+                upstream asset.
+            downstream_partitions_def (PartitionsDefinition): The partitions definition for the
+                downstream asset.
+            upstream_partitions_def (PartitionsDefinition): The partitions definition for the
+                upstream asset.
+        """
+
+
+@experimental
+@whitelist_for_serdes
+class IdentityPartitionMapping(PartitionMapping, NamedTuple("_IdentityPartitionMapping", [])):
+    def get_upstream_partitions_for_partition_range(
+        self,
+        downstream_partition_key_range: Optional[PartitionKeyRange],
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        if downstream_partitions_def is None or downstream_partition_key_range is None:
+            check.failed("downstream asset is not partitioned")
+
+        return downstream_partition_key_range
+
+    def get_downstream_partitions_for_partition_range(
+        self,
+        upstream_partition_key_range: PartitionKeyRange,
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        return upstream_partition_key_range
+
+
+@experimental
+@whitelist_for_serdes
+class AllPartitionMapping(PartitionMapping, NamedTuple("_AllPartitionMapping", [])):
+    def get_upstream_partitions_for_partition_range(
+        self,
+        downstream_partition_key_range: Optional[PartitionKeyRange],
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        return PartitionKeyRange(
+            upstream_partitions_def.get_first_partition_key(),
+            upstream_partitions_def.get_last_partition_key(),
+        )
+
+    def get_downstream_partitions_for_partition_range(
+        self,
+        upstream_partition_key_range: PartitionKeyRange,
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        raise NotImplementedError()
+
+
+@experimental
+@whitelist_for_serdes
+class LastPartitionMapping(PartitionMapping, NamedTuple("_LastPartitionMapping", [])):
+    def get_upstream_partitions_for_partition_range(
+        self,
+        downstream_partition_key_range: Optional[PartitionKeyRange],
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        last_partition_key = upstream_partitions_def.get_last_partition_key()
+        return PartitionKeyRange(last_partition_key, last_partition_key)
+
+    def get_downstream_partitions_for_partition_range(
+        self,
+        upstream_partition_key_range: PartitionKeyRange,
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        raise NotImplementedError()
+
+
+def infer_partition_mapping(
+    partition_mapping: Optional[PartitionMapping], partitions_def: Optional[PartitionsDefinition]
+) -> PartitionMapping:
+    if partition_mapping is not None:
+        return partition_mapping
+    elif partitions_def is not None:
+        return partitions_def.get_default_partition_mapping()
+    else:
+        return AllPartitionMapping()
+
+
+def get_builtin_partition_mapping_types():
+    from dagster._core.definitions.time_window_partition_mapping import TimeWindowPartitionMapping
+
+    return (
+        AllPartitionMapping,
+        IdentityPartitionMapping,
+        LastPartitionMapping,
+        TimeWindowPartitionMapping,
+    )
---
+++ packages/dagster/dagster-1.1.6-py3-none-any.whl/dagster/_core/definitions/time_window_partition_mapping.py
@@ -0,0 +1,91 @@
+from typing import NamedTuple, Optional, cast
+
+import dagster._check as check
+from dagster._core.definitions.partition import PartitionsDefinition
+from dagster._core.definitions.partition_key_range import PartitionKeyRange
+from dagster._core.definitions.partition_mapping import PartitionMapping
+from dagster._core.definitions.time_window_partitions import TimeWindowPartitionsDefinition
+from dagster._core.errors import DagsterInvalidDefinitionError
+from dagster._serdes import whitelist_for_serdes
+
+
+@whitelist_for_serdes
+class TimeWindowPartitionMapping(PartitionMapping, NamedTuple("_TimeWindowPartitionMapping", [])):
+    """
+    The default mapping between two TimeWindowPartitionsDefinitions.
+
+    A partition in the downstream partitions definition is mapped to all partitions in the upstream
+    asset whose time windows overlap it.
+
+    This means that, if the upstream and downstream partitions definitions share the same time
+    period, then this mapping is essentially the identity partition mapping - plus conversion of
+    datetime formats.
+
+    If the upstream time period is coarser than the downstream time period, then each partition in
+    the downstream asset will map to a single (larger) upstream partition. E.g. if the downstream is
+    hourly and the upstream is daily, then each hourly partition in the downstream will map to the
+    daily partition in the upstream that contains that hour.
+
+    If the upstream time period is finer than the downstream time period, then each partition in the
+    downstream asset will map to multiple upstream partitions. E.g. if the downstream is daily and
+    the upstream is hourly, then each daily partition in the downstream asset will map to the 24
+    hourly partitions in the upstream that occur on that day.
+    """
+
+    def get_upstream_partitions_for_partition_range(
+        self,
+        downstream_partition_key_range: Optional[PartitionKeyRange],
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        if downstream_partitions_def is None or downstream_partition_key_range is None:
+            check.failed("downstream asset is not partitioned")
+
+        return self._map_partitions(
+            downstream_partitions_def, upstream_partitions_def, downstream_partition_key_range
+        )
+
+    def get_downstream_partitions_for_partition_range(
+        self,
+        upstream_partition_key_range: PartitionKeyRange,
+        downstream_partitions_def: Optional[PartitionsDefinition],
+        upstream_partitions_def: PartitionsDefinition,
+    ) -> PartitionKeyRange:
+        if downstream_partitions_def is None:
+            check.failed("downstream asset is not partitioned")
+
+        return self._map_partitions(
+            upstream_partitions_def, downstream_partitions_def, upstream_partition_key_range
+        )
+
+    def _map_partitions(
+        self,
+        from_partitions_def: PartitionsDefinition,
+        to_partitions_def: PartitionsDefinition,
+        from_partition_key_range: PartitionKeyRange,
+    ) -> PartitionKeyRange:
+        if not isinstance(from_partitions_def, TimeWindowPartitionsDefinition) or not isinstance(
+            from_partitions_def, TimeWindowPartitionsDefinition
+        ):
+            raise DagsterInvalidDefinitionError(
+                "TimeWindowPartitionMappings can only operate on TimeWindowPartitionsDefinitions"
+            )
+        from_partitions_def = cast(TimeWindowPartitionsDefinition, from_partitions_def)
+        to_partitions_def = cast(TimeWindowPartitionsDefinition, to_partitions_def)
+
+        if to_partitions_def.timezone != from_partitions_def.timezone:
+            raise DagsterInvalidDefinitionError("Timezones don't match")
+
+        from_start_dt = from_partitions_def.start_time_for_partition_key(
+            from_partition_key_range.start
+        )
+        from_end_dt = from_partitions_def.end_time_for_partition_key(from_partition_key_range.end)
+
+        to_start_partition_key = to_partitions_def.get_partition_key_for_timestamp(
+            from_start_dt.timestamp(), end_closed=False
+        )
+        to_end_partition_key = to_partitions_def.get_partition_key_for_timestamp(
+            from_end_dt.timestamp(), end_closed=True
+        )
+
+        return PartitionKeyRange(to_start_partition_key, to_end_partition_key)
