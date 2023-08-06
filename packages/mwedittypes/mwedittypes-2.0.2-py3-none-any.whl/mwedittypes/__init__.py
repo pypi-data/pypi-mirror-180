90c83bc3ec9f79d1ccc34622eb60c7c4fb319a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:29 +0000

    Add package mwedittypes/2.0.2/mwedittypes-2.0.2-py3-none-any.whl

---
+++ packages/mwedittypes/mwedittypes-2.0.2-py3-none-any.whl/mwedittypes/__init__.py
@@ -0,0 +1,11 @@
+from .mwedittypes import StructuredEditTypes, SimpleEditTypes
+
+__title__ = "mwedittypes"
+__summary__ = "mwedittypes is a package that supports edit diffs and action detection for Wikipedia"
+__url__ = "https://github.com/geohci/edit-types"
+
+__version__ = "2.0.2"
+
+__license__ = "MIT License"
+
+__all__ = ["StructuredEditTypes", "SimpleEditTypes"]
