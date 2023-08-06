c238a6c751b19b66e917f24bee4b88a5c4bee5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:01:33 +0000

    Add package PyQtGuiLib/0.0.1.2/PyQtGuiLib-0.0.1.2-py3-none-any.whl

---
+++ packages/PyQtGuiLib/PyQtGuiLib-0.0.1.2-py3-none-any.whl/GuiLib/header/Qt/qtWidgets.py
@@ -0,0 +1,13 @@
+from GuiLib.header.versions import PYQT_VERSIONS
+
+if PYQT_VERSIONS == "PyQt5":
+    pass
+
+if PYQT_VERSIONS == "PyQt6":
+    pass
+
+if PYQT_VERSIONS == "PySide2":
+    pass
+
+if PYQT_VERSIONS == "PySide6":
+    pass
