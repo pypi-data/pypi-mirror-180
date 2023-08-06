1a0f02f5f6db55c1852ad81dfc7d96f3fd45c502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:45 +0000

    Add package f3dasm/0.2.91/f3dasm-0.2.91.tar.gz

---
+++ packages/f3dasm/f3dasm-0.2.91.tar.gz/f3dasm-0.2.91/src/f3dasm/sampling/__init__.py
@@ -0,0 +1,5 @@
+from .latinhypercube import LatinHypercube
+from .randomuniform import RandomUniform
+from .sobolsequence import SobolSequence
+
+SAMPLERS = [LatinHypercube, RandomUniform, SobolSequence]
