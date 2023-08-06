3a312e7bb6f30339f6beecfd627355f4ca129993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:45 +0000

    Add package abmjktmyhellopkg/0.0.2/abmjktmyhellopkg-0.0.2.tar.gz

---
+++ packages/abmjktmyhellopkg/abmjktmyhellopkg-0.0.2.tar.gz/abmjktmyhellopkg-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Test create package'
+
+# Setting up
+setup(
+    name="abmjktmyhellopkg",
+    version=VERSION,
+    author="dhonyabumuhammad (Jakarta)",
+    author_email="<dp20bp@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['hellohello'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
