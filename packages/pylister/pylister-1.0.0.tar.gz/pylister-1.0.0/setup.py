5125bff043b2d9866f0eaa6d6fe32a2decb28ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:41 +0000

    Add package pylister/1.0.0/pylister-1.0.0.tar.gz

---
+++ packages/pylister/pylister-1.0.0.tar.gz/pylister-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pylister',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pylister = pylister:cli',
+            'pyl = pylister:cli',
+        ],
+    },
+)
