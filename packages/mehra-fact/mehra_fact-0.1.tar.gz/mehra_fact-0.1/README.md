3b078ba6205e462bc2b166c82b7a44dafd5e3c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:13 +0000

    Add package mehra-fact/0.1/mehra_fact-0.1.tar.gz

---
+++ packages/mehra-fact/mehra_fact-0.1.tar.gz/mehra_fact-0.1/README.md
@@ -0,0 +1,3 @@
+# Instruction how to use hm
+## Section telling how install module
+<p>Some text explaing how to install the module to python environment.</p>
---
+++ packages/mehra-fact/mehra_fact-0.1.tar.gz/mehra_fact-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='mehra_fact',
+    version='0.1',
+    description='This give the factorial of any number.',
+    author='Hitesh Mehra',
+    packages=[]
+)
