a8f92715577d3b86a41613e1810c6c0559cdcc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:26 +0000

    Add package pancakekit/0.1.3/pancakekit-0.1.3.tar.gz

---
+++ packages/pancakekit/pancakekit-0.1.3.tar.gz/pancakekit-0.1.3/src/pancakekit/toppings/__init__.py
@@ -0,0 +1,5 @@
+from .basic import Button, Label, Text, Input, DictInput, Slider, Row, Column
+from .image import ImageBox, ImageCard
+from .table import Table
+from .scientific import ArrayToImageBox
+from .automatic import FromFunction, topping_from_object
