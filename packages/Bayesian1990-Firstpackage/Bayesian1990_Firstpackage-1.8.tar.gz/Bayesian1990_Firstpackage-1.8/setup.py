6328bc65488515c7ed9693916996444f72be28d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:14 +0000

    Add package Bayesian1990-Firstpackage/1.8/Bayesian1990_Firstpackage-1.8.tar.gz

---
+++ packages/Bayesian1990-Firstpackage/Bayesian1990_Firstpackage-1.8.tar.gz/Bayesian1990_Firstpackage-1.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bayesian1990_Firstpackage',
+      version='1.8',
+      description='Probability distributions',
+      packages=['Bayesian1990_Firstpackage'],
+      author='Alessio Polymeropoulos',
+      author_email='polymeropoulos@hotmail.com',      
+      zip_safe=False)
