77ceee08fc5162f43474f049c18474f4684869a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:53 +0000

    Add package milad-utils/0.1.1/milad_utils-0.1.1.tar.gz

---
+++ packages/milad-utils/milad_utils-0.1.1.tar.gz/milad_utils-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['milad']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'milad-utils',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'Milad Alizadeh',
+    'author_email': 'milad@cohere.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
