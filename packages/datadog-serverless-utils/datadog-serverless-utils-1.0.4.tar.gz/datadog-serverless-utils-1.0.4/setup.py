bf98b6c31d0eb82beb79a0045b3415fb1ff60d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:31 +0000

    Add package datadog-serverless-utils/1.0.4/datadog-serverless-utils-1.0.4.tar.gz

---
+++ packages/datadog-serverless-utils/datadog-serverless-utils-1.0.4.tar.gz/datadog-serverless-utils-1.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='datadog-serverless-utils',
+    version='1.0.4',
+    url='https://github.com/newtonx-inc/datadog-serverless-utils',
+    author='NewtonX',
+    author_email='',
+    python_requires='>=3.4',
+    install_requires=["ddtrace>=1.6.3", "pytest>=7.0.1"],
+    test_suite="tests.test_utils",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    packages=find_packages("src", exclude="tests"),
+    description='Utilities for integration between serverless execution environments and DataDog',
+)
