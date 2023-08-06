03ccc19672eba998ec5f25877294e3a7f2fe0c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:07 +0000

    Add package ApiCrudloggingMessage/0.0.3/ApiCrudloggingMessage-0.0.3.tar.gz

---
+++ packages/ApiCrudloggingMessage/ApiCrudloggingMessage-0.0.3.tar.gz/ApiCrudloggingMessage-0.0.3/ApiCrudloggingMessage/__init__.py
@@ -0,0 +1,25 @@
+from django.http.response import JsonResponse
+
+def login_valid():  
+    return JsonResponse("Valid User",safe=False)
+
+def loginApi_invalid():
+    return "INVALID USER"
+
+def postApi_valid():
+    return "Added Successfully"
+
+def postApi_invalid():
+    return "Failed to Add"
+
+def putApi_valid():
+    return "Updated Successfully"
+
+def putApi_invalid():
+    return "Failed to Update"
+
+def deleteApi_valid():
+    return "Deleted Successfully"
+
+def deleteApi_invalid():
+    return "Failed to Delete"
