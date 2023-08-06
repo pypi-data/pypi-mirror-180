38d58d161e093a8d104a596f2e0c8cf647c01c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:10 +0000

    Add package deepchecks/0.10.0/deepchecks-0.10.0-py3-none-any.whl

---
+++ packages/deepchecks/deepchecks-0.10.0-py3-none-any.whl/deepchecks/vision/base_checks.py
@@ -0,0 +1,299 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021-2022 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
+"""Module for vision base checks."""
+import warnings
+from typing import Any, Dict, Mapping, Optional, Sequence, Union
+
+import torch
+from ignite.metrics import Metric
+from torch import nn
+
+from deepchecks.core.check_result import CheckResult
+from deepchecks.core.checks import DatasetKind, ModelOnlyBaseCheck, SingleDatasetBaseCheck, TrainTestBaseCheck
+from deepchecks.core.errors import DeepchecksValueError
+from deepchecks.utils.ipython import ProgressBarGroup
+from deepchecks.vision import deprecation_warnings  # pylint: disable=unused-import # noqa: F401
+from deepchecks.vision._shared_docs import docstrings
+from deepchecks.vision.batch_wrapper import Batch
+from deepchecks.vision.context import Context
+from deepchecks.vision.utils.vision_properties import STATIC_PROPERTIES_FORMAT
+from deepchecks.vision.vision_data import VisionData
+
+__all__ = [
+    'SingleDatasetCheck',
+    'TrainTestCheck',
+    'ModelOnlyCheck',
+]
+
+
+class SingleDatasetCheck(SingleDatasetBaseCheck):
+    """Parent class for checks that only use one dataset."""
+
+    context_type = Context
+
+    @docstrings
+    def run(
+        self,
+        dataset: VisionData,
+        model: Optional[nn.Module] = None,
+        model_name: str = '',
+        scorers: Optional[Mapping[str, Metric]] = None,
+        scorers_per_class: Optional[Mapping[str, Metric]] = None,
+        device: Union[str, torch.device, None] = None,
+        random_state: int = 42,
+        n_samples: Optional[int] = 10_000,
+        with_display: bool = True,
+        predictions: Optional[Dict[int, Union[Sequence[torch.Tensor], torch.Tensor]]] = None,
+        train_predictions: Optional[Dict[int, Union[Sequence[torch.Tensor], torch.Tensor]]] = None,
+        test_predictions: Optional[Dict[int, Union[Sequence[torch.Tensor], torch.Tensor]]] = None,
+        train_properties: Optional[STATIC_PROPERTIES_FORMAT] = None,
+        test_properties: Optional[STATIC_PROPERTIES_FORMAT] = None
+
+    ) -> CheckResult:
+        """Run check.
+
+        Parameters
+        ----------
+        dataset: VisionData
+            VisionData object to process
+        model: Optional[nn.Module] , default None
+            pytorch neural network module instance
+        {additional_context_params:2*indent}
+        """
+        assert self.context_type is not None
+        if train_predictions is not None:
+            warnings.warn('train_predictions is deprecated, please use predictions instead.',
+                          DeprecationWarning, stacklevel=2)
+        if test_predictions is not None:
+            warnings.warn('test_predictions is deprecated and ignored.',
+                          DeprecationWarning, stacklevel=2)
+        if (train_predictions is not None) and (predictions is not None):
+            raise DeepchecksValueError('Cannot accept both train_predictions and predictions, please pass the data only'
+                                       ' to predictions.')
+
+        train_predictions = train_predictions if train_predictions is not None else predictions
+        with ProgressBarGroup() as progressbar_factory:
+
+            with progressbar_factory.create_dummy(name='Validating Input'):
+                # Context is copying the data object, then not using the original after the init
+                context: Context = self.context_type(
+                    dataset,
+                    model=model,
+                    model_name=model_name,
+                    scorers=scorers,
+                    scorers_per_class=scorers_per_class,
+                    device=device,
+                    random_state=random_state,
+                    n_samples=n_samples,
+                    with_display=with_display,
+                    train_predictions=train_predictions,
+                    train_properties=train_properties,
+                    test_properties=test_properties
+                )
+                self.initialize_run(context, DatasetKind.TRAIN)
+
+            context.train.init_cache()
+
+            for i, batch in enumerate(progressbar_factory.create(
+                iterable=context.train,
+                name='Ingesting Batches',
+                unit='Batch'
+            )):
+                batch = Batch(batch, context, DatasetKind.TRAIN, i)
+                context.train.update_cache(batch)
+                self.update(context, batch, DatasetKind.TRAIN)
+
+            with progressbar_factory.create_dummy(name='Computing Check', unit='Check'):
+                result = self.compute(context, DatasetKind.TRAIN)
+                context.finalize_check_result(result, self)
+                context.add_is_sampled_footnote(result, DatasetKind.TRAIN)
+                return result
+
+    def initialize_run(self, context: Context, dataset_kind: DatasetKind):
+        """Initialize run before starting updating on batches. Optional."""
+        pass
+
+    def update(self, context: Context, batch: Any, dataset_kind: DatasetKind):
+        """Update internal check state with given batch."""
+        raise NotImplementedError()
+
+    def compute(self, context: Context, dataset_kind: DatasetKind) -> CheckResult:
+        """Compute final check result based on accumulated internal state."""
+        raise NotImplementedError()
+
+
+class TrainTestCheck(TrainTestBaseCheck):
+    """Parent class for checks that compare two datasets.
+
+    The class checks train dataset and test dataset for model training and test.
+    """
+
+    context_type = Context
+
+    @docstrings
+    def run(
+        self,
+        train_dataset: VisionData,
+        test_dataset: VisionData,
+        model: Optional[nn.Module] = None,
+        model_name: str = '',
+        scorers: Optional[Mapping[str, Metric]] = None,
+        scorers_per_class: Optional[Mapping[str, Metric]] = None,
+        device: Union[str, torch.device, None] = None,
+        random_state: int = 42,
+        n_samples: Optional[int] = 10_000,
+        with_display: bool = True,
+        train_predictions: Optional[Dict[int, Union[Sequence[torch.Tensor], torch.Tensor]]] = None,
+        test_predictions: Optional[Dict[int, Union[Sequence[torch.Tensor], torch.Tensor]]] = None,
+        train_properties: Optional[STATIC_PROPERTIES_FORMAT] = None,
+        test_properties: Optional[STATIC_PROPERTIES_FORMAT] = None
+    ) -> CheckResult:
+        """Run check.
+
+        Parameters
+        ----------
+        train_dataset: VisionData
+            VisionData object, representing data an neural network was fitted on
+        test_dataset: VisionData
+            VisionData object, representing data an neural network predicts on
+        model: Optional[nn.Module] , default None
+            pytorch neural network module instance
+        {additional_context_params:2*indent}
+        """
+        assert self.context_type is not None
+
+        with ProgressBarGroup() as progressbar_factory:
+
+            with progressbar_factory.create_dummy(name='Validating Input'):
+                # Context is copying the data object, then not using the original after the init
+                context: Context = self.context_type(
+                    train_dataset,
+                    test_dataset,
+                    model=model,
+                    model_name=model_name,
+                    scorers=scorers,
+                    scorers_per_class=scorers_per_class,
+                    device=device,
+                    random_state=random_state,
+                    n_samples=n_samples,
+                    with_display=with_display,
+                    train_predictions=train_predictions,
+                    test_predictions=test_predictions,
+                    train_properties=train_properties,
+                    test_properties=test_properties
+                )
+                self.initialize_run(context)
+
+            train_pbar = progressbar_factory.create(
+                iterable=context.train,
+                name='Ingesting Batches - Train Dataset',
+                unit='Batch'
+            )
+
+            context.train.init_cache()
+
+            for i, batch in enumerate(train_pbar):
+                batch = Batch(batch, context, DatasetKind.TRAIN, i)
+                context.train.update_cache(batch)
+                self.update(context, batch, DatasetKind.TRAIN)
+
+            context.test.init_cache()
+
+            for i, batch in enumerate(progressbar_factory.create(
+                iterable=context.test,
+                name='Ingesting Batches - Test Dataset',
+                unit='Batch'
+            )):
+                batch = Batch(batch, context, DatasetKind.TEST, i)
+                context.test.update_cache(batch)
+                self.update(context, batch, DatasetKind.TEST)
+
+            with progressbar_factory.create_dummy(name='Computing Check', unit='Check'):
+                result = self.compute(context)
+                context.finalize_check_result(result, self)
+                context.add_is_sampled_footnote(result)
+                return result
+
+    def initialize_run(self, context: Context):
+        """Initialize run before starting updating on batches. Optional."""
+        pass
+
+    def update(self, context: Context, batch: Any, dataset_kind: DatasetKind):
+        """Update internal check state with given batch for either train or test."""
+        raise NotImplementedError()
+
+    def compute(self, context: Context) -> CheckResult:
+        """Compute final check result based on accumulated internal state."""
+        raise NotImplementedError()
+
+
+class ModelOnlyCheck(ModelOnlyBaseCheck):
+    """Parent class for checks that only use a model and no datasets."""
+
+    context_type = Context
+
+    @docstrings
+    def run(
+        self,
+        model: nn.Module,
+        model_name: str = '',
+        scorers: Optional[Mapping[str, Metric]] = None,
+        scorers_per_class: Optional[Mapping[str, Metric]] = None,
+        device: Union[str, torch.device, None] = None,
+        random_state: int = 42,
+        n_samples: Optional[int] = None,
+        with_display: bool = True,
+        train_predictions: Optional[Dict[int, Union[Sequence[torch.Tensor], torch.Tensor]]] = None,
+        test_predictions: Optional[Dict[int, Union[Sequence[torch.Tensor], torch.Tensor]]] = None,
+        train_properties: Optional[STATIC_PROPERTIES_FORMAT] = None,
+        test_properties: Optional[STATIC_PROPERTIES_FORMAT] = None
+    ) -> CheckResult:
+        """Run check.
+
+        Parameters
+        ----------
+        model: nn.Module
+            pytorch neural network module instance
+        {additional_context_params:2*indent}
+        """
+        assert self.context_type is not None
+
+        with ProgressBarGroup() as progressbar_factory:
+
+            with progressbar_factory.create_dummy(name='Validating Input'):
+                context: Context = self.context_type(
+                    model=model,
+                    model_name=model_name,
+                    scorers=scorers,
+                    scorers_per_class=scorers_per_class,
+                    device=device,
+                    random_state=random_state,
+                    n_samples=n_samples,
+                    with_display=with_display,
+                    train_predictions=train_predictions,
+                    test_predictions=test_predictions,
+                    train_properties=train_properties,
+                    test_properties=test_properties
+                )
+                self.initialize_run(context)
+
+            with progressbar_factory.create_dummy(name='Computing Check', unit='Check'):
+                result = self.compute(context)
+                context.finalize_check_result(result, self)
+                return result
+
+    def initialize_run(self, context: Context):
+        """Initialize run before starting updating on batches. Optional."""
+        pass
+
+    def compute(self, context: Context) -> CheckResult:
+        """Compute final check result."""
+        raise NotImplementedError()
---
+++ packages/deepchecks/deepchecks-0.10.0-py3-none-any.whl/deepchecks/vision/suites/default_suites.py
@@ -0,0 +1,316 @@
+# ----------------------------------------------------------------------------
+# Copyright (C) 2021-2022 Deepchecks (https://www.deepchecks.com)
+#
+# This file is part of Deepchecks.
+# Deepchecks is distributed under the terms of the GNU Affero General
+# Public License (version 3 or later).
+# You should have received a copy of the GNU Affero General Public License
+# along with Deepchecks.  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+#
+# pylint: disable=unused-argument
+"""Functions for loading the default (built-in) vision suites for various validation stages.
+
+Each function returns a new suite that is initialized with a list of checks and default conditions.
+It is possible to customize these suites by editing the checks and conditions inside it after the suites' creation.
+"""
+from typing import Any, Dict, List, Tuple
+
+from ignite.metrics import Metric
+
+from deepchecks.vision import Suite
+from deepchecks.vision.checks import (ClassPerformance, ConfusionMatrixReport,  # SimilarImageLeakage,
+                                      HeatmapComparison, ImageDatasetDrift, ImagePropertyDrift, ImagePropertyOutliers,
+                                      ImageSegmentPerformance, LabelPropertyOutliers, MeanAveragePrecisionReport,
+                                      MeanAverageRecallReport, ModelErrorAnalysis, NewLabels, PropertyLabelCorrelation,
+                                      PropertyLabelCorrelationChange, SimpleModelComparison, TrainTestLabelDrift,
+                                      TrainTestPredictionDrift)
+
+__all__ = ['train_test_validation', 'model_evaluation', 'full_suite', 'data_integrity']
+
+
+def train_test_validation(n_top_show: int = 5,
+                          label_properties: List[Dict[str, Any]] = None,
+                          image_properties: List[Dict[str, Any]] = None,
+                          random_state: int = None,
+                          **kwargs) -> Suite:
+    """Suite for validating correctness of train-test split, including distribution, \
+    integrity and leakage checks.
+
+    List of Checks:
+        .. list-table:: List of Checks
+           :widths: 50 50
+           :header-rows: 1
+
+           * - Check Example
+             - API Reference
+           * - :ref:`plot_vision_new_labels`
+             - :class:`~deepchecks.vision.checks.train_test_validation.NewLabels`
+           * - :ref:`plot_vision_similar_image_leakage`
+             - :class:`~deepchecks.vision.checks.train_test_validation.SimilarImageLeakage`
+           * - :ref:`plot_vision_heatmap_comparison`
+             - :class:`~deepchecks.vision.checks.train_test_validation.HeatmapComparison`
+           * - :ref:`plot_vision_train_test_label_drift`
+             - :class:`~deepchecks.vision.checks.train_test_validation.TrainTestLabelDrift`
+           * - :ref:`plot_vision_image_property_drift`
+             - :class:`~deepchecks.vision.checks.train_test_validation.ImagePropertyDrift`
+           * - :ref:`plot_vision_image_dataset_drift`
+             - :class:`~deepchecks.vision.checks.train_test_validation.ImageDatasetDrift`
+           * - :ref:`plot_vision_feature_label_correlation_change`
+             - :class:`~deepchecks.vision.checks.train_test_validation.PropertyLabelCorrelationChange`
+
+    Parameters
+    ----------
+    n_top_show : int, default: 5
+        Number of images to show for checks that show images.
+    label_properties : List[Dict[str, Any]], default: None
+        List of properties. Replaces the default deepchecks properties.
+        Each property is a dictionary with keys ``'name'`` (str), ``method`` (Callable) and ``'output_type'`` (str),
+        representing attributes of said method. 'output_type' must be one of:
+
+        - ``'numeric'`` - for continuous ordinal outputs.
+        - ``'categorical'`` - for discrete, non-ordinal outputs. These can still be numbers,
+          but these numbers do not have inherent value.
+        - ``'class_id'`` - for properties that return the class_id. This is used because these
+          properties are later matched with the ``VisionData.label_map``, if one was given.
+
+        For more on image / label properties, see the guide about :ref:`vision_properties_guide`.
+
+    image_properties : List[Dict[str, Any]], default: None
+        List of properties. Replaces the default deepchecks properties.
+        Each property is a dictionary with keys ``'name'`` (str), ``method`` (Callable) and ``'output_type'`` (str),
+        representing attributes of said method. 'output_type' must be one of:
+
+        - ``'numeric'`` - for continuous ordinal outputs.
+        - ``'categorical'`` - for discrete, non-ordinal outputs. These can still be numbers,
+          but these numbers do not have inherent value.
+
+        For more on image / label properties, see the guide about :ref:`vision_properties_guide`.
+    random_state : int, default: None
+        Random seed for all checks.
+    **kwargs : dict
+        additional arguments to pass to the checks.
+
+    Returns
+    -------
+    Suite
+        A Suite for validating correctness of train-test split, including distribution, \
+        integrity and leakage checks.
+
+    Examples
+    --------
+    >>> from deepchecks.vision.suites import train_test_validation
+    >>> suite = train_test_validation(n_top_show=3)
+    >>> result = suite.run(n_samples=800)
+    >>> result.show()
+
+    See Also
+    --------
+    :ref:`vision_classification_tutorial`
+    :ref:`vision_detection_tutorial`
+    """
+    args = locals()
+    args.pop('kwargs')
+    non_none_args = {k: v for k, v in args.items() if v is not None}
+    kwargs = {**non_none_args, **kwargs}
+
+    return Suite(
+        'Train Test Validation Suite',
+        NewLabels(**kwargs).add_condition_new_label_ratio_less_or_equal(),
+        # SimilarImageLeakage(**kwargs).add_condition_similar_images_less_or_equal(),
+        HeatmapComparison(**kwargs),
+        TrainTestLabelDrift(**kwargs).add_condition_drift_score_less_than(),
+        ImagePropertyDrift(**kwargs).add_condition_drift_score_less_than(),
+        ImageDatasetDrift(**kwargs),
+        PropertyLabelCorrelationChange(**kwargs).add_condition_property_pps_difference_less_than(),
+    )
+
+
+def model_evaluation(alternative_metrics: Dict[str, Metric] = None,
+                     area_range: Tuple[float, float] = (32**2, 96**2),
+                     image_properties: List[Dict[str, Any]] = None,
+                     prediction_properties: List[Dict[str, Any]] = None,
+                     random_state: int = 42,
+                     **kwargs) -> Suite:
+    """Suite for evaluating the model's performance over different metrics, segments, error analysis, \
+       comparing to baseline, and more.
+
+    List of Checks:
+        .. list-table:: List of Checks
+           :widths: 50 50
+           :header-rows: 1
+
+           * - Check Example
+             - API Reference
+           * - :ref:`plot_vision_class_performance`
+             - :class:`~deepchecks.vision.checks.model_evaluation.ClassPerformance`
+           * - :ref:`plot_vision_mean_average_precision_report`
+             - :class:`~deepchecks.vision.checks.model_evaluation.MeanAveragePrecisionReport`
+           * - :ref:`plot_vision_mean_average_recall_report`
+             - :class:`~deepchecks.vision.checks.model_evaluation.MeanAverageRecallReport`
+           * - :ref:`plot_vision_train_test_prediction_drift`
+             - :class:`~deepchecks.vision.checks.model_evaluation.TrainTestPredictionDrift`
+           * - :ref:`plot_vision_simple_model_comparison`
+             - :class:`~deepchecks.vision.checks.model_evaluation.SimpleModelComparison`
+           * - :ref:`plot_vision_confusion_matrix`
+             - :class:`~deepchecks.vision.checks.model_evaluation.ConfusionMatrixReport`
+           * - :ref:`plot_vision_image_segment_performance`
+             - :class:`~deepchecks.vision.checks.model_evaluation.ImageSegmentPerformance`
+           * - :ref:`plot_vision_model_error_analysis`
+             - :class:`~deepchecks.vision.checks.model_evaluation.ModelErrorAnalysis`
+
+    Parameters
+    ----------
+    alternative_metrics : Dict[str, Metric], default: None
+        A dictionary of metrics, where the key is the metric name and the value is an ignite.Metric object whose score
+        should be used. If None are given, use the default metrics.
+    area_range: tuple, default: (32**2, 96**2)
+        Slices for small/medium/large buckets. (For object detection tasks only)
+    image_properties : List[Dict[str, Any]], default: None
+        List of properties. Replaces the default deepchecks properties.
+        Each property is a dictionary with keys ``'name'`` (str), ``method`` (Callable) and ``'output_type'`` (str),
+        representing attributes of said method. 'output_type' must be one of:
+
+        - ``'numeric'`` - for continuous ordinal outputs.
+        - ``'categorical'`` - for discrete, non-ordinal outputs. These can still be numbers,
+          but these numbers do not have inherent value.
+
+        For more on image / label properties, see the guide about :ref:`vision_properties_guide`.
+    prediction_properties : List[Dict[str, Any]], default: None
+        List of properties. Replaces the default deepchecks properties.
+        Each property is a dictionary with keys ``'name'`` (str), ``method`` (Callable) and ``'output_type'`` (str),
+        representing attributes of said method. 'output_type' must be one of:
+
+        - ``'numeric'`` - for continuous ordinal outputs.
+        - ``'categorical'`` - for discrete, non-ordinal outputs. These can still be numbers,
+          but these numbers do not have inherent value.
+        - ``'class_id'`` - for properties that return the class_id. This is used because these
+          properties are later matched with the ``VisionData.label_map``, if one was given.
+
+        For more on image / label properties, see the guide about :ref:`vision_properties_guide`.
+    random_state : int, default: 42
+        random seed for all checks.
+    **kwargs : dict
+        additional arguments to pass to the checks.
+
+    Returns
+    -------
+    Suite
+        A suite for evaluating the model's performance.
+
+    Examples
+    --------
+    >>> from deepchecks.vision.suites import model_evaluation
+    >>> suite = model_evaluation()
+    >>> result = suite.run()
+    >>> result.show()
+
+    See Also
+    --------
+    :ref:`vision_classification_tutorial`
+    :ref:`vision_detection_tutorial`
+    """
+    args = locals()
+    args.pop('kwargs')
+    non_none_args = {k: v for k, v in args.items() if v is not None}
+    kwargs = {**non_none_args, **kwargs}
+
+    return Suite(
+        'Model Evaluation Suite',
+        ClassPerformance(**kwargs).add_condition_train_test_relative_degradation_less_than(),
+        MeanAveragePrecisionReport(**kwargs).add_condition_average_mean_average_precision_greater_than(),
+        MeanAverageRecallReport(**kwargs),
+        TrainTestPredictionDrift(**kwargs).add_condition_drift_score_less_than(),
+        SimpleModelComparison(**kwargs).add_condition_gain_greater_than(),
+        ConfusionMatrixReport(**kwargs),
+        ImageSegmentPerformance(**kwargs).add_condition_score_from_mean_ratio_greater_than(),
+        ModelErrorAnalysis(**kwargs)
+    )
+
+
+def data_integrity(image_properties: List[Dict[str, Any]] = None,
+                   n_show_top: int = 5,
+                   label_properties: List[Dict[str, Any]] = None,
+                   **kwargs) -> Suite:
+    """
+    Create a suite that includes integrity checks.
+
+    List of Checks:
+        .. list-table:: List of Checks
+           :widths: 50 50
+           :header-rows: 1
+
+           * - Check Example
+             - API Reference
+           * - :ref:`plot_vision_image_property_outliers`
+             - :class:`~deepchecks.vision.checks.data_integrity.ImagePropertyOutliers`
+           * - :ref:`plot_vision_label_property_outliers`
+             - :class:`~deepchecks.vision.checks.model_evaluation.LabelPropertyOutliers`
+
+    Parameters
+    ----------
+    image_properties : List[Dict[str, Any]], default: None
+        List of properties. Replaces the default deepchecks properties.
+        Each property is a dictionary with keys ``'name'`` (str), ``method`` (Callable) and ``'output_type'`` (str),
+        representing attributes of said method. 'output_type' must be one of:
+
+        - ``'numeric'`` - for continuous ordinal outputs.
+        - ``'categorical'`` - for discrete, non-ordinal outputs. These can still be numbers,
+          but these numbers do not have inherent value.
+
+        For more on image / label properties, see the guide about :ref:`vision_properties_guide`.
+    n_show_top : int , default: 5
+        number of samples to show from each direction (upper limit and bottom limit)
+    label_properties : List[Dict[str, Any]], default: None
+        List of properties. Replaces the default deepchecks properties.
+        Each property is a dictionary with keys ``'name'`` (str), ``method`` (Callable) and ``'output_type'`` (str),
+        representing attributes of said method. 'output_type' must be one of:
+
+        - ``'numeric'`` - for continuous ordinal outputs.
+        - ``'categorical'`` - for discrete, non-ordinal outputs. These can still be numbers,
+          but these numbers do not have inherent value.
+        - ``'class_id'`` - for properties that return the class_id. This is used because these
+          properties are later matched with the ``VisionData.label_map``, if one was given.
+
+        For more on image / label properties, see the guide about :ref:`vision_properties_guide`.
+    **kwargs : dict
+        additional arguments to pass to the checks.
+
+    Returns
+    -------
+    Suite
+        A suite that includes integrity checks.
+
+    Examples
+    --------
+    >>> from deepchecks.vision.suites import data_integrity
+    >>> suite = data_integrity()
+    >>> result = suite.run()
+    >>> result.show()
+
+    See Also
+    --------
+    :ref:`vision_classification_tutorial`
+    :ref:`vision_detection_tutorial`
+    """
+    args = locals()
+    args.pop('kwargs')
+    non_none_args = {k: v for k, v in args.items() if v is not None}
+    kwargs = {**non_none_args, **kwargs}
+
+    return Suite(
+        'Data Integrity Suite',
+        ImagePropertyOutliers(**kwargs),
+        LabelPropertyOutliers(**kwargs),
+        PropertyLabelCorrelation(**kwargs)
+    )
+
+
+def full_suite(**kwargs) -> Suite:
+    """Create a suite that includes many of the implemented checks, for a quick overview of your model and data."""
+    return Suite(
+        'Full Suite',
+        model_evaluation(**kwargs),
+        train_test_validation(**kwargs),
+        data_integrity(**kwargs)
+    )
