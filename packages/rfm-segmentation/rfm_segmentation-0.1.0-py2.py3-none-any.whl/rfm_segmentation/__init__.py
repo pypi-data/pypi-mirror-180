cafce3ca85481b5254df414f0aa2a8e923afeb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:05 +0000

    Add package rfm-segmentation/0.1.0/rfm_segmentation-0.1.0-py2.py3-none-any.whl

---
+++ packages/rfm-segmentation/rfm_segmentation-0.1.0-py2.py3-none-any.whl/rfm_segmentation/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for RFM Package.
+
+rfm_segmentation
+========
+
+A package for customer segmentation with RFM analysis, tree map  and pie chart visualizations of customer segments
+
+"""
+
+__author__ = """Esmila Sahakyan"""
+__email__ = 'esmila_sahakyan@edu.aua.am'
+__version__ = '0.1.0'
