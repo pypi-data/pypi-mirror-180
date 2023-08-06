a7e2b846c34e7203ec80d7a76f97bd86f90ac4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:17 +0000

    Add package XeLib/0.8/XeLib-0.8.tar.gz

---
+++ packages/XeLib/XeLib-0.8.tar.gz/XeLib-0.8/XeLib/color.py
@@ -0,0 +1,17 @@
+from colorama import Fore, init
+
+# Initialize the colorama module with autoreset enabled
+init(autoreset=True)
+
+# Define a function that takes a string and a color number
+# and returns the string with the specified color
+def color(text, color):
+    if color == 1:
+        # Return the text with green color
+        return(Fore.GREEN + text + Fore.RESET)
+    elif color == 2:
+        # Return the text with red color
+        return(Fore.RED + text + Fore.RESET)
+    elif color == 3:
+        # Return the text with magenta color
+        return(Fore.MAGENTA + text + Fore.RESET)
---
+++ packages/XeLib/XeLib-0.8.tar.gz/XeLib-0.8/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'XeLib',
+  packages = ['XeLib'],
+  version = '0.8',
+  license='mit',
+  description = "Xemulated's QOL Library",
+  author = 'Xemulated',
+  author_email = 'xemulated@tuta.io',
+  url = 'https://github.com/xemulat',
+  download_url = 'https://github.com/xemulat/XeLib/archive/refs/tags/0.8.tar.gz',
+  keywords = ['QOL', 'xemulated', 'downloader'],
+  install_requires=['colorama', 'ping3', 'tqdm', 'functools'],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
