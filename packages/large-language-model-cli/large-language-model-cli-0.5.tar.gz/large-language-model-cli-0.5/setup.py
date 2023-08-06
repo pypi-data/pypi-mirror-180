5a0731c37282d59830651cea29e21eee598c96e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:02 +0000

    Add package large-language-model-cli/0.5/large-language-model-cli-0.5.tar.gz

---
+++ packages/large-language-model-cli/large-language-model-cli-0.5.tar.gz/large-language-model-cli-0.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import distutils.core
+
+setuptools.setup(
+    name="large-language-model-cli",
+    version=0.5,
+    author="Talwrii",
+    long_description_content_type="text/markdown",
+    author_email="Talwrii@googlemail.com",
+    description="Command-line for interacting with large language models. not TUI. chatgpt",
+    license="mit",
+    keywords="ChatGPT",
+    url="https://github.com/talwrii/llmcli",
+    packages=["llmcli"],
+    long_description=open("readme.md").read(),
+    entry_points={"console_scripts": ["llmcli=llmcli:main"]},
+    install_requires=["revChatGPT"],
+    classifiers=[],
+)
