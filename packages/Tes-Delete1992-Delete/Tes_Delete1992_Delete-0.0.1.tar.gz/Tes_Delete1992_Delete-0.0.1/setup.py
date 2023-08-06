2bb82a3ab79b55384d605f957b38db6bc56c8ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:40 +0000

    Add package Tes-Delete1992-Delete/0.0.1/Tes_Delete1992_Delete-0.0.1.tar.gz

---
+++ packages/Tes-Delete1992-Delete/Tes_Delete1992_Delete-0.0.1.tar.gz/Tes_Delete1992_Delete-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Tes'
+LONG_DESCRIPTION = 'Test'
+
+# Setting up
+setup(
+    name="Tes_Delete1992_Delete",
+    version=VERSION,
+    author="Vijayakumar",
+    author_email="<x21188955@student.ncirl.ie>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
