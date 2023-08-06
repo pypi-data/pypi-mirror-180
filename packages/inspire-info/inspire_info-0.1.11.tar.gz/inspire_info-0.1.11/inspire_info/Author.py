acd6d149799eae20cde492afa31894238f0fb2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:26 +0000

    Add package inspire-info/0.1.11/inspire_info-0.1.11.tar.gz

---
+++ packages/inspire-info/inspire_info-0.1.11.tar.gz/inspire_info-0.1.11/inspire_info/Author.py
@@ -0,0 +1,35 @@
+
+class Author(object):
+
+    def __init__(self, author):
+        self.author = author
+        self.full_name = author["full_name"]
+        self.uuid = author["uuid"]
+
+    @property
+    def affiliations(self):
+        if "affiliations" in self.author.keys():
+            affiliations = [
+                affiliation["value"]
+                for affiliation in self.author["affiliations"]
+            ]
+        elif "raw_affiliations" in self.author.keys():
+            affiliations = [
+                affiliation["value"]
+                for affiliation in self.author["raw_affiliations"]
+            ]
+        else:
+            affiliations = None
+        return affiliations
+
+    @property
+    def bai(self):
+        if "ids" in self.author.keys():
+            for id in self.author["ids"]:
+                if id["schema"] == "INSPIRE BAI":
+                    return id["value"]
+        return None
+
+
+    def __repr__(self):
+        return "Author(" + self.full_name + ")"
