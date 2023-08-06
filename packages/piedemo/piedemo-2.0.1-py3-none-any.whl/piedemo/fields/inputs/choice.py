db4ab9ed58bb076f9a70b41324b8bacf885b5ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package piedemo/2.0.1/piedemo-2.0.1-py3-none-any.whl

---
+++ packages/piedemo/piedemo-2.0.1-py3-none-any.whl/piedemo/fields/inputs/choice.py
@@ -0,0 +1,37 @@
+import os
+import os
+from .base import InputField
+
+
+class InputChoiceField(InputField):
+    def __init__(self,
+                 name,
+                 choices,
+                 enum_type=None,
+                 use_values=False,
+                 optional=False):
+        super(InputChoiceField, self).__init__(name,
+                                               optional=optional)
+        self.choices = choices
+        self.enum_type = enum_type
+        self.use_values = use_values
+
+    def generate(self):
+        return {
+            "card": "ChoiceCard",
+            "data": {
+                "name": self.name,
+                "optional": self.optional,
+                "choices": self.choices,
+            }
+        }
+
+    def parse(self, data):
+        if self.optional and hasattr(data, 'disabled') and data.disabled == 'on':
+            return None
+        if self.enum_type is None:
+            return data
+        enum_member = getattr(self.enum_type, data)
+        if self.use_values:
+            return enum_member.value
+        return enum_member
