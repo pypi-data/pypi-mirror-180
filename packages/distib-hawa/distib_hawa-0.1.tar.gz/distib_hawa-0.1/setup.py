efe694ad4e3f3c39bcd0f3382f7f3290ba2e128e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:40 +0000

    Add package distib-hawa/0.1/distib_hawa-0.1.tar.gz

---
+++ packages/distib-hawa/distib_hawa-0.1.tar.gz/distib_hawa-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distib_hawa',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distib_hawa'],
+      author = 'Hawa Desai',
+      author_email = 'techknowledge.cal@gmail.com',
+      zip_safe=False)
