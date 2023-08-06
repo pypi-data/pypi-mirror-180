bf5841ca13e4454df2a1112f8b3d997ca620d351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:11 +0000

    Add package bionty/0.5.6/bionty-0.5.6-py2.py3-none-any.whl

---
+++ packages/bionty/bionty-0.5.6-py2.py3-none-any.whl/bionty/_species/_core.py
@@ -0,0 +1,46 @@
+from functools import cached_property
+from typing import Optional
+
+import pandas as pd
+
+from .._settings import s3_bionty_assets
+from .._table import EntityTable
+
+SPECIES_FILENAME = "VpdUdouFahpvStwddqTwk.parquet"
+
+
+class Species(EntityTable):
+    """Species.
+
+    Args:
+        id: Field name that should constitute the primary reference for each
+            value. It will also be the primary key in the corresponding SQL EntityTable.
+    """
+
+    def __init__(
+        self,
+        id: Optional[str] = None,
+        database: Optional[str] = None,
+        version: Optional[str] = None,
+    ):
+        super().__init__(id=id, database=database, version=version)
+        self._id_field = "common_name" if id is None else id
+        self._lookup_col = "common_name"
+
+    @cached_property
+    def df(self) -> pd.DataFrame:
+        """DataFrame.
+
+        See ingestion: https://lamin.ai/docs/bionty-assets/ingest/ensembl-species
+        """
+        cloudpath = s3_bionty_assets(SPECIES_FILENAME)
+        self._filepath = cloudpath.fspath
+
+        df = pd.read_parquet(self._filepath)
+        df.columns = df.columns.str.lower().str.replace(" ", "_")
+        if not df.index.is_numeric():
+            df = df.reset_index().copy()
+        df = df[~df[self._id_field].isnull()]
+        df.common_name = df.common_name.str.lower()
+        df.scientific_name = df.scientific_name.str.lower()
+        return df.set_index(self._id_field)
