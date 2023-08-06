dd07d34d42ca74fd199ed0f5a02f9d7ea10f418e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:58 +0000

    Add package Bayesian1990-model/3.0/Bayesian1990_model-3.0.tar.gz

---
+++ packages/Bayesian1990-model/Bayesian1990_model-3.0.tar.gz/Bayesian1990_model-3.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bayesian1990_model',
+      version='3.0',
+      description='Linear Regression From Scratch',
+      packages=['Bayesian1990_model'],
+      author='Alessio Polymeropoulos',
+      author_email='polymeropoulos@hotmail.com',      
+      zip_safe=False)
