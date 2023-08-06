140a03987f06ffba4d06fc59ef62ccbec7e5b099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:09 +0000

    Add package jpnfmt/0.0.0/jpnfmt-0.0.0.tar.gz

---
+++ packages/jpnfmt/jpnfmt-0.0.0.tar.gz/jpnfmt-0.0.0/jpnfmt/__init__.py
@@ -0,0 +1,11 @@
+from .__version__ import __version__
+from .jpnfmt import textwrap_force
+from .jpnfmt import textwrap_overrun
+
+
+__all__ = [
+    '__version__',
+    'jpnfmt',
+    'textwrap_force',
+    'textwrap_overrun',
+]
