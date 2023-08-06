e96c7a35700f4f190345fbbbc70697000a92e6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:24 +0000

    Add package chpypi/1.2.3/chpypi-1.2.3.tar.gz

---
+++ packages/chpypi/chpypi-1.2.3.tar.gz/chpypi-1.2.3/chpypi/settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Created : maker
+# @Time    : 1992
+# @Project : chpypi
+# @Software: PyCharm
+
+proxiesKey = "6ienvavm"
+
+# 代理服务器
+proxyHost = "http-dyn.abuyun.com"
+proxyPort = "9020"
+
+# 代理隧道验证信息
+proxyUser = "HKCK7DQ36X0862BD"
+proxyPass = "BDA2D6665414DFB2"
+
+
+
+
+
+
---
+++ packages/chpypi/chpypi-1.2.3.tar.gz/chpypi-1.2.3/chpypi/tools.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Created : maker
+# @Time    : 1992
+# @Project : chpypi
+# @Software: PyCharm
+from chpypi import getproxies
+from chpypi import login
+
+
+class HTTPTOOLS:
+    def __init__(self):
+        ...
+
+    @classmethod
+    def login_chen_hai(cls):
+        return login.LOGIN
+
+    @classmethod
+    def get_proxies(cls):
+        return getproxies.PROXIES
+
+
+
+
+
+
