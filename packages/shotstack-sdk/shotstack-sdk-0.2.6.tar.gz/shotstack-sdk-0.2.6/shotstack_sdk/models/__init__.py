47ccd0789893cf50c2962d9ec18baed828625bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:02 +0000

    Add package shotstack-sdk/0.2.6/shotstack-sdk-0.2.6.tar.gz

---
+++ packages/shotstack-sdk/shotstack-sdk-0.2.6.tar.gz/shotstack-sdk-0.2.6/shotstack_sdk/models/__init__.py
@@ -0,0 +1,61 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from shotstack_sdk.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from shotstack_sdk.model.asset import Asset
+from shotstack_sdk.model.asset_render_response import AssetRenderResponse
+from shotstack_sdk.model.asset_response import AssetResponse
+from shotstack_sdk.model.asset_response_attributes import AssetResponseAttributes
+from shotstack_sdk.model.asset_response_data import AssetResponseData
+from shotstack_sdk.model.audio_asset import AudioAsset
+from shotstack_sdk.model.clip import Clip
+from shotstack_sdk.model.crop import Crop
+from shotstack_sdk.model.destinations import Destinations
+from shotstack_sdk.model.edit import Edit
+from shotstack_sdk.model.flip_transformation import FlipTransformation
+from shotstack_sdk.model.font import Font
+from shotstack_sdk.model.html_asset import HtmlAsset
+from shotstack_sdk.model.image_asset import ImageAsset
+from shotstack_sdk.model.luma_asset import LumaAsset
+from shotstack_sdk.model.merge_field import MergeField
+from shotstack_sdk.model.mux_destination import MuxDestination
+from shotstack_sdk.model.mux_destination_options import MuxDestinationOptions
+from shotstack_sdk.model.offset import Offset
+from shotstack_sdk.model.output import Output
+from shotstack_sdk.model.poster import Poster
+from shotstack_sdk.model.probe_response import ProbeResponse
+from shotstack_sdk.model.queued_response import QueuedResponse
+from shotstack_sdk.model.queued_response_data import QueuedResponseData
+from shotstack_sdk.model.range import Range
+from shotstack_sdk.model.render_response import RenderResponse
+from shotstack_sdk.model.render_response_data import RenderResponseData
+from shotstack_sdk.model.rotate_transformation import RotateTransformation
+from shotstack_sdk.model.s3_destination import S3Destination
+from shotstack_sdk.model.s3_destination_options import S3DestinationOptions
+from shotstack_sdk.model.shotstack_destination import ShotstackDestination
+from shotstack_sdk.model.size import Size
+from shotstack_sdk.model.skew_transformation import SkewTransformation
+from shotstack_sdk.model.soundtrack import Soundtrack
+from shotstack_sdk.model.template import Template
+from shotstack_sdk.model.template_data_response import TemplateDataResponse
+from shotstack_sdk.model.template_data_response_data import TemplateDataResponseData
+from shotstack_sdk.model.template_list_response import TemplateListResponse
+from shotstack_sdk.model.template_list_response_data import TemplateListResponseData
+from shotstack_sdk.model.template_list_response_item import TemplateListResponseItem
+from shotstack_sdk.model.template_render import TemplateRender
+from shotstack_sdk.model.template_response import TemplateResponse
+from shotstack_sdk.model.template_response_data import TemplateResponseData
+from shotstack_sdk.model.thumbnail import Thumbnail
+from shotstack_sdk.model.timeline import Timeline
+from shotstack_sdk.model.title_asset import TitleAsset
+from shotstack_sdk.model.track import Track
+from shotstack_sdk.model.transformation import Transformation
+from shotstack_sdk.model.transition import Transition
+from shotstack_sdk.model.video_asset import VideoAsset
