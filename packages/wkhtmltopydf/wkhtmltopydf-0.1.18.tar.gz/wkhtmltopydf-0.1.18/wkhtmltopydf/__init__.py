c30def0a097c0039f9252da6b295ea5b4f37d669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:13 +0000

    Add package wkhtmltopydf/0.1.18/wkhtmltopydf-0.1.18.tar.gz

---
+++ packages/wkhtmltopydf/wkhtmltopydf-0.1.18.tar.gz/wkhtmltopydf-0.1.18/wkhtmltopydf/__init__.py
@@ -0,0 +1,67 @@
+import requests
+import gzip
+import shutil
+import threading
+
+from typing import Literal
+from pathlib import Path
+
+HOME = Path.home()
+
+REPO = "https://github.com/zakird/wkhtmltopdf_binary_gem"
+
+VERSIONS = [
+    "archlinux_amd64",
+    "centos_6_amd64",
+    "centos_6_i386",
+    "centos_7_amd64",
+    "centos_7_i386",
+    "centos_8_amd64",
+    "debian_10_amd64",
+    "debian_10_i386",
+    "debian_9_amd64",
+    "debian_9_i386",
+    "macos_cocoa",
+    "ubuntu_16.04_amd64",
+    "ubuntu_16.04_i386",
+    "ubuntu_18.04_amd64",
+    "ubuntu_18.04_i386",
+    "ubuntu_20.04_amd64",
+    "ubuntu_22.04_amd64",
+]
+
+
+def wkhtmltopdf_bin(
+    version: Literal[tuple(VERSIONS)] = "ubuntu_22.04_amd64",
+    final_path: Path = HOME,
+    final_name: str = "wkhtmltopdf",
+):
+    if version not in VERSIONS:
+        raise ValueError("Please choose a valid version to download")
+
+    filename = f"wkhtmltopdf_{version}.gz"
+    url = f"{REPO}/blob/master/bin/{filename}?raw=true"
+    gz_path = HOME / filename
+    bin_path = final_path / final_name
+
+    r = requests.get(url, stream=True)
+    th_id = threading.current_thread()
+    print("========================================== 0 ", th_id)
+    with threading.Lock():
+        print("========================================== 1 ", th_id)
+        with open(gz_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+        print("========================================== 2 ", th_id)
+        with gzip.open(gz_path, "rb") as f_in:
+            with open(bin_path, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        print("========================================== 3 ", th_id)
+        gz_path.unlink()
+
+    print("========================================== 4 ", th_id)
+    return str(bin_path)
+
+
+wkhtmltopdf_path = wkhtmltopdf_bin()
