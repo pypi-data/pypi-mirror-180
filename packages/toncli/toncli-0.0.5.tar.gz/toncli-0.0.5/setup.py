a9d70837dc9effa3ec1098877e46622c5a3ba031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:17 +0000

    Add package toncli/0.0.5/toncli-0.0.5.tar.gz

---
+++ packages/toncli/toncli-0.0.5.tar.gz/toncli-0.0.5/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+from glob import glob
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", encoding="utf-8") as fh:
+    install_requires = fh.read().split('\n')
+
+setuptools.setup(
+    name="toncli",
+    version="0.0.5",
+    author="Andrey Tvorozhkov",
+    author_email="andrey@head-labs.com",
+    description="Easy to use CLI for fift / func projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/disintar/toncli",
+    include_package_data=True,
+    project_urls={
+        "Bug Tracker": "https://github.com/disintar/toncli/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apple Public Source License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+    package_dir={
+        "": "src",
+    },
+    install_requires=install_requires,
+    packages=['toncli'],
+    python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'toncli = toncli.main:main',
+        ],
+    }
+)
