43d36a26ed2722db2f8554d35c8f10d8c0c62857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:30:27 +0000

    Add package nonebot-plugin-gpt/0.1.17/nonebot_plugin_gpt-0.1.17.tar.gz

---
+++ packages/nonebot-plugin-gpt/nonebot_plugin_gpt-0.1.17.tar.gz/nonebot_plugin_gpt-0.1.17/nonebot_plugin_gpt/config.py
@@ -0,0 +1,17 @@
+from nonebot import get_driver
+from pydantic import BaseModel, Extra
+from typing import Optional
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    gpt_session_token: str
+    gpt_api_key: str
+    gpt_sudoers: list[int]
+    gpt_probability: float = 0
+    gpt_proxy: Optional[str] = None
+    gpt_api_baseurl: str = 'https://chat.openai.com'
+    gpt_timeout: int = 20
+    gpt_request_minimal_interval: int = 5
+
+
+gpt_config: Config = Config.parse_obj(get_driver().config)
