a02668e93a5a43f90e4cd5516ecc9deab64f37d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:56 +0000

    Add package ioutil/1.0.0/ioutil-1.0.0-py3-none-any.whl

---
+++ packages/ioutil/ioutil-1.0.0-py3-none-any.whl/ioutil/_file.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import abc
+import os
+from srutil import util
+from typing import AnyStr
+
+meta_interface = util.metaclass('interface')
+
+
+class IOInterface(metaclass=meta_interface):
+
+    def write(self, data: AnyStr, path: AnyStr | os.PathLike[AnyStr], **kwargs) -> bool:
+        pass
+
+    def read(self, path: AnyStr | os.PathLike[AnyStr], **kwargs):
+        pass
+
+
+class _File:
+    @staticmethod
+    def _get_stream(file: AnyStr | os.PathLike[AnyStr], mode: str):
+        return open(file, mode)
+
+    @abc.abstractmethod
+    def write(self, **kwargs) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def read(self, **kwargs):
+        pass
