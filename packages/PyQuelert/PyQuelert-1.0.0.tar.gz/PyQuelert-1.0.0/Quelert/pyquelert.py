13d915cca47cefed04514164799976b2247d772c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:07 +0000

    Add package PyQuelert/1.0.0/PyQuelert-1.0.0.tar.gz

---
+++ packages/PyQuelert/PyQuelert-1.0.0.tar.gz/PyQuelert-1.0.0/Quelert/pyquelert.py
@@ -0,0 +1,7 @@
+import os
+
+def ql(name):
+    os.system(f"rename {name} Quelert.py")
+
+# Vesion --> 1.0.0
+# Python-Version --> 3.6 -- 3.11.1
---
+++ packages/PyQuelert/PyQuelert-1.0.0.tar.gz/PyQuelert-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='PyQuelert',
+	version='1.0.0',
+	author='Georgy2008',
+	author_email='zadvornow2908@gmail.com',
+	description='Format for files',
+	packages=['Quelert'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
