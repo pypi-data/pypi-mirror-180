fecd5127225b192dbb52c1f36895e2dc8aee0680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:41 +0000

    Add package lap-data/4.0.44/lap_data-4.0.44.tar.gz

---
+++ packages/lap-data/lap_data-4.0.44.tar.gz/lap_data-4.0.44/src/lap_data/__init__.py
@@ -0,0 +1,7 @@
+version_info = (4, 0, 44)
+
+__version__ = '.'.join(map(str, version_info))
+__title__ = 'Laser Aided Profiler Data Access Library'
+__date__ = "9.12.2022"
+
+from lap_data.lcmodel import LCModel
