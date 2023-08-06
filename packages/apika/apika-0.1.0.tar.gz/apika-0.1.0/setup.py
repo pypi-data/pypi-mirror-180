e45ba0e2a9d87e4f88697b3cff72823b3fefd1b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:46 +0000

    Add package apika/0.1.0/apika-0.1.0.tar.gz

---
+++ packages/apika/apika-0.1.0.tar.gz/apika-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+setuptools.setup(
+    name="apika",
+    version="0.1.0",
+    author="huihui",
+    description="aio pika",
+    long_description="aio pika",
+    long_description_content_type="text/markdown",
+    url="https://github.com/JiehuiSun/apika",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+
+)
