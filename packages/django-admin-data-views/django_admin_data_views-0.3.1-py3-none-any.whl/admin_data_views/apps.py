0f408a355a1be7bec10c11bf103b13b84374163f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package django-admin-data-views/0.3.1/django_admin_data_views-0.3.1-py3-none-any.whl

---
+++ packages/django-admin-data-views/django_admin_data_views-0.3.1-py3-none-any.whl/admin_data_views/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+__all__ = [
+    "AdminDataViewsConfig",
+]
+
+
+class AdminDataViewsConfig(AppConfig):
+    name = "admin_data_views"
+    verbose_name = "Admin Data Views"
