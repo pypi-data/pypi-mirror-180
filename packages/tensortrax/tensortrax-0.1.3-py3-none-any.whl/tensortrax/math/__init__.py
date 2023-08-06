8040043fc555c7e8c5c34b36174a2b678e9a75fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:01 +0000

    Add package tensortrax/0.1.3/tensortrax-0.1.3-py3-none-any.whl

---
+++ packages/tensortrax/tensortrax-0.1.3-py3-none-any.whl/tensortrax/math/__init__.py
@@ -0,0 +1,29 @@
+r"""
+ _                            
+| |                          ████████╗██████╗  █████╗ ██╗  ██╗
+| |_ ___ _ __  ___  ___  _ __╚══██╔══╝██╔══██╗██╔══██╗╚██╗██╔╝
+| __/ _ \ '_ \/ __|/ _ \| '__|  ██║   ██████╔╝███████║ ╚███╔╝ 
+| ||  __/ | | \__ \ (_) | |     ██║   ██╔══██╗██╔══██║ ██╔██╗ 
+ \__\___|_| |_|___/\___/|_|     ██║   ██║  ██║██║  ██║██╔╝ ██╗
+                                ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝  
+"""
+
+from . import _linalg as linalg
+from ._math_tensor import (
+    sin,
+    cos,
+    tan,
+    sinh,
+    cosh,
+    tanh,
+    exp,
+    dot,
+    ddot,
+    trace,
+    transpose,
+    sum,
+    sqrt,
+    einsum,
+    matmul,
+)
+from . import _math_array as array
