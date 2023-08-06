281e6b339aa4b78b1d8e4d1846ebaa1d2199908c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package taktile-auth/0.0.15/taktile_auth-0.0.15-py3-none-any.whl

---
+++ packages/taktile-auth/taktile_auth-0.0.15-py3-none-any.whl/taktile_auth/settings.py
@@ -0,0 +1,19 @@
+import pathlib
+
+import pkg_resources
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    ENV: str = "local"
+    RESOURCE_PATH: pathlib.Path = pathlib.Path(
+        pkg_resources.resource_filename(
+            "taktile_auth", "assets/resources.yaml"
+        )
+    )
+    ROLE_PATH: pathlib.Path = pathlib.Path(
+        pkg_resources.resource_filename("taktile_auth", "assets/roles.yaml")
+    )
+
+
+settings = Settings()
