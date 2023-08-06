f4a9e29b7792d952b6a7f2518b31b944f1a4dbb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:14 +0000

    Add package s2b-task/0.0.3/s2b_task-0.0.3.tar.gz

---
+++ packages/s2b-task/s2b_task-0.0.3.tar.gz/s2b_task-0.0.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['s2b_task']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.10.2,<2.0.0', 'pytest>=7.2.0,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['do_task = s2b_task.task_executor:do_task']}
+
+setup_kwargs = {
+    'name': 's2b-task',
+    'version': '0.0.3',
+    'description': 'Python tasks manager for python3.11',
+    'long_description': 'None',
+    'author': 'nixissmtp',
+    'author_email': 'nixissmtp@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
