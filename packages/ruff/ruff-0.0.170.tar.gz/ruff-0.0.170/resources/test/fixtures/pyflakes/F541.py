7df972f79916f33c80f0c7a2cf8e09e173d30723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:26 +0000

    Add package ruff/0.0.170/ruff-0.0.170.tar.gz

---
+++ packages/ruff/ruff-0.0.170.tar.gz/ruff-0.0.170/resources/test/fixtures/pyflakes/F541.py
@@ -0,0 +1,11 @@
+a = "abc"
+b = f"ghi{'jkl'}"
+
+c = f"def"
+d = f"def" + "ghi"
+e = (
+    f"def" +
+    "ghi"
+)
+
+g = f"ghi{123:{45}}"
---
+++ packages/ruff/ruff-0.0.170.tar.gz/ruff-0.0.170/src/ast/mod.rs
@@ -0,0 +1,8 @@
+pub mod cast;
+pub mod function_type;
+pub mod helpers;
+pub mod operations;
+pub mod relocate;
+pub mod types;
+pub mod visitor;
+pub mod whitespace;
