5fd8e6c2104d2d505c5a836a4b9e7870a550956a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:21 +0000

    Add package bambi/0.9.2/bambi-0.9.2.tar.gz

---
+++ packages/bambi/bambi-0.9.2.tar.gz/bambi-0.9.2/bambi/backend/links.py
@@ -0,0 +1,43 @@
+import numpy as np
+import aesara.tensor as at
+
+
+def probit(x):
+    """Probit function that ensures result is in (0, 1)"""
+    eps = np.finfo(float).eps
+    result = 0.5 + 0.5 * at.erf(x / at.sqrt(2))
+    result = at.switch(at.eq(result, 0), eps, result)
+    result = at.switch(at.eq(result, 1), 1 - eps, result)
+
+    return result
+
+
+def cloglog(x):
+    """Cloglog function that ensures result is in (0, 1)"""
+    eps = np.finfo(float).eps
+    result = 1 - at.exp(-at.exp(x))
+    result = at.switch(at.eq(result, 0), eps, result)
+    result = at.switch(at.eq(result, 1), 1 - eps, result)
+
+    return result
+
+
+def logit(x):
+    """Logit function that ensures result is in (0, 1)"""
+    eps = np.finfo(float).eps
+    result = at.sigmoid(x)
+    result = at.switch(at.eq(result, 0), eps, result)
+    result = at.switch(at.eq(result, 1), 1 - eps, result)
+    return result
+
+
+def identity(x):
+    return x
+
+
+def inverse_squared(x):
+    return at.reciprocal(at.sqrt(x))
+
+
+def arctan_2(x):
+    return 2 * at.arctan(x)
