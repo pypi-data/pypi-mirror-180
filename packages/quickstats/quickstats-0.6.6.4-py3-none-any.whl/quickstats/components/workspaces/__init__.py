efab76929dd4f082cd4efb27fc0866e5ecf3d18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:53 +0000

    Add package quickstats/0.6.6.4/quickstats-0.6.6.4-py3-none-any.whl

---
+++ packages/quickstats/quickstats-0.6.6.4-py3-none-any.whl/quickstats/components/workspaces/__init__.py
@@ -0,0 +1,8 @@
+from quickstats.components.workspaces.xml_ws_base import XMLWSBase, WSObjectType, XMLAttribType, BracketType
+from quickstats.components.workspaces.sample import Sample
+from quickstats.components.workspaces.systematic import Systematic
+from quickstats.components.workspaces.asimov_handler import AsimovHandler
+from quickstats.components.workspaces.xml_ws_builder import XMLWSBuilder
+from quickstats.components.workspaces.xml_ws_modifier import XMLWSModifier
+from quickstats.components.workspaces.xml_ws_combiner import XMLWSCombiner
+from quickstats.components.workspaces.ws_comparer import WSComparer
