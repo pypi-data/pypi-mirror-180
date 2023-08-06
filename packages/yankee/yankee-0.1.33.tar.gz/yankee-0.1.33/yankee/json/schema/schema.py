f42f5f348cefcbd40560b88726cd1056c8b3518f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:40 +0000

    Add package yankee/0.1.33/yankee-0.1.33.tar.gz

---
+++ packages/yankee/yankee-0.1.33.tar.gz/yankee-0.1.33/yankee/json/schema/schema.py
@@ -0,0 +1,18 @@
+from yankee.base import schema
+
+from .fields import List
+from .key import JsonMixin
+
+
+class Schema(JsonMixin, schema.Schema):
+    pass
+
+
+class PolymorphicSchema(JsonMixin, schema.PolymorphicSchema):
+    pass
+
+class RegexSchema(JsonMixin, schema.RegexSchema):
+    pass
+
+class ZipSchema(JsonMixin, schema.ZipSchema):
+    pass
