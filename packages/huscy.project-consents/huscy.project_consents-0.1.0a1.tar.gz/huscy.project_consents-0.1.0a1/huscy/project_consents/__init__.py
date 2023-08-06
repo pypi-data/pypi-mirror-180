ee49a3c23cc273c479eebc26f3471d725687abea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:33 +0000

    Add package huscy.project-consents/0.1.0a1/huscy.project_consents-0.1.0a1.tar.gz

---
+++ packages/huscy.project-consents/huscy.project_consents-0.1.0a1.tar.gz/huscy.project_consents-0.1.0a1/huscy/project_consents/__init__.py
@@ -0,0 +1,3 @@
+VERSION = (0, 1, 0, 'alpha', 1)
+
+__version__ = '.'.join(str(x) for x in VERSION)
---
+++ packages/huscy.project-consents/huscy.project_consents-0.1.0a1.tar.gz/huscy.project_consents-0.1.0a1/huscy/project_consents/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from huscy.consents.models import Consent, ConsentFile
+from huscy.projects.models import Project
+from huscy.subjects.models import Subject
+
+
+class ProjectConsent(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    consent = models.ForeignKey(Consent, on_delete=models.PROTECT)
+
+
+class ProjectConsentFile(models.Model):
+    project_consent = models.ForeignKey(ProjectConsent, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    consent_file = models.OneToOneField(ConsentFile, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = 'project_consent', 'subject'
