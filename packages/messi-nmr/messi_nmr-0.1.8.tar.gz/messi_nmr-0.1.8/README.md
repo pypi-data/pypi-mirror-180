f7066b0fbd9c36374e38a63f9fa580894ba6cad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:20 +0000

    Add package messi-nmr/0.1.8/messi_nmr-0.1.8.tar.gz

---
+++ packages/messi-nmr/messi_nmr-0.1.8.tar.gz/messi_nmr-0.1.8/README.md
@@ -0,0 +1,23 @@
+# MESSI 
+
+## Multi Ensamble Strategy for Structural Elucidation
+
+Authors: Ariel M. Sarotti & María M. Zanardi 
+
+Usage: `MESSI_RCCLab: Multi Ensamble Strategy for Structural Elucidation` 
+
+### Installation Requirements
+
+**MESSI.py** needs python 3.8 or later to work. You can install the module from the command line console using:
+`pip3 install MESSI`
+
+### User Guide
+
+You need to create a folder containing the following files: 
+
+      1. The gaussian outputs of the NMR and NBO calculations (all conformers for all isomers).
+      
+      2. The excel file containing the experimental data and the labels of each nucleus associated with each experimental value.
+
+See the [project repository](https://github.com/Sarotti-Lab/ML_J_DP4) for more details.
+We still have to make GitHub repository
---
+++ packages/messi-nmr/messi_nmr-0.1.8.tar.gz/messi_nmr-0.1.8/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+  
+with open('README.md') as file:
+    long_description = file.read()
+
+short_description = 'MESSI: Multi Ensamble Strategy for Structural Elucidation'
+requirements = ['tk', 
+                'pandas', 
+                'numpy', 
+                'scipy', 
+                'openpyxl',
+                'pathlib',
+                'scikit-learn']
+  
+
+setup(
+        name ='messi_nmr',
+        version ='0.1.8',
+        author='María M. Zanardi & Ariel M. Sarotti',
+        author_email='zanardi@inv.rosario-conicet.gov.ar',
+        
+        #url='https://github.com/Sarotti-Lab/ML_J_DP4',
+        
+        description =short_description	,
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        
+        #packages=find_packages(where="messi_nmr"),
+        #package_dir={"": "messi_nmr"},
+        #package_data={"UserManual": ["*.pdf"]},
+        
+        entry_points = {'gui_scripts': ['messi = messi_nmr.messi_nmr:main']},
+        
+        include_package_data = True, 
+        package_data = {'':['UserGuide/*',
+                            'Example_messi_nmr/*']},
+        
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"],
+        keywords ='NMR structural elucidation',
+        install_requires = requirements
+)
+
+
