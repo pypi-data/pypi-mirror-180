090600da2589f131d20d356aa6496e5175b855df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:00 +0000

    Add package vikikef-date-time-lib/0.0.1/vikikef_date_time_lib-0.0.1-py3-none-any.whl

---
+++ packages/vikikef-date-time-lib/vikikef_date_time_lib-0.0.1-py3-none-any.whl/src/date_properties_pkg/date_properties.py
@@ -0,0 +1,12 @@
+import datetime
+import time
+t = datetime.datetime.now()
+class CallSample:
+
+        
+        def ReturnMyVal():    
+                    return 'Wole Olojede'
+                    def ReturnCurrentTime():    
+                                return  time.strftime("%H:%M:%S", time.localtime())
+                                def ReturnCurrentDate():    
+                                            return datetime.date
