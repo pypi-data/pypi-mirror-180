9c88377e9cca0e597e4717b547448f19d9bd0abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:37 +0000

    Add package funAD/1.0.0/funAD-1.0.0.tar.gz

---
+++ packages/funAD/funAD-1.0.0.tar.gz/funAD-1.0.0/src/funAD/__init__.py
@@ -0,0 +1,6 @@
+from .operations import *
+from .function import *
+from .optimizers import *
+
+__all__=['sin', 'cos', 'tan', 'sigmoid',  'arcsin', 'arccos', 'arctan', 'sinh', 'cosh', 'tanh', 'exp', 'log', 'sqrt', 'function',  'GD']
+
