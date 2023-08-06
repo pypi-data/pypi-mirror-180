3b5f123f5200b666e32b3c3b6847f8833425162e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:51 +0000

    Add package i-rly-like-lemons/1.1/i_rly_like_lemons-1.1.tar.gz

---
+++ packages/i-rly-like-lemons/i_rly_like_lemons-1.1.tar.gz/i_rly_like_lemons-1.1/i_rly_like_lemons/keywords.py
@@ -0,0 +1,46 @@
+#  __                                      __  __                  __  __  __                        __                                                       
+# |  \                                    |  \|  \                |  \|  \|  \                      |  \                                                      
+#  \$$        ______    ______    ______  | $$| $$ __    __       | $$ \$$| $$   __   ______        | $$  ______   ______ ____    ______   _______    _______ 
+# |  \       /      \  /      \  |      \ | $$| $$|  \  |  \      | $$|  \| $$  /  \ /      \       | $$ /      \ |      \    \  /      \ |       \  /       \
+# | $$      |  $$$$$$\|  $$$$$$\  \$$$$$$\| $$| $$| $$  | $$      | $$| $$| $$_/  $$|  $$$$$$\      | $$|  $$$$$$\| $$$$$$\$$$$\|  $$$$$$\| $$$$$$$\|  $$$$$$$
+# | $$      | $$   \$$| $$    $$ /      $$| $$| $$| $$  | $$      | $$| $$| $$   $$ | $$    $$      | $$| $$    $$| $$ | $$ | $$| $$  | $$| $$  | $$ \$$    \ 
+# | $$      | $$      | $$$$$$$$|  $$$$$$$| $$| $$| $$__/ $$      | $$| $$| $$$$$$\ | $$$$$$$$      | $$| $$$$$$$$| $$ | $$ | $$| $$__/ $$| $$  | $$ _\$$$$$$\
+# | $$      | $$       \$$     \ \$$    $$| $$| $$ \$$    $$      | $$| $$| $$  \$$\ \$$     \      | $$ \$$     \| $$ | $$ | $$ \$$    $$| $$  | $$|       $$
+#  \$$       \$$        \$$$$$$$  \$$$$$$$ \$$ \$$ _\$$$$$$$       \$$ \$$ \$$   \$$  \$$$$$$$       \$$  \$$$$$$$ \$$  \$$  \$$  \$$$$$$  \$$   \$$ \$$$$$$$ 
+#                                                 |  \__| $$                                                                                                  
+#                                                  \$$    $$                                                                                                  
+#                                                   \$$$$$$                                                                                                   
+#
+#
+#       - source code
+#       - made by 8AAFFF
+#       - keyword detection submodule
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# import the get_words script
+import get_words
+# import the extras script
+import extras
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# detect all keywords from a text
+def detect_keywords(text):
+    # define all the NOT keyword words as non_keywords
+    non_keywords = get_words.get_words("non_kwrds").split("\n")
+    # clean the text from unwanted characters and break it into a list
+    broken_text = extras.cleanup(text).split()
+    
+    # loop over each word from the original text
+    for x in range(len(broken_text)):
+        # compare each word from the original text to all the NON keywords
+        for y in range(len(non_keywords)):
+            if broken_text[x] == non_keywords[y]:
+                # if the current broken_text[x] word is NOT a keyword, then we remove it
+                # so that by the end only words that ARE keywords are left
+                broken_text[x] = ""
+
+    # return the all the keywords in a list
+    # did it like this bcs pop() didnt work for some reason (sorry future devs)
+    return " ".join(broken_text).split()
