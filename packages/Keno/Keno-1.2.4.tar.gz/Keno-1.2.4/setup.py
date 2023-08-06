bce099b52278d060410cf05407b5ea7c445bd625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:56 +0000

    Add package Keno/1.2.4/Keno-1.2.4.tar.gz

---
+++ packages/Keno/Keno-1.2.4.tar.gz/Keno-1.2.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='Keno',
+    version='1.2.4',
+    description='A Keno Game for Python',
+    url='https://github.com/Xtarii/KenoPython',
+    author='Lord Alvin Hansen',
+    author_email='alvin.hansen@elev.ga.ntig.se',
+    license='BSD 2-clause',
+    packages=['keno'],
+    install_requires=['styles>=0.1.5'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
