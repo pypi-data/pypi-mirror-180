34944a039fddeb8d96a19fc7e0ce8c847269c6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:07 +0000

    Add package qualifyzedatautils/2.22/qualifyzedatautils-2.22-py3-none-any.whl

---
+++ packages/qualifyzedatautils/qualifyzedatautils-2.22-py3-none-any.whl/qualifyzedatautils/s3utils/file_upload.py
@@ -0,0 +1,9 @@
+
+from ..connections.dwh_awsservicer import initialize_dwhservicer_credentials, establish_dwhservicer_session
+
+aws_dwhsession = establish_dwhservicer_session()
+print(99999992222222222)
+
+
+
+
