778fe53d84ae8da92c3428d1fc8141df2add4fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:52 +0000

    Add package clothes-sdk-ashuk203/1.0.6/clothes_sdk_ashuk203-1.0.6-py3-none-any.whl

---
+++ packages/clothes-sdk-ashuk203/clothes_sdk_ashuk203-1.0.6-py3-none-any.whl/clothes_sdk_ashuk203/apis/tag_to_api.py
@@ -0,0 +1,17 @@
+import typing_extensions
+
+from clothes_sdk_ashuk203.apis.tags import TagValues
+from clothes_sdk_ashuk203.apis.tags.default_api import DefaultApi
+
+TagToApi = typing_extensions.TypedDict(
+    'TagToApi',
+    {
+        TagValues.DEFAULT: DefaultApi,
+    }
+)
+
+tag_to_api = TagToApi(
+    {
+        TagValues.DEFAULT: DefaultApi,
+    }
+)
---
+++ packages/clothes-sdk-ashuk203/clothes_sdk_ashuk203-1.0.6-py3-none-any.whl/clothes_sdk_ashuk203/models/__init__.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from clothes_sdk_ashuk203.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from clothes_sdk_ashuk203.model.error_message import ErrorMessage
+from clothes_sdk_ashuk203.model.id import Id
+from clothes_sdk_ashuk203.model.logo import Logo
+from clothes_sdk_ashuk203.model.new_tshirt import NewTshirt
+from clothes_sdk_ashuk203.model.size import Size
+from clothes_sdk_ashuk203.model.tshirt import Tshirt
