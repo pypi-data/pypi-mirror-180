ca18be75887029a9ab33ae4dc14293dc16b179e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:05 +0000

    Add package vsdeband/0.6.1/vsdeband-0.6.1-py3-none-any.whl

---
+++ packages/vsdeband/vsdeband-0.6.1-py3-none-any.whl/vsdeband/abstract.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+from dataclasses import dataclass
+from vstools import inject_self, vs
+
+__all__ = [
+    'Grainer',
+
+    'Debander'
+]
+
+
+class Grainer(ABC):
+    @dataclass
+    class SupportsConfig:
+        static: bool
+        dynamic: bool
+        size: bool
+
+    config: SupportsConfig
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.kwargs = kwargs
+
+        super().__init__()
+
+    def __post_init__(self) -> None:
+        ...
+
+    def _perform_graining(
+        self, clip: vs.VideoNode, strength: float | tuple[float, float], dynamic: bool | int = True, **kwargs: Any
+    ) -> vs.VideoNode:
+        raise NotImplementedError
+
+    @inject_self.cached
+    def grain(
+        self, clip: vs.VideoNode, strength: float | tuple[float, float], dynamic: bool | int = True, **kwargs: Any
+    ) -> vs.VideoNode:
+        ...
+
+
+class Debander(Grainer):
+    @abstractmethod
+    @inject_self
+    def deband(self, clip: vs.VideoNode, **kwargs: Any) -> vs.VideoNode:
+        ...
