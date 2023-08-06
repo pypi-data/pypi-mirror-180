a237534ce66d9eb8d62e242b24b8ca5c427c06de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:26 +0000

    Add package bigdl-nano/2.2.0b20221208/bigdl_nano-2.2.0b20221208-py3-none-macosx_10_11_x86_64.whl

---
+++ packages/bigdl-nano/bigdl_nano-2.2.0b20221208-py3-none-macosx_10_11_x86_64.whl/bigdl/nano/deps/openvino/openvino_api.py
@@ -0,0 +1,67 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from functools import partial
+
+
+def PytorchOpenVINOModel(model, input_sample=None, thread_num=None,
+                         logging=True, config=None, **export_kwargs):
+    """
+    Create a OpenVINO model from pytorch.
+
+    :param model: Pytorch model to be converted to OpenVINO for inference or
+                  path to Openvino saved model.
+    :param input_sample: A set of inputs for trace, defaults to None if you have trace before or
+                         model is a LightningModule with any dataloader attached, defaults to None.
+    :param thread_num: a int represents how many threads(cores) is needed for
+                       inference. default: None.
+    :param logging: whether to log detailed information of model conversion. default: True.
+    :param config: The config to be inputted in core.compile_model.
+    :param **export_kwargs: will be passed to torch.onnx.export function.
+    :return: PytorchOpenVINOModel model for OpenVINO inference.
+    """
+    from .pytorch.model import PytorchOpenVINOModel
+    return PytorchOpenVINOModel(model=model,
+                                input_sample=input_sample,
+                                thread_num=thread_num,
+                                logging=logging,
+                                config=config,
+                                **export_kwargs)
+
+
+def load_openvino_model(path):
+    from .pytorch.model import PytorchOpenVINOModel
+    return PytorchOpenVINOModel._load(path)
+
+
+def KerasOpenVINOModel(model, thread_num=None, config=None, logging=True):
+    """
+    Create a OpenVINO model from Keras.
+
+    :param model: Keras model to be converted to OpenVINO for inference or
+                  path to Openvino saved model.
+    :param thread_num: a int represents how many threads(cores) is needed for
+                       inference. default: None.
+    :param config: The config to be inputted in core.compile_model.
+    :param logging: whether to log detailed information of model conversion. default: True.
+    :return: KerasOpenVINOModel model for OpenVINO inference.
+    """
+    from .tf.model import KerasOpenVINOModel
+    return KerasOpenVINOModel(model, thread_num=thread_num, config=config, logging=logging)
+
+
+def OpenVINOModel(model, device='CPU'):
+    from .core.model import OpenVINOModel
+    return OpenVINOModel(model, device)
