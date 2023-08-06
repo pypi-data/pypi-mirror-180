741cd06e48e336e649b0b11e2862d0871d478996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:20 +0000

    Add package image-to-excel/1.0.0/image_to_excel-1.0.0-py3-none-any.whl

---
+++ packages/image-to-excel/image_to_excel-1.0.0-py3-none-any.whl/image_to_excel/__init__.py
@@ -0,0 +1,18 @@
+""".. include:: ../../README.md"""  # noqa
+
+import logging
+
+from ._helpers import ColoredFormatter as _ColoredFormatter
+from ._helpers import SuccessLogger as _SuccessLogger
+from .base import BaseClass
+from .cli import cli_main
+
+# set up logging for the package
+logging.setLoggerClass(_SuccessLogger)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console = logging.StreamHandler()
+console.setFormatter(_ColoredFormatter("[%(name)s] (%(levelname)s): %(message)s"))
+logger.addHandler(console)
+
+__all__ = ["BaseClass", "cli_main"]
