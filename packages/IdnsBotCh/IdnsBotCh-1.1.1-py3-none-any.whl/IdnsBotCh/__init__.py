69f685698894d3a2673414b76b00d081e89a3686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:18 +0000

    Add package IdnsBotCh/1.1.1/IdnsBotCh-1.1.1-py3-none-any.whl

---
+++ packages/IdnsBotCh/IdnsBotCh-1.1.1-py3-none-any.whl/IdnsBotCh/__init__.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from time import sleep
+import re
+from selenium.webdriver.chrome.options import Options
+
+'''
+   安装IdnsBotCh:
+   pip install IdnsBotCh
+   
+   模块导入:
+   import IdnsBotCh
+   
+   发送一条"helloworld"
+   IdnsBotCh.send("helloworld")
+   
+   如果有人发送hello，回复你好
+   if IdnsBotCh.Bot.message=="hello":
+       IdnsBotCh.Bot.send_msg("你好")
+                   
+   使用愉快！！！
+'''
+
+headpr = "\033[34m"
+endpr = "\033[0m"
+headless = Options()
+headless.add_argument("--headless")
+headless.add_argument("--disable-gpu")
+
+web = webdriver.Chrome(options=headless)
+web.get("http://chat.idnsportal.com/idns_cn")
+sleep(10)
+
+
+class Bot:
+    def send_msg(text):
+        try:
+            finded = web.find_element_by_xpath('//*[@id="message"]')
+            finded.send_keys(text)
+            web.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/a[2]').click()
+            web.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        except:
+            print(headpr + "技能冷却中" + endpr)
+
+    ret = web.page_source
+    a = '<div class="message-text">(.*?)</div>'
+    message = re.findall(a, ret, flags=re.S)[-1]
+    e = '<div class="message-name">(.*?)</div>'
+    username = re.findall(e, ret)[-1].split(" | ")[0]
