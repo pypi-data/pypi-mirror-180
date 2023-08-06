1c59656b1ac04e94b831c9d91a1c4224f19017eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:14 +0000

    Add package awcon/1.1.0/awcon-1.1.0.tar.gz

---
+++ packages/awcon/awcon-1.1.0.tar.gz/awcon-1.1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="awcon",
+    version="1.1.0",
+    author="WiSpace",
+    author_email="wiforumit@gmail.com",
+    description="AnyWord connector",
+    long_description="AnyWord connector for creating libraries in python",
+    long_description_content_type="text/markdown",
+    url="https://wispace.ru/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8"
+    ],
+    python_requires='>=3.6',
+)
