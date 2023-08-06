6c12fd95750c126acba0fca349e20caf8dd2f98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:03 +0000

    Add package pecs-framework/1.2.0/pecs_framework-1.2.0.tar.gz

---
+++ packages/pecs-framework/pecs_framework-1.2.0.tar.gz/pecs_framework-1.2.0/pecs_framework/__init__.py
@@ -0,0 +1,15 @@
+from .component import Component
+from .domain import Domain, Entity
+from .engine import Engine
+from .loader import Loader
+from .events import EntityEvent
+
+
+__all__ = [
+    'Component',
+    'Domain',
+    'Entity',
+    'Engine',
+    'Loader',
+    'EntityEvent',
+]
