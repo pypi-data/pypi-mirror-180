5a8605e2690380ff5c00bf11cf3b6efd4a17ccbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:31:43 +0000

    Add package meu-pacote-aa/1.2.0/meu_pacote_aa-1.2.0-py3-none-any.whl

---
+++ packages/meu-pacote-aa/meu_pacote_aa-1.2.0-py3-none-any.whl/meu_pacote_aa/aa.py
@@ -0,0 +1,13 @@
+from hello import hello_word
+
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog='meu_pacote_aa', description='Meu pacote python aa')
+    parser.add_argument('name', type=str, help='O seu nome')
+
+    args = parser.parse_args()
+
+    hello_word(args.name)
+
