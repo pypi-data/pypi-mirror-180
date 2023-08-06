f84231af9a84f699d6cf49658b1d04e67a464888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:58 +0000

    Add package IdnsBotCh/1.1.7/IdnsBotCh-1.1.7.tar.gz

---
+++ packages/IdnsBotCh/IdnsBotCh-1.1.7.tar.gz/IdnsBotCh-1.1.7/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("C:/Users/admin/Desktop/IdnsBotCh/README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(name='IdnsBotCh',  # 包名
+      version='1.1.7',  # 版本号
+      description='一套基于selenium谷歌驱动的idns机器人框架',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='mikezhou_talk',
+      author_email='1678594309@qq.com',
+      install_requires=[],
+      license='BSD License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
