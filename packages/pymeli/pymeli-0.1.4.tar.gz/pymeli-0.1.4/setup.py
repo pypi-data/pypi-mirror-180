180bbd188c3319a94a92fe9a3c1c758f60f9f465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package pymeli/0.1.4/pymeli-0.1.4.tar.gz

---
+++ packages/pymeli/pymeli-0.1.4.tar.gz/pymeli-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages, Extension
+from os import path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pymeli',
+    version='0.1.4',
+    description='A nice SDK for Mercado Libre',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/labrixdigital/pymeli',
+    author='Ernesto Monroy',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='meli',
+    packages=find_packages(include=['pymeli']),
+    python_requires='>=3.5',
+    project_urls={
+        'Bug Reports': 'https://github.com/labrixdigital/pymeli/issues',
+        'Source': 'https://github.com/labrixdigital/pymeli',
+    },
+    install_requires = ['requests']
+)
