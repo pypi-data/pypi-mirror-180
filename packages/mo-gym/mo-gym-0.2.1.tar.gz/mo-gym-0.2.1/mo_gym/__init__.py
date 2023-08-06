e09cae6a2addca73b9ee297082e574a8ebaa5dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:19 +0000

    Add package mo-gym/0.2.1/mo-gym-0.2.1.tar.gz

---
+++ packages/mo-gym/mo-gym-0.2.1.tar.gz/mo-gym-0.2.1/mo_gym/__init__.py
@@ -0,0 +1,16 @@
+import mo_gym.breakable_bottles
+import mo_gym.deep_sea_treasure
+import mo_gym.fishwood
+import mo_gym.four_room
+import mo_gym.fruit_tree
+import mo_gym.highway
+import mo_gym.mario
+import mo_gym.minecart
+import mo_gym.mountain_car
+import mo_gym.mujoco
+import mo_gym.reacher
+import mo_gym.resource_gathering
+import mo_gym.water_reservoir
+import mo_gym.continuous_mountain_car
+from mo_gym.evaluation import *
+from mo_gym.utils import *
---
+++ packages/mo-gym/mo-gym-0.2.1.tar.gz/mo-gym-0.2.1/mo_gym/resource_gathering/__init__.py
@@ -0,0 +1,7 @@
+from gym.envs.registration import register
+
+register(
+    id="resource-gathering-v0",
+    entry_point="mo_gym.resource_gathering.resource_gathering:ResourceGathering",
+    max_episode_steps=100,
+)
