c5aaf4c3423a162ac5ef6df89321f573050089fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:28 +0000

    Add package validatortoolbox/1.6.8/validatortoolbox-1.6.8.tar.gz

---
+++ packages/validatortoolbox/validatortoolbox-1.6.8.tar.gz/validatortoolbox-1.6.8/toolbox/stats.py
@@ -0,0 +1,7 @@
+from utils.library import loadVarFile
+from utils.toolbox import menuTopperRegular
+from subprocess import PIPE, run
+
+if __name__ == "__main__":
+    loadVarFile()
+    menuTopperRegular()
