6b171f61fcfd2b86e853fd9eb4841faf30354b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:11 +0000

    Add package mkpyez/0.2/mkpyez-0.2.tar.gz

---
+++ packages/mkpyez/mkpyez-0.2.tar.gz/mkpyez-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="mkpyez",
+version="0.2",
+description="this is made only for making work easy",
+author="Shreyas Naik",
+packages=['mkpyez'],
+license='MIT',
+install_requires =[])
