be66af8383b657d8450cae133a2fe089585127f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:13 +0000

    Add package discord-ipc/0.0.4/discord_ipc-0.0.4-py3-none-any.whl

---
+++ packages/discord-ipc/discord_ipc-0.0.4-py3-none-any.whl/discord/ext/ipc/__init__.py
@@ -0,0 +1,18 @@
+import discord
+
+if discord.version_info.major < 2:
+    raise RuntimeError("You must have discord.py (v2.0 or greater) to use this library.")
+
+__title__ = "discord-ipc"
+__author__ = "benforster"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2022, Ben Forster"
+__version__ = "0.0.4"
+
+
+from .errors import BaseException, NoEndpointFoundError, MulticastFailure, InvalidReturn, ServerAlreadyStarted
+from .client import Client
+from .server import Server
+from .objects import ClientPayload, ServerResponse
+
+
