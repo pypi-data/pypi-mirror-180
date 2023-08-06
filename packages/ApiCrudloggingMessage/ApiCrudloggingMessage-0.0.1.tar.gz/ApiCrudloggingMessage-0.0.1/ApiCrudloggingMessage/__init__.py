3c716116ffb7d52637b2160f77fe9c4f3568e350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:52 +0000

    Add package ApiCrudloggingMessage/0.0.1/ApiCrudloggingMessage-0.0.1.tar.gz

---
+++ packages/ApiCrudloggingMessage/ApiCrudloggingMessage-0.0.1.tar.gz/ApiCrudloggingMessage-0.0.1/ApiCrudloggingMessage/__init__.py
@@ -0,0 +1,23 @@
+def login_valid(num1, num2):  
+    return "Valid User"
+
+def loginApi_invalid(num1, num2):
+    return "INVALID USER"
+
+def postApi_valid(num1, num2):
+    return "Added Successfully"
+
+def postApi_invalid(num1, num2):
+    return "Failed to Add"
+
+def putApi_valid(num1, num2):
+    return "Updated Successfully"
+
+def putApi_invalid(num1, num2):
+    return "Failed to Update"
+
+def deleteApi_valid(num1, num2):
+    return "Deleted Successfully"
+
+def deleteApi_invalid(num1, num2):
+    return "Failed to Delete"
