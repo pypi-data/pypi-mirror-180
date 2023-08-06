2d65c3ba6ef41ec7882314144b2c10d6a097c0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:51 +0000

    Add package sktl/2.3.2/sktl-2.3.2-cp37-cp37m-win_amd64.whl

---
+++ packages/sktl/sktl-2.3.2-cp37-cp37m-win_amd64.whl/sktl/__init__.py
@@ -0,0 +1,7 @@
+from .send import *
+from .tpool import tPool
+from .truecaptcha import trueCaptcha
+from .db import db
+from .smailpro import smailPro
+from .msapi import OneDrive
+from .snapmail import snapmail
