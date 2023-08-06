4891d8562fd46e7018ace2c844d998d66685b581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:22 +0000

    Add package irbis/0.2.376/irbis-0.2.376.tar.gz

---
+++ packages/irbis/irbis-0.2.376.tar.gz/irbis-0.2.376/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="irbis",
+    version="0.2.376",
+    author="Alexey Mironov",
+    author_email="amironov73@gmail.com",
+    description="Framework for client development for popular Prussian library computer system IRBIS64",
+    keywords='IRBIS64',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/amironov73/PythonIrbis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
