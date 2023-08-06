e18047d37892a921fa383c56b47441ef12a43ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:31 +0000

    Add package django-freeze/0.9.0/django_freeze-0.9.0-py3-none-any.whl

---
+++ packages/django-freeze/django_freeze-0.9.0-py3-none-any.whl/freeze/urls.py
@@ -0,0 +1,16 @@
+from django.urls import re_path
+
+from freeze.views import download_static_site, generate_static_site
+
+urlpatterns = [
+    re_path(
+        r"^download-static-site/$",
+        download_static_site,
+        name="freeze_download_static_site",
+    ),
+    re_path(
+        r"^generate-static-site/$",
+        generate_static_site,
+        name="freeze_generate_static_site",
+    ),
+]
