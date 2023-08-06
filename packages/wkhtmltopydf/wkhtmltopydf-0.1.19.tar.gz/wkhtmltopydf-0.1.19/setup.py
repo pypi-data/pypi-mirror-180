741f8778d5eafe33a44fccdfaa1674fac944cb73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:13 +0000

    Add package wkhtmltopydf/0.1.19/wkhtmltopydf-0.1.19.tar.gz

---
+++ packages/wkhtmltopydf/wkhtmltopydf-0.1.19.tar.gz/wkhtmltopydf-0.1.19/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wkhtmltopydf']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'wkhtmltopydf',
+    'version': '0.1.19',
+    'description': '',
+    'long_description': '',
+    'author': 'abonur',
+    'author_email': 'sm7.abonur@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
