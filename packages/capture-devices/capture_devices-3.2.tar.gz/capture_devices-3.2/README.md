e07dfe13224de75a8d2d66a40adcbd234f2c1f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:10 +0000

    Add package capture-devices/3.2/capture_devices-3.2.tar.gz

---
+++ packages/capture-devices/capture_devices-3.2.tar.gz/capture_devices-3.2/README.md
@@ -0,0 +1,4 @@
+# capture-device-list
+This repository using DirectShow with ffmpeg in order to enumarate video and audio devices and selecting specific device ID's that you can use on OpenCV, video processing and live streaming.
+
+
---
+++ packages/capture-devices/capture_devices-3.2.tar.gz/capture_devices-3.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '3.2' 
+DESCRIPTION = 'Get list of Capture Devices easily with Directshow and Python'
+LONG_DESCRIPTION = 'Simplest way to connect DirectShow Windows API with FFmpeg and list all capture devices with alternative names.\
+    The user can save the results to a file or print either "video", "audio", or both "audio, video" devices. '
+
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="capture_devices", 
+        version=VERSION,
+        author="Egemen Gulpinar",
+        author_email="<egemengulpinar@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        license = 'MIT',
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['DirectShow', 'Windows','Capture Device','Device List','FFmpeg'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+            "Topic :: Software Development",
+        ]
+)
