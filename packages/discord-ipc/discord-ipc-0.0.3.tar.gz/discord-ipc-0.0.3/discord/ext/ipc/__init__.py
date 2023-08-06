f5b8d43a5398b5d40c6a78c778f0697a7df59c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:04 +0000

    Add package discord-ipc/0.0.3/discord-ipc-0.0.3.tar.gz

---
+++ packages/discord-ipc/discord-ipc-0.0.3.tar.gz/discord-ipc-0.0.3/discord/ext/ipc/__init__.py
@@ -0,0 +1,11 @@
+import collections
+
+from discord.ext.ipc.client import Client
+from discord.ext.ipc.server import Server
+from discord.ext.ipc.errors import *
+
+
+_VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
+
+version = "0.0.3"
+version_info = _VersionInfo(0, 0, 3, "final", 0)
