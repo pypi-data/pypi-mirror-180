0baaf6d8040568297b81d1ab5fab9f107e443444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:47 +0000

    Add package 20221206.1418/3.0/20221206.1418-3.0.tar.gz

---
+++ packages/20221206.1418/20221206.1418-3.0.tar.gz/20221206.1418-3.0/eyes_recognition_module/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "xieshangce"
+__email__ = '807877537@qq.com'
+__version__ = 'v2.0'
+
+from .face_eyes_recognition_ops import encode_faces, load_encode, recognize_faces_image
+
