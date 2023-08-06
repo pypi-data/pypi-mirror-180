477e7f72b5f15438421ac03bbed95b8593c92626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:06 +0000

    Add package ossctl/0.0.5/ossctl-0.0.5.tar.gz

---
+++ packages/ossctl/ossctl-0.0.5.tar.gz/ossctl-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ossctl",
+    version="0.0.5",
+    author="wangziling100",
+    author_email="wangziling100@163.com",
+    description="oss command line controller",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
