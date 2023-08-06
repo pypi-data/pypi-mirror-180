65bd78c86c6a33e9f2fcb28b8e6f321c92706de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:04 +0000

    Add package onnx-hameln/0.0.2/onnx_hameln-0.0.2.tar.gz

---
+++ packages/onnx-hameln/onnx_hameln-0.0.2.tar.gz/onnx_hameln-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="onnx_hameln",
+    version="0.0.2",
+    author="irasin",
+    author_email="edad811@gmail.com",
+    url="https://github.com/irasin/onnx_hameln",
+    description="an onnx rewrite tool",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "onnx",
+        "onnxoptimizer",
+        "networkx"
+    ],
+    python_requires='>=3.6',
+    license="Apache License 2.0"
+)
