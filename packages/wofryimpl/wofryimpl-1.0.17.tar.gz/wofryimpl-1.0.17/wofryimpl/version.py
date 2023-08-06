2442aca78287317ed44e3da39fd5c95d7d160e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:06 +0000

    Add package wofryimpl/1.0.17/wofryimpl-1.0.17.tar.gz

---
+++ packages/wofryimpl/wofryimpl-1.0.17.tar.gz/wofryimpl-1.0.17/wofryimpl/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofry SETUP.PY
+short_version = '1.0.17'
+version = '1.0.17'
+full_version = '1.0.17'
+git_revision = '80fa1796f20badd0472c5d0651568800a6637e1d'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
