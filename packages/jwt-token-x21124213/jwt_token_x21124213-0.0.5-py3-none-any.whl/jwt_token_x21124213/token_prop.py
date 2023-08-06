35aea835bd1228a3e1656f4cc84bcd573a31a9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:02 +0000

    Add package jwt-token-x21124213/0.0.5/jwt_token_x21124213-0.0.5-py3-none-any.whl

---
+++ packages/jwt-token-x21124213/jwt_token_x21124213-0.0.5-py3-none-any.whl/jwt_token_x21124213/token_prop.py
@@ -0,0 +1,55 @@
+import jwt
+from jwt.exceptions import ExpiredSignatureError
+    
+my_secret = 't0k#N$#CR#T'
+
+def generateJWTToken(username):
+    
+    payload_data = {
+        "sub": "1313",
+        "name": username,
+        "nickname": username[0:2]
+    }
+    
+    try:
+        token = jwt.encode(
+            algorithm='HS256',
+            payload=payload_data,
+            key=my_secret
+        )
+        
+        return token
+        
+    except ExpiredSignatureError as error:
+        print(f'Unable to encode the token, error: {error}')
+
+
+def verifyJWTToken(token):
+    
+    try:
+    
+        header_data = jwt.get_unverified_header(token)
+        #print("header: ", header_data)
+        
+        decoded_payload = jwt.decode(
+            token,
+            key=my_secret,
+            algorithms=[header_data['alg'], ]
+        )
+    
+        return decoded_payload
+
+    except ExpiredSignatureError as error:
+        print(f'Unable to decode the token, error: {error}')
+    
+#Testing code here    
+if __name__ == '__main__':
+    
+    username = "Raj"
+    
+    #Success Scenario
+    token = generateJWTToken(username)
+    print("\n Generated Token: {}".format(token))
+
+    decodedValue = verifyJWTToken(token)
+    print("\n Decoded Payload: {}".format(decodedValue))
