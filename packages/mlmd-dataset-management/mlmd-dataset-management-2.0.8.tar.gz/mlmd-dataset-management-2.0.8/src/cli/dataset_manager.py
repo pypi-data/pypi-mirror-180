ab8cb79331f1cbf7bb57cb45a8db2de2b6b6b62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:19 +0000

    Add package mlmd-dataset-management/2.0.8/mlmd-dataset-management-2.0.8.tar.gz

---
+++ packages/mlmd-dataset-management/mlmd-dataset-management-2.0.8.tar.gz/mlmd-dataset-management-2.0.8/src/cli/dataset_manager.py
@@ -0,0 +1,28 @@
+
+def program(a1, a2):
+    print(a1, a2)
+
+def main():
+    import sys
+    import os
+    arg1 = sys.argv[1]
+    if arg1 == "run-moap":
+        program(arg1)
+        return
+    if arg1 == "get-prediction":
+        program(arg1)
+        return
+    if arg1 == "get-validation":
+        program(arg1)
+        return
+    if arg1 == "set-dataset":
+        arg2 = sys.argv[1]
+        os.environ["DATASET_MANAGEMENT"] = arg2
+        return
+    if arg1 == "get-dataset":
+        print(os.environ["DATASET_MANAGEMENT"])
+        return
+    print("Unsupport command: {}".format(arg1))
+
+if __name__ == "__main__":
+    main()
