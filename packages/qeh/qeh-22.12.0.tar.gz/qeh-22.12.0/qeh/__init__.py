18e727e6412a25268ba94d82c8dd1eea5d00fcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:05 +0000

    Add package qeh/22.12.0/qeh-22.12.0.tar.gz

---
+++ packages/qeh/qeh-22.12.0.tar.gz/qeh-22.12.0/qeh/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '22.12.0'
+name = "qeh"
+
+from qeh.qeh import (Heterostructure,  # noqa
+                     interpolate_building_blocks,
+                     make_heterostructure,
+                     plot_plasmons,
+                     expand_layers)
