920f2f2b0c214a4ac386a0476503a060ad887e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:13 +0000

    Add package MozPhab/1.2.3rc0/MozPhab-1.2.3rc0.tar.gz

---
+++ packages/MozPhab/MozPhab-1.2.3rc0.tar.gz/MozPhab-1.2.3rc0/tests/test_sentry.py
@@ -0,0 +1,48 @@
+from unittest import mock
+from unittest.mock import MagicMock
+
+from mozphab import mozphab
+from mozphab.config import config
+
+
+@mock.patch("mozphab.mozphab.parse_args")
+@mock.patch("mozphab.mozphab.init_sentry")
+@mock.patch("mozphab.mozphab.check_for_updates", new=mock.Mock(return_value=None))
+def test_sentry_not_enabled_if_development(mock_init_sentry, mock_parse_args):
+    config.report_to_sentry = True
+    args = MagicMock()
+    args.needs_repo = False  # skip FS operations
+    mock_parse_args.return_value = args
+
+    mozphab.main([], is_development=True)
+
+    mock_init_sentry.assert_not_called()
+
+
+@mock.patch("mozphab.mozphab.parse_args")
+@mock.patch("mozphab.mozphab.init_sentry")
+@mock.patch("mozphab.mozphab.check_for_updates", new=mock.Mock(return_value=None))
+def test_sentry_not_enabled_if_config_disabled(mock_init_sentry, mock_parse_args):
+    config.report_to_sentry = False
+    args = MagicMock()
+    args.needs_repo = False  # skip FS operations
+    mock_parse_args.return_value = args
+
+    mozphab.main([], is_development=True)
+
+    mock_init_sentry.assert_not_called()
+
+
+@mock.patch("mozphab.mozphab.parse_args")
+@mock.patch("mozphab.mozphab.init_sentry")
+@mock.patch("mozphab.mozphab.telemetry")
+@mock.patch("mozphab.mozphab.check_for_updates", new=mock.Mock(return_value=None))
+def test_sentry_enabled(_telemetry, mock_init_sentry, mock_parse_args):
+    config.report_to_sentry = True
+    args = MagicMock()
+    args.needs_repo = False  # skip FS operations
+    mock_parse_args.return_value = args
+
+    mozphab.main([], is_development=False)
+
+    mock_init_sentry.assert_called_once()
