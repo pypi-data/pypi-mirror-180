57b7bcc1c0c9e3febfbb53432348769a0f238fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:47 +0000

    Add package outliers-nerdward/0.3/outliers_nerdward-0.3.tar.gz

---
+++ packages/outliers-nerdward/outliers_nerdward-0.3.tar.gz/outliers_nerdward-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='outliers_nerdward',
+    version='0.3',
+    description='A simple package to wrap some outlier detection functionality',
+    author='Ohakim Nnaemeka',
+    license='MIT',
+    packages=['outliers'],
+    zip_safe=False
+    )
