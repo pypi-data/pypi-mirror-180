e286d74ade57ef25c2bcf85b3cfc2f22fd54d4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:33 +0000

    Add package bytecodemanipulation/0.2.0/bytecodemanipulation-0.2.0.tar.gz

---
+++ packages/bytecodemanipulation/bytecodemanipulation-0.2.0.tar.gz/bytecodemanipulation-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="bytecodemanipulation",
+    version="0.2.0",
+    author="uuk",
+    author_email="uuk1301@gmail.com",
+    description="High level python bytecode manipulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uuk0/PyBytecodeManipulator",
+    project_urls={
+        "Bug Tracker": "https://github.com/uuk0/PyBytecodeManipulator/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    packages=["bytecodemanipulation"]
+    + [
+        "bytecodemanipulation." + e
+        for e in setuptools.find_packages(where="bytecodemanipulation")
+    ],
+    python_requires=">=3.10",
+)
