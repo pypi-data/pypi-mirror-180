fa09db56b51e015467eb9f114b28cf6b277ea813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:00 +0000

    Add package prefect/2.7.1/prefect-2.7.1.tar.gz

---
+++ packages/prefect/prefect-2.7.1.tar.gz/prefect-2.7.1/src/prefect/orion/ui/assets/usePageTitle.6ac38a71.js
@@ -0,0 +1,1 @@
+import{c as n,g as r,B as u}from"./index.a71d6c38.js";function i(...e){const o=[...e,"Prefect Orion"],c=n(()=>o.map(t=>r(t)).filter(t=>t!==null).join(" \u2022 "));u(()=>document.title=c.value)}export{i as u};
