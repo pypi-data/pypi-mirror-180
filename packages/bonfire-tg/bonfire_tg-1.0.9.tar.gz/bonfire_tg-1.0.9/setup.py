5a512e3a56d18ddba6b4c349044ea3e24ba8610c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:52 +0000

    Add package bonfire-tg/1.0.9/bonfire_tg-1.0.9.tar.gz

---
+++ packages/bonfire-tg/bonfire_tg-1.0.9.tar.gz/bonfire_tg-1.0.9/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+# Открытие README.md и присвоение его long_description.
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+# Определение requests как requirements для того, чтобы этот пакет работал. Зависимости проекта.
+# requirements = ["requests<=2.21.0"]
+
+# Функция, которая принимает несколько аргументов. Она присваивает эти значения пакету.
+setuptools.setup(
+	# Имя дистрибутива пакета. Оно должно быть уникальным, поэтому добавление вашего имени пользователя в конце является обычным делом.
+	name="bonfire_tg",
+	# Номер версии вашего пакета. Обычно используется семантическое управление версиями.
+	version="1.0.9",
+	# Имя автора.
+	author="Help_urself",
+	# Его почта.
+	author_email="alexliulev1@gmail.com",
+	# Краткое описание, которое будет показано на странице PyPi.
+	description="Speed telegram api libraly",
+	# Длинное описание, которое будет отображаться на странице PyPi. Использует README.md репозитория для заполнения.
+	long_description=long_description,
+	# Определяет тип контента, используемый в long_description.
+	long_description_content_type="text/markdown",
+	# URL-адрес, представляющий домашнюю страницу проекта. Большинство проектов ссылаются на репозиторий.
+	url="https://github.com/Help-urself/bonfire",
+	# Находит все пакеты внутри проекта и объединяет их в дистрибутив.
+	packages=setuptools.find_packages(),
+	# requirements или dependencies, которые будут установлены вместе с пакетом, когда пользователь установит его через pip.
+	# install_requires=requirements,
+	# Предоставляет pip некоторые метаданные о пакете. Также отображается на странице PyPi.
+	install_requires = ['requests >= 2.27.1','colorama >= 0.4.6','APScheduler >= 3.9.1.post1'],
+	classifiers=[
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	# Требуемая версия Python.
+	python_requires='>=3.7',
+)
