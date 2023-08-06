ab4bec77c30f664e38cdec420a0d544e4c5ed63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package py-assimilator/0.1.1/py_assimilator-0.1.1.tar.gz

---
+++ packages/py-assimilator/py_assimilator-0.1.1.tar.gz/py_assimilator-0.1.1/assimilator/alchemy/database/specifications.py
@@ -0,0 +1,15 @@
+from assimilator.database.specification import Specification
+
+
+class FilterSpecification(Specification):
+    def __init__(self, *filters, **filters_by):
+        self.filters = filters
+        self.filters_by = filters_by
+
+    def apply(self, query):
+        return query.filter(*self.filters).filter_by(**self.filters_by)
+
+
+class ValueInSpecification(FilterSpecification):
+    def __init__(self, field, values):
+        super(ValueInSpecification, self).__init__(field.in_(values))
