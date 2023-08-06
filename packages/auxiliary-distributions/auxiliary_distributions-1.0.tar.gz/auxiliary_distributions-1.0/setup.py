7f981d167c2511060ebff316650e0d8114bb70cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:28 +0000

    Add package auxiliary-distributions/1.0/auxiliary_distributions-1.0.tar.gz

---
+++ packages/auxiliary-distributions/auxiliary_distributions-1.0.tar.gz/auxiliary_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='auxiliary_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['auxiliary_distributions'],
+      zip_safe=False)
