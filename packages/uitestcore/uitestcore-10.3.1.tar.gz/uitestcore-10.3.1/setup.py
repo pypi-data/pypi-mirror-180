368e4f01e65a2770cf62ed10afacd6be280d33f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:27:26 +0000

    Add package uitestcore/10.3.1/uitestcore-10.3.1.tar.gz

---
+++ packages/uitestcore/uitestcore-10.3.1.tar.gz/uitestcore-10.3.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="uitestcore",
+    version="10.3.1",
+    description="Package providing common functionality for UI automation test packs",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    homepage="https://github.com/nhsuk/ui-test-core/",
+    packages=["uitestcore", "uitestcore.utilities"],
+    install_requires=[
+        "certifi==2022.9.24",
+        "chardet==5.0.0",
+        "idna==3.4",
+        "pyhamcrest==2.0.4",
+        "python-dateutil==2.8.2",
+        "requests==2.28.1",
+        "selenium==4.4.3",
+        "six==1.16.0",
+        "urllib3==1.26.12",
+        "axe-selenium-python-nhsuk"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
