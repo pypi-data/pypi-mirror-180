f250c0dfb66b584a43d91bb2bfd6417e2016ca22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:52 +0000

    Add package tsp-c/0.0.30/tsp-c-0.0.30.tar.gz

---
+++ packages/tsp-c/tsp-c-0.0.30.tar.gz/tsp-c-0.0.30/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+from setuptools import dist
+
+class BinaryDistribution(dist.Distribution):
+    def has_ext_modules(tsp_c):
+        return True
+        
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tsp-c",
+    version="0.0.30",
+    author="Udom Janjarassuk",
+    author_email="kjudom@gmail.com",
+    description="A wrapper for c++ to solve the Traveling Salesman Problem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kjudom/tsp-c",
+    project_urls={
+        "Bug Tracker": "https://github.com/kjudom/tsp-c/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        #"Operating System :: POSIX :: LINUX",        
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    # packages=['tsp_c'],
+    python_requires=">=3.6",
+    
+    # make sure the shared library is included
+    # package_data={'tsp_c': ['_tsp_c_lnx.so']},    # for Linux
+    # package_data={'tsp_c': ['_tsp_c_win.so']},      # for Windows, not working
+    package_data={'tsp_c': ['_tsp_c.so']},
+    include_package_data=True,
+)
