a9dc4bd34c9524c64286309cbb2224a5cf5fdbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:29 +0000

    Add package circuitsvis/1.32.0/circuitsvis-1.32.0-py3-none-any.whl

---
+++ packages/circuitsvis/circuitsvis-1.32.0-py3-none-any.whl/circuitsvis/attention.py
@@ -0,0 +1,79 @@
+"""Attention visualisations"""
+from typing import List, Optional, Union
+
+import numpy as np
+import torch
+from circuitsvis.utils.render import RenderedHTML, render
+
+
+def attention_patterns(
+    tokens: List[str],
+    attention: Union[list, np.ndarray, torch.Tensor],
+) -> RenderedHTML:
+    """Attention Patterns
+
+    Visualization of attention head patterns.
+
+    Args:
+        tokens: List of tokens (e.g. `["A", "person"]`)
+        attention: Attention tensor of the shape [num_heads x dest_tokens x
+        src_tokens]
+
+    Returns:
+        Html: Attention patterns visualization
+    """
+    return render(
+        "AttentionPatterns",
+        tokens=tokens,
+        attention=attention,
+    )
+
+
+def attention_pattern(
+    tokens: List[str],
+    attention: Union[list, np.ndarray, torch.Tensor],
+    max_value: Optional[float] = None,
+    min_value: Optional[float] = None,
+    negative_color: Optional[str] = None,
+    show_axis_labels: Optional[bool] = None,
+    positive_color: Optional[str] = None,
+) -> RenderedHTML:
+    """Attention Pattern
+
+    Attention pattern from destination to source tokens. Displays a heatmap of
+    attention values (hover to see the specific values).
+
+    Args:
+        tokens: List of tokens (e.g. `["A", "person"]`). Must be the same length
+        as the list of values.
+        attention: Attention head activations of the shape [dest_tokens x
+        src_tokens]
+        max_value: Maximum value. Used to determine how dark the token color is
+        when positive (i.e. based on how close it is to the maximum value).
+        min_value: Minimum value. Used to determine how dark the token color is
+        when negative (i.e. based on how close it is to the minimum value).
+        negative_color: Color for negative values. This can be any valid CSS
+        color string. Be mindful of color blindness if not using the default
+        here.
+        show_axis_labels: Whether to show axis labels.
+        positive_color: Color for positive values. This can be any valid CSS
+        color string. Be mindful of color blindness if not using the default
+        here.
+
+    Returns:
+        Html: Attention pattern visualization
+    """
+    kwargs = {
+        "tokens": tokens,
+        "attention": attention,
+        "minValue": min_value,
+        "maxValue": max_value,
+        "negativeColor": negative_color,
+        "positiveColor": positive_color,
+        "showAxisLabels": show_axis_labels,
+    }
+
+    return render(
+        "AttentionPattern",
+        **kwargs
+    )
