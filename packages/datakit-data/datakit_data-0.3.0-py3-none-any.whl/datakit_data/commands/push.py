bd763777e72d6d4395684756cb5e633280cc7d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:29 +0000

    Add package datakit-data/0.3.0/datakit_data-0.3.0-py3-none-any.whl

---
+++ packages/datakit-data/datakit_data-0.3.0-py3-none-any.whl/datakit_data/commands/push.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+from cliff.command import Command
+from datakit import CommandHelpers
+
+from ..extra_flags import ExtraFlags
+from ..project_mixin import ProjectMixin
+from ..s3 import S3
+
+
+class Push(ProjectMixin, CommandHelpers, Command):
+
+    "Push local data to S3"
+
+    def get_parser(self, prog_name):
+        parser = super(Push, self).get_parser(prog_name)
+        parser.add_argument(
+            'args',
+            nargs=argparse.REMAINDER,
+            help="One or more boolean S3 sync flags" +
+            " without leading dashes, e.g. delete or dryrun"
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        user_profile = self.project_configs['aws_user_profile']
+        bucket = self.project_configs['s3_bucket']
+        if not os.path.exists("config/datakit-data.json"):
+            self.log.info("No config file found - have you run `datakit data init`?")
+            return
+        if bucket is "":
+            self.log.info("No bucket specified in config - no data pushed")
+            return
+        s3 = S3(user_profile, bucket)
+        clean_flags = ExtraFlags.convert(parsed_args.args)
+        s3.push(
+            'data/',
+            self.project_configs['s3_path'],
+            extra_flags=clean_flags
+        )
