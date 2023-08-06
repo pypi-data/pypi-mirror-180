39a94f1d32c1d8cb35ac48bda2f5e6ee337df702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:33 +0000

    Add package py-Ayiin/0.1.4/py_Ayiin-0.1.4-py3-none-any.whl

---
+++ packages/py-Ayiin/py_Ayiin-0.1.4-py3-none-any.whl/pyAyiin/__init__.py
@@ -0,0 +1,104 @@
+# py - Ayiin
+# Copyright (C) 2022-2023 @AyiinXd
+#
+# This file is a part of < https://github.com/AyiinXd/pyAyiin >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/AyiinXd/pyAyiin/blob/main/LICENSE/>.
+#
+# FROM py-Ayiin <https://github.com/AyiinXd/pyAyiin>
+# t.me/AyiinChat & t.me/AyiinSupport
+
+
+# ========================×========================
+#            Jangan Hapus Credit Ngentod
+# ========================×========================
+
+import asyncio
+import logging
+import sys
+import time
+from aiohttp import ClientSession
+
+from pyAyiin.Clients import *
+from pyAyiin.methods import *
+from pyAyiin.pyrogram import AyiinMethods
+from pyAyiin.pyrogram import eod, eor
+from pyAyiin.xd import GenSession
+from pyAyiin.telethon.ayiin import *
+
+
+# Bot Logs setup:
+logging.basicConfig(
+    format="[%(name)s] - [%(levelname)s] - %(message)s",
+    level=logging.INFO,
+)
+logging.getLogger("pyAyiin").setLevel(logging.INFO)
+logging.getLogger("fipper").setLevel(logging.ERROR)
+logging.getLogger("fipper.client").setLevel(logging.ERROR)
+logging.getLogger("fipper.session.auth").setLevel(logging.ERROR)
+logging.getLogger("fipper.session.session").setLevel(logging.ERROR)
+
+
+logs = logging.getLogger(__name__)
+
+
+__copyright__ = "Copyright (C) 2022-present AyiinXd <https://github.com/AyiinXd>"
+__license__ = "GNU General Public License v3.0 (GPL-3.0)"
+__version__ = "0.1.4"
+ayiin_ver = "0.0.3"
+
+
+adB = AyiinDB()
+
+DEVS = [997461844, 1905050903, 1965424892]
+
+StartTime = time.time()
+
+
+class PyrogramXd(AyiinMethods, GenSession, Methods):
+    pass
+
+
+class TelethonXd(AyiinMethod, GenSession, Methods):
+    pass
+
+
+suc_msg = (f"""
+========================×========================
+           Credit Py-Ayiin {__version__}
+========================×========================
+"""
+)
+
+fail_msg = (f"""
+========================×========================
+      Commit Yang Bener Bego Biar Gak Error
+           Credit Py-Ayiin {__version__}
+========================×========================
+"""
+)
+
+run_as_module = False
+
+if sys.argv[0] == "-m":
+    run_as_module = True
+
+    from .decorator import *
+
+    print("\n\n" + __copyright__ + "\n" + __license__)
+    print(suc_msg)
+
+    update_envs()
+
+    CMD_HELP = {}
+    adB = AyiinDB()
+    aiosession = ClientSession()
+    loop = asyncio.get_event_loop()
+    HOSTED_ON = where_hosted()
+else:
+    logs.info("\n\n" + __copyright__ + "\n" + __license__)
+
+    adB = AyiinDB()
+    aiosession = ClientSession()
+    loop = asyncio.get_event_loop()
+    HOSTED_ON = where_hosted()
