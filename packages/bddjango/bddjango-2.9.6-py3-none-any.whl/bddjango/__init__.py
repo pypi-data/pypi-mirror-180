0b1d58aa3d32984bcbd67da9af430b4dd5580284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:48 +0000

    Add package bddjango/2.9.6/bddjango-2.9.6-py3-none-any.whl

---
+++ packages/bddjango/bddjango-2.9.6-py3-none-any.whl/bddjango/__init__.py
@@ -0,0 +1,18 @@
+from warnings import warn
+from .pure import *
+
+
+def version():
+    v = "2.9.6"
+    return v
+
+
+try:
+    from .django import *
+except Exception as e:
+    warn('导入django失败? --- ' + str(e))
+
+try:
+    from .myFIelds import AliasField  # 这个只能在这里引用, 不然`adminclass`报错
+except Exception as e:
+    warn('导入`AliasField`失败? --- ' + str(e))
