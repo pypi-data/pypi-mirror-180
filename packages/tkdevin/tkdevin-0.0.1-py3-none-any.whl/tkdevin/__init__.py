013e7f8f93ace683cf378585361d25ee8c0d40b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:02 +0000

    Add package tkdevin/0.0.1/tkdevin-0.0.1-py3-none-any.whl

---
+++ packages/tkdevin/tkdevin-0.0.1-py3-none-any.whl/tkdevin/__init__.py
@@ -0,0 +1,4 @@
+from tkdevin.mark import *
+from tkdevin.gradient import use_gradient, draw_gradient
+from tkdevin.move import use_move, make_movable_object
+from tkdevin.tooltip import use_tooltip, set_tooltip
