7e5451b4e8acc89ae94a8bb7035ebdd36ec9cfe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:28 +0000

    Add package fqllang/0.4.0/fqllang-0.4.0.tar.gz

---
+++ packages/fqllang/fqllang-0.4.0.tar.gz/fqllang-0.4.0/src/fqllang/dsl/utils.py
@@ -0,0 +1,11 @@
+def getGrammar():
+    with open(f"fqllang/dsl/fqlgrammar.tx") as f:
+        return f.read()
+
+def getGrammarPath():
+    return "fqllang/dsl/fqlgrammar.tx"
+
+def getClassName(object):
+    return object.__class__.__name__
+
+
