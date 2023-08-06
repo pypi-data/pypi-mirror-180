c94b716831bcb3f7f878237d58bcb350b6eef452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:53 +0000

    Add package kybra/0.1.0/kybra-0.1.0.tar.gz

---
+++ packages/kybra/kybra-0.1.0.tar.gz/kybra-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kybra',
+    version='0.1.0',
+    package_data={
+        'kybra': ['compiler/**', 'canisters/**']
+    },
+    include_package_data=True,
+    packages=['kybra'],
+    install_requires=['modulegraph==0.19.3']
+)
