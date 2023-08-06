fff72b7c881df644f80a5a4c32d545c504e10d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:16 +0000

    Add package test1393691/1.0/test1393691-1.0.tar.gz

---
+++ packages/test1393691/test1393691-1.0.tar.gz/test1393691-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='test1393691',
+    packages=['test1393691'],
+    version='1.0',
+    license='MIT',
+    description='test',
+    author='test',
+    keywords=['test'],
+    install_requires=[''],
+    classifiers=['Development Status :: 5 - Production/Stable', 'Intended Audience :: Developers', 'Topic :: Software Development :: Libraries :: Python Modules', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.10']
+)
