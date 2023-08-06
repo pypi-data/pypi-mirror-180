2e86e1b6e8826d9720e815c450182bc291e9d816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:59 +0000

    Add package keeps/0.0.1/keeps-0.0.1.tar.gz

---
+++ packages/keeps/keeps-0.0.1.tar.gz/keeps-0.0.1/clitools/config/config_loader.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from clitools.command_group_master import CommandGroupMaster
+from clitools.config.common import get_global_commands_file_path
+from clitools.config.yaml_parser import YamlParser
+
+
+@dataclass
+class ConfigLoader:
+    def load(self) -> CommandGroupMaster:
+        """
+        Check for the existence of local- and global configuration files, and if they do not exist,
+        ask the user if they should be created.
+        """
+        global_command_groups = self._load_global()
+        local_command_groups = self._load_local()
+        return CommandGroupMaster(
+            local_command_groups=local_command_groups, global_command_groups=global_command_groups
+        )
+
+    def _load_global(self):
+        global_commands_file_path = get_global_commands_file_path()
+        if global_commands_file_path.exists():
+            return YamlParser().parse(global_commands_file_path)
+        return {}
+
+    def _load_local(self):
+        if Path("commands.yaml").exists():
+            return YamlParser().parse(Path("commands.yaml"))
+        return {}
---
+++ packages/keeps/keeps-0.0.1.tar.gz/keeps-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['clitools', 'clitools.config', 'clitools.screens']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0', 'keyboard>=0.13.5,<0.14.0', 'pyyaml>=6.0,<7.0']
+
+entry_points = \
+{'console_scripts': ['clitools = clitools.cli:clitools']}
+
+setup_kwargs = {
+    'name': 'keeps',
+    'version': '0.0.1',
+    'description': 'Test project',
+    'long_description': '# clitools\n\n[![Release](https://img.shields.io/github/v/release/fpgmaas/clitools)](https://img.shields.io/github/v/release/fpgmaas/clitools)\n[![Build status](https://img.shields.io/github/workflow/status/fpgmaas/clitools/Main/main)](https://github.com/fpgmaas/clitools/actions/workflows/main.yml?query=branch%3Amain)\n[![codecov](https://codecov.io/gh/fpgmaas/clitools/branch/main/graph/badge.svg)](https://codecov.io/gh/fpgmaas/clitools)\n[![Commit activity](https://img.shields.io/github/commit-activity/m/fpgmaas/clitools)](https://img.shields.io/github/commit-activity/m/fpgmaas/clitools)\n[![License](https://img.shields.io/github/license/fpgmaas/clitools)](https://img.shields.io/github/license/fpgmaas/clitools)\n\nTest project\n\n- **Github repository**: <https://github.com/fpgmaas/clitools/>\n- **Documentation** <https://fpgmaas.github.io/clitools/>\n\n## Getting started with your project\n\nFirst, create a repository on GitHub with the same name as this project, and then run the following commands:\n\n``` bash\ngit init -b main\ngit add .\ngit commit -m "init commit"\ngit remote add origin git@github.com:fpgmaas/clitools.git\ngit push -u origin main\n```\n\nFinally, install the environment and the pre-commit hooks with \n\n```bash\nmake install\n```\n\nYou are now ready to start development on your project! The CI/CD\npipeline will be triggered when you open a pull request, merge to main,\nor when you create a new release.\n\nTo finalize the set-up for publishing to PyPi or Artifactory, see\n[here](https://fpgmaas.github.io/clitools/features/publishing/#set-up-for-pypi).\nFor activating the automatic documentation with MkDocs, see\n[here](https://fpgmaas.github.io/clitools/features/mkdocs/#enabling-the-documentation-on-github).\nTo enable the code coverage reports, see [here](https://fpgmaas.github.io/clitools/features/codecov/).\n\n## Releasing a new version\n\n\n\n---\n\nRepository initiated with [fpgmaas/clitools](https://github.com/fpgmaas/clitools).',
+    'author': 'Florian Maas',
+    'author_email': 'ffpgmaas@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/fpgmaas/clitools',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<=3.11',
+}
+
+
+setup(**setup_kwargs)
