9cc63cfe51e22424ee0f7343df372639af3fa243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:40 +0000

    Add package lywt/0.0.12/lywt-0.0.12.tar.gz

---
+++ packages/lywt/lywt-0.0.12.tar.gz/lywt-0.0.12/README.md
@@ -0,0 +1,3 @@
+# 你可以在 dist 目录里下载到最新的和老版本的 lywt
+
+# 同时你可以使用命令行 pip install lywt 在你当前的 python 环境中安装 lywt
---
+++ packages/lywt/lywt-0.0.12.tar.gz/lywt-0.0.12/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'lywt'
+DESCRIPTION = 'laolu的工具包'
+URL = 'https://github.com/LuShengcan'
+AUTHOR_EMAIL = 'shengcan_lu@foxmail.com'
+AUTHOR = 'laolu'
+REQUIRES_PYTHON = '>=3.0'
+VERSION = '0.0.12'
+
+# 打包命令
+# python setup.py sdist build
+# twine upload dist/lywt-0.0.11.tar.gz
+
+# What packages are required for this module to be executed?
+REQUIRED = ['requests', 'bs4', 'lxml', 'windnd']
+
+# Setting.
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    license="MIT"
+)
