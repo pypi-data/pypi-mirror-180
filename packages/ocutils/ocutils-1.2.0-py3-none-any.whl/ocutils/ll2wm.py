d753a5091202b6f0a0975c799fac62a9d5ccc94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:30 +0000

    Add package ocutils/1.2.0/ocutils-1.2.0-py3-none-any.whl

---
+++ packages/ocutils/ocutils-1.2.0-py3-none-any.whl/ocutils/ll2wm.py
@@ -0,0 +1,38 @@
+"""
+ll2wm function definition
+"""
+
+import numpy
+from ocutils import earth_radius
+
+
+def ll2wm(lat, lon, lon0=0):
+
+    """
+    Convert latitude and longitude to Web-Mercator coordinates.
+
+    args...
+        lat: Numpy array of latitudes (degrees)
+        lon: Numpy array of longitudes (degrees)
+    kwargs...
+        lon0: longitude of reference meridian (degrees)
+    returns...
+        x: Numpy array of horizontal distances (m)
+        y: Numpy array of vertical distances (m)
+    """
+
+    if numpy.any(lat <= -90) or numpy.any(lat >= 90):
+        raise ValueError('Latitude must be > -90 and < 90 degrees')
+
+    if numpy.any(lon < -180) or numpy.any(lon > 180):
+        raise ValueError('Longitude must be >= -180 and <= 180 degrees')
+
+    lonr = numpy.deg2rad(lon)
+    lonr0 = numpy.deg2rad(lon0)
+    latr = numpy.deg2rad(lat)
+
+    x = earth_radius * (lonr - lonr0)
+
+    y = earth_radius * numpy.log(numpy.tan(numpy.pi / 4 + latr / 2))
+
+    return x, y
