4a1b356a1f949e88f4892a460e77bf02b86bf06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:38 +0000

    Add package rubpy/5.1.1/rubpy-5.1.1.tar.gz

---
+++ packages/rubpy/rubpy-5.1.1.tar.gz/rubpy-5.1.1/rubpy/__init__.py
@@ -0,0 +1,15 @@
+from .accounts._client import _Client as Client
+from .bots import _Client as Bot
+from .crypto import Crypto
+from .util import Utils
+from .exceptions import (
+    InvaildAuth,
+    InvalidInput,
+    TooRequests,
+    Repeated,
+    NotRegistered,
+)
+
+
+__version__ = '5.1.1'
+__author__ = 'Shayan Heidari'
