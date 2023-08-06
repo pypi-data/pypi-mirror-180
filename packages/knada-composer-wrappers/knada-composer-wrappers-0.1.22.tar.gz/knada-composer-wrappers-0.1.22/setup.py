984ae579b075d7ce40f06cbf0265ae3bc8b096cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:25 +0000

    Add package knada-composer-wrappers/0.1.22/knada-composer-wrappers-0.1.22.tar.gz

---
+++ packages/knada-composer-wrappers/knada-composer-wrappers-0.1.22.tar.gz/knada-composer-wrappers-0.1.22/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='knada-composer-wrappers',
+    version='0.1.22',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+    # metadata to display on PyPI
+    author="NAV IKT",
+    url="https://github.com/navikt/knada-composer-wrappers",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
