21a5c37e7505d9e0df5dc10c86bd1d8f5752e363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:59 +0000

    Add package nerualpha/3.3.2/nerualpha-3.3.2-py3-none-any.whl

---
+++ packages/nerualpha/nerualpha-3.3.2-py3-none-any.whl/nerualpha/services/config/IConfig.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field, asdict
+from typing import Dict, List, Generic, TypeVar
+from abc import ABC, abstractmethod
+
+from nerualpha.IBridge import IBridge
+from nerualpha.services.config.urlObject import UrlObject
+
+
+#interface
+class IConfig(ABC):
+    bridge:IBridge
+    instanceServiceName:str
+    applicationId:str
+    apiApplicationId:str
+    apiAccountId:str
+    instanceId:str
+    privateKey:str
+    debug:bool
+    appUrl:str
+    assetUrl:str
+    namespace:str
+    @abstractmethod
+    def getExecutionUrl(self,func,pathname = None):
+        pass
