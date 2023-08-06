e4f990bdbb3c3f34edcb17ba27cfd07df7d9e4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:51 +0000

    Add package geometry1/0.1/geometry1-0.1.tar.gz

---
+++ packages/geometry1/geometry1-0.1.tar.gz/geometry1-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geometry1',
+    version='0.1',
+    author='Nikita Egorov',
+    author_email='egorovnikita2020@gmail.com',
+    description='This package is useful for some geometric calculations',
+    long_description=open('README.md').read(),
+    packages=find_packages()
+)
