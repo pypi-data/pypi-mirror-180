9f30cf1d3bf04402bbe8b3bb15c9b6c9413a4d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:10 +0000

    Add package ez-profile/0.1.3/ez_profile-0.1.3.tar.gz

---
+++ packages/ez-profile/ez_profile-0.1.3.tar.gz/ez_profile-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+__ver = "0.1.3"
+
+setup(
+    name="ez_profile",
+    packages=find_packages(include=["ez_profile"]),
+    version=__ver,
+    description="A basic wrapper around cProfile with optional snakeviz integration",
+    author="oddbookworm",
+    author_email="andrewryancoffey@hotmail.com",
+    license="MIT",
+    install_requires=['snakeviz'],
+    url="https://github.com/oddbookworm/ez_profile",
+    download_url="https://github.com/oddbookworm/ez_profile/archive/refs/tags/v{__ver}.tar.gz",
+    keywords=["ez_profile"],
+    classifiers=[],
+)
