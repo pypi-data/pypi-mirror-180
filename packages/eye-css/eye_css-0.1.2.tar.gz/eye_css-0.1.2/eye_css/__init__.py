746795eff4d2a930747d2d52fc94c7c49345740a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:19 +0000

    Add package eye-css/0.1.2/eye_css-0.1.2.tar.gz

---
+++ packages/eye-css/eye_css-0.1.2.tar.gz/eye_css-0.1.2/eye_css/__init__.py
@@ -0,0 +1,13 @@
+# MAYOWA OBISESAN
+# AUGUST 18, 2022.
+
+"""
+Eye CSS
+
+A Dynamic Utility-First CSS Library written in python.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Mayowa Obisesan"
+__credits__ = "Create Inc."
+
