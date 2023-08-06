39512b5e8e47003769ec1d08ddc37f963ce70e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:15 +0000

    Add package poetry-build-test/1.0/poetry_build_test-1.0.tar.gz

---
+++ packages/poetry-build-test/poetry_build_test-1.0.tar.gz/poetry_build_test-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='poetry_build_test',
+    version='1.0',
+    description='FXC py pkg test',
+    author='JFrog',
+    author_email='jfrog@jfrog.com',
+    packages=['poetry_build_test']
+)
