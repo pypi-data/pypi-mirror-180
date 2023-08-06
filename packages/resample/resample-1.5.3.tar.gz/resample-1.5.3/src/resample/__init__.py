4d47fa1078ccaceaa47c16e7fcc4f95e79a1c379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:59 +0000

    Add package resample/1.5.3/resample-1.5.3.tar.gz

---
+++ packages/resample/resample-1.5.3.tar.gz/resample-1.5.3/src/resample/__init__.py
@@ -0,0 +1,15 @@
+"""
+Resampling tools for Python.
+
+This library offers randomisation-based inference in Python based on data resampling
+and permutation. The following functionality is implemented.
+
+- Bootstrap samples (ordinary or balanced with optional stratification) from N-D arrays
+- Apply parametric bootstrap (Gaussian, Poisson, gamma, etc.) on samples
+- Compute bootstrap confidence intervals (percentile or BCa) for any estimator
+- Jackknife estimates of bias and variance of any estimator
+- Permutation-based variants of traditional statistical tests (t-test, K-S test, etc.)
+- Tools for working with empirical distributions (CDF, quantile, etc.)
+"""
+
+from ._version import version as __version__  # noqa
