295ea15c4e0f6e7f0fd57e2a0ae31bdb7a89f43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:27 +0000

    Add package x21218315/0.0.1/x21218315-0.0.1.tar.gz

---
+++ packages/x21218315/x21218315-0.0.1.tar.gz/x21218315-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='x21218315',
+  version='0.0.1',
+  description='publish email ',
+#   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Manoj K',
+  author_email='x21218315@student.ncirl.ie',
+#   license='MIT', 
+  classifiers=classifiers,
+  keywords='x21218315',
+  packages=find_packages(),
+  install_requires=[''] 
+)
