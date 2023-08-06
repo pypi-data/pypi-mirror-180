e3e19b454ea2387c9c969b6183b977cdf9c30163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:08 +0000

    Add package opendrift-leeway-webgui/2022.12.0/opendrift_leeway_webgui-2022.12.0-py3-none-any.whl

---
+++ packages/opendrift-leeway-webgui/opendrift_leeway_webgui-2022.12.0-py3-none-any.whl/opendrift_leeway_webgui/leeway/tasks.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+from datetime import datetime
+
+from django.apps import apps
+from django.conf import settings
+from celery import shared_task
+
+from .celery import app
+
+from .utils import send_result_mail
+
+@app.task
+def run_leeway_simulation(request_id):
+    """
+    Get parameters for simulation from database and kick off the simulation
+    process in a docker container. The result is then mailed to the user.
+    """
+    LeewaySimulation = apps.get_model(app_label='leeway', model_name='LeewaySimulation')
+    simulation = LeewaySimulation.objects.get(uuid=request_id)
+    simulation.simulation_started = datetime.now()
+    simulation.save()
+    params = ["docker", "run", "--volume",
+              "{}:/code/leeway".format(settings.SIMULATION_ROOT),
+              "opendrift/opendrift",
+              "python3", "leeway/simulation.py",
+              "--longitude", str(simulation.longitude),
+              "--latitude", str(simulation.latitude),
+              "--radius", str(simulation.radius),
+              "--number", str(settings.OPENDRIFT_NUMBER_DRIFTERS),
+              "--start-time", str(simulation.start_time.strftime("%Y-%m-%d %H:%M")),
+              "--duration", str(simulation.duration),
+              "--id", str(simulation.uuid)]
+    sim_proc = subprocess.Popen(params)
+    sim_proc.communicate()
+    simulation.simulation_finished = datetime.now()
+    send_result_mail(simulation)
+    simulation.save()
