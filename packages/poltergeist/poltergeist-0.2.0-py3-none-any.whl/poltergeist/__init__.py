cac31720853189b18df15c1fbb30f0a5c04cd983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:41 +0000

    Add package poltergeist/0.2.0/poltergeist-0.2.0-py3-none-any.whl

---
+++ packages/poltergeist/poltergeist-0.2.0-py3-none-any.whl/poltergeist/__init__.py
@@ -0,0 +1,55 @@
+import functools
+from dataclasses import dataclass
+from typing import Callable, Generic, NoReturn, ParamSpec, Type, TypeVar
+
+P = ParamSpec("P")
+T = TypeVar("T")
+E = TypeVar("E", bound=BaseException)
+
+
+@dataclass(repr=False, frozen=True, slots=True)
+class Ok(Generic[T, E]):
+    _value: T
+
+    def __repr__(self) -> str:
+        return f"Ok({repr(self._value)})"
+
+    def unwrap(self) -> T:
+        return self._value
+
+
+@dataclass(repr=False, frozen=True, slots=True)
+class Err(Generic[T, E]):
+    _err: E
+
+    def __repr__(self) -> str:
+        return f"Err({repr(self._err)})"
+
+    def unwrap(self) -> NoReturn:
+        raise self._err
+
+
+Result = Ok[T, E] | Err[T, E]
+
+
+def poltergeist(
+    error: Type[E],
+) -> Callable[[Callable[P, T]], Callable[P, Ok[T, E] | Err[T, E]]]:
+    """
+    Decorator that wraps the result of a function into an Ok object if it
+    executes without raising an exception. Otherwise, returns an Err object with
+    the exception raised by the function.
+    """
+
+    def decorator(func: Callable[P, T]) -> Callable[P, Ok[T, E] | Err[T, E]]:
+        @functools.wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> Ok[T, E] | Err[T, E]:
+            try:
+                result = func(*args, **kwargs)
+            except error as e:
+                return Err(e)
+            return Ok(result)
+
+        return wrapper
+
+    return decorator
