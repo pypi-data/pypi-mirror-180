6fb6cf12f1429e012e957891b65dc7334cd792ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:33 +0000

    Add package pdfCropMargins/1.1.0/pdfCropMargins-1.1.0-py3-none-any.whl

---
+++ packages/pdfCropMargins/pdfCropMargins-1.1.0-py3-none-any.whl/pdfCropMargins/__init__.py
@@ -0,0 +1,10 @@
+"""
+pdfCropMargins
+"""
+
+
+__version__ = "1.1.0" # major version, minor version, patch (see PEP440)
+
+# The only function designed to be called from a user's Python code.
+from pdfCropMargins.pdfCropMargins import crop
+
