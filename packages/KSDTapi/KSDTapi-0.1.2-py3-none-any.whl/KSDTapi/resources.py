5a89203478c77bd4b55a80b37969ecff8d795936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:01 +0000

    Add package KSDTapi/0.1.2/KSDTapi-0.1.2-py3-none-any.whl

---
+++ packages/KSDTapi/KSDTapi-0.1.2-py3-none-any.whl/KSDTapi/resources.py
@@ -0,0 +1,10 @@
+import requests
+
+def getResource():
+    url = "http://3.39.12.67:8083/qcc/jobs"
+
+    response = requests.get(f"{url}") # , params=params)
+
+    print("[LIST] ", response.text, flush=True)
+
+    return response.text
