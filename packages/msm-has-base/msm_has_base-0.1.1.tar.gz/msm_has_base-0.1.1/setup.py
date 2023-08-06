fe2eb20f910f5f1bf255d69c38a012667a13a7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:44 +0000

    Add package msm-has-base/0.1.1/msm_has_base-0.1.1.tar.gz

---
+++ packages/msm-has-base/msm_has_base-0.1.1.tar.gz/msm_has_base-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['msm_has_base']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'msm-has-base',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': '袁首京',
+    'author_email': 'yuanshoujing@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
