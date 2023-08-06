c9932f2d63fb424aaecbb33884d1670d84f7013a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package pygazpar/1.2.0a55/pygazpar-1.2.0a55-py37-none-any.whl

---
+++ packages/pygazpar/pygazpar-1.2.0a55-py37-none-any.whl/pygazpar/enum.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+# ------------------------------------------------------------------------------------------------------------
+class PropertyName(Enum):
+    TIME_PERIOD = "time_period"
+    START_INDEX = "start_index_m3"
+    END_INDEX = "end_index_m3"
+    VOLUME = "volume_m3"
+    ENERGY = "energy_kwh"
+    CONVERTER_FACTOR = "converter_factor_kwh/m3"
+    TEMPERATURE = "temperature_degC"
+    TYPE = "type"
+    TIMESTAMP = "timestamp"
+
+
+# ------------------------------------------------------------------------------------------------------------
+class Frequency(Enum):
+    HOURLY = 0
+    DAILY = 1
+    WEEKLY = 2
+    MONTHLY = 3
+    YEARLY = 4
+
+    def __str__(self):
+        return self.name
