854bdc04644d29dd0cfcd7f13bcf19b78205ffe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:05 +0000

    Add package sparta-memcached/0.1.4/sparta_memcached-0.1.4-py3-none-any.whl

---
+++ packages/sparta-memcached/sparta_memcached-0.1.4-py3-none-any.whl/sparta/memcached/dummy.py
@@ -0,0 +1,52 @@
+from sparta.memcached.base import SpartaCache
+
+
+class SpartaCacheDummy(SpartaCache):
+    """
+    Dummy implementation, useful to disable caching without breaking code deps.
+    """
+
+    def func_decorator(self, key: str, time: int = None):
+        def decorator(f):
+            return f
+
+        return decorator
+
+    def atomic_append(self, key, value, _retries=3, *args, **kwargs):
+        pass
+
+    def add(self, key, *args, **kwargs):
+        pass
+
+    def append(self, key, *args, **kwargs):
+        pass
+
+    def cas(self, key, *args, **kwargs):
+        pass
+
+    def decr(self, key, *args, **kwargs):
+        pass
+
+    def delete(self, key, *args, **kwargs):
+        pass
+
+    def get(self, key, *args, **kwargs):
+        pass
+
+    def gets(self, key, *args, **kwargs):
+        pass
+
+    def incr(self, key, *args, **kwargs):
+        pass
+
+    def prepend(self, key, *args, **kwargs):
+        pass
+
+    def replace(self, key, *args, **kwargs):
+        pass
+
+    def set(self, key, *args, **kwargs):
+        pass
+
+    def touch(self, key, *args, **kwargs):
+        pass
