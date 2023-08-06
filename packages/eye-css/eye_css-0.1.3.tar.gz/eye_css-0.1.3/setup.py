758f5c32913a461172be78075cef0194103baa26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:48 +0000

    Add package eye-css/0.1.3/eye_css-0.1.3.tar.gz

---
+++ packages/eye-css/eye_css-0.1.3.tar.gz/eye_css-0.1.3/setup.py
@@ -0,0 +1,24 @@
+# MAYOWA OBISESAN
+# August 18, 2022.
+
+from setuptools import setup
+
+setup(
+    name='pyexample',
+    version='0.1.0',
+    description='A dynamic utility-first css library written in python',
+    url='https://github.com/EYE_CSS',
+    author='Mayowa Obisesan',
+    author_email='mayowaobi74@gmail.com',
+    license='MIT',
+    packages=['eye_css'],
+    install_requires=['watchdog~=2.1.9'],
+    classifiers=[
+        'Development Status :: 5 - Production',
+        'Intended Audience :: Web/Design',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux :: Windows',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10.6',
+    ],
+)
