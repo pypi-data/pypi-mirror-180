5cd91a80da0a6890e3c258e5f1859e7e8ce05844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:08 +0000

    Add package rsocket/0.4.6/rsocket-0.4.6-py3-none-any.whl

---
+++ packages/rsocket/rsocket-0.4.6-py3-none-any.whl/rsocket/handlers/request_response_responder.py
@@ -0,0 +1,30 @@
+from asyncio import Future
+
+from rsocket.frame import CancelFrame, Frame
+from rsocket.rsocket import RSocket
+from rsocket.streams.stream_handler import StreamHandler
+
+
+class RequestResponseResponder(StreamHandler):
+    def __init__(self, socket: RSocket, future: Future):
+        super().__init__(socket)
+        self.future = future
+
+    def setup(self):
+        self.future.add_done_callback(self.future_done)
+
+    def future_done(self, future):
+        if self.future.cancelled():
+            pass
+        elif not future.exception():
+            self.socket.send_payload(
+                self.stream_id, future.result(), complete=True)
+        else:
+            self.socket.send_error(self.stream_id, future.exception())
+
+        self._finish_stream()
+
+    def frame_received(self, frame: Frame):
+        if isinstance(frame, CancelFrame):
+            self.future.cancel()
+            self._finish_stream()
---
+++ packages/rsocket/rsocket-0.4.6-py3-none-any.whl/rsocket/streams/stream_from_async_generator.py
@@ -0,0 +1,23 @@
+from typing import AsyncGenerator, Tuple
+
+from rsocket.async_helpers import async_range
+from rsocket.payload import Payload
+from rsocket.streams.exceptions import FinishedIterator
+from rsocket.streams.stream_from_generator import StreamFromGenerator
+
+
+class StreamFromAsyncGenerator(StreamFromGenerator):
+    async def _start_generator(self):
+        self._iteration = self._generator().__aiter__()
+
+    async def _generate_next_n(self, n: int) -> AsyncGenerator[Tuple[Payload, bool], None]:
+        is_complete_sent = False
+        async for i in async_range(n):
+            try:
+                next_value = await self._iteration.__anext__()
+                is_complete_sent = next_value[1]
+                yield next_value
+            except StopAsyncIteration:
+                if not is_complete_sent:
+                    raise FinishedIterator()
+                return
