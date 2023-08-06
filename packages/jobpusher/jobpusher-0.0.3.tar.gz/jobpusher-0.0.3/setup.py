48fc9686202b0593a5d698999fb3dd6e12c82521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:29 +0000

    Add package jobpusher/0.0.3/jobpusher-0.0.3.tar.gz

---
+++ packages/jobpusher/jobpusher-0.0.3.tar.gz/jobpusher-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('LICENSE', 'r') as f:
+    license = f.read()
+
+setup(
+    name='jobpusher',
+    version='0.0.3',
+    description='Job Pusher',
+    author='TankNee',
+    author_email='nee@tanknee.cn',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=license,
+    packages=find_packages(include=['jobpusher', 'jobpusher.*']),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    keywords='jobpusher notifier pusher',
+    include_package_data=True,
+)
