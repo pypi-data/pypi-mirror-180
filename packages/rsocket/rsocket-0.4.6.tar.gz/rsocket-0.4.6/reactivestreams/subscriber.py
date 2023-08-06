8b40eaee0767a074966e1b89a0c96b830b31c732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:08 +0000

    Add package rsocket/0.4.6/rsocket-0.4.6.tar.gz

---
+++ packages/rsocket/rsocket-0.4.6.tar.gz/rsocket-0.4.6/reactivestreams/subscriber.py
@@ -0,0 +1,43 @@
+from abc import ABCMeta, abstractmethod
+from typing import Optional
+
+from reactivestreams.subscription import Subscription
+
+
+class Subscriber(metaclass=ABCMeta):
+    """
+    Handles stream events.
+    """
+
+    @abstractmethod
+    def on_subscribe(self, subscription: Subscription):
+        ...
+
+    @abstractmethod
+    def on_next(self, value, is_complete=False):
+        ...
+
+    @abstractmethod
+    def on_error(self, exception: Exception):
+        ...
+
+    @abstractmethod
+    def on_complete(self):
+        ...
+
+
+class DefaultSubscriber(Subscriber):
+    def __init__(self):
+        self.subscription: Optional[Subscription] = None
+
+    def on_next(self, value, is_complete=False):
+        pass
+
+    def on_error(self, exception: Exception):
+        pass
+
+    def on_subscribe(self, subscription: Subscription):
+        self.subscription = subscription
+
+    def on_complete(self):
+        pass
