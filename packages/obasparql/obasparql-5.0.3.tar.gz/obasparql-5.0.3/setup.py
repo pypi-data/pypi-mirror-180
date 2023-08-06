eacc38244b125d57b52af4c297586045a84c471a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:01 +0000

    Add package obasparql/5.0.3/obasparql-5.0.3.tar.gz

---
+++ packages/obasparql/obasparql-5.0.3.tar.gz/obasparql-5.0.3/setup.py
@@ -0,0 +1,57 @@
+import pathlib
+from setuptools import setup
+import codecs
+
+version = ""
+with open('VERSION') as fp:
+    version = fp.read()
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+with codecs.open('README.md', mode='r', encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="obasparql",
+    version=version,
+    description="OBA Sparql Manager",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KnowledgeCaptureAndDiscovery/oba_sparql",
+    author="Maximiliano Osorio",
+    author_email="mosorio@isi.edu",
+    license="Apache-2",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Unix",
+    ],
+    packages=["obasparql"],
+    include_package_data=True,
+    install_requires=[
+        "validators==0.20.0",
+        "pythonql3==0.9.83",
+        "pyaml==21.10.1",
+        "rdflib==6.2.0",
+        "requests>=2.20.0",
+        "simplejson==3.17.6",
+        "six>=1.11.0",
+        "urllib3<1.27,>=1.21.1 ",
+        "webencodings==0.5.1",
+        "starlette==0.20.4",
+        "PyLD>=2.0.3",
+    ]
+)
