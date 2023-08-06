87d920cf83bca02eb8676b2b15dc575b584367e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:49 +0000

    Add package pycarol/2.54.6/pycarol-2.54.6.tar.gz

---
+++ packages/pycarol/pycarol-2.54.6.tar.gz/pycarol-2.54.6/test/test_deprecated.py
@@ -0,0 +1,23 @@
+""" Tests for deprecated features inside pyCarol
+
+"""
+from unittest import mock
+import warnings
+
+import deprecation
+
+import pycarol
+
+
+def test_luigi_extension_deprecated():
+    with warnings.catch_warnings(record=True) as w:
+        # Cause all warnings to always be triggered.
+        warnings.simplefilter("default", category=DeprecationWarning)
+        import pycarol.luigi_extension
+        assert "luigi_extension is deprecated" in str(w[0].message)
+
+
+@deprecation.fail_if_not_removed
+def test_sql_deprecated() -> None:
+    carol = mock.MagicMock()
+    pycarol.SQL(carol)
