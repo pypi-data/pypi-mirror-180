96fb6e5c6f228b9aa661803f2ae76d8b8c386150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:51 +0000

    Add package power-grid-model-io/1.0.2/power_grid_model_io-1.0.2-py3-none-any.whl

---
+++ packages/power-grid-model-io/power_grid_model_io-1.0.2-py3-none-any.whl/power_grid_model_io/filters/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model IO project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+"""
+These functions can be used in the mapping files to apply functions to tabular data
+"""
+
+from power_grid_model_io.filters._filters import (
+    all_true,
+    any_true,
+    complex_inverse_imaginary_part,
+    complex_inverse_real_part,
+    degrees_to_clock,
+    get_winding,
+    has_value,
+    is_greater_than,
+    multiply,
+    subtract,
+    value_or_default,
+    value_or_zero,
+)
