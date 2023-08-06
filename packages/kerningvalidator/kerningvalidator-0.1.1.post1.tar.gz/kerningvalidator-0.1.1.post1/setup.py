23ce11428d20c7c7065e562d080ab4defce450ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:06 +0000

    Add package kerningvalidator/0.1.1.post1/kerningvalidator-0.1.1.post1.tar.gz

---
+++ packages/kerningvalidator/kerningvalidator-0.1.1.post1.tar.gz/kerningvalidator-0.1.1.post1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import platform
+import os
+
+WIN = platform.system() == "Windows"
+MAC = platform.system() == "Darwin"
+LINUX = platform.system() == "Linux"
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+install_requires = [
+    line
+    for line in open(os.path.join(path, "requirements.txt")).readlines()
+    if bool(line.strip()) and not line.strip().startswith("#")
+]
+
+setup(
+    name="kerningvalidator",  # How you named your package folder (MyLib)
+    version="0.1.1.post1",  # .post2
+    license="apache-2.0",
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    author="Yanone",  # Type in your name
+    author_email="post@yanone.de",  # Type in your E-Mail
+    url="https://github.com/yanone/kerningvalidator",
+    # Provide either the link to your github or to your website
+    keywords=["fonts"],  # Keywords that define your package best
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "kerningvalidator = kerningvalidator.cli:main",
+        ]
+    },
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    include_package_data=True,
+)
