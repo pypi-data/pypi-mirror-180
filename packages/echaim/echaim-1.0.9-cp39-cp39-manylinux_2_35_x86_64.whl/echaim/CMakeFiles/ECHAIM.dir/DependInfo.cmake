1bc5e44a9b97a5a54fb43d378a398e67739b2d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:36 +0000

    Add package echaim/1.0.9/echaim-1.0.9-cp39-cp39-manylinux_2_35_x86_64.whl

---
+++ packages/echaim/echaim-1.0.9-cp39-cp39-manylinux_2_35_x86_64.whl/echaim/CMakeFiles/ECHAIM.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/ECHAIMFIRI2018.c" "CMakeFiles/ECHAIM.dir/source_c/lib/ECHAIMFIRI2018.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/ECHAIMFIRI2018.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/FIRITools.c" "CMakeFiles/ECHAIM.dir/source_c/lib/FIRITools.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/FIRITools.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/aacgmlib_v2.c" "CMakeFiles/ECHAIM.dir/source_c/lib/aacgmlib_v2.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/aacgmlib_v2.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/astalglib.c" "CMakeFiles/ECHAIM.dir/source_c/lib/astalglib.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/astalglib.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/calcBot.c" "CMakeFiles/ECHAIM.dir/source_c/lib/calcBot.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/calcBot.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/calcHmF1.c" "CMakeFiles/ECHAIM.dir/source_c/lib/calcHmF1.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/calcHmF1.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/calcMF2.c" "CMakeFiles/ECHAIM.dir/source_c/lib/calcMF2.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/calcMF2.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/calcPert.c" "CMakeFiles/ECHAIM.dir/source_c/lib/calcPert.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/calcPert.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/calcPrecip.c" "CMakeFiles/ECHAIM.dir/source_c/lib/calcPrecip.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/calcPrecip.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/calcTop.c" "CMakeFiles/ECHAIM.dir/source_c/lib/calcTop.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/calcTop.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/date.c" "CMakeFiles/ECHAIM.dir/source_c/lib/date.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/date.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/densityFunc.c" "CMakeFiles/ECHAIM.dir/source_c/lib/densityFunc.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/densityFunc.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/densityPath.c" "CMakeFiles/ECHAIM.dir/source_c/lib/densityPath.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/densityPath.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/densityProfile.c" "CMakeFiles/ECHAIM.dir/source_c/lib/densityProfile.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/densityProfile.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/energyFluxModel.c" "CMakeFiles/ECHAIM.dir/source_c/lib/energyFluxModel.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/energyFluxModel.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/energyFunctions.c" "CMakeFiles/ECHAIM.dir/source_c/lib/energyFunctions.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/energyFunctions.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/errorCodes.c" "CMakeFiles/ECHAIM.dir/source_c/lib/errorCodes.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/errorCodes.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/genmag.c" "CMakeFiles/ECHAIM.dir/source_c/lib/genmag.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/genmag.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/getCoefs.c" "CMakeFiles/ECHAIM.dir/source_c/lib/getCoefs.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/getCoefs.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/getDBDate.c" "CMakeFiles/ECHAIM.dir/source_c/lib/getDBDate.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/getDBDate.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/getDir.c" "CMakeFiles/ECHAIM.dir/source_c/lib/getDir.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/getDir.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/getFIRI.c" "CMakeFiles/ECHAIM.dir/source_c/lib/getFIRI.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/getFIRI.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/getIndices.c" "CMakeFiles/ECHAIM.dir/source_c/lib/getIndices.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/getIndices.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/getMagCoords.c" "CMakeFiles/ECHAIM.dir/source_c/lib/getMagCoords.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/getMagCoords.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/getPrecipModel.c" "CMakeFiles/ECHAIM.dir/source_c/lib/getPrecipModel.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/getPrecipModel.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/igrflib.c" "CMakeFiles/ECHAIM.dir/source_c/lib/igrflib.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/igrflib.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/mathlib.c" "CMakeFiles/ECHAIM.dir/source_c/lib/mathlib.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/mathlib.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/maxIndices.c" "CMakeFiles/ECHAIM.dir/source_c/lib/maxIndices.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/maxIndices.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/mlt_v2.c" "CMakeFiles/ECHAIM.dir/source_c/lib/mlt_v2.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/mlt_v2.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/nrlmsise-00_data.c" "CMakeFiles/ECHAIM.dir/source_c/lib/nrlmsise-00_data.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/nrlmsise-00_data.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/nrlmsise00.c" "CMakeFiles/ECHAIM.dir/source_c/lib/nrlmsise00.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/nrlmsise00.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/otherIndices.c" "CMakeFiles/ECHAIM.dir/source_c/lib/otherIndices.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/otherIndices.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/parameterAE.c" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterAE.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterAE.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/parameterBot.c" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterBot.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterBot.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/parameterHmF1.c" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterHmF1.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterHmF1.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/parameterHmF2.c" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterHmF2.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterHmF2.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/parameterNmF2.c" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterNmF2.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterNmF2.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/parameterPert.c" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterPert.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterPert.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/parameterTop.c" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterTop.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/parameterTop.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/prepInd.c" "CMakeFiles/ECHAIM.dir/source_c/lib/prepInd.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/prepInd.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/rtime.c" "CMakeFiles/ECHAIM.dir/source_c/lib/rtime.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/rtime.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/runNRLMSISE.c" "CMakeFiles/ECHAIM.dir/source_c/lib/runNRLMSISE.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/runNRLMSISE.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/singleOutput.c" "CMakeFiles/ECHAIM.dir/source_c/lib/singleOutput.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/singleOutput.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/sqlite3.c" "CMakeFiles/ECHAIM.dir/source_c/lib/sqlite3.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/sqlite3.c.o.d"
+  "/home/lap1dem/dev-python/echaim/src/echaim/source_c/lib/updateLocalDB.c" "CMakeFiles/ECHAIM.dir/source_c/lib/updateLocalDB.c.o" "gcc" "CMakeFiles/ECHAIM.dir/source_c/lib/updateLocalDB.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ packages/echaim/echaim-1.0.9-cp39-cp39-manylinux_2_35_x86_64.whl/echaim/CMakeFiles/ECHAIM_bug_report.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ECHAIM_bug_report.
+# This may be replaced when dependencies are built.
