bd5f56877463acb207320307a1d6cc570ee55b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:44 +0000

    Add package graph-measures/0.1.56/graph_measures-0.1.56-cp310-cp310-win_amd64.whl

---
+++ packages/graph-measures/graph_measures-0.1.56-cp310-cp310-win_amd64.whl/graphMeasures/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+
+from .features_for_any_graph import FeatureCalculator
+from .features_meta import features_meta
+from .features_meta import accelerated_features_meta
+
+warnings.filterwarnings("ignore")
