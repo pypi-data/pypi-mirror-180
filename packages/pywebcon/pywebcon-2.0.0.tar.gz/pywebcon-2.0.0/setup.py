f3b8eb9390ba746a362f7295530ed436ee5bfd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:00 +0000

    Add package pywebcon/2.0.0/pywebcon-2.0.0.tar.gz

---
+++ packages/pywebcon/pywebcon-2.0.0.tar.gz/pywebcon-2.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+with open("README.md", "r") as fhandle:
+    long_description = fhandle.read() 
+setuptools.setup(
+    name="pywebcon", 
+    version="2.0.0", 
+    author="pydob",
+    description="Check if websites/apis are online", 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(), 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], 
+    python_requires='>=3.6', 
+)
