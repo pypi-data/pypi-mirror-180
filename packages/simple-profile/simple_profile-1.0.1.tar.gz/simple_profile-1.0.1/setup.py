a69209c3adf6989c0691f22bcd1eda89abe53ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:25 +0000

    Add package simple-profile/1.0.1/simple_profile-1.0.1.tar.gz

---
+++ packages/simple-profile/simple_profile-1.0.1.tar.gz/simple_profile-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['simple_profile']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'simple-profile',
+    'version': '1.0.1',
+    'description': 'Simple profile decorator to monitor execution time and memory usage.',
+    'long_description': '# simple-profile\nSimple profile decorator for Python\n',
+    'author': 'João Brilhante',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/JoaoBrlt/simple-profile',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
