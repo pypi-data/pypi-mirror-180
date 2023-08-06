42b48f39a63198d396b9b7e285cf1cc2d2fd8aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:54 +0000

    Add package model-package/2.1/model_package-2.1.tar.gz

---
+++ packages/model-package/model_package-2.1.tar.gz/model_package-2.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='model_package',
+      version='2.1',
+      description='Implement Linear Regression',
+      packages=['model_package'],
+      author='Alessio Polymeropoulos',
+      author_email='polymeropoulos@hotmail.com',      
+      zip_safe=False)
