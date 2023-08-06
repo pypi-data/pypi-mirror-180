353133907d6b8dd29ef3e8c49e6574c036db243a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:05 +0000

    Add package py-classic-error/1.0.0/py_classic_error-1.0.0.tar.gz

---
+++ packages/py-classic-error/py_classic_error-1.0.0.tar.gz/py_classic_error-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name = 'py_classic_error',         
+  py_modules =  ['py_classic_error'],  
+  version = '1.0.0',      
+  license='The Unlicense',        
+  description = 'py_classic_error is a classic way to show errors',  
+  author = 'suresh pandiyan',                  
+  author_email = 'suresh.pandiyan1@outlook.com',       
+  keywords = ['python', 'error', 'classic error','errors','traceback','pythonusers','python3'],  
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: The Unlicense (Unlicense)', 
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11'
+  ],
+)
