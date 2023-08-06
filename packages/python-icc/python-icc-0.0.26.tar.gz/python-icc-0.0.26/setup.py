68fb90cce6bc1a5e2ea9f395a58475bc339e9f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:59 +0000

    Add package python-icc/0.0.26/python-icc-0.0.26.tar.gz

---
+++ packages/python-icc/python-icc-0.0.26.tar.gz/python-icc-0.0.26/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+README = """
+Home automation data models, packaged to prevent code duplication throughout microservices.
+"""
+
+setup(
+    name="python-icc",
+    version="0.0.26",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    description="IoT Control Center home automation project packages",
+    url="https://github.com/patterson-project/icc-models",
+    author="Patterson Project",
+    author_email="patterson.n.adrian@gmail.com",
+    license="MIT",
+    packages=["icc"],
+    zip_safe=False,
+)
