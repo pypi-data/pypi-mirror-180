b2a6b9467778b03d45524ff9953ece3f1e1324cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:32 +0000

    Add package cutadapt/4.2rc1/cutadapt-4.2rc1.tar.gz

---
+++ packages/cutadapt/cutadapt-4.2rc1.tar.gz/cutadapt-4.2rc1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, Extension
+import setuptools_scm  # noqa  Ensure it’s installed
+
+extensions = [
+    Extension("cutadapt._align", sources=["src/cutadapt/_align.pyx"]),
+    Extension("cutadapt.qualtrim", sources=["src/cutadapt/qualtrim.pyx"]),
+    Extension("cutadapt.info", sources=["src/cutadapt/info.pyx"]),
+]
+
+setup(ext_modules=extensions)
