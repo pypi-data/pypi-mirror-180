c63d41c32b312a8fa84c0e15ebb64123176fb916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:57 +0000

    Add package osdatahub/1.2.1/osdatahub-1.2.1-py3-none-any.whl

---
+++ packages/osdatahub/osdatahub-1.2.1-py3-none-any.whl/osdatahub/__init__.py
@@ -0,0 +1,9 @@
+from osdatahub.extent import Extent
+from osdatahub.FeaturesAPI import FeaturesAPI
+from osdatahub.PlacesAPI import PlacesAPI
+from osdatahub.NamesAPI import NamesAPI
+from osdatahub.LinkedIdentifiersAPI import LinkedIdentifiersAPI
+from osdatahub.DownloadsAPI import OpenDataDownload, DataPackageDownload
+from osdatahub.NGD import NGD
+
+__version__ = "1.2.1"
